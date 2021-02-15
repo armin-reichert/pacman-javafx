9f5c2b82681bb13d2a998ec4a23d757fe8191375
Armin Reichert <armin.reichert@web.de>
2021-02-15 12:44:52 +0100

    improved

--- pacman-ui-fx/src/main/java/de/amr/games/pacman/ui/fx/pacman/IntermissionScene1.java
+++ pacman-ui-fx/src/main/java/de/amr/games/pacman/ui/fx/pacman/IntermissionScene1.java
@@ -62,7 +62,7 @@ public class IntermissionScene1 extends AbstractPacManGameScene<PacManSceneRende
 
 		blinky.visible = true;
 		blinky.state = HUNTING_PAC;
-		blinky.position = pac.position.sum(t(4), 0);
+		blinky.position = pac.position.sum(t(3), 0);
 		blinky.speed = pac.speed * 1.04f;
 		blinky.dir = blinky.wishDir = LEFT;
 
