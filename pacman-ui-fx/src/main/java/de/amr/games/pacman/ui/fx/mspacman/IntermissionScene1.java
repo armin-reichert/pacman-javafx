a06f40703c4141d9c9ddb772951cb9406bf80fe9
Armin Reichert <armin.reichert@web.de>
2021-02-17 08:04:37 +0100

    doc

--- pacman-ui-fx/src/main/java/de/amr/games/pacman/ui/fx/mspacman/IntermissionScene1.java
+++ pacman-ui-fx/src/main/java/de/amr/games/pacman/ui/fx/mspacman/IntermissionScene1.java
@@ -17,6 +17,16 @@ import de.amr.games.pacman.ui.fx.common.AbstractPacManGameScene;
 import javafx.geometry.Rectangle2D;
 import javafx.scene.paint.Color;
 
+/**
+ * Intermission scene 2: "They meet".
+ * <p>
+ * Pac-Man leads Inky and Ms. Pac-Man leads Pinky. Soon, the two Pac-Men are about to collide, they
+ * quickly move upwards, causing Inky and Pinky to collide and vanish. Finally, Pac-Man and Ms.
+ * Pac-Man face each other at the top of the screen and a big pink heart appears above them. (Played
+ * after round 2)
+ * 
+ * @author Armin Reichert
+ */
 public class IntermissionScene1 extends AbstractPacManGameScene<MsPacManSceneRendering> {
 
 	enum Phase {
