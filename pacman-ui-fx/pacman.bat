a4c5ac9fcc6501e7ac2195db09f9341152532b3c
Armin Reichert <armin.reichert@web.de>
2021-02-21 15:17:46 +0100

    batch file cosmetics

--- pacman-ui-fx/pacman.bat
+++ pacman-ui-fx/pacman.bat
@@ -1,9 +1,23 @@
-REM Usage: pacman.bat or pacman.bat -mspacman
+:: Usage: pacman.bat or pacman.bat -mspacman
+@SETLOCAL
+
+SET "JAVA_HOME=C:\Program Files\Java\jdk-15.0.2"
+SET "JFX_HOME=C:\Program Files\Java\javafx-sdk-15.0.1"
+SET "JFX_RT=C:\Users\armin\.m2\repository"
+
+SET "CLASSPATH=..\..\pacman-basic\pacman\target\pacman-basic-1.0.jar;.\target\pacman-ui-fx-1.0.jar;^
+ "%JFX_HOME%\lib\javafx.base.jar";^
+ "%JFX_HOME%\lib\javafx.controls.jar";^
+ "%JFX_HOME%\lib\javafx.graphics.jar";^
+ "%JFX_HOME%\lib\javafx.media.jar";^
+ "%JFX_RT%\org\openjfx\javafx-controls\15\javafx-controls-15.jar";^
+ "%JFX_RT%\org\openjfx\javafx-controls\15\javafx-controls-15-win.jar";^
+ "%JFX_RT%\org\openjfx\javafx-graphics\15\javafx-graphics-15.jar";^
+ "%JFX_RT%\org\openjfx\javafx-graphics\15\javafx-graphics-15-win.jar";^
+ "%JFX_RT%\org\openjfx\javafx-base\15\javafx-base-15.jar";^
+ "%JFX_RT%\org\openjfx\javafx-base\15\javafx-base-15-win.jar""
  
-set "JAVA_HOME=C:\Program Files\Java\jdk-15.0.2"
-set "JFX_HOME=C:\Program Files\Java\javafx-sdk-15.0.1"
-set "JFX_RT=C:\Users\armin\.m2\repository"
+"%JAVA_HOME%\bin\javaw.exe"^
+ --module-path "%JFX_HOME%\lib" --add-modules javafx.controls -cp %CLASSPATH% de.amr.games.pacman.ui.fx.app.PacManGameAppFX %*
 
-set "JFX_LIBS="%JFX_HOME%\lib\javafx.base.jar";"%JFX_HOME%\lib\javafx.controls.jar";"%JFX_HOME%\lib\javafx.graphics.jar";"%JFX_HOME%\lib\javafx.media.jar";"%JFX_RT%\org\openjfx\javafx-controls\15\javafx-controls-15.jar";"%JFX_RT%\org\openjfx\javafx-controls\15\javafx-controls-15-win.jar";"%JFX_RT%\org\openjfx\javafx-graphics\15\javafx-graphics-15.jar";"%JFX_RT%\org\openjfx\javafx-graphics\15\javafx-graphics-15-win.jar";"%JFX_RT%\org\openjfx\javafx-base\15\javafx-base-15.jar";"%JFX_RT%\org\openjfx\javafx-base\15\javafx-base-15-win.jar"
-
-"%JAVA_HOME%\bin\javaw.exe" --module-path "%JFX_HOME%\lib" --add-modules javafx.controls -classpath "C:\Users\armin\git\pacman-basic\pacman\target\pacman-basic-1.0.jar";"C:\Users\armin\git\pacman-javafx\pacman-ui-fx\target\pacman-ui-fx-1.0.jar";%JFX_LIBS%" de.amr.games.pacman.ui.fx.app.PacManGameAppFX %*
+@ENDLOCAL
