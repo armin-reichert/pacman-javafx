80ca9466cdb3e30aa9808f163e0e03017d807566
Armin Reichert <armin.reichert@web.de>
2021-02-23 11:19:18 +0100

    cleanup

--- pacman-ui-fx/src/main/java/de/amr/games/pacman/ui/fx/mspacman/entities/Heart.java
+++ pacman-ui-fx/src/main/java/de/amr/games/pacman/ui/fx/mspacman/entities/Heart.java
@@ -1,15 +1,7 @@
 package de.amr.games.pacman.ui.fx.mspacman.entities;
 
 import de.amr.games.pacman.model.guys.GameEntity;
-import de.amr.games.pacman.ui.fx.rendering.FXRendering;
-import javafx.scene.canvas.GraphicsContext;
 
 public class Heart extends GameEntity {
 
-	public Heart() {
-	}
-
-	public void draw(GraphicsContext g, FXRendering rendering) {
-		rendering.drawHeart(g, this);
-	}
 }
