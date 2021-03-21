4d7bcc70e9c7e1fef0efbfd1036815648f2bf5b5
Armin Reichert <armin.reichert@web.de>
2021-03-21 06:42:59 +0100

    infra

--- pacman-ui-fx/src/main/java/module-info.java
+++ pacman-ui-fx/src/main/java/module-info.java
@@ -2,11 +2,10 @@ module de.amr.games.pacman.ui.fx {
 
 	// TODO implement sound with FX code
 	requires transitive java.desktop;
-
 	requires transitive javafx.controls;
-	requires transitive javafx.graphics;
-
 	requires de.amr.games.pacman;
+
+	// this should come from a public repository
 	requires jimModelImporterJFX;
 
 	exports de.amr.games.pacman.ui.fx.app to javafx.graphics;
