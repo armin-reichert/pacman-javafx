5ff34b09e998be9102cd26d7af51041048ea1a54
Armin Reichert <armin.reichert@web.de>
2021-03-22 12:06:00 +0100

    improved

--- pacman-ui-fx/src/main/java/de/amr/games/pacman/ui/fx/HUD.java
+++ pacman-ui-fx/src/main/java/de/amr/games/pacman/ui/fx/HUD.java
@@ -1,5 +1,6 @@
 package de.amr.games.pacman.ui.fx;
 
+import de.amr.games.pacman.lib.TickTimer;
 import de.amr.games.pacman.ui.fx.common.Env;
 import de.amr.games.pacman.ui.fx.common.scene2d.AbstractGameScene2D;
 import javafx.geometry.Pos;
@@ -12,12 +13,12 @@ import javafx.scene.text.Text;
 
 public class HUD extends HBox {
 
-	private final PacManGameUI_JavaFX userInterface;
+	private final PacManGameUI_JavaFX ui;
 	private final Text textView;
 	private String text;
 
-	public HUD(PacManGameUI_JavaFX userInterface, Pos dockingPosition) {
-		this.userInterface = userInterface;
+	public HUD(PacManGameUI_JavaFX ui, Pos dockingPosition) {
+		this.ui = ui;
 		textView = new Text();
 		textView.setFill(Color.LIGHTGREEN);
 		textView.setFont(Font.font("Monospace", 14));
@@ -28,7 +29,7 @@ public class HUD extends HBox {
 
 	private void line(String column1, String fmtColumn2, Object... args) {
 		String column2 = String.format(fmtColumn2, args) + "\n";
-		text += String.format("%-10s: %s", column1, column2);
+		text += String.format("%-12s: %s", column1, column2);
 	}
 
 	private String cameraInfo(Camera camera) {
@@ -39,23 +40,26 @@ public class HUD extends HBox {
 
 	public void update() {
 		text = "";
-		line("Game Type", "%s", userInterface.controller.gameVariant());
-		line("Game State", "%s", userInterface.controller.state);
-		line("Level", "%d", userInterface.controller.game().levelNumber);
+		line("Game Variant", "%s", ui.controller.gameVariant());
+		line("Game State", "%s", ui.controller.state);
+		TickTimer stateTimer = ui.controller.timer();
+		line("", "Running:   %s", stateTimer.ticked());
+		line("", "Remaining: %s",
+				stateTimer.ticksRemaining() == Long.MAX_VALUE ? "indefinite" : stateTimer.ticksRemaining());
+		line("Level", "%d", ui.controller.game().levelNumber);
 		line("Paused", "%s (Key CTRL+P)", Env.$paused.get() ? "YES" : "NO");
-		line("Window", "w=%.0f h=%.0f", userInterface.mainScene.getWindow().getWidth(),
-				userInterface.mainScene.getWindow().getHeight());
-		line("Main scene", "w=%.0f h=%.0f", userInterface.mainScene.getWidth(), userInterface.mainScene.getHeight());
-		line("Game scene", "w=%.0f h=%.0f (%s)", userInterface.currentGameScene.getFXSubScene().getWidth(),
-				userInterface.currentGameScene.getFXSubScene().getHeight(),
-				userInterface.currentGameScene.getClass().getSimpleName());
-		if (userInterface.currentGameScene instanceof AbstractGameScene2D) {
-			AbstractGameScene2D scene2D = (AbstractGameScene2D) userInterface.currentGameScene;
+		line("Window", "w=%.0f h=%.0f", ui.mainScene.getWindow().getWidth(), ui.mainScene.getWindow().getHeight());
+		line("Main scene", "w=%.0f h=%.0f", ui.mainScene.getWidth(), ui.mainScene.getHeight());
+		line("Game scene:", "%s", ui.currentGameScene.getClass().getSimpleName());
+		line("", "w=%.0f h=%.0f", ui.currentGameScene.getFXSubScene().getWidth(),
+				ui.currentGameScene.getFXSubScene().getHeight());
+		if (ui.currentGameScene instanceof AbstractGameScene2D) {
+			AbstractGameScene2D scene2D = (AbstractGameScene2D) ui.currentGameScene;
 			line("Canvas2D", "w=%.0f h=%.0f", scene2D.getCanvas().getWidth(), scene2D.getCanvas().getHeight());
 		}
-		line("Camera", "%s (CTRL+S)", cameraInfo(userInterface.currentGameScene.getActiveCamera()));
-		line("Autopilot", "%s (Key A)", userInterface.controller.autopilot.enabled ? "ON" : "OFF");
-		line("Immunity", "%s (Key I)", userInterface.controller.game().player.immune ? "ON" : "OFF");
+		line("Camera", "%s (CTRL+S)", cameraInfo(ui.currentGameScene.getActiveCamera()));
+		line("Autopilot", "%s (Key A)", ui.controller.autopilot.enabled ? "ON" : "OFF");
+		line("Immunity", "%s (Key I)", ui.controller.game().player.immune ? "ON" : "OFF");
 		line("3D scenes", "%s (Key CTRL+3)", Env.$use3DScenes.get() ? "ON" : "OFF");
 		textView.setText(text);
 	}
