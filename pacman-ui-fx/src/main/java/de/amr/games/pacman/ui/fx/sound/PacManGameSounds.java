b7cede1855474c67cd53cf36635bde7add7caf14
Armin Reichert <armin.reichert@web.de>
2021-03-30 12:48:00 +0200

    fix

--- pacman-ui-fx/src/main/java/de/amr/games/pacman/ui/fx/sound/PacManGameSounds.java
+++ pacman-ui-fx/src/main/java/de/amr/games/pacman/ui/fx/sound/PacManGameSounds.java
@@ -8,24 +8,22 @@ import de.amr.games.pacman.ui.sound.PacManGameSound;
 
 public class PacManGameSounds {
 
-	private static final PacManGameSounds IT = new PacManGameSounds();
+	private static final Map<PacManGameSound, URL> pacManSounds = new EnumMap<>(PacManGameSound.class);
+	private static final Map<PacManGameSound, URL> msPacManSounds = new EnumMap<>(PacManGameSound.class);
 
-	private final Map<PacManGameSound, URL> pacManSounds = new EnumMap<>(PacManGameSound.class);
-	private final Map<PacManGameSound, URL> msPacManSounds = new EnumMap<>(PacManGameSound.class);
-
-	private URL url(String path) {
-		return getClass().getResource(path);
+	private static URL url(String path) {
+		return PacManGameSounds.class.getResource(path);
 	}
 
 	public static URL mrPacManSoundURL(PacManGameSound sound) {
-		return IT.pacManSounds.get(sound);
+		return pacManSounds.get(sound);
 	}
 
 	public static URL msPacManSoundURL(PacManGameSound sound) {
-		return IT.msPacManSounds.get(sound);
+		return msPacManSounds.get(sound);
 	}
 
-	private PacManGameSounds() {
+	static {
 		//@formatter:off
 		pacManSounds.put(PacManGameSound.CREDIT,             url("/pacman/sound/credit.wav"));
 		pacManSounds.put(PacManGameSound.EXTRA_LIFE,         url("/pacman/sound/extend.wav"));
@@ -40,7 +38,6 @@ public class PacManGameSounds {
 		pacManSounds.put(PacManGameSound.GHOST_SIREN_2,      url("/pacman/sound/siren_2.wav"));
 		pacManSounds.put(PacManGameSound.GHOST_SIREN_3,      url("/pacman/sound/siren_3.wav"));
 		pacManSounds.put(PacManGameSound.GHOST_SIREN_4,      url("/pacman/sound/siren_4.wav"));
-		pacManSounds.put(PacManGameSound.GHOST_SIREN_5,      url("/pacman/sound/siren_5.wav"));
 		pacManSounds.put(PacManGameSound.INTERMISSION_1,     url("/pacman/sound/intermission.wav"));
 		pacManSounds.put(PacManGameSound.INTERMISSION_2,     url("/pacman/sound/intermission.wav"));
 		pacManSounds.put(PacManGameSound.INTERMISSION_3,     url("/pacman/sound/intermission.wav"));
@@ -54,11 +51,10 @@ public class PacManGameSounds {
 		msPacManSounds.put(PacManGameSound.PACMAN_POWER,     url("/mspacman/sound/Scared Ghost.wav"));
 		msPacManSounds.put(PacManGameSound.GHOST_EATEN,      url("/mspacman/sound/Ghost.wav"));
 		msPacManSounds.put(PacManGameSound.GHOST_RETURNING_HOME,       url("/mspacman/sound/Ghost Eyes.wav"));
-		msPacManSounds.put(PacManGameSound.GHOST_SIREN_1,    url("/mspacman/sound/Ghost Noise.wav"));
-		msPacManSounds.put(PacManGameSound.GHOST_SIREN_2,    url("/mspacman/sound/Ghost Noise 1.wav"));
-		msPacManSounds.put(PacManGameSound.GHOST_SIREN_3,    url("/mspacman/sound/Ghost Noise 2.wav"));
-		msPacManSounds.put(PacManGameSound.GHOST_SIREN_4,    url("/mspacman/sound/Ghost Noise 3.wav"));
-		msPacManSounds.put(PacManGameSound.GHOST_SIREN_5,    url("/mspacman/sound/Ghost Noise 4.wav"));
+		msPacManSounds.put(PacManGameSound.GHOST_SIREN_1,    url("/mspacman/sound/Ghost Noise 1.wav"));
+		msPacManSounds.put(PacManGameSound.GHOST_SIREN_2,    url("/mspacman/sound/Ghost Noise 2.wav"));
+		msPacManSounds.put(PacManGameSound.GHOST_SIREN_3,    url("/mspacman/sound/Ghost Noise 3.wav"));
+		msPacManSounds.put(PacManGameSound.GHOST_SIREN_4,    url("/mspacman/sound/Ghost Noise 4.wav"));
 		msPacManSounds.put(PacManGameSound.INTERMISSION_1,   url("/mspacman/sound/They Meet Act 1.wav"));
 		msPacManSounds.put(PacManGameSound.INTERMISSION_2,   url("/mspacman/sound/The Chase Act 2.wav"));
 		msPacManSounds.put(PacManGameSound.INTERMISSION_3,   url("/mspacman/sound/Junior Act 3.wav"));
