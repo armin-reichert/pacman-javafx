a1d1046c77d3d133b444845c5b3a55e0fce87394
Armin <Armin@Intel-NUC8i7HNKQC.fritz.box>
2021-04-07 12:24:40 +0200

    Bonus3D

--- pacman-ui-fx/src/main/java/de/amr/games/pacman/ui/fx/entities/_3d/Ghost3D.java
+++ pacman-ui-fx/src/main/java/de/amr/games/pacman/ui/fx/entities/_3d/Ghost3D.java
@@ -5,16 +5,16 @@ import java.util.function.Supplier;
 import de.amr.games.pacman.lib.Direction;
 import de.amr.games.pacman.model.common.Ghost;
 import de.amr.games.pacman.model.common.GhostState;
+import de.amr.games.pacman.ui.fx.rendering.GameRendering2D;
 import de.amr.games.pacman.ui.fx.rendering.GameRendering3D_Assets;
 import javafx.scene.Group;
 import javafx.scene.Node;
+import javafx.scene.image.Image;
 import javafx.scene.paint.Color;
 import javafx.scene.paint.PhongMaterial;
+import javafx.scene.shape.Box;
 import javafx.scene.shape.MeshView;
 import javafx.scene.shape.Sphere;
-import javafx.scene.text.Font;
-import javafx.scene.text.FontWeight;
-import javafx.scene.text.Text;
 import javafx.scene.transform.Rotate;
 
 /**
@@ -24,99 +24,103 @@ import javafx.scene.transform.Rotate;
  */
 public class Ghost3D implements Supplier<Node> {
 
-	private final Ghost ghost;
-	private Group root;
-	private Text bountyText;
-	private PhongMaterial normalSkin;
-	private PhongMaterial blueSkin;
-	private MeshView meshView;
+    private final Ghost ghost;
+    private final GameRendering2D rendering2D;
+    private final Group root;
+    private final Box bountyShape;
+    private final PhongMaterial bountySkin;
+    private final PhongMaterial normalSkin;
+    private final PhongMaterial blueSkin;
+    private final MeshView meshView;
 
-	@Override
-	public Node get() {
-		return root;
+    @Override
+    public Node get() {
+	return root;
+    }
+
+    public Ghost3D(Ghost ghost, GameRendering2D rendering2D) {
+	this.rendering2D = rendering2D;
+	this.ghost = ghost;
+
+	normalSkin = GameRendering3D_Assets.ghostSkin(ghost.id);
+	blueSkin = new PhongMaterial(Color.CORNFLOWERBLUE);
+
+	meshView = GameRendering3D_Assets.createGhostMeshView(ghost.id);
+	meshView.setMaterial(normalSkin);
+	meshView.getTransforms().add(new Rotate(90, Rotate.X_AXIS));
+	Group coloredGhost = new Group(meshView);
+
+	bountyShape = new Box(8, 8, 8);
+	bountySkin = new PhongMaterial(Color.WHITE);
+	bountyShape.setMaterial(bountySkin);
+	Group bountyGhost = new Group(bountyShape);
+
+	Sphere[] pearls = new Sphere[3];
+	for (int i = 0; i < pearls.length; ++i) {
+	    pearls[i] = new Sphere(1);
+	    pearls[i].setMaterial(normalSkin);
+	    pearls[i].setTranslateX(i * 3);
 	}
+	Group deadGhost = new Group(pearls);
 
-	public Ghost3D(Ghost ghost) {
-		this.ghost = ghost;
+	root = new Group(coloredGhost, bountyGhost, deadGhost);
+	selectChild(0);
+    }
 
-		normalSkin = GameRendering3D_Assets.ghostSkin(ghost.id);
-		blueSkin = new PhongMaterial(Color.CORNFLOWERBLUE);
-
-		meshView = GameRendering3D_Assets.createGhostMeshView(ghost.id);
-		meshView.setMaterial(normalSkin);
-		meshView.getTransforms().add(new Rotate(90, Rotate.X_AXIS));
-		Group coloredGhost = new Group(meshView);
-
-		bountyText = new Text();
-		bountyText.setText(String.valueOf(ghost.bounty));
-		bountyText.setFont(Font.font("Sans", FontWeight.MEDIUM, 16));
-		bountyText.setFill(Color.CYAN);
-		Group bountyGhost = new Group(bountyText);
-
-		Sphere[] pearls = new Sphere[3];
-		for (int i = 0; i < pearls.length; ++i) {
-			pearls[i] = new Sphere(1);
-			pearls[i].setMaterial(normalSkin);
-			pearls[i].setTranslateX(i * 3);
-		}
-		Group deadGhost = new Group(pearls);
-
-		root = new Group(coloredGhost, bountyGhost, deadGhost);
-		selectChild(0);
+    private void selectChild(int index) {
+	for (int i = 0; i < 3; ++i) {
+	    root.getChildren().get(i).setVisible(i == index);
 	}
+    }
 
-	private void selectChild(int index) {
-		for (int i = 0; i < 3; ++i) {
-			root.getChildren().get(i).setVisible(i == index);
-		}
+    public void update() {
+	root.setVisible(ghost.visible);
+	root.setTranslateX(ghost.position.x);
+	root.setTranslateY(ghost.position.y);
+	root.setViewOrder(-(ghost.position.y + 5)); // TODO
+	if (ghost.bounty > 0) {
+	    Image bountySprite = rendering2D.subImage(rendering2D.getBountyNumberSpritesMap().get(ghost.bounty));
+	    bountySkin.setBumpMap(bountySprite);
+	    bountySkin.setDiffuseMap(bountySprite);
+	    root.setRotate(0);
+	    selectChild(1);
+	} else if (ghost.is(GhostState.DEAD) || ghost.is(GhostState.ENTERING_HOUSE)) {
+	    root.setRotationAxis(Rotate.Z_AXIS);
+	    root.setRotate(ghost.dir == Direction.UP || ghost.dir == Direction.DOWN ? 90 : 0);
+	    selectChild(2);
+	} else if (ghost.is(GhostState.FRIGHTENED)) {
+	    meshView.setMaterial(blueSkin);
+	    turnTowardsMoveDirection();
+	    selectChild(0);
+	} else {
+	    meshView.setMaterial(normalSkin);
+	    turnTowardsMoveDirection();
+	    selectChild(0);
 	}
+    }
 
-	public void update() {
-		root.setVisible(ghost.visible);
-		root.setTranslateX(ghost.position.x);
-		root.setTranslateY(ghost.position.y);
-		root.setViewOrder(-(ghost.position.y + 5)); // TODO
-		if (ghost.bounty > 0) {
-			bountyText.setText(String.valueOf(ghost.bounty));
-			root.setRotate(0);
-			selectChild(1);
-		} else if (ghost.is(GhostState.DEAD) || ghost.is(GhostState.ENTERING_HOUSE)) {
-			root.setRotationAxis(Rotate.Z_AXIS);
-			root.setRotate(ghost.dir == Direction.UP || ghost.dir == Direction.DOWN ? 90 : 0);
-			selectChild(2);
-		} else if (ghost.is(GhostState.FRIGHTENED)) {
-			meshView.setMaterial(blueSkin);
-			turnTowardsMoveDirection();
-			selectChild(0);
-		} else {
-			meshView.setMaterial(normalSkin);
-			turnTowardsMoveDirection();
-			selectChild(0);
-		}
+    private void turnTowardsMoveDirection() {
+	root.setRotationAxis(Rotate.Y_AXIS);
+	root.setRotate(0);
+	switch (ghost.dir) {
+	case LEFT:
+	    root.setRotationAxis(Rotate.Z_AXIS);
+	    root.setRotate(180);
+	    break;
+	case RIGHT:
+	    root.setRotationAxis(Rotate.Z_AXIS);
+	    root.setRotate(0);
+	    break;
+	case UP:
+	    root.setRotationAxis(Rotate.Z_AXIS);
+	    root.setRotate(-90);
+	    break;
+	case DOWN:
+	    root.setRotationAxis(Rotate.Z_AXIS);
+	    root.setRotate(90);
+	    break;
+	default:
+	    break;
 	}
-
-	private void turnTowardsMoveDirection() {
-		root.setRotationAxis(Rotate.Y_AXIS);
-		root.setRotate(0);
-		switch (ghost.dir) {
-		case LEFT:
-			root.setRotationAxis(Rotate.Z_AXIS);
-			root.setRotate(180);
-			break;
-		case RIGHT:
-			root.setRotationAxis(Rotate.Z_AXIS);
-			root.setRotate(0);
-			break;
-		case UP:
-			root.setRotationAxis(Rotate.Z_AXIS);
-			root.setRotate(-90);
-			break;
-		case DOWN:
-			root.setRotationAxis(Rotate.Z_AXIS);
-			root.setRotate(90);
-			break;
-		default:
-			break;
-		}
-	}
+    }
 }
--- pacman-ui-fx/src/main/java/de/amr/games/pacman/ui/fx/scenes/common/_3d/PlayScene3D.java
+++ pacman-ui-fx/src/main/java/de/amr/games/pacman/ui/fx/scenes/common/_3d/PlayScene3D.java
@@ -64,306 +64,306 @@ import javafx.util.Duration;
  */
 public class PlayScene3D implements GameScene {
 
-	private static final String[] CONGRATS = { "Well done", "Congrats", "Awesome", "You did it", "You're the man*in",
-			"WTF", "You old cheating bastard" };
+    private static final String[] CONGRATS = { "Well done", "Congrats", "Awesome", "You did it", "You're the man*in",
+	    "WTF", "You old cheating bastard" };
 
-	private final SubScene fxScene;
+    private final SubScene fxScene;
 
-	private final PerspectiveCamera staticCamera = new PerspectiveCamera(true);
-	private final PerspectiveCamera moveableCamera = new PerspectiveCamera(true);
-	private final CameraController cameraController = new CameraController(staticCamera);
+    private final PerspectiveCamera staticCamera = new PerspectiveCamera(true);
+    private final PerspectiveCamera moveableCamera = new PerspectiveCamera(true);
+    private final CameraController cameraController = new CameraController(staticCamera);
 
-	private final SoundManager sounds;
-	private PlaySceneSoundHandler playSceneSoundHandler;
-	private PacManGameController gameController;
+    private final SoundManager sounds;
+    private PlaySceneSoundHandler playSceneSoundHandler;
+    private PacManGameController gameController;
 
-	private AmbientLight ambientLight = new AmbientLight(Color.AZURE);
-	private PointLight pointLight = new PointLight(Color.AZURE);
-	private CoordinateSystem coordSystem;
-	private Group tgMaze;
-	private Player3D player;
-	private Map<Ghost, Ghost3D> ghosts3D;
-	private Maze3D maze;
-	private List<Energizer3D> energizers;
-	private List<Pellet3D> pellets;
-	private Bonus3D bonus3D;
-	private ScoreNotReally3D score3D;
-	private LivesCounter3D livesCounter3D;
+    private AmbientLight ambientLight = new AmbientLight(Color.AZURE);
+    private PointLight pointLight = new PointLight(Color.AZURE);
+    private CoordinateSystem coordSystem;
+    private Group tgMaze;
+    private Player3D player;
+    private Map<Ghost, Ghost3D> ghosts3D;
+    private Maze3D maze;
+    private List<Energizer3D> energizers;
+    private List<Pellet3D> pellets;
+    private Bonus3D bonus3D;
+    private ScoreNotReally3D score3D;
+    private LivesCounter3D livesCounter3D;
 
-	// TODO this is only a temporary solution
-	private GameRendering2D rendering2D_MsPacMan = new GameRendering2D_MsPacMan();
-	private GameRendering2D rendering2D_PacMan = new GameRendering2D_PacMan();
+    // TODO this is only a temporary solution
+    private GameRendering2D rendering2D_MsPacMan = new GameRendering2D_MsPacMan();
+    private GameRendering2D rendering2D_PacMan = new GameRendering2D_PacMan();
 
-	public PlayScene3D(SoundManager sounds) {
-		this.sounds = sounds;
-		fxScene = new SubScene(new Group(), 800, 600, true, SceneAntialiasing.BALANCED);
-		fxScene.addEventHandler(KeyEvent.KEY_PRESSED, cameraController::handleKeyEvent);
-		useStaticCamera();
+    public PlayScene3D(SoundManager sounds) {
+	this.sounds = sounds;
+	fxScene = new SubScene(new Group(), 800, 600, true, SceneAntialiasing.BALANCED);
+	fxScene.addEventHandler(KeyEvent.KEY_PRESSED, cameraController::handleKeyEvent);
+	useStaticCamera();
+    }
+
+    @Override
+    public PacManGameController getGameController() {
+	return gameController;
+    }
+
+    @Override
+    public void setGameController(PacManGameController gameController) {
+	this.gameController = gameController;
+	playSceneSoundHandler = new PlaySceneSoundHandler(gameController, sounds);
+    }
+
+    @Override
+    public OptionalDouble aspectRatio() {
+	return OptionalDouble.empty();
+    }
+
+    @Override
+    public String toString() {
+	return getClass().getSimpleName() + "@" + hashCode();
+    }
+
+    @Override
+    public void stopAllSounds() {
+	sounds.stopAll();
+    }
+
+    private void buildSceneGraph() {
+	final GameVariant gameVariant = gameController.gameVariant();
+	final GameRendering2D rendering2D = gameVariant == GameVariant.MS_PACMAN ? rendering2D_MsPacMan
+		: rendering2D_PacMan;
+
+	fxScene.setFill(Color.rgb(20, 20, 60));
+
+	maze = new Maze3D(game(), GameRendering2D_Assets.getMazeWallColor(game().currentLevel.mazeNumber));
+	PhongMaterial foodMaterial = GameRendering3D_Assets.foodMaterial(gameVariant, game().currentLevel.mazeNumber);
+
+	energizers = game().currentLevel.world.energizerTiles()//
+		.map(tile -> new Energizer3D(tile, foodMaterial))//
+		.collect(Collectors.toList());
+
+	pellets = game().currentLevel.world.tiles()//
+		.filter(game().currentLevel.world::isFoodTile)//
+		.filter(not(game().currentLevel.world::isEnergizerTile))//
+		.map(tile -> new Pellet3D(tile, foodMaterial)).collect(Collectors.toList());
+
+	player = new Player3D(game().player);
+	ghosts3D = game().ghosts()
+		.collect(Collectors.toMap(Function.identity(), ghost -> new Ghost3D(ghost, rendering2D)));
+
+	bonus3D = new Bonus3D(gameVariant, rendering2D);
+
+	score3D = new ScoreNotReally3D();
+	livesCounter3D = new LivesCounter3D(game().player, 1, 1);
+
+	tgMaze = new Group();
+
+	tgMaze.setTranslateX(-0.5 * game().currentLevel.world.numCols() * TS);
+	tgMaze.setTranslateY(-0.5 * game().currentLevel.world.numRows() * TS);
+
+	tgMaze.getChildren().addAll(score3D.get(), livesCounter3D.get());
+	tgMaze.getChildren().addAll(maze.getBricks());
+	tgMaze.getChildren().addAll(collect(energizers));
+	tgMaze.getChildren().addAll(collect(pellets));
+	tgMaze.getChildren().addAll(player.get());
+	tgMaze.getChildren().addAll(collect(ghosts3D.values()));
+	tgMaze.getChildren().add(bonus3D.get());
+	tgMaze.getChildren().addAll(ambientLight, pointLight);
+
+	coordSystem = new CoordinateSystem(150);
+	fxScene.setRoot(new Group(coordSystem.getNode(), tgMaze));
+    }
+
+    private Collection<Node> collect(Collection<? extends Supplier<Node>> items) {
+	return items.stream().map(Supplier::get).collect(Collectors.toList());
+    }
+
+    @Override
+    public void setAvailableSize(double width, double height) {
+	// data binding does the job
+    }
+
+    @Override
+    public SubScene getFXSubScene() {
+	return fxScene;
+    }
+
+    @Override
+    public Camera getActiveCamera() {
+	return fxScene.getCamera();
+    }
+
+    @Override
+    public void useMoveableCamera(boolean on) {
+	if (on) {
+	    useMoveableCamera();
+	} else {
+	    useStaticCamera();
+	}
+    }
+
+    private void useStaticCamera() {
+	staticCamera.setNearClip(0.1);
+	staticCamera.setFarClip(10000.0);
+	staticCamera.setTranslateX(0);
+	staticCamera.setTranslateY(270);
+	staticCamera.setTranslateZ(-460);
+	staticCamera.setRotationAxis(Rotate.X_AXIS);
+	staticCamera.setRotate(30);
+	fxScene.setCamera(staticCamera);
+    }
+
+    private void useMoveableCamera() {
+	moveableCamera.setNearClip(0.1);
+	moveableCamera.setFarClip(10000.0);
+	moveableCamera.setTranslateZ(-250);
+	moveableCamera.setRotationAxis(Rotate.X_AXIS);
+	moveableCamera.setRotate(30);
+	fxScene.setCamera(moveableCamera);
+    }
+
+    private void updateCamera() {
+	if (getActiveCamera() == moveableCamera) {
+	    double x = Math.min(10, lerp(moveableCamera.getTranslateX(), player.get().getTranslateX()));
+	    double y = Math.max(50, lerp(moveableCamera.getTranslateY(), player.get().getTranslateY()));
+	    moveableCamera.setTranslateX(x);
+	    moveableCamera.setTranslateY(y);
+	}
+    }
+
+    private static double lerp(double current, double target) {
+	return current + (target - current) * 0.02;
+    }
+
+    @Override
+    public void start() {
+	log("Game scene %s: start", this);
+	buildSceneGraph();
+    }
+
+    @Override
+    public void end() {
+	log("Game scene %s: end", this);
+    }
+
+    @Override
+    public void update() {
+	score3D.setHiscoreOnly(gameController.isAttractMode());
+	score3D.update(game());
+	score3D.get().setRotationAxis(Rotate.X_AXIS);
+	score3D.get().setRotate(getActiveCamera().getRotate());
+	livesCounter3D.get().setVisible(!gameController.isAttractMode());
+	livesCounter3D.update(game());
+	energizers.forEach(energizer3D -> energizer3D.update(game()));
+	pellets.forEach(pellet3D -> pellet3D.update(game()));
+	player.update();
+	game().ghosts().map(ghosts3D::get).forEach(Ghost3D::update);
+	bonus3D.update(game().bonus);
+	updateCamera();
+	playSceneSoundHandler.onUpdate();
+    }
+
+    @Override
+    public void onGameEvent(PacManGameEvent gameEvent) {
+	playSceneSoundHandler.onGameEvent(gameEvent);
+
+	if (gameEvent instanceof PacManGameStateChangedEvent) {
+	    onGameStateChange((PacManGameStateChangedEvent) gameEvent);
 	}
 
-	@Override
-	public PacManGameController getGameController() {
-		return gameController;
+	else if (gameEvent instanceof ExtraLifeEvent) {
+	    gameController.userInterface.showFlashMessage("Extra life!");
 	}
 
-	@Override
-	public void setGameController(PacManGameController gameController) {
-		this.gameController = gameController;
-		playSceneSoundHandler = new PlaySceneSoundHandler(gameController, sounds);
+	else if (gameEvent instanceof BonusActivatedEvent) {
+	    bonus3D.showSymbol(game().bonus);
 	}
 
-	@Override
-	public OptionalDouble aspectRatio() {
-		return OptionalDouble.empty();
+	else if (gameEvent instanceof BonusExpiredEvent) {
+	    bonus3D.hide();
 	}
 
-	@Override
-	public String toString() {
-		return getClass().getSimpleName() + "@" + hashCode();
+	else if (gameEvent instanceof BonusEatenEvent) {
+	    bonus3D.showPoints(game().bonus);
+	}
+    }
+
+    private void onGameStateChange(PacManGameStateChangedEvent event) {
+	AbstractGameModel gameModel = gameController.game();
+
+	playSceneSoundHandler.onGameStateChange(event.oldGameState, event.newGameState);
+
+	// enter READY
+	if (event.newGameState == PacManGameState.READY) {
+	    setSceneColor();
 	}
 
-	@Override
-	public void stopAllSounds() {
-		sounds.stopAll();
+	// enter HUNTING
+	if (event.newGameState == PacManGameState.HUNTING) {
+	    energizers.forEach(Energizer3D::startPumping);
 	}
 
-	private void buildSceneGraph() {
-		final GameVariant gameVariant = gameController.gameVariant();
-		final AbstractGameModel game = gameController.game();
-
-		fxScene.setFill(Color.rgb(20, 20, 60));
-
-		maze = new Maze3D(game, GameRendering2D_Assets.getMazeWallColor(game.currentLevel.mazeNumber));
-		PhongMaterial foodMaterial = GameRendering3D_Assets.foodMaterial(gameVariant, game.currentLevel.mazeNumber);
-
-		energizers = game.currentLevel.world.energizerTiles()//
-				.map(tile -> new Energizer3D(tile, foodMaterial))//
-				.collect(Collectors.toList());
-
-		pellets = game.currentLevel.world.tiles()//
-				.filter(game.currentLevel.world::isFoodTile)//
-				.filter(not(game.currentLevel.world::isEnergizerTile))//
-				.map(tile -> new Pellet3D(tile, foodMaterial)).collect(Collectors.toList());
-
-		player = new Player3D(game.player);
-		ghosts3D = game.ghosts().collect(Collectors.toMap(Function.identity(), Ghost3D::new));
-
-		bonus3D = new Bonus3D(gameVariant,
-				gameVariant == GameVariant.MS_PACMAN ? rendering2D_MsPacMan : rendering2D_PacMan);
-
-		score3D = new ScoreNotReally3D();
-		livesCounter3D = new LivesCounter3D(game.player, 1, 1);
-
-		tgMaze = new Group();
-
-		tgMaze.setTranslateX(-0.5 * game.currentLevel.world.numCols() * TS);
-		tgMaze.setTranslateY(-0.5 * game.currentLevel.world.numRows() * TS);
-
-		tgMaze.getChildren().addAll(score3D.get(), livesCounter3D.get());
-		tgMaze.getChildren().addAll(maze.getBricks());
-		tgMaze.getChildren().addAll(collect(energizers));
-		tgMaze.getChildren().addAll(collect(pellets));
-		tgMaze.getChildren().addAll(player.get());
-		tgMaze.getChildren().addAll(collect(ghosts3D.values()));
-		tgMaze.getChildren().add(bonus3D.get());
-		tgMaze.getChildren().addAll(ambientLight, pointLight);
-
-		coordSystem = new CoordinateSystem(150);
-		fxScene.setRoot(new Group(coordSystem.getNode(), tgMaze));
+	// exit HUNTING
+	if (event.oldGameState == PacManGameState.HUNTING && event.newGameState != PacManGameState.GHOST_DYING) {
+	    energizers.forEach(Energizer3D::stopPumping);
+	    bonus3D.hide();
 	}
 
-	private Collection<Node> collect(Collection<? extends Supplier<Node>> items) {
-		return items.stream().map(Supplier::get).collect(Collectors.toList());
+	// enter PACMAN_DYING
+	if (event.newGameState == PacManGameState.PACMAN_DYING) {
+	    playAnimationPlayerDying();
 	}
 
-	@Override
-	public void setAvailableSize(double width, double height) {
-		// data binding does the job
+	// enter LEVEL_COMPLETE
+	if (event.newGameState == PacManGameState.LEVEL_COMPLETE) {
+	    gameModel.ghosts().forEach(ghost -> ghost.visible = false);
+	    playAnimationLevelComplete();
 	}
 
-	@Override
-	public SubScene getFXSubScene() {
-		return fxScene;
+	// enter LEVEL_STARTING
+	if (event.newGameState == PacManGameState.LEVEL_STARTING) {
+	    playAnimationLevelStarting();
 	}
+    }
 
-	@Override
-	public Camera getActiveCamera() {
-		return fxScene.getCamera();
-	}
-
-	@Override
-	public void useMoveableCamera(boolean on) {
-		if (on) {
-			useMoveableCamera();
-		} else {
-			useStaticCamera();
-		}
-	}
-
-	private void useStaticCamera() {
-		staticCamera.setNearClip(0.1);
-		staticCamera.setFarClip(10000.0);
-		staticCamera.setTranslateX(0);
-		staticCamera.setTranslateY(270);
-		staticCamera.setTranslateZ(-460);
-		staticCamera.setRotationAxis(Rotate.X_AXIS);
-		staticCamera.setRotate(30);
-		fxScene.setCamera(staticCamera);
-	}
-
-	private void useMoveableCamera() {
-		moveableCamera.setNearClip(0.1);
-		moveableCamera.setFarClip(10000.0);
-		moveableCamera.setTranslateZ(-250);
-		moveableCamera.setRotationAxis(Rotate.X_AXIS);
-		moveableCamera.setRotate(30);
-		fxScene.setCamera(moveableCamera);
-	}
-
-	private void updateCamera() {
-		if (getActiveCamera() == moveableCamera) {
-			double x = Math.min(10, lerp(moveableCamera.getTranslateX(), player.get().getTranslateX()));
-			double y = Math.max(50, lerp(moveableCamera.getTranslateY(), player.get().getTranslateY()));
-			moveableCamera.setTranslateX(x);
-			moveableCamera.setTranslateY(y);
-		}
-	}
-
-	private static double lerp(double current, double target) {
-		return current + (target - current) * 0.02;
-	}
-
-	@Override
-	public void start() {
-		log("Game scene %s: start", this);
-		buildSceneGraph();
-	}
-
-	@Override
-	public void end() {
-		log("Game scene %s: end", this);
-	}
-
-	@Override
-	public void update() {
-		AbstractGameModel game = gameController.game();
-		score3D.setHiscoreOnly(gameController.isAttractMode());
-		score3D.update(game);
-		score3D.get().setRotationAxis(Rotate.X_AXIS);
-		score3D.get().setRotate(getActiveCamera().getRotate());
-		livesCounter3D.get().setVisible(!gameController.isAttractMode());
-		livesCounter3D.update(game);
-		energizers.forEach(energizer3D -> energizer3D.update(game));
-		pellets.forEach(pellet3D -> pellet3D.update(game));
-		player.update();
-		game.ghosts().map(ghosts3D::get).forEach(Ghost3D::update);
-		bonus3D.update(game.bonus);
-		updateCamera();
-		playSceneSoundHandler.onUpdate();
-	}
-
-	@Override
-	public void onGameEvent(PacManGameEvent gameEvent) {
-		playSceneSoundHandler.onGameEvent(gameEvent);
-
-		if (gameEvent instanceof PacManGameStateChangedEvent) {
-			onGameStateChange((PacManGameStateChangedEvent) gameEvent);
-		}
-
-		else if (gameEvent instanceof ExtraLifeEvent) {
-			gameController.userInterface.showFlashMessage("Extra life!");
-		}
-
-		else if (gameEvent instanceof BonusActivatedEvent) {
-			bonus3D.showSymbol(game().bonus);
-		}
-
-		else if (gameEvent instanceof BonusExpiredEvent) {
-			bonus3D.hide();
-		}
-
-		else if (gameEvent instanceof BonusEatenEvent) {
-			bonus3D.showPoints(game().bonus);
-		}
-	}
-
-	private void onGameStateChange(PacManGameStateChangedEvent event) {
-		AbstractGameModel gameModel = gameController.game();
-
-		playSceneSoundHandler.onGameStateChange(event.oldGameState, event.newGameState);
-
-		// enter READY
-		if (event.newGameState == PacManGameState.READY) {
-			setSceneColor();
-		}
-
-		// enter HUNTING
-		if (event.newGameState == PacManGameState.HUNTING) {
-			energizers.forEach(Energizer3D::startPumping);
-		}
-
-		// exit HUNTING
-		if (event.oldGameState == PacManGameState.HUNTING && event.newGameState != PacManGameState.GHOST_DYING) {
-			energizers.forEach(Energizer3D::stopPumping);
-			bonus3D.hide();
-		}
-
-		// enter PACMAN_DYING
-		if (event.newGameState == PacManGameState.PACMAN_DYING) {
-			playAnimationPlayerDying();
-		}
-
-		// enter LEVEL_COMPLETE
-		if (event.newGameState == PacManGameState.LEVEL_COMPLETE) {
-			gameModel.ghosts().forEach(ghost -> ghost.visible = false);
-			playAnimationLevelComplete();
-		}
-
-		// enter LEVEL_STARTING
-		if (event.newGameState == PacManGameState.LEVEL_STARTING) {
-			playAnimationLevelStarting();
-		}
-	}
-
-	private void setSceneColor() {
-		if (gameController.gameVariant() == GameVariant.PACMAN) {
-			Color color = Color.rgb(20, 20, 60);
-			fxScene.setFill(color);
-			ambientLight.setColor(Color.AZURE);
-		} else {
-			Color mazeColor = GameRendering2D_Assets.getMazeWallColor(gameController.game().currentLevel.mazeNumber);
+    private void setSceneColor() {
+	if (gameController.gameVariant() == GameVariant.PACMAN) {
+	    Color color = Color.rgb(20, 20, 60);
+	    fxScene.setFill(color);
+	    ambientLight.setColor(Color.AZURE);
+	} else {
+	    Color mazeColor = GameRendering2D_Assets.getMazeWallColor(gameController.game().currentLevel.mazeNumber);
 //			fxScene.setFill(mazeColor);
-			ambientLight.setColor(mazeColor);
-		}
+	    ambientLight.setColor(mazeColor);
 	}
+    }
 
-	private void playAnimationPlayerDying() {
-		// TODO implement this
+    private void playAnimationPlayerDying() {
+	// TODO implement this
 
-	}
+    }
 
-	private void playAnimationLevelComplete() {
-		AbstractGameModel game = gameController.game();
-		game.player.visible = false;
-		game.ghosts().forEach(ghost -> ghost.visible = false);
-		gameController.userInterface.showFlashMessage(String.format("%s!\n\nLevel %d complete.",
-				CONGRATS[new Random().nextInt(CONGRATS.length)], game.currentLevelNumber), 2);
+    private void playAnimationLevelComplete() {
+	AbstractGameModel game = gameController.game();
+	game.player.visible = false;
+	game.ghosts().forEach(ghost -> ghost.visible = false);
+	gameController.userInterface.showFlashMessage(String.format("%s!\n\nLevel %d complete.",
+		CONGRATS[new Random().nextInt(CONGRATS.length)], game.currentLevelNumber), 2);
 
-		gameController.stateTimer().reset();
-		PauseTransition pause = new PauseTransition(Duration.seconds(3));
-		pause.setOnFinished(e -> gameController.stateTimer().forceExpiration());
-		pause.play();
-	}
+	gameController.stateTimer().reset();
+	PauseTransition pause = new PauseTransition(Duration.seconds(3));
+	pause.setOnFinished(e -> gameController.stateTimer().forceExpiration());
+	pause.play();
+    }
 
-	private void playAnimationLevelStarting() {
-		gameController.stateTimer().reset();
-		gameController.userInterface.showFlashMessage("Entering Level " + gameController.game().currentLevelNumber);
+    private void playAnimationLevelStarting() {
+	gameController.stateTimer().reset();
+	gameController.userInterface.showFlashMessage("Entering Level " + gameController.game().currentLevelNumber);
 
-		ScaleTransition animation = new ScaleTransition(Duration.seconds(3), tgMaze);
-		animation.setDelay(Duration.seconds(2));
-		animation.setFromZ(0);
-		animation.setToZ(1);
-		animation.setOnFinished(e -> gameController.stateTimer().forceExpiration());
-		animation.play();
-	}
+	ScaleTransition animation = new ScaleTransition(Duration.seconds(3), tgMaze);
+	animation.setDelay(Duration.seconds(2));
+	animation.setFromZ(0);
+	animation.setToZ(1);
+	animation.setOnFinished(e -> gameController.stateTimer().forceExpiration());
+	animation.play();
+    }
 }
