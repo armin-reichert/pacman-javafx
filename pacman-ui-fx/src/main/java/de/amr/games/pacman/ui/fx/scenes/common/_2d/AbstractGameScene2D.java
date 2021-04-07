cbf6a7694ba679467e2fb8a621a4084f2dcb0ade
Armin Reichert <armin.reichert@web.de>
2021-04-07 10:52:27 +0200

    cleanup

--- pacman-ui-fx/src/main/java/de/amr/games/pacman/ui/fx/scenes/common/_2d/AbstractGameScene2D.java
+++ pacman-ui-fx/src/main/java/de/amr/games/pacman/ui/fx/scenes/common/_2d/AbstractGameScene2D.java
@@ -7,7 +7,6 @@ import java.util.OptionalDouble;
 
 import de.amr.games.pacman.controller.PacManGameController;
 import de.amr.games.pacman.controller.event.PacManGameEvent;
-import de.amr.games.pacman.model.common.AbstractGameModel;
 import de.amr.games.pacman.ui.fx.rendering.GameRendering2D;
 import de.amr.games.pacman.ui.fx.scenes.common.GameScene;
 import de.amr.games.pacman.ui.fx.sound.SoundManager;
@@ -70,10 +69,6 @@ public abstract class AbstractGameScene2D implements GameScene {
 
 	public GameRendering2D getRendering() {
 		return rendering;
-	}
-
-	public AbstractGameModel game() {
-		return gameController.game();
 	}
 
 	@Override
