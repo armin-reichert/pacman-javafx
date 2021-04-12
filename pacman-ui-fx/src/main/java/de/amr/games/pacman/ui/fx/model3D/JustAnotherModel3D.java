5ee08cf451eddba0746a994afb0e67846cbf286d
Armin <Armin@Intel-NUC8i7HNKQC.fritz.box>
2021-04-12 17:43:48 +0200

    cleanup

--- pacman-ui-fx/src/main/java/de/amr/games/pacman/ui/fx/model3D/JustAnotherModel3D.java
+++ pacman-ui-fx/src/main/java/de/amr/games/pacman/ui/fx/model3D/JustAnotherModel3D.java
@@ -7,12 +7,7 @@ import com.interactivemesh.jfx.importer.obj.ObjModelImporter;
 
 import de.amr.games.pacman.ui.fx.Env;
 import de.amr.games.pacman.ui.fx.rendering.GameRendering3D_Assets;
-import javafx.scene.Group;
-import javafx.scene.paint.Color;
-import javafx.scene.paint.PhongMaterial;
 import javafx.scene.shape.MeshView;
-import javafx.scene.transform.Rotate;
-import javafx.scene.transform.Translate;
 
 /**
  * 3D model from things I found somewhere on the Internet. Will be replaced soon.
@@ -25,9 +20,6 @@ public class JustAnotherModel3D {
 
 	private Map<String, MeshView> ghostMeshViewsByName;
 
-	private Map<String, MeshView> playerMeshViewsByName;
-	private Map<String, PhongMaterial> playerMaterialsByName;
-
 	private JustAnotherModel3D() {
 		ObjModelImporter objImporter = new ObjModelImporter();
 		try {
@@ -36,19 +28,6 @@ public class JustAnotherModel3D {
 		} catch (ImportException e) {
 			e.printStackTrace();
 		}
-		try {
-			objImporter.read(getClass().getResource("/common/temp/pacman1.obj"));
-			playerMeshViewsByName = objImporter.getNamedMeshViews();
-			playerMaterialsByName = objImporter.getNamedMaterials();
-		} catch (ImportException e) {
-			e.printStackTrace();
-		} finally {
-			objImporter.close();
-		}
-	}
-
-	public PhongMaterial getPlayerMaterial(String name) {
-		return playerMaterialsByName.get(name);
 	}
 
 	public MeshView createGhost() {
@@ -57,23 +36,5 @@ public class JustAnotherModel3D {
 		GameRendering3D_Assets.scaleNode(meshView, 8);
 		meshView.drawModeProperty().bind(Env.$drawMode);
 		return meshView;
-	}
-
-	public Group createPacMan() {
-		MeshView body = new MeshView(playerMeshViewsByName.get("Sphere_Sphere.002_Material.001").getMesh());
-		body.setMaterial(new PhongMaterial(Color.YELLOW));
-		body.drawModeProperty().bind(Env.$drawMode);
-
-		MeshView glasses = new MeshView(playerMeshViewsByName.get("Sphere_Sphere.002_Material.002").getMesh());
-		glasses.setMaterial(new PhongMaterial(Color.rgb(60, 60, 60)));
-		glasses.drawModeProperty().bind(Env.$drawMode);
-
-		Translate centering = GameRendering3D_Assets.centerNodeOverOrigin(body);
-		glasses.getTransforms().add(centering);
-
-		Group root = new Group(body, glasses);
-		root.getTransforms().add(new Rotate(90, Rotate.X_AXIS));
-		GameRendering3D_Assets.scaleNode(root, 8);
-		return root;
 	}
 }
