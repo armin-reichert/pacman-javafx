fce8ae6fd40ef7801155d2f2fd9dc64445e5dc72
Armin Reichert <armin.reichert@web.de>
2021-04-16 09:52:53 +0200

    refac

--- pacman-ui-fx/src/main/java/de/amr/games/pacman/ui/fx/entities/_3d/Ghost3D.java
+++ pacman-ui-fx/src/main/java/de/amr/games/pacman/ui/fx/entities/_3d/Ghost3D.java
@@ -10,8 +10,10 @@ import de.amr.games.pacman.ui.fx.rendering.Rendering2D;
 import de.amr.games.pacman.ui.fx.rendering.Rendering2D_Assets;
 import javafx.scene.Group;
 import javafx.scene.Node;
+import javafx.scene.image.Image;
 import javafx.scene.paint.Color;
 import javafx.scene.paint.PhongMaterial;
+import javafx.scene.shape.Box;
 import javafx.scene.shape.MeshView;
 import javafx.scene.transform.Rotate;
 
@@ -23,18 +25,21 @@ import javafx.scene.transform.Rotate;
 public class Ghost3D extends Group implements Supplier<Node> {
 
 	private final Ghost ghost;
+	private final Rendering2D rendering2D;
 	private final Group coloredGhost;
 	private PhongMaterial normalSkin;
 	private PhongMaterial blueSkin;
 	private final Group deadGhost;
-	private final BountyShape3D bountyShape;
+	private final Box bountyShape;
 
 	public Ghost3D(Ghost ghost, Rendering2D rendering2D) {
 		this.ghost = ghost;
+		this.rendering2D = rendering2D;
 		normalSkin = new PhongMaterial(Rendering2D_Assets.getGhostColor(ghost.id));
 		blueSkin = new PhongMaterial(Color.CORNFLOWERBLUE);
 		coloredGhost = GianmarcosModel3D.IT.createGhost();
-		bountyShape = new BountyShape3D(rendering2D);
+		bountyShape = new Box(8, 8, 8);
+		bountyShape.setMaterial(new PhongMaterial());
 		deadGhost = GianmarcosModel3D.IT.createGhostEyes();
 		setBlueSkin(false);
 	}
@@ -49,25 +54,28 @@ public class Ghost3D extends Group implements Supplier<Node> {
 		meshView.setMaterial(blue ? blueSkin : normalSkin);
 	}
 
-	private void setSingleChild(Node node) {
-		getChildren().clear();
-		getChildren().add(node);
+	private void setBounty(int value) {
+		Image sprite = rendering2D.subImage(rendering2D.getBountyNumberSpritesMap().get(value));
+		PhongMaterial material = (PhongMaterial) bountyShape.getMaterial();
+		material.setBumpMap(sprite);
+		material.setDiffuseMap(sprite);
 	}
 
 	public void update() {
 		setVisible(ghost.visible);
 		setTranslateX(ghost.position.x);
 		setTranslateY(ghost.position.y);
+		getChildren().clear();
 		if (ghost.bounty > 0) {
 			setRotationAxis(Rotate.X_AXIS);
 			setRotate(0);
-			bountyShape.setBounty(ghost.bounty);
-			setSingleChild(bountyShape);
+			setBounty(ghost.bounty);
+			getChildren().add(bountyShape);
 		} else if (ghost.is(GhostState.DEAD) || ghost.is(GhostState.ENTERING_HOUSE)) {
 			setRotationAxis(Rotate.Z_AXIS);
 			Direction dir = ghost.dir;
 			setRotate(dir == Direction.LEFT ? 0 : dir == Direction.RIGHT ? 180 : dir == Direction.UP ? 90 : -90);
-			setSingleChild(deadGhost);
+			getChildren().add(deadGhost);
 		} else {
 			setRotationAxis(Rotate.Y_AXIS);
 			setRotate(0);
@@ -75,7 +83,7 @@ public class Ghost3D extends Group implements Supplier<Node> {
 			Direction dir = ghost.is(GhostState.FRIGHTENED) ? ghost.dir : ghost.wishDir;
 			setRotate(dir == Direction.LEFT ? 0 : dir == Direction.RIGHT ? 180 : dir == Direction.UP ? 90 : -90);
 			setBlueSkin(ghost.is(GhostState.FRIGHTENED));
-			setSingleChild(coloredGhost);
+			getChildren().add(coloredGhost);
 		}
 	}
 }
