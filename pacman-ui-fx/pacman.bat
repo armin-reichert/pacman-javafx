0874c47ecfa71b679c95bdaa5c439174c23d9a4f
Armin Reichert <armin.reichert@web.de>
2021-04-17 07:47:37 +0200

    make this work again

--- pacman-ui-fx/pacman.bat
+++ pacman-ui-fx/pacman.bat
@@ -1,27 +1,10 @@
 :: Usage: pacman.bat or pacman.bat -mspacman
-
 ::@ECHO OFF
 @SETLOCAL
-
 SET "JAVA_HOME=C:\Program Files\Java\jdk-15.0.2"
-SET "MAVEN_REPOSITORY=C:\Users\armin\.m2\repository"
 SET "JFX_LIB=C:\Program Files\Java\javafx-sdk-15.0.1\lib"
 SET "PACMAN_LIB=..\..\pacman-basic\pacman-core\target\pacman-core-1.0.jar"
 SET "PACMAN_UI_FX=.\target\pacman-ui-fx-1.0.jar"
-SET APP=de.amr.games.pacman.ui.fx.app.PacManGameAppFX
-
-SET "CLASSPATH=%PACMAN_LIB%;%PACMAN_UI_FX%"^
- ";%JFXLIB%\javafx.base.jar"^
- ";%JFXLIB%\javafx.controls.jar"^
- ";%JFXLIB%\javafx.graphics.jar"^
- ";%MAVEN_REPOSITORY%\org\openjfx\javafx-controls\15\javafx-controls-15.jar"^
- ";%MAVEN_REPOSITORY%\org\openjfx\javafx-controls\15\javafx-controls-15-win.jar"^
- ";%MAVEN_REPOSITORY%\org\openjfx\javafx-graphics\15\javafx-graphics-15.jar"^
- ";%MAVEN_REPOSITORY%\org\openjfx\javafx-graphics\15\javafx-graphics-15-win.jar"^
- ";%MAVEN_REPOSITORY%\org\openjfx\javafx-base\15\javafx-base-15.jar"^
- ";%MAVEN_REPOSITORY%\org\openjfx\javafx-base\15\javafx-base-15-win.jar"^
- ";%MAVEN_REPOSITORY%\com\interactivemesh\jimObjModelImporterJFX\1.0\jimObjModelImporterJFX-1.0.jar"
-
-"%JAVA_HOME%"\bin\javaw.exe --module-path "%JFX_LIB%" --add-modules javafx.controls -cp %CLASSPATH% %APP% %*
-
+SET CLASSPATH="%PACMAN_LIB%;%PACMAN_UI_FX%;%JFX_LIB%\javafx.base.jar;%JFX_LIB%\javafx.controls.jar;%JFX_LIB%\javafx.graphics.jar;..\interactivemesh\jars\jimObjModelImporterJFX.jar"
+"%JAVA_HOME%"\bin\javaw.exe --module-path "%JFX_LIB%" --add-modules javafx.controls -cp %CLASSPATH% de.amr.games.pacman.ui.fx.app.PacManGameAppFX %*
 @ENDLOCAL
