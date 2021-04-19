a5fb118bd1c02117ac23ec080a964cb0eb6ca6bd
Armin <Armin@Intel-NUC8i7HNKQC.fritz.box>
2021-04-19 14:47:17 +0200

    wav->mp3

--- pacman-ui-fx/src/main/java/de/amr/games/pacman/ui/fx/sound/PacManGameSounds.java
+++ pacman-ui-fx/src/main/java/de/amr/games/pacman/ui/fx/sound/PacManGameSounds.java
@@ -25,39 +25,39 @@ public class PacManGameSounds {
 
 	static {
 		//@formatter:off
-		pacManSounds.put(PacManGameSound.CREDIT,             url("/pacman/sound/credit.wav"));
-		pacManSounds.put(PacManGameSound.EXTRA_LIFE,         url("/pacman/sound/extend.wav"));
-		pacManSounds.put(PacManGameSound.GAME_READY,         url("/pacman/sound/game_start.wav"));
-		pacManSounds.put(PacManGameSound.BONUS_EATEN,        url("/pacman/sound/eat_fruit.wav"));
-		pacManSounds.put(PacManGameSound.PACMAN_MUNCH,       url("/pacman/sound/munch_1.wav"));
-		pacManSounds.put(PacManGameSound.PACMAN_DEATH,       url("/pacman/sound/death_1.wav"));
-		pacManSounds.put(PacManGameSound.PACMAN_POWER,       url("/pacman/sound/power_pellet.wav"));
-		pacManSounds.put(PacManGameSound.GHOST_EATEN,        url("/pacman/sound/eat_ghost.wav"));
-		pacManSounds.put(PacManGameSound.GHOST_RETURNING_HOME,         url("/pacman/sound/retreating.wav"));
-		pacManSounds.put(PacManGameSound.GHOST_SIREN_1,      url("/pacman/sound/siren_1.wav"));
-		pacManSounds.put(PacManGameSound.GHOST_SIREN_2,      url("/pacman/sound/siren_2.wav"));
-		pacManSounds.put(PacManGameSound.GHOST_SIREN_3,      url("/pacman/sound/siren_3.wav"));
-		pacManSounds.put(PacManGameSound.GHOST_SIREN_4,      url("/pacman/sound/siren_4.wav"));
-		pacManSounds.put(PacManGameSound.INTERMISSION_1,     url("/pacman/sound/intermission.wav"));
-		pacManSounds.put(PacManGameSound.INTERMISSION_2,     url("/pacman/sound/intermission.wav"));
-		pacManSounds.put(PacManGameSound.INTERMISSION_3,     url("/pacman/sound/intermission.wav"));
+		pacManSounds.put(PacManGameSound.CREDIT,             url("/pacman/sound/credit.mp3"));
+		pacManSounds.put(PacManGameSound.EXTRA_LIFE,         url("/pacman/sound/extend.mp3"));
+		pacManSounds.put(PacManGameSound.GAME_READY,         url("/pacman/sound/game_start.mp3"));
+		pacManSounds.put(PacManGameSound.BONUS_EATEN,        url("/pacman/sound/eat_fruit.mp3"));
+		pacManSounds.put(PacManGameSound.PACMAN_MUNCH,       url("/pacman/sound/munch_1.mp3"));
+		pacManSounds.put(PacManGameSound.PACMAN_DEATH,       url("/pacman/sound/death_1.mp3"));
+		pacManSounds.put(PacManGameSound.PACMAN_POWER,       url("/pacman/sound/power_pellet.mp3"));
+		pacManSounds.put(PacManGameSound.GHOST_EATEN,        url("/pacman/sound/eat_ghost.mp3"));
+		pacManSounds.put(PacManGameSound.GHOST_RETURNING_HOME,         url("/pacman/sound/retreating.mp3"));
+		pacManSounds.put(PacManGameSound.GHOST_SIREN_1,      url("/pacman/sound/siren_1.mp3"));
+		pacManSounds.put(PacManGameSound.GHOST_SIREN_2,      url("/pacman/sound/siren_2.mp3"));
+		pacManSounds.put(PacManGameSound.GHOST_SIREN_3,      url("/pacman/sound/siren_3.mp3"));
+		pacManSounds.put(PacManGameSound.GHOST_SIREN_4,      url("/pacman/sound/siren_4.mp3"));
+		pacManSounds.put(PacManGameSound.INTERMISSION_1,     url("/pacman/sound/intermission.mp3"));
+		pacManSounds.put(PacManGameSound.INTERMISSION_2,     url("/pacman/sound/intermission.mp3"));
+		pacManSounds.put(PacManGameSound.INTERMISSION_3,     url("/pacman/sound/intermission.mp3"));
 
-		msPacManSounds.put(PacManGameSound.CREDIT,           url("/mspacman/sound/Coin Credit.wav"));
-		msPacManSounds.put(PacManGameSound.EXTRA_LIFE,       url("/mspacman/sound/Extra Life.wav"));
-		msPacManSounds.put(PacManGameSound.GAME_READY,       url("/mspacman/sound/Start.wav"));
-		msPacManSounds.put(PacManGameSound.BONUS_EATEN,      url("/mspacman/sound/Fruit.wav"));
-		msPacManSounds.put(PacManGameSound.PACMAN_MUNCH,     url("/mspacman/sound/Ms. Pac Man Pill.wav"));
-		msPacManSounds.put(PacManGameSound.PACMAN_DEATH,     url("/mspacman/sound/Died.wav"));
-		msPacManSounds.put(PacManGameSound.PACMAN_POWER,     url("/mspacman/sound/Scared Ghost.wav"));
-		msPacManSounds.put(PacManGameSound.GHOST_EATEN,      url("/mspacman/sound/Ghost.wav"));
-		msPacManSounds.put(PacManGameSound.GHOST_RETURNING_HOME,       url("/mspacman/sound/Ghost Eyes.wav"));
-		msPacManSounds.put(PacManGameSound.GHOST_SIREN_1,    url("/mspacman/sound/Ghost Noise 1.wav"));
-		msPacManSounds.put(PacManGameSound.GHOST_SIREN_2,    url("/mspacman/sound/Ghost Noise 2.wav"));
-		msPacManSounds.put(PacManGameSound.GHOST_SIREN_3,    url("/mspacman/sound/Ghost Noise 3.wav"));
-		msPacManSounds.put(PacManGameSound.GHOST_SIREN_4,    url("/mspacman/sound/Ghost Noise 4.wav"));
-		msPacManSounds.put(PacManGameSound.INTERMISSION_1,   url("/mspacman/sound/They Meet Act 1.wav"));
-		msPacManSounds.put(PacManGameSound.INTERMISSION_2,   url("/mspacman/sound/The Chase Act 2.wav"));
-		msPacManSounds.put(PacManGameSound.INTERMISSION_3,   url("/mspacman/sound/Junior Act 3.wav"));
+		msPacManSounds.put(PacManGameSound.CREDIT,           url("/mspacman/sound/Coin Credit.mp3"));
+		msPacManSounds.put(PacManGameSound.EXTRA_LIFE,       url("/mspacman/sound/Extra Life.mp3"));
+		msPacManSounds.put(PacManGameSound.GAME_READY,       url("/mspacman/sound/Start.mp3"));
+		msPacManSounds.put(PacManGameSound.BONUS_EATEN,      url("/mspacman/sound/Fruit.mp3"));
+		msPacManSounds.put(PacManGameSound.PACMAN_MUNCH,     url("/mspacman/sound/Ms. Pac Man Pill.mp3"));
+		msPacManSounds.put(PacManGameSound.PACMAN_DEATH,     url("/mspacman/sound/Died.mp3"));
+		msPacManSounds.put(PacManGameSound.PACMAN_POWER,     url("/mspacman/sound/Scared Ghost.mp3"));
+		msPacManSounds.put(PacManGameSound.GHOST_EATEN,      url("/mspacman/sound/Ghost.mp3"));
+		msPacManSounds.put(PacManGameSound.GHOST_RETURNING_HOME,       url("/mspacman/sound/Ghost Eyes.mp3"));
+		msPacManSounds.put(PacManGameSound.GHOST_SIREN_1,    url("/mspacman/sound/Ghost Noise 1.mp3"));
+		msPacManSounds.put(PacManGameSound.GHOST_SIREN_2,    url("/mspacman/sound/Ghost Noise 2.mp3"));
+		msPacManSounds.put(PacManGameSound.GHOST_SIREN_3,    url("/mspacman/sound/Ghost Noise 3.mp3"));
+		msPacManSounds.put(PacManGameSound.GHOST_SIREN_4,    url("/mspacman/sound/Ghost Noise 4.mp3"));
+		msPacManSounds.put(PacManGameSound.INTERMISSION_1,   url("/mspacman/sound/They Meet Act 1.mp3"));
+		msPacManSounds.put(PacManGameSound.INTERMISSION_2,   url("/mspacman/sound/The Chase Act 2.mp3"));
+		msPacManSounds.put(PacManGameSound.INTERMISSION_3,   url("/mspacman/sound/Junior Act 3.mp3"));
 		//@formatter:on
 	}
 }
