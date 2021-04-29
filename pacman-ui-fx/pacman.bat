55eb6b9e69a34ab9fb3e6e6871bd50e086bdf41f
Armin Reichert <armin.reichert@web.de>
2021-04-29 12:58:17 +0200

    cleanup

--- pacman-ui-fx/pacman.bat
+++ pacman-ui-fx/pacman.bat
@@ -8,21 +8,13 @@
 @echo off
 @setlocal
 
+:: adapt these
 set JDK=C:\Program Files\Java\jdk-15.0.2
 set JFX=C:\Program Files\Java\javafx-sdk-15.0.1
 set GIT=C:\Users\armin\git
-set MAVEN_REPOSITORY=C:\Users\armin\.m2\repository
-
-set pacman-core-jar=%GIT%\pacman-basic\pacman-core\target\pacman-core-1.0.jar
+set MVN_REPO=C:\Users\armin\.m2\repository
 set pacman-ui-fx-jar=%GIT%\pacman-javafx\pacman-ui-fx\target\pacman-ui-fx-1.0.jar
 
-set CLASSPATH="%pacman-core-jar%;^
-%pacman-ui-fx-jar%;^
-%JFX%\lib\javafx.base.jar;^
-%JFX%\lib\javafx.controls.jar;^
-%JFX%\lib\javafx.media.jar;^
-..\interactivemesh\jars\jimObjModelImporterJFX.jar"
-
-"%JDK%\bin\javaw.exe" -Dfile.encoding=UTF-8 -p "%GIT%\pacman-javafx\pacman-ui-fx\target\classes;%GIT%\pacman-javafx\interactivemesh\jars\jimObjModelImporterJFX.jar;%MAVEN_REPOSITORY%\org\openjfx\javafx-controls\15.0.1\javafx-controls-15.0.1-win.jar;%MAVEN_REPOSITORY%\org\openjfx\javafx-graphics\15.0.1\javafx-graphics-15.0.1-win.jar;%MAVEN_REPOSITORY%\org\openjfx\javafx-base\15.0.1\javafx-base-15.0.1-win.jar;%MAVEN_REPOSITORY%\org\openjfx\javafx-media\15.0.1\javafx-media-15.0.1-win.jar;%GIT%\pacman-basic\pacman-core\target\classes" -classpath "%MAVEN_REPOSITORY%\org\openjfx\javafx-controls\15.0.1\javafx-controls-15.0.1.jar;%MAVEN_REPOSITORY%\org\openjfx\javafx-graphics\15.0.1\javafx-graphics-15.0.1.jar;%MAVEN_REPOSITORY%\org\openjfx\javafx-base\15.0.1\javafx-base-15.0.1.jar;%MAVEN_REPOSITORY%\org\openjfx\javafx-media\15.0.1\javafx-media-15.0.1.jar" -m de.amr.games.pacman.ui.fx/de.amr.games.pacman.ui.fx.app.PacManGameAppFX %*
+"%JDK%\bin\javaw.exe" -Dfile.encoding=UTF-8 -p "%pacman-ui-fx-jar%;%GIT%\pacman-javafx\interactivemesh\jars\jimObjModelImporterJFX.jar;%MVN_REPO%\org\openjfx\javafx-controls\15.0.1\javafx-controls-15.0.1-win.jar;%MVN_REPO%\org\openjfx\javafx-graphics\15.0.1\javafx-graphics-15.0.1-win.jar;%MVN_REPO%\org\openjfx\javafx-base\15.0.1\javafx-base-15.0.1-win.jar;%MVN_REPO%\org\openjfx\javafx-media\15.0.1\javafx-media-15.0.1-win.jar;%GIT%\pacman-basic\pacman-core\target\classes" -classpath "%MVN_REPO%\org\openjfx\javafx-controls\15.0.1\javafx-controls-15.0.1.jar;%MVN_REPO%\org\openjfx\javafx-graphics\15.0.1\javafx-graphics-15.0.1.jar;%MVN_REPO%\org\openjfx\javafx-base\15.0.1\javafx-base-15.0.1.jar;%MVN_REPO%\org\openjfx\javafx-media\15.0.1\javafx-media-15.0.1.jar" -m de.amr.games.pacman.ui.fx/de.amr.games.pacman.ui.fx.app.PacManGameAppFX %*
 
 @endlocal
