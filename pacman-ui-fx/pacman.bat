e9fb18ece92a7758fa531b3332851569f00fbae9
Armin Reichert <armin.reichert@web.de>
2021-04-29 13:03:09 +0200

    cleanup

--- pacman-ui-fx/pacman.bat
+++ pacman-ui-fx/pacman.bat
@@ -10,11 +10,12 @@
 
 :: adapt these
 set JDK=C:\Program Files\Java\jdk-15.0.2
-set JFX=C:\Program Files\Java\javafx-sdk-15.0.1
+set JFX=C:\Program Files\Java\javafx-sdk-15.0.2
 set GIT=C:\Users\armin\git
 set MVN_REPO=C:\Users\armin\.m2\repository
+set MVN_JFX_VERSION=15.0.1
 set pacman-ui-fx-jar=%GIT%\pacman-javafx\pacman-ui-fx\target\pacman-ui-fx-1.0.jar
 
-"%JDK%\bin\javaw.exe" -Dfile.encoding=UTF-8 -p "%pacman-ui-fx-jar%;%GIT%\pacman-javafx\interactivemesh\jars\jimObjModelImporterJFX.jar;%MVN_REPO%\org\openjfx\javafx-controls\15.0.1\javafx-controls-15.0.1-win.jar;%MVN_REPO%\org\openjfx\javafx-graphics\15.0.1\javafx-graphics-15.0.1-win.jar;%MVN_REPO%\org\openjfx\javafx-base\15.0.1\javafx-base-15.0.1-win.jar;%MVN_REPO%\org\openjfx\javafx-media\15.0.1\javafx-media-15.0.1-win.jar;%GIT%\pacman-basic\pacman-core\target\classes" -classpath "%MVN_REPO%\org\openjfx\javafx-controls\15.0.1\javafx-controls-15.0.1.jar;%MVN_REPO%\org\openjfx\javafx-graphics\15.0.1\javafx-graphics-15.0.1.jar;%MVN_REPO%\org\openjfx\javafx-base\15.0.1\javafx-base-15.0.1.jar;%MVN_REPO%\org\openjfx\javafx-media\15.0.1\javafx-media-15.0.1.jar" -m de.amr.games.pacman.ui.fx/de.amr.games.pacman.ui.fx.app.PacManGameAppFX %*
+"%JDK%\bin\javaw.exe" -Dfile.encoding=UTF-8 -p "%pacman-ui-fx-jar%;%GIT%\pacman-javafx\interactivemesh\jars\jimObjModelImporterJFX.jar;%MVN_REPO%\org\openjfx\javafx-controls\%MVN_JFX_VERSION%\javafx-controls-%MVN_JFX_VERSION%-win.jar;%MVN_REPO%\org\openjfx\javafx-graphics\%MVN_JFX_VERSION%\javafx-graphics-%MVN_JFX_VERSION%-win.jar;%MVN_REPO%\org\openjfx\javafx-base\%MVN_JFX_VERSION%\javafx-base-%MVN_JFX_VERSION%-win.jar;%MVN_REPO%\org\openjfx\javafx-media\%MVN_JFX_VERSION%\javafx-media-%MVN_JFX_VERSION%-win.jar;%GIT%\pacman-basic\pacman-core\target\classes" -classpath "%MVN_REPO%\org\openjfx\javafx-controls\%MVN_JFX_VERSION%\javafx-controls-%MVN_JFX_VERSION%.jar;%MVN_REPO%\org\openjfx\javafx-graphics\%MVN_JFX_VERSION%\javafx-graphics-%MVN_JFX_VERSION%.jar;%MVN_REPO%\org\openjfx\javafx-base\%MVN_JFX_VERSION%\javafx-base-%MVN_JFX_VERSION%.jar;%MVN_REPO%\org\openjfx\javafx-media\%MVN_JFX_VERSION%\javafx-media-%MVN_JFX_VERSION%.jar" -m de.amr.games.pacman.ui.fx/de.amr.games.pacman.ui.fx.app.PacManGameAppFX %*
 
 @endlocal
