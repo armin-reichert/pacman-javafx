3a67f39cab7e2eb7c2c341e867e778a99cd8ca3f
Armin Reichert <armin.reichert@web.de>
2021-06-28 10:54:13 +0200

    cleanup

--- pacman-ui-fx/src/main/java/de/amr/games/pacman/ui/fx/rendering/Rendering2D.java
+++ pacman-ui-fx/src/main/java/de/amr/games/pacman/ui/fx/rendering/Rendering2D.java
@@ -26,7 +26,7 @@ public abstract class Rendering2D {
 	public Rendering2D(String spritesheetPath, int cellSize) {
 		spritesheet = new Spritesheet(spritesheetPath, cellSize);
 	}
-	
+
 	public Spritesheet getSpritesheet() {
 		return spritesheet;
 	}
@@ -45,29 +45,42 @@ public abstract class Rendering2D {
 		return null; // should not happen
 	}
 
+	/**
+	 * @return font used in score and game state display
+	 */
 	public Font getScoreFont() {
 		return Rendering2D_Assets.ARCADE_FONT;
 	}
 
 	/**
-	 * Draws a game entity centered over its collision box (one square tile)
+	 * Renders a game entity centered over its tile position
 	 * 
-	 * @param g            the graphics context
-	 * @param entity       the guy
-	 * @param spriteBounds sprite (region) in spritsheet
+	 * @param g      the graphics context
+	 * @param entity the entity getting rendered
+	 * @param sb     sprite bounds in spritsheet
 	 */
-	public void renderEntity(GraphicsContext g, GameEntity entity, Rectangle2D spriteBounds) {
-		if (entity.isVisible() && spriteBounds != null) {
-			g.drawImage(spritesheet.getSource(), spriteBounds.getMinX(), spriteBounds.getMinY(), spriteBounds.getWidth(),
-					spriteBounds.getHeight(), entity.position().x - spriteBounds.getWidth() / 2 + HTS,
-					entity.position().y - spriteBounds.getHeight() / 2 + HTS, spriteBounds.getWidth(), spriteBounds.getHeight());
+	public void renderEntity(GraphicsContext g, GameEntity entity, Rectangle2D sb) {
+		if (entity.isVisible() && sb != null) {
+			g.drawImage(spritesheet.getSource(), sb.getMinX(), sb.getMinY(), sb.getWidth(), sb.getHeight(),
+					entity.position().x - sb.getWidth() / 2 + HTS, entity.position().y - sb.getHeight() / 2 + HTS, sb.getWidth(),
+					sb.getHeight());
 		}
 	}
 
-	public void renderSprite(GraphicsContext g, Rectangle2D spriteBounds, double x, double y) {
-		g.drawImage(spritesheet.getSource(), spriteBounds.getMinX(), spriteBounds.getMinY(), spriteBounds.getWidth(),
-				spriteBounds.getHeight(), x, y, spriteBounds.getWidth(), spriteBounds.getHeight());
+	/**
+	 * Renders a sprite at a given location.
+	 * 
+	 * @param g  the graphics context
+	 * @param sb sprite bounds in spritesheet
+	 * @param x  render location x
+	 * @param y  render location y
+	 */
+	public void renderSprite(GraphicsContext g, Rectangle2D sb, double x, double y) {
+		g.drawImage(spritesheet.getSource(), sb.getMinX(), sb.getMinY(), sb.getWidth(), sb.getHeight(), x, y, sb.getWidth(),
+				sb.getHeight());
 	}
+
+	// Maze
 
 	public abstract Image getMazeFullImage(int mazeNumber);
 
@@ -90,6 +103,8 @@ public abstract class Rendering2D {
 	public abstract TimedSequence<Rectangle2D> createGhostFlashingAnimation();
 
 	public abstract Map<Direction, TimedSequence<Rectangle2D>> createGhostReturningHomeAnimations();
+
+	// Sprites
 
 	public abstract Rectangle2D getLifeImage();
 
