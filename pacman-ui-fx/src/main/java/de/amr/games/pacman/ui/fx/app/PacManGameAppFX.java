3f6732dc008e0d8c98b9ed069c18d318647b27eb
Armin Reichert <armin.reichert@web.de>
2021-07-04 07:38:31 +0200

    refac

--- pacman-ui-fx/src/main/java/de/amr/games/pacman/ui/fx/app/PacManGameAppFX.java
+++ pacman-ui-fx/src/main/java/de/amr/games/pacman/ui/fx/app/PacManGameAppFX.java
@@ -49,74 +49,71 @@ public class PacManGameAppFX extends Application {
 		launch(args);
 	}
 
-	private static final String[] PARAMETER_NAMES = { "-2D", "-3D", "-height", "-mspacman", "-pacman" };
+	private static class Options {
 
-	// these are configurable via the command-line
-	private double windowHeight = 576;
-	private boolean use3DScenes = true;
-	private GameVariant gameVariant = GameVariant.PACMAN;
+		static final String[] NAMES = { "-2D", "-3D", "-height", "-mspacman", "-pacman" };
 
-	private PacManGameController gameController;
+		double windowHeight = 576;
+		boolean use3DScenes = true;
+		GameVariant gameVariant = GameVariant.PACMAN;
 
-	private void parseParameters(List<String> params) {
-		List<String> parameterNamesList = Arrays.asList(PARAMETER_NAMES);
-		int i = 0;
-		while (i < params.size()) {
+		Options(List<String> params) {
+			List<String> parameterNamesList = Arrays.asList(NAMES);
+			int i = 0;
+			while (i < params.size()) {
 
-			if ("-height".equals(params.get(i))) {
-				if (i + 1 == params.size() || parameterNamesList.contains(params.get(i + 1))) {
-					log("!!! Error parsing parameters: missing height value.");
-				} else {
-					++i;
-					try {
-						windowHeight = Double.parseDouble(params.get(i));
-					} catch (NumberFormatException x) {
-						log("!!! Error parsing parameters: '%s' is no legal height value.", params.get(i));
+				if ("-height".equals(params.get(i))) {
+					if (i + 1 == params.size() || parameterNamesList.contains(params.get(i + 1))) {
+						log("!!! Error parsing parameters: missing height value.");
+					} else {
+						++i;
+						try {
+							windowHeight = Double.parseDouble(params.get(i));
+						} catch (NumberFormatException x) {
+							log("!!! Error parsing parameters: '%s' is no legal height value.", params.get(i));
+						}
 					}
 				}
-			}
 
-			else if ("-mspacman".equals(params.get(i))) {
-				gameVariant = GameVariant.MS_PACMAN;
-			}
+				else if ("-mspacman".equals(params.get(i))) {
+					gameVariant = GameVariant.MS_PACMAN;
+				}
 
-			else if ("-pacman".equals(params.get(i))) {
-				gameVariant = GameVariant.PACMAN;
-			}
+				else if ("-pacman".equals(params.get(i))) {
+					gameVariant = GameVariant.PACMAN;
+				}
 
-			else if ("-2D".equals(params.get(i))) {
-				use3DScenes = false;
-			}
+				else if ("-2D".equals(params.get(i))) {
+					use3DScenes = false;
+				}
 
-			else if ("-3D".equals(params.get(i))) {
-				use3DScenes = true;
-			}
+				else if ("-3D".equals(params.get(i))) {
+					use3DScenes = true;
+				}
 
-			else {
-				log("!!! Error parsing parameters: Found garbage '%s'", params.get(i));
-			}
+				else {
+					log("!!! Error parsing parameters: Found garbage '%s'", params.get(i));
+				}
 
-			++i;
+				++i;
+			}
 		}
 	}
 
 	@Override
-	public void init() throws Exception {
-		parseParameters(getParameters().getRaw());
-		log("Game variant: %s, window height: %.0f, 3D: %s", gameVariant, windowHeight, use3DScenes);
-	}
-
-	@Override
 	public void start(Stage stage) throws IOException {
-		gameController = new PacManGameController();
-		gameController.selectGameVariant(gameVariant);
-		gameController.setUI(new PacManGameUI_JavaFX(stage, gameController, windowHeight));
+		Options options = new Options(getParameters().getRaw());
+		log("Game variant: %s, window height: %.0f, 3D: %s", options.gameVariant, options.windowHeight,
+				options.use3DScenes);
+		PacManGameController gameController = new PacManGameController();
+		gameController.selectGameVariant(options.gameVariant);
+		gameController.setUI(new PacManGameUI_JavaFX(stage, gameController, options.windowHeight));
 		var gameLoop = new GameLoop( //
 				new GameLoopTask("Controller Step", gameController::step), //
 				new GameLoopTask("UI Update      ", gameController.getUI()::update));
 		Env.$totalTicks.bind(gameLoop.$totalTicks);
 		Env.$fps.bind(gameLoop.$fps);
-		Env.$use3DScenes.set(use3DScenes);
+		Env.$use3DScenes.set(options.use3DScenes);
 		gameLoop.start();
 	}
 }
