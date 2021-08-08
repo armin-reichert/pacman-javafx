54eb7a54a25d72c6f822d67bfc0c03ddc72574ee
Armin Reichert <armin.reichert@web.de>
2021-08-08 10:09:47 +0200

    localize

--- pacman-ui-fx/src/main/java/de/amr/games/pacman/ui/fx/scenes/common/TrashTalk.java
+++ pacman-ui-fx/src/main/java/de/amr/games/pacman/ui/fx/scenes/common/TrashTalk.java
@@ -1,32 +1,17 @@
 package de.amr.games.pacman.ui.fx.scenes.common;
 
+import java.util.ResourceBundle;
+
 import de.amr.games.pacman.ui.fx.util.RandomEntrySelector;
 
 public class TrashTalk {
 
-	public static final RandomEntrySelector<String> CHEAT_TALK = new RandomEntrySelector<>(//
-			"You old cheating bastard!", //
-			"I told you, I will erase your hard disk!", //
-			"Cheaters are the worst human beings!", //
-			"Do you think I will not notice this?", //
-			"Ah, Mr. Super-Clever again", //
-			"STOP! CHEATING! NOW!" //
-	);
+	public static final RandomEntrySelector<String> CHEAT_TALK = load("/common/cheating_talk");
+	public static final RandomEntrySelector<String> LEVEL_COMPLETE_TALK = load("/common/level_complete_talk");
+	public static final RandomEntrySelector<String> GAME_OVER_TALK = load("/common/game_over_talk");
 
-	public static final RandomEntrySelector<String> LEVEL_COMPLETE_TALK = new RandomEntrySelector<>(//
-			"Well done!", //
-			"Congratulations!", //
-			"Awesome!", //
-			"You really did it!", //
-			"You're the man*in!", //
-			"WTF!"//
-	);
-
-	public static final RandomEntrySelector<String> GAME_OVER_TALK = new RandomEntrySelector<>(//
-			"You stone cold loser!", //
-			"I would say you fucked up!", //
-			"This game is OVER!", //
-			"Go ahead and cry!", //
-			"That's all you've got?"//
-	);
+	private static RandomEntrySelector<String> load(String bundleName) {
+		ResourceBundle bundle = ResourceBundle.getBundle(bundleName);
+		return new RandomEntrySelector<>(bundle.keySet().stream().sorted().map(bundle::getString).toArray(String[]::new));
+	}
 }
