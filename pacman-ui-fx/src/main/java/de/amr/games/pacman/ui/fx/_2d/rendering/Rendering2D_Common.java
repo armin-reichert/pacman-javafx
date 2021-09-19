9a512a555523817fd8efe2062b510b3fec855cb0
Armin Reichert <armin.reichert@web.de>
2021-09-19 08:07:41 +0200

    refac

--- pacman-ui-fx/src/main/java/de/amr/games/pacman/ui/fx/_2d/rendering/Rendering2D_Common.java
+++ pacman-ui-fx/src/main/java/de/amr/games/pacman/ui/fx/_2d/rendering/Rendering2D_Common.java
@@ -1,7 +1,14 @@
 package de.amr.games.pacman.ui.fx._2d.rendering;
 
+import static de.amr.games.pacman.lib.Direction.DOWN;
+import static de.amr.games.pacman.lib.Direction.LEFT;
+import static de.amr.games.pacman.lib.Direction.RIGHT;
+import static de.amr.games.pacman.lib.Direction.UP;
 import static de.amr.games.pacman.model.world.PacManGameWorld.HTS;
 
+import java.io.InputStream;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Map;
 
 import de.amr.games.pacman.lib.Direction;
@@ -10,6 +17,8 @@ import de.amr.games.pacman.model.common.GameEntity;
 import de.amr.games.pacman.model.pacman.Bonus;
 import javafx.geometry.Rectangle2D;
 import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.image.Image;
+import javafx.scene.image.WritableImage;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 
@@ -18,12 +27,102 @@ import javafx.scene.text.Font;
  * 
  * @author Armin Reichert
  */
-public abstract class Rendering2D_Common extends Spritesheet {
+public abstract class Rendering2D_Common {
 
 	public static final Font ARCADE_FONT = Font.loadFont(resource("/emulogic.ttf"), 8);
 
-	public Rendering2D_Common(String path, int rasterSize) {
-		super(path, rasterSize);
+	public static InputStream resource(String path) {
+		return Rendering2D_Common.class.getResourceAsStream(path);
+	}
+
+	/**
+	 * @param source    source image
+	 * @param exchanges map of color exchanges
+	 * @return copy of source image with colors exchanged as given
+	 */
+	public static Image colorsExchanged(Image source, Map<Color, Color> exchanges) {
+		WritableImage newImage = new WritableImage((int) source.getWidth(), (int) source.getHeight());
+		for (int x = 0; x < source.getWidth(); ++x) {
+			for (int y = 0; y < source.getHeight(); ++y) {
+				Color oldColor = source.getPixelReader().getColor(x, y);
+				for (Map.Entry<Color, Color> entry : exchanges.entrySet()) {
+					if (oldColor.equals(entry.getKey())) {
+						newImage.getPixelWriter().setColor(x, y, entry.getValue());
+					}
+				}
+			}
+		}
+		return newImage;
+	}
+
+	protected final Image spritesheet;
+	protected final int rasterSize;
+	protected List<Direction> directionOrder = Arrays.asList(RIGHT, LEFT, UP, DOWN);
+
+	public Rendering2D_Common(String spritesheetPath, int rasterSize) {
+		spritesheet = new Image(resource(spritesheetPath));
+		this.rasterSize = rasterSize;
+	}
+
+	public Image getSpritesheet() {
+		return spritesheet;
+	}
+
+	public int getRasterSize() {
+		return rasterSize;
+	}
+
+	/**
+	 * @param dir direction
+	 * @return index used for this direction in the spritesheet
+	 */
+	public int dirIndex(Direction dir) {
+		return directionOrder.indexOf(dir);
+	}
+
+	public Image createSubImage(Rectangle2D region) {
+		return createSubImage((int) region.getMinX(), (int) region.getMinY(), (int) region.getWidth(),
+				(int) region.getHeight());
+	}
+
+	public Image createSubImage(int x, int y, int width, int height) {
+		WritableImage subImage = new WritableImage(width, height);
+		subImage.getPixelWriter().setPixels(0, 0, width, height, spritesheet.getPixelReader(), x, y);
+		return subImage;
+	}
+
+	/**
+	 * @param col grid column (x)
+	 * @param row grid row (y)
+	 * @return region at given coordinates
+	 */
+	public Rectangle2D region(int col, int row) {
+		return region(col, row, 1, 1);
+	}
+
+	/**
+	 * @param col     grid column (x)
+	 * @param row     grid row (y)
+	 * @param numCols number of grid columns
+	 * @param numRows number of grid rows
+	 * @return region at given grid coordinates
+	 */
+	public Rectangle2D region(int col, int row, int numCols, int numRows) {
+		return region(0, 0, col, row, numCols, numRows);
+	}
+
+	/**
+	 * @param originX origin x-coordinate
+	 * @param originY origin y-coordinate
+	 * @param col     grid column (x)
+	 * @param row     grid row (y)
+	 * @param numCols number of grid columns
+	 * @param numRows number of grid rows
+	 * @return region at given grid coordinates relative to given origin
+	 */
+	public Rectangle2D region(int originX, int originY, int col, int row, int numCols, int numRows) {
+		return new Rectangle2D(originX + col * rasterSize, originY + row * rasterSize, numCols * rasterSize,
+				numRows * rasterSize);
 	}
 
 	/**
@@ -79,7 +178,7 @@ public abstract class Rendering2D_Common extends Spritesheet {
 	 * @param y      render location y
 	 */
 	public void renderSprite(GraphicsContext g, Rectangle2D region, double x, double y) {
-		g.drawImage(getImage(), region.getMinX(), region.getMinY(), region.getWidth(), region.getHeight(), x, y,
+		g.drawImage(spritesheet, region.getMinX(), region.getMinY(), region.getWidth(), region.getHeight(), x, y,
 				region.getWidth(), region.getHeight());
 	}
 
