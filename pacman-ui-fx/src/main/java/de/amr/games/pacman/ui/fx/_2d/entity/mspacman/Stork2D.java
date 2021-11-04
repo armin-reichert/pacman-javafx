8983069cf1e1874601babad04127f7e795349765
Armin Reichert <armin.reichert@web.de>
2021-11-04 11:12:52 +0100

    adapted

--- pacman-ui-fx/src/main/java/de/amr/games/pacman/ui/fx/_2d/entity/mspacman/Stork2D.java
+++ pacman-ui-fx/src/main/java/de/amr/games/pacman/ui/fx/_2d/entity/mspacman/Stork2D.java
@@ -24,25 +24,24 @@ SOFTWARE.
 package de.amr.games.pacman.ui.fx._2d.entity.mspacman;
 
 import de.amr.games.pacman.lib.TimedSequence;
-import de.amr.games.pacman.model.mspacman.entities.Stork;
+import de.amr.games.pacman.model.common.GameEntity;
 import de.amr.games.pacman.ui.fx._2d.entity.common.Renderable2D;
 import de.amr.games.pacman.ui.fx._2d.rendering.mspacman.Rendering2D_MsPacMan;
 import javafx.geometry.Rectangle2D;
 import javafx.scene.canvas.GraphicsContext;
 
 /**
- * 2D representation of the stork flying through intermission scene 3 of Ms.
- * Pac-Man.
+ * 2D representation of the stork flying through intermission scene 3 of Ms. Pac-Man.
  * 
  * @author Armin Reichert
  */
 public class Stork2D implements Renderable2D {
 
-	private final Stork stork;
+	private final GameEntity stork;
 	private final Rendering2D_MsPacMan rendering;
 	public final TimedSequence<Rectangle2D> animation;
 
-	public Stork2D(Stork stork, Rendering2D_MsPacMan rendering) {
+	public Stork2D(GameEntity stork, Rendering2D_MsPacMan rendering) {
 		this.stork = stork;
 		this.rendering = rendering;
 		animation = rendering.createStorkFlyingAnimation();
