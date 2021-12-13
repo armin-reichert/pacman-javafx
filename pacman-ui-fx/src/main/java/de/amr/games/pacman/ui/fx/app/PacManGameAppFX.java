806f4b09ac6dcbfdc19c12a8cc003a831edacd61
Armin Reichert <armin.reichert@web.de>
2021-12-13 18:38:03 +0100

    license

--- pacman-ui-fx/src/main/java/de/amr/games/pacman/ui/fx/app/PacManGameAppFX.java
+++ pacman-ui-fx/src/main/java/de/amr/games/pacman/ui/fx/app/PacManGameAppFX.java
@@ -21,7 +21,6 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
  */
-
 package de.amr.games.pacman.ui.fx.app;
 
 import static de.amr.games.pacman.lib.Logging.log;
