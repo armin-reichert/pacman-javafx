b9b34571586aaa5ea44a18cae6aa03c32d8fc936
Armin Reichert <armin.reichert@web.de>
2021-12-27 13:30:14 +0100

    Add original copyright message

--- pacman-ui-fx/src/main/java/de/amr/games/pacman/ui/fx/_2d/scene/mspacman/MsPacMan_IntroScene.java
+++ pacman-ui-fx/src/main/java/de/amr/games/pacman/ui/fx/_2d/scene/mspacman/MsPacMan_IntroScene.java
@@ -39,6 +39,7 @@ import de.amr.games.pacman.ui.fx._2d.entity.common.Ghost2D;
 import de.amr.games.pacman.ui.fx._2d.entity.common.Player2D;
 import de.amr.games.pacman.ui.fx._2d.scene.common.AbstractGameScene2D;
 import de.amr.games.pacman.ui.fx.scene.Scenes;
+import javafx.scene.image.Image;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 
@@ -49,6 +50,7 @@ import javafx.scene.text.Font;
  */
 public class MsPacMan_IntroScene extends AbstractGameScene2D {
 
+	private Image midwayLogo = new Image(getClass().getResourceAsStream("/mspacman/graphics/midway.png"));
 	private IntroController sceneController;
 	private GameScore2D score2D;
 	private GameScore2D hiscore2D;
@@ -111,17 +113,19 @@ public class MsPacMan_IntroScene extends AbstractGameScene2D {
 		gc.setFill(Color.ORANGE);
 		gc.fillText("\"MS PAC-MAN\"", t(sceneController.tileTitle.x), t(sceneController.tileTitle.y));
 		drawAnimatedBoard(32, 16);
-		if (sceneController.currentStateID == IntroState.PRESENTING_GHOSTS) {
+		if (sceneController.currentStateID == IntroState.BEGIN) {
+			drawCopyright();
+		} else if (sceneController.currentStateID == IntroState.PRESENTING_GHOSTS) {
 			drawPresentingGhost(sceneController.ghosts[sceneController.currentGhostIndex]);
 		} else if (sceneController.currentStateID == IntroState.PRESENTING_MSPACMAN) {
 			drawStarringMsPacMan();
 		} else if (sceneController.currentStateID == IntroState.WAITING_FOR_GAME) {
 			drawStarringMsPacMan();
-			drawPointsAnimation(26);
-			drawPressKeyToStart(32);
+			drawPressKeyToStart(26);
 		}
 		ghosts2D.forEach(ghost2D -> ghost2D.render(gc));
 		msPacMan2D.render(gc);
+		drawCopyright();
 	}
 
 	private void drawPresentingGhost(Ghost ghost) {
@@ -174,19 +178,14 @@ public class MsPacMan_IntroScene extends AbstractGameScene2D {
 		}
 	}
 
-	private void drawPointsAnimation(int tileY) {
-		int x = t(10), y = t(tileY);
-		if (sceneController.blinking.frame()) {
-			gc.setFill(Color.PINK);
-			gc.fillOval(x, y + t(1) - 2, 10, 10);
-			gc.fillRect(x + 6, y - t(1) + 2, 2, 2);
-		}
-		gc.setFill(Color.WHITE);
+	private void drawCopyright() {
+		double scale = 36.0 / 391;
+		gc.drawImage(midwayLogo, t(6), t(28) + 3, scale * 400, scale * 391);
+		gc.setFill(Color.RED);
+		gc.setFont(Font.font("Dialog", 10.0));
+		gc.fillText("\u00a9", t(11), t(30));
 		gc.setFont(rendering.getScoreFont());
-		gc.fillText("10", x + t(2), y);
-		gc.fillText("50", x + t(2), y + t(2));
-		gc.setFont(Font.font(rendering.getScoreFont().getName(), 6));
-		gc.fillText("PTS", x + t(5), y);
-		gc.fillText("PTS", x + t(5), y + t(2));
+		gc.fillText("MIDWAY MFG CO", t(13), t(30));
+		gc.fillText("1980/1981", t(14), t(32));
 	}
 }
