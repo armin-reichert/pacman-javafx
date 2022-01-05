6d29d12bbb5a856d71712c9ae20f2e99e3a2b930
Armin Reichert <armin.reichert@web.de>
2022-01-05 10:45:13 +0100

    refac

--- pacman-ui-fx/src/main/java/de/amr/games/pacman/ui/fx/_3d/entity/Player3D.java
+++ pacman-ui-fx/src/main/java/de/amr/games/pacman/ui/fx/_3d/entity/Player3D.java
@@ -48,7 +48,7 @@ import javafx.util.Duration;
  */
 public class Player3D extends Creature3D {
 
-	public class ImpaleAnimation extends Transition {
+	private class ImpaleAnimation extends Transition {
 
 		private PhongMaterial material = new PhongMaterial(Color.YELLOW);
 
@@ -64,22 +64,44 @@ public class Player3D extends Creature3D {
 					lerp(from.getBlue(), to.getBlue(), t));
 			material.setDiffuseColor(color);
 		}
-	};
+	}
+
+	private class TurningAnimation {
+
+		private RotateTransition rotate = new RotateTransition(Duration.seconds(0.25), Player3D.this);
+		private Direction targetDir;
+
+		public TurningAnimation() {
+			reset();
+		}
+
+		public void reset() {
+			rotate.setAxis(Rotate.Z_AXIS);
+			rotate.setFromAngle(rotationAngle(player.dir()));
+			targetDir = player.dir();
+		}
+
+		public void update() {
+			if (targetDir != player.dir()) {
+				rotate.stop();
+				int[] angles = rotationAngles(player.dir(), targetDir);
+				rotate.setFromAngle(angles[1]);
+				rotate.setToAngle(angles[0]);
+				rotate.play();
+				targetDir = player.dir();
+			}
+		}
+	}
 
 	public final Pac player;
-
-	private final RotateTransition turningAnimation;
-	private final PointLight light;
 	private final Group model3D;
-	private Direction targetDir;
+	private final TurningAnimation turningAnimation;
 
 	public Player3D(Pac player, Group model3D) {
 		this.player = player;
 		this.model3D = model3D;
-		targetDir = player.dir();
-		turningAnimation = new RotateTransition(Duration.seconds(0.25), this);
-		turningAnimation.setAxis(Rotate.Z_AXIS);
-		light = new PointLight(Color.WHITE);
+		turningAnimation = new TurningAnimation();
+		PointLight light = new PointLight(Color.WHITE);
 		light.setTranslateZ(-4);
 		getChildren().addAll(model3D, light);
 		reset();
@@ -87,35 +109,26 @@ public class Player3D extends Creature3D {
 
 	public void reset() {
 		head().setMaterial(new PhongMaterial(Color.YELLOW));
-		setVisible(player.visible && !outsideMaze(player));
 		setScaleX(1.05);
 		setScaleY(1.05);
 		setScaleZ(1.05);
+		setVisible(player.visible && !outsideMaze(player));
 		setTranslateX(player.position.x + HTS);
 		setTranslateY(player.position.y + HTS);
 		setTranslateZ(-HTS);
-		setRotationAxis(Rotate.Z_AXIS);
-		setRotate(rotationAngle(player.dir()));
+		turningAnimation.reset();
 	}
 
 	public void update() {
 		setVisible(player.visible && !outsideMaze(player));
 		setTranslateX(player.position.x + HTS);
 		setTranslateY(player.position.y + HTS);
-		if (player.dead) {
-			return;
-		}
-		if (targetDir != player.dir()) {
-			turningAnimation.stop();
-			int[] angles = rotationAngles(player.dir(), targetDir);
-			turningAnimation.setFromAngle(angles[1]);
-			turningAnimation.setToAngle(angles[0]);
-			turningAnimation.play();
-			targetDir = player.dir();
+		if (!player.dead) {
+			turningAnimation.update();
 		}
 	}
 
-	public ImpaleAnimation createImpaleAnimation(Duration duration) {
+	public Transition createImpaleAnimation(Duration duration) {
 		ImpaleAnimation impale = new ImpaleAnimation(duration);
 		head().setMaterial(impale.material);
 		return impale;
