1ac7e30c23010e168cd1a9cfaef2ed2f517807a5
Armin Reichert <armin.reichert@web.de>
2022-02-01 12:23:13 +0100

    log

--- pacman-ui-fx/src/main/java/de/amr/games/pacman/ui/fx/_3d/scene/PlayScene3D.java
+++ pacman-ui-fx/src/main/java/de/amr/games/pacman/ui/fx/_3d/scene/PlayScene3D.java
@@ -122,6 +122,8 @@ public class PlayScene3D extends DefaultGameEventHandler implements GameScene {
 			PerspectiveCamera cam = new PerspectiveCamera(true);
 			fxSubScene.setCamera(cam);
 			cams.values().forEach(cc -> cc.attachTo(cam));
+			log("Subscene for game scene '%s' created, width=%.0f, height=%.0f", name(), fxSubScene.getWidth(),
+					fxSubScene.getHeight());
 		}
 	}
 
