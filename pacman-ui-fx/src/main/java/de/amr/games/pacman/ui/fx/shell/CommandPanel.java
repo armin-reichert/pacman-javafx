2682c5573acf2c9a5d56c77c4abfb06284cc8b5a
Armin Reichert <armin.reichert@web.de>
2022-03-24 08:05:59 +0100

    command panel

--- pacman-ui-fx/src/main/java/de/amr/games/pacman/ui/fx/shell/CommandPanel.java
+++ pacman-ui-fx/src/main/java/de/amr/games/pacman/ui/fx/shell/CommandPanel.java
@@ -23,10 +23,13 @@ SOFTWARE.
  */
 package de.amr.games.pacman.ui.fx.shell;
 
+import de.amr.games.pacman.model.common.GameVariant;
 import de.amr.games.pacman.ui.fx.app.Env;
 import de.amr.games.pacman.ui.fx.util.U;
 import javafx.beans.value.ObservableValue;
 import javafx.scene.control.CheckBox;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Control;
 import javafx.scene.control.Slider;
 import javafx.scene.layout.GridPane;
 import javafx.scene.paint.Color;
@@ -41,6 +44,7 @@ public class CommandPanel extends GridPane {
 	private final Font textFont = Font.font("Monospace", 14);
 	private int row;
 
+	private final Slider sliderTargetFrameRate;
 	private final CheckBox cbAutopilot;
 	private final CheckBox cbImmunity;
 	private final CheckBox cbUse3DScene;
@@ -49,6 +53,8 @@ public class CommandPanel extends GridPane {
 	private final CheckBox cbWireframeMode;
 	private final CheckBox cbShowTiles;
 
+	private final ComboBox<String> comboGameVariant;
+
 	public CommandPanel(GameUI ui, int width) {
 		this.ui = ui;
 		setBackground(U.colorBackground(new Color(0.3, 0.3, 0.3, 0.6)));
@@ -56,9 +62,14 @@ public class CommandPanel extends GridPane {
 		setMinWidth(width);
 		setWidth(width);
 		setMaxWidth(width);
-		addSlider("Framerate", 10, 200, 60).valueProperty().addListener(($1, oldVal, newVal) -> {
+
+		sliderTargetFrameRate = addSlider("Framerate", 10, 200, 60);
+		sliderTargetFrameRate.valueProperty().addListener(($1, oldVal, newVal) -> {
 			ui.setTargetFrameRate(newVal.intValue());
 		});
+		comboGameVariant = addComboBox("GameVariant", GameVariant.MS_PACMAN, GameVariant.PACMAN);
+		comboGameVariant
+				.setOnAction(e -> ui.gameController.selectGameVariant(GameVariant.valueOf(comboGameVariant.getValue())));
 		cbAutopilot = addCheckBox("Autopilot", ui::toggleAutopilot);
 		cbImmunity = addCheckBox("Player immune", ui::toggleImmunity);
 		cbUse3DScene = addCheckBox("Use 3D scene", ui::toggle3D);
@@ -73,6 +84,7 @@ public class CommandPanel extends GridPane {
 	private void onVisibilityChange(ObservableValue<? extends Boolean> $1, Boolean wasVisible, Boolean becomesVisible) {
 		if (becomesVisible) {
 			Env.$paused.set(true);
+			sliderTargetFrameRate.setValue(Env.gameLoop.getTargetFrameRate());
 			cbAutopilot.setSelected(ui.gameController.autoControlled);
 			cbImmunity.setSelected(ui.gameController.game.player.immune);
 			cbUse3DScene.setSelected(Env.$3D.get());
@@ -80,36 +92,46 @@ public class CommandPanel extends GridPane {
 			cbAxesVisible.setSelected(Env.$axesVisible.get());
 			cbWireframeMode.setSelected(Env.$drawMode3D.get() == DrawMode.LINE);
 			cbShowTiles.setSelected(Env.$tilesVisible.get());
+			comboGameVariant.setValue(ui.gameController.gameVariant.toString());
 		} else {
 			Env.$paused.set(false);
 		}
 	}
 
-	private CheckBox addCheckBox(String text, Runnable callback) {
+	private void addRow(String labelText, Control control) {
+		Text label = new Text(labelText);
+		label.setFill(textColor);
+		label.setFont(textFont);
+		add(label, 0, row);
+		add(control, 1, row++);
+	}
+
+	private CheckBox addCheckBox(String labelText, Runnable callback) {
 		CheckBox cb = new CheckBox();
 		cb.setTextFill(textColor);
 		cb.setFont(textFont);
 		cb.setOnAction(e -> callback.run());
-		Text label = new Text(text);
-		label.setFill(textColor);
-		label.setFont(textFont);
-		add(label, 0, row);
-		add(cb, 1, row++);
+		addRow(labelText, cb);
 		return cb;
 	}
 
-	private Slider addSlider(String text, int min, int max, int value) {
+	private Slider addSlider(String labelText, int min, int max, int value) {
 		Slider slider = new Slider(min, max, value);
 		slider.setShowTickLabels(true);
 		slider.setShowTickMarks(true);
 		slider.setMinorTickCount(5);
 		slider.setMajorTickUnit(50);
 		slider.setMinWidth(200);
-		Text label = new Text(text);
-		label.setFill(textColor);
-		label.setFont(textFont);
-		add(label, 0, row);
-		add(slider, 1, row++);
+		addRow(labelText, slider);
 		return slider;
+	}
+
+	private ComboBox<String> addComboBox(String labelText, Object... items) {
+		ComboBox<String> combo = new ComboBox<>();
+		for (Object item : items) {
+			combo.getItems().add(String.valueOf(item));
+		}
+		addRow(labelText, combo);
+		return combo;
 	}
 }
