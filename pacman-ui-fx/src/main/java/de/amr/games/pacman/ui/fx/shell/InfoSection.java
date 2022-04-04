7818878675d8b8326777a104a664192c9e3383ce
Armin Reichert <armin.reichert@web.de>
2022-04-04 06:51:39 +0200

    use InfoSection

--- pacman-ui-fx/src/main/java/de/amr/games/pacman/ui/fx/shell/InfoSection.java
+++ pacman-ui-fx/src/main/java/de/amr/games/pacman/ui/fx/shell/InfoSection.java
@@ -28,12 +28,26 @@ import java.util.List;
 import java.util.function.Supplier;
 
 import de.amr.games.pacman.ui.fx.util.U;
+import javafx.collections.FXCollections;
+import javafx.geometry.HPos;
 import javafx.geometry.Insets;
+import javafx.geometry.VPos;
+import javafx.scene.Node;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
+import javafx.scene.control.Slider;
+import javafx.scene.control.Spinner;
 import javafx.scene.control.TitledPane;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
+import javafx.scene.text.TextAlignment;
 
 public class InfoSection extends TitledPane {
 
@@ -42,6 +56,8 @@ public class InfoSection extends TitledPane {
 	public Color textColor = Color.WHITE;
 	public Font textFont = Font.font("Monospace", 12);
 	public Font labelFont = Font.font("Sans", 12);
+	public Color headerColor = Color.YELLOW;
+	public Font headerFont = Font.font("Arial", FontWeight.BOLD, 16);
 	private int row;
 
 	public InfoSection(String title) {
@@ -79,5 +95,72 @@ public class InfoSection extends TitledPane {
 
 	public InfoText addInfo(String labelText, String value) {
 		return addInfo(labelText, () -> value);
+	}
+
+	public void addRow(String labelText, Node child) {
+		Label label = new Label(labelText);
+		label.setTextFill(textColor);
+		label.setFont(textFont);
+		content.add(label, 0, row);
+		content.add(child, 1, row);
+		++row;
+	}
+
+	public void addSectionHeader(String title) {
+		Text header = new Text(title);
+		header.setFill(headerColor);
+		header.setFont(headerFont);
+		header.setTextAlignment(TextAlignment.CENTER);
+		GridPane.setConstraints(header, 0, row, 2, 1, HPos.CENTER, VPos.CENTER, Priority.NEVER, Priority.NEVER,
+				new Insets(12, 0, 8, 0));
+		content.getChildren().add(header);
+		++row;
+	}
+
+	public Button addButton(String labelText, String buttonText, Runnable action) {
+		Button button = new Button(buttonText);
+		button.setOnAction(e -> action.run());
+		addRow(labelText, button);
+		return button;
+	}
+
+	public Button[] addButtons(String labelText, String... buttonTexts) {
+		HBox hbox = new HBox();
+		Button[] buttons = new Button[buttonTexts.length];
+		for (int i = 0; i < buttonTexts.length; ++i) {
+			buttons[i] = new Button(buttonTexts[i]);
+			hbox.getChildren().add(buttons[i]);
+		}
+		addRow(labelText, hbox);
+		return buttons;
+	}
+
+	public CheckBox addCheckBox(String labelText, Runnable callback) {
+		CheckBox cb = new CheckBox();
+		cb.setTextFill(textColor);
+		cb.setFont(textFont);
+		cb.setOnAction(e -> callback.run());
+		addRow(labelText, cb);
+		return cb;
+	}
+
+	@SuppressWarnings("unchecked")
+	public <T> ComboBox<T> addComboBox(String labelText, T... items) {
+		var combo = new ComboBox<T>(FXCollections.observableArrayList(items));
+		addRow(labelText, combo);
+		return combo;
+	}
+
+	public Slider addSlider(String labelText, double min, double max, double value) {
+		Slider slider = new Slider(min, max, value);
+		slider.setMinWidth(200);
+		addRow(labelText, slider);
+		return slider;
+	}
+
+	public Spinner<Integer> addSpinner(String labelText, int min, int max, int initialValue) {
+		Spinner<Integer> spinner = new Spinner<>(min, max, initialValue);
+		addRow(labelText, spinner);
+		return spinner;
 	}
 }
--- pacman-ui-fx/src/main/java/de/amr/games/pacman/ui/fx/shell/SettingsPanel.java
+++ pacman-ui-fx/src/main/java/de/amr/games/pacman/ui/fx/shell/SettingsPanel.java
@@ -28,50 +28,32 @@ import de.amr.games.pacman.model.common.GameVariant;
 import de.amr.games.pacman.ui.fx._3d.scene.Perspective;
 import de.amr.games.pacman.ui.fx.app.Env;
 import de.amr.games.pacman.ui.fx.app.GameLoop;
-import de.amr.games.pacman.ui.fx.util.U;
-import javafx.collections.FXCollections;
-import javafx.geometry.HPos;
-import javafx.geometry.Insets;
-import javafx.geometry.VPos;
-import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.ComboBox;
-import javafx.scene.control.Label;
 import javafx.scene.control.Slider;
 import javafx.scene.control.Spinner;
-import javafx.scene.layout.GridPane;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.Priority;
-import javafx.scene.paint.Color;
+import javafx.scene.layout.VBox;
 import javafx.scene.shape.DrawMode;
-import javafx.scene.text.Font;
-import javafx.scene.text.FontWeight;
-import javafx.scene.text.Text;
-import javafx.scene.text.TextAlignment;
 
 /**
  * Panel for configuring the game and the UI.
  * 
  * @author Armin Reichert
  */
-public class SettingsPanel extends GridPane {
+public class SettingsPanel extends VBox {
 
 	private final GameUI ui;
-	private final Color textColor = Color.WHITE;
-	private final Font textFont = Font.font("Sans", 12);
-	private final Color headerColor = Color.YELLOW;
-	private final Font headerFont = Font.font("Arial", FontWeight.BOLD, 16);
-	private int row;
 
-	private class Commands {
+	private class SectionCommands extends InfoSection {
 		private ComboBox<GameVariant> comboGameVariant;
 		private Button[] btnsSimulation;
 		private Button[] btnsGameControl;
 		private Spinner<Integer> spinnerLevel;
 		private Button btnIntermissionTest;
 
-		public void add() {
+		public SectionCommands() {
+			super("Commands");
 			btnsSimulation = addButtons("Simulation", "Pause", "Step");
 			btnsSimulation[0].setOnAction(e -> ui.togglePaused());
 			btnsSimulation[1].setOnAction(e -> GameLoop.get().runSingleStep(true));
@@ -86,8 +68,7 @@ public class SettingsPanel extends GridPane {
 			btnsGameControl[1].setOnAction(e -> ui.quitCurrentGameScene());
 			btnsGameControl[2].setOnAction(e -> ui.enterNextLevel());
 			spinnerLevel = addSpinner("Level", 1, 100, ui.gameController.game.levelNumber);
-			spinnerLevel.valueProperty()
-					.addListener(($value, oldValue, newValue) -> ui.enterLevel(newValue.intValue()));
+			spinnerLevel.valueProperty().addListener(($value, oldValue, newValue) -> ui.enterLevel(newValue.intValue()));
 			btnIntermissionTest = addButton("Intermission scenes", "Start", ui::startIntermissionScenesTest);
 		}
 
@@ -101,22 +82,21 @@ public class SettingsPanel extends GridPane {
 			btnsGameControl[1].setDisable(ui.gameController.state == GameState.INTRO);
 			btnsGameControl[2].setDisable(ui.gameController.state != GameState.HUNTING);
 			spinnerLevel.getValueFactory().setValue(ui.gameController.game.levelNumber);
-			spinnerLevel.setDisable(
-					ui.gameController.state != GameState.READY && ui.gameController.state != GameState.HUNTING
-							&& ui.gameController.state != GameState.LEVEL_STARTING);
-			btnIntermissionTest.setDisable(ui.gameController.state == GameState.INTERMISSION_TEST
-					|| ui.gameController.state != GameState.INTRO);
+			spinnerLevel.setDisable(ui.gameController.state != GameState.READY && ui.gameController.state != GameState.HUNTING
+					&& ui.gameController.state != GameState.LEVEL_STARTING);
+			btnIntermissionTest.setDisable(
+					ui.gameController.state == GameState.INTERMISSION_TEST || ui.gameController.state != GameState.INTRO);
 		}
 	}
 
-	private class SettingsGeneral {
+	private class SectionGeneral extends InfoSection {
 		private Slider sliderTargetFrameRate;
 		private CheckBox cbAutopilot;
 		private CheckBox cbImmunity;
 		private CheckBox cbUsePlayScene3D;
 
-		public void add() {
-			addSectionHeader("General Settings");
+		public SectionGeneral() {
+			super("General Settings");
 			sliderTargetFrameRate = addSlider("Framerate", 0, 120, 60);
 			sliderTargetFrameRate.setSnapToTicks(true);
 			sliderTargetFrameRate.setShowTickLabels(true);
@@ -139,7 +119,7 @@ public class SettingsPanel extends GridPane {
 		}
 	}
 
-	private class Settings3D {
+	private class Section3D extends InfoSection {
 		private ComboBox<Perspective> comboPerspective;
 		private ComboBox<Integer> comboResolution;
 		private Slider sliderWallHeight;
@@ -147,15 +127,14 @@ public class SettingsPanel extends GridPane {
 		private CheckBox cbAxesVisible;
 		private CheckBox cbWireframeMode;
 
-		public void add() {
-			addSectionHeader("3D Settings");
+		public Section3D() {
+			super("3D Settings");
 			comboPerspective = addComboBox("Perspective", Perspective.values());
 			comboPerspective.setOnAction(e -> Env.$perspective.set(comboPerspective.getValue()));
 			comboResolution = addComboBox("Maze resolution", 1, 2, 4, 8);
 			comboResolution.setOnAction(e -> Env.$mazeResolution.set(comboResolution.getValue()));
 			sliderWallHeight = addSlider("Maze wall height", 0, 10, 8);
-			sliderWallHeight.valueProperty()
-					.addListener(($value, _old, _new) -> Env.$mazeWallHeight.set(_new.doubleValue()));
+			sliderWallHeight.valueProperty().addListener(($value, _old, _new) -> Env.$mazeWallHeight.set(_new.doubleValue()));
 			cbUseFloorTexture = addCheckBox("Maze floor texture", ui::toggleUseMazeFloorTexture);
 			cbAxesVisible = addCheckBox("Show axes", ui::toggleAxesVisible);
 			cbWireframeMode = addCheckBox("Wireframe mode", ui::toggleDrawMode);
@@ -177,11 +156,11 @@ public class SettingsPanel extends GridPane {
 		}
 	}
 
-	private class Settings2D {
+	private class Section2D extends InfoSection {
 		private CheckBox cbTilesVisible;
 
-		public void add() {
-			addSectionHeader("2D Settings");
+		public Section2D() {
+			super("2D Settings");
 			cbTilesVisible = addCheckBox("Show tiles", ui::toggleTilesVisible);
 		}
 
@@ -191,97 +170,25 @@ public class SettingsPanel extends GridPane {
 		}
 	}
 
-	private final Commands commands = new Commands();
-	private final SettingsGeneral settingsGeneral = new SettingsGeneral();
-	private final Settings3D settings3D = new Settings3D();
-	private final Settings2D settings2D = new Settings2D();
+	private final SectionCommands sectionCcommands;
+	private final SectionGeneral sectionGeneral;
+	private final Section3D section3D;
+	private final Section2D section2D;
 
 	public SettingsPanel(GameUI ui) {
 		this.ui = ui;
-
-		setBackground(U.colorBackground(new Color(0.3, 0.3, 0.3, 0.6)));
-		setPadding(new Insets(5));
-		setHgap(20);
-		setVgap(4);
+		sectionCcommands = new SectionCommands();
+		sectionGeneral = new SectionGeneral();
+		section3D = new Section3D();
+		section2D = new Section2D();
 		setVisible(false);
-
-		commands.add();
-		settingsGeneral.add();
-		settings3D.add();
-		settings2D.add();
+		getChildren().addAll(sectionCcommands, sectionGeneral, section3D, section2D);
 	}
 
 	public void update() {
-		commands.update();
-		settingsGeneral.update();
-		settings3D.update();
-		settings2D.update();
-	}
-
-	private void addRow(String labelText, Node child) {
-		Label label = new Label(labelText);
-		label.setTextFill(textColor);
-		label.setFont(textFont);
-		add(label, 0, row);
-		add(child, 1, row);
-		++row;
-	}
-
-	private void addSectionHeader(String title) {
-		Text header = new Text(title);
-		header.setFill(headerColor);
-		header.setFont(headerFont);
-		header.setTextAlignment(TextAlignment.CENTER);
-		setConstraints(header, 0, row, 2, 1, HPos.CENTER, VPos.CENTER, Priority.NEVER, Priority.NEVER,
-				new Insets(12, 0, 8, 0));
-		getChildren().add(header);
-		++row;
-	}
-
-	private Button addButton(String labelText, String buttonText, Runnable action) {
-		Button button = new Button(buttonText);
-		button.setOnAction(e -> action.run());
-		addRow(labelText, button);
-		return button;
-	}
-
-	private Button[] addButtons(String labelText, String... buttonTexts) {
-		HBox hbox = new HBox();
-		Button[] buttons = new Button[buttonTexts.length];
-		for (int i = 0; i < buttonTexts.length; ++i) {
-			buttons[i] = new Button(buttonTexts[i]);
-			hbox.getChildren().add(buttons[i]);
-		}
-		addRow(labelText, hbox);
-		return buttons;
-	}
-
-	private CheckBox addCheckBox(String labelText, Runnable callback) {
-		CheckBox cb = new CheckBox();
-		cb.setTextFill(textColor);
-		cb.setFont(textFont);
-		cb.setOnAction(e -> callback.run());
-		addRow(labelText, cb);
-		return cb;
-	}
-
-	@SuppressWarnings("unchecked")
-	private <T> ComboBox<T> addComboBox(String labelText, T... items) {
-		var combo = new ComboBox<T>(FXCollections.observableArrayList(items));
-		addRow(labelText, combo);
-		return combo;
-	}
-
-	private Slider addSlider(String labelText, double min, double max, double value) {
-		Slider slider = new Slider(min, max, value);
-		slider.setMinWidth(200);
-		addRow(labelText, slider);
-		return slider;
-	}
-
-	private Spinner<Integer> addSpinner(String labelText, int min, int max, int initialValue) {
-		Spinner<Integer> spinner = new Spinner<>(min, max, initialValue);
-		addRow(labelText, spinner);
-		return spinner;
+		sectionCcommands.update();
+		sectionGeneral.update();
+		section3D.update();
+		section2D.update();
 	}
 }
