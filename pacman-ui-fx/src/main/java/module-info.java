2c97e351fb699f7af58893f1d687689b0b59d245
Armin Reichert <armin.reichert@web.de>
2022-05-23 05:33:52 +0200

    license

--- pacman-ui-fx/src/main/java/module-info.java
+++ pacman-ui-fx/src/main/java/module-info.java
@@ -1,7 +1,7 @@
 /*
  * MIT License
  * 
- * Copyright (c) 2021 Armin Reichert
+ * Copyright (c) 2021-22 Armin Reichert
  * 
  * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
  * documentation files (the "Software"), to deal in the Software without restriction, including without limitation the
