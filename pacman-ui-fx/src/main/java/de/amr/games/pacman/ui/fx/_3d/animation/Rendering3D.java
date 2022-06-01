3bcff7b6982b71e3871a7933e70091bbe241a7ca
Armin Reichert <armin.reichert@web.de>
2022-06-01 06:26:05 +0200

    refac

--- pacman-ui-fx/src/main/java/de/amr/games/pacman/ui/fx/_3d/animation/Rendering3D.java
+++ pacman-ui-fx/src/main/java/de/amr/games/pacman/ui/fx/_3d/animation/Rendering3D.java
@@ -22,7 +22,7 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 */
 
-package de.amr.games.pacman.ui.fx._2d.rendering.common;
+package de.amr.games.pacman.ui.fx._3d.animation;
 
 import de.amr.games.pacman.model.common.actors.Ghost;
 import javafx.scene.paint.Color;
