3819818e2442c7922cbba3ba8bfe88a87adccbff
Armin Reichert <armin.reichert@web.de>
2022-06-20 06:49:38 +0200

    refac

--- pacman-ui-fx/src/main/java/de/amr/games/pacman/ui/fx/_3d/entity/Maze3D.java
+++ pacman-ui-fx/src/main/java/de/amr/games/pacman/ui/fx/_3d/entity/Maze3D.java
@@ -24,7 +24,6 @@ SOFTWARE.
 
 package de.amr.games.pacman.ui.fx._3d.entity;
 
-import static de.amr.games.pacman.lib.Logging.log;
 import static de.amr.games.pacman.model.common.actors.GhostState.LEAVING_HOUSE;
 import static de.amr.games.pacman.model.common.world.World.TS;
 
@@ -38,7 +37,6 @@ import de.amr.games.pacman.model.common.actors.GhostState;
 import de.amr.games.pacman.model.common.world.FloorPlan;
 import de.amr.games.pacman.model.common.world.World;
 import de.amr.games.pacman.ui.fx._3d.animation.RaiseAndLowerWallAnimation;
-import de.amr.games.pacman.ui.fx.app.Env;
 import de.amr.games.pacman.ui.fx.util.U;
 import javafx.animation.Animation;
 import javafx.animation.PauseTransition;
@@ -53,7 +51,6 @@ import javafx.scene.Node;
 import javafx.scene.image.Image;
 import javafx.scene.paint.Color;
 import javafx.scene.paint.PhongMaterial;
-import javafx.scene.shape.Box;
 import javafx.util.Duration;
 
 /**
@@ -64,6 +61,7 @@ import javafx.util.Duration;
 public class Maze3D {
 
 	public static class WallProperties {
+		DoubleProperty wallHeight;
 		double brickSize;
 		PhongMaterial baseMaterial;
 		PhongMaterial topMaterial;
@@ -97,12 +95,18 @@ public class Maze3D {
 				floor.showSolid(floorSolidColor);
 			}
 		});
-		resolution.addListener((obs, oldVal, newVal) -> erect(wallSideColor, wallTopColor, doorColor));
 		root.getChildren().addAll(floor, wallsGroup, doorsGroup, foodGroup);
-		erect(wallSideColor, wallTopColor, doorColor);
+
+		var floorPlan = new FloorPlan(resolution.get(), world);
+		new Mason(floorPlan, wallsGroup, doorsGroup).erectBuilding(world, wallHeight, wallSideColor, wallTopColor,
+				doorColor);
 
 		addFood(world, foodColor);
 
+		resolution.addListener((obs, oldVal, newVal) -> {
+			Mason m = new Mason(new FloorPlan(newVal.intValue(), world), wallsGroup, doorsGroup);
+			m.erectBuilding(world, wallHeight, wallSideColor, wallTopColor, doorColor);
+		});
 	}
 
 	public Group getRoot() {
@@ -127,11 +131,6 @@ public class Maze3D {
 
 	public Animation createMazeFlashingAnimation(int times) {
 		return times > 0 ? new RaiseAndLowerWallAnimation(times) : new PauseTransition(Duration.seconds(1));
-	}
-
-	public void erect(Color wallBaseColor, Color wallTopColor, Color doorColor) {
-		createWalls(wallBaseColor, wallTopColor);
-		createDoors(doorColor);
 	}
 
 	public void reset() {
@@ -160,127 +159,6 @@ public class Maze3D {
 
 	private boolean isGhostGettingAccess(Ghost ghost, V2d doorCenter) {
 		return ghost.position.euclideanDistance(doorCenter) <= (ghost.is(LEAVING_HOUSE) ? TS : 3 * TS);
-	}
-
-	private void createWalls(Color wallBaseColor, Color wallTopColor) {
-		WallProperties wp = new WallProperties();
-		wp.baseMaterial = new PhongMaterial(wallBaseColor);
-		wp.baseMaterial.setSpecularColor(wallBaseColor.brighter());
-		wp.topMaterial = new PhongMaterial(wallTopColor);
-		wp.brickSize = TS / resolution.get();
-
-		var floorPlan = new FloorPlan(resolution.get(), world);
-		wallsGroup.getChildren().clear();
-		addHorizontalWalls(floorPlan, wp);
-		addVerticalWalls(floorPlan, wp);
-		addCorners(floorPlan, wp);
-		log("Built 3D maze (resolution=%d, wall height=%.2f)", resolution.get(), wallHeight.get());
-	}
-
-	private void createDoors(Color doorColor) {
-		var leftDoor = new Door3D(world.ghostHouse().doorTileLeft(), true, doorColor);
-		leftDoor.doorHeight.bind(wallHeight);
-		var rightDoor = new Door3D(world.ghostHouse().doorTileRight(), false, doorColor);
-		rightDoor.doorHeight.bind(wallHeight);
-		doorsGroup.getChildren().setAll(leftDoor.getNode(), rightDoor.getNode());
-	}
-
-	private void addHorizontalWalls(FloorPlan floorPlan, WallProperties wp) {
-		for (int y = 0; y < floorPlan.sizeY(); ++y) {
-			int leftX = -1;
-			int sizeX = 0;
-			for (int x = 0; x < floorPlan.sizeX(); ++x) {
-				if (floorPlan.get(x, y) == FloorPlan.HWALL) {
-					if (leftX == -1) {
-						leftX = x;
-						sizeX = 1;
-					} else {
-						sizeX++;
-					}
-				} else {
-					if (leftX != -1) {
-						addWall(leftX, y, sizeX, 1, wp);
-						leftX = -1;
-					}
-				}
-				if (x == floorPlan.sizeX() - 1 && leftX != -1) {
-					addWall(leftX, y, sizeX, 1, wp);
-					leftX = -1;
-				}
-			}
-			if (y == floorPlan.sizeY() - 1 && leftX != -1) {
-				addWall(leftX, y, sizeX, 1, wp);
-			}
-		}
-	}
-
-	private void addVerticalWalls(FloorPlan floorPlan, WallProperties wp) {
-		for (int x = 0; x < floorPlan.sizeX(); ++x) {
-			int topY = -1;
-			int sizeY = 0;
-			for (int y = 0; y < floorPlan.sizeY(); ++y) {
-				if (floorPlan.get(x, y) == FloorPlan.VWALL) {
-					if (topY == -1) {
-						topY = y;
-						sizeY = 1;
-					} else {
-						sizeY++;
-					}
-				} else {
-					if (topY != -1) {
-						addWall(x, topY, 1, sizeY, wp);
-						topY = -1;
-					}
-				}
-				if (y == floorPlan.sizeY() - 1 && topY != -1) {
-					addWall(x, topY, 1, sizeY, wp);
-					topY = -1;
-				}
-			}
-			if (x == floorPlan.sizeX() - 1 && topY != -1) {
-				addWall(x, topY, 1, sizeY, wp);
-			}
-		}
-	}
-
-	private void addCorners(FloorPlan floorPlan, WallProperties wp) {
-		for (int x = 0; x < floorPlan.sizeX(); ++x) {
-			for (int y = 0; y < floorPlan.sizeY(); ++y) {
-				if (floorPlan.get(x, y) == FloorPlan.CORNER) {
-					addWall(x, y, 1, 1, wp);
-				}
-			}
-		}
-	}
-
-	/**
-	 * Adds a wall at given position. A wall consists of a base and a top part which can have different color and
-	 * material.
-	 * 
-	 * @param x          x-coordinate of top-left brick
-	 * @param y          y-coordinate of top-left brick
-	 * @param numBricksX number of bricks in x-direction
-	 * @param numBricksY number of bricks in y-direction
-	 * @param wp         wall properties
-	 */
-	private void addWall(int x, int y, int numBricksX, int numBricksY, WallProperties wp) {
-		Box base = new Box(numBricksX * wp.brickSize, numBricksY * wp.brickSize, wallHeight.get());
-		base.depthProperty().bind(wallHeight);
-		base.setMaterial(wp.baseMaterial);
-		base.translateZProperty().bind(wallHeight.multiply(-0.5));
-		base.drawModeProperty().bind(Env.$drawMode3D);
-
-		double topHeight = 0.5;
-		Box top = new Box(numBricksX * wp.brickSize, numBricksY * wp.brickSize, topHeight);
-		top.setMaterial(wp.topMaterial);
-		top.translateZProperty().bind(base.translateZProperty().subtract(wallHeight.add(topHeight + 0.1).multiply(0.5)));
-		top.drawModeProperty().bind(Env.$drawMode3D);
-
-		Group wall = new Group(base, top);
-		wall.setTranslateX((x + 0.5 * numBricksX) * wp.brickSize);
-		wall.setTranslateY((y + 0.5 * numBricksY) * wp.brickSize);
-
-		wallsGroup.getChildren().add(wall);
 	}
 
 	private void addFood(World world, Color foodColor) {
@@ -324,5 +202,4 @@ public class Maze3D {
 	private Stream<Energizer3D> energizers() {
 		return foodNodes().filter(Energizer3D.class::isInstance).map(Energizer3D.class::cast);
 	}
-
 }
