22179bbe08cfe8e545266af761c7e31c9ded147a
Armin Reichert <armin.reichert@web.de>
2022-06-26 18:09:52 +0200

    license

--- pacman-ui-fx/src/main/java/de/amr/games/pacman/ui/fx/_3d/entity/Maze3D.java
+++ pacman-ui-fx/src/main/java/de/amr/games/pacman/ui/fx/_3d/entity/Maze3D.java
@@ -21,7 +21,6 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 */
-
 package de.amr.games.pacman.ui.fx._3d.entity;
 
 import static de.amr.games.pacman.model.common.actors.GhostState.LEAVING_HOUSE;
