f288659851c0bee52a9c692cc35ce6ee12b0a3c0
Armin Reichert <armin.reichert@web.de>
2022-06-29 12:49:13 +0200

    improved

--- pacman-ui-fx/src/main/java/de/amr/games/pacman/ui/fx/_3d/animation/SquirtingAnimation.java
+++ pacman-ui-fx/src/main/java/de/amr/games/pacman/ui/fx/_3d/animation/SquirtingAnimation.java
@@ -76,9 +76,8 @@ public class SquirtingAnimation extends Transition {
 	protected void interpolate(double t) {
 		for (int i = 0; i < drops.length; ++i) {
 			var drop = drops[i];
-			if (drop.getTranslateZ() >= -0.5 // reached floor
-					&& world.insideMap(drop.getTranslateX(), drop.getTranslateY())) {
-				drop.setScaleZ(0.01);
+			if (drop.getTranslateZ() >= -1.0 && world.insideMap(drop.getTranslateX(), drop.getTranslateY())) {
+				drop.setScaleZ(0.1);
 				veloc[i] = Point3D.ZERO;
 			} else {
 				drop.setVisible(true);
