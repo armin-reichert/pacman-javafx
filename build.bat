06b08e219ed1fb21945582b67522a3e0c81778f4
Armin Reichert <armin.reichert@web.de>
2022-07-01 05:48:13 +0200

    Update build.bat

--- build.bat
+++ build.bat
@@ -2,7 +2,7 @@
 pushd interactivemesh
 call mvn install:install-file -Dfile=jars/jimObjModelImporterJFX.jar -DpomFile=pom.xml
 popd
-pushd pacman-ui-fx
+pushd pacman_ui_fx
 call mvn clean install
 popd
 pushd pacman-ui-fx-deploy
