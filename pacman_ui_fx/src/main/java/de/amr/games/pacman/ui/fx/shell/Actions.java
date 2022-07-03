957f184794337f87106b835aabfe786a40a1c3d0
Armin Reichert <armin.reichert@web.de>
2022-07-03 21:22:25 +0200

    improved

--- pacman_ui_fx/src/main/java/de/amr/games/pacman/ui/fx/shell/Actions.java
+++ pacman_ui_fx/src/main/java/de/amr/games/pacman/ui/fx/shell/Actions.java
@@ -182,9 +182,10 @@ public class Actions {
 	}
 
 	public static void toggleUse3DScene() {
+		Env.toggle(Env.use3D);
 		if (theUI.getSceneManager().sceneExistsInBothDimensions()) {
-			Env.toggle(Env.use3D);
 			theUI.onChangeDimension();
+		} else {
 			showFlashMessage(Texts.message(Env.use3D.get() ? "use_3D_scene" : "use_2D_scene"));
 		}
 	}
