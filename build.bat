7e8e0a903c1ebf7cbfdea30ceab09a94b8e823da
Armin Reichert <armin.reichert@web.de>
2022-07-10 09:24:46 +0200

    build script

--- build.bat
+++ build.bat
@@ -1,4 +1,7 @@
 @echo off
+pushd ..\pacman-basic\pacman-core
+call mvn clean install
+popd
 pushd interactivemesh
 call mvn install:install-file -Dfile=jars/jimObjModelImporterJFX.jar -DpomFile=pom.xml
 popd
