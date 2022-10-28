a5fad87ca392f4d54cdbda220b324debd900b864
Armin Reichert <armin.reichert@web.de>
2022-10-28 09:43:18 +0200

    refac

--- pacman_ui_fx/src/main/java/de/amr/games/pacman/ui/fx/_2d/scene/common/GuysInfo.java
+++ pacman_ui_fx/src/main/java/de/amr/games/pacman/ui/fx/_2d/scene/common/GuysInfo.java
@@ -56,24 +56,51 @@ public class GuysInfo {
 	private static final String[] MS_PACMAN_BONUS_NAMES = { "CHERRIES", "STRAWBERRY", "PEACH", "PRETZEL", "APPLE", "PEAR",
 			"BANANA" };
 
+	// 0..3: ghost info, 4: Pac info, 5: bonus info
+	private static final int NUM_INFOS = 6;
+
 	private final GameScene2D hostScene;
 	private final List<Pane> panes = new ArrayList<>();
 	private final List<Text> texts = new ArrayList<>();
 
 	public GuysInfo(GameScene2D hostScene) {
 		this.hostScene = hostScene;
-		for (int i = 0; i < 6; ++i) {
+		for (int i = 0; i < NUM_INFOS; ++i) {
 			var text = new Text();
 			text.setTextAlignment(TextAlignment.CENTER);
 			text.setFill(Color.WHITE);
 			texts.add(text);
-		}
-		for (int i = 0; i < 6; ++i) {
-			var pane = new VBox(texts.get(i));
-			pane.setBackground(Ufx.colorBackground(Color.rgb(200, 200, 255, 0.5)));
-			panes.add(pane);
+			var textBox = new VBox(text);
+			textBox.setBackground(Ufx.colorBackground(Color.rgb(200, 200, 255, 0.5)));
+			panes.add(textBox);
 		}
 		hostScene.infoLayer.getChildren().addAll(panes);
+	}
+
+	public void update() {
+		var game = hostScene.ctx.game();
+		for (int i = 0; i < NUM_INFOS; ++i) {
+			if (i < 4) {
+				var ghost = game.theGhosts[i];
+				updateInfo(panes.get(i), texts.get(i), ghostInfo(ghost), ghost);
+				panes.get(i).setVisible(ghost.isVisible());
+			} else if (i == 4) {
+				updateInfo(panes.get(i), texts.get(i), pacInfo(game.pac), game.pac);
+				panes.get(i).setVisible(game.pac.isVisible());
+			} else {
+				var bonus = game.bonus();
+				updateInfo(panes.get(i), texts.get(i), bonusInfo(bonus), bonus.entity());
+				panes.get(i).setVisible(bonus.state() != BonusState.INACTIVE);
+			}
+		}
+	}
+
+	private void updateInfo(Pane pane, Text text, String info, Entity entity) {
+		text.setText(info);
+		var textSize = text.getBoundsInLocal();
+		var scaling = hostScene.getScaling();
+		pane.setTranslateX((entity.getPosition().x() + World.HTS) * scaling - textSize.getWidth() / 2);
+		pane.setTranslateY(entity.getPosition().y() * scaling - textSize.getHeight());
 	}
 
 	private String locationInfo(Creature guy) {
@@ -84,7 +111,7 @@ public class GuysInfo {
 		return "Velocity: %s%ndir:%s wish:%s".formatted(guy.getVelocity(), guy.moveDir(), guy.wishDir());
 	}
 
-	private String fmtAnimationState(EntityAnimation animation, Direction dir) {
+	private String animationStateInfo(EntityAnimation animation, Direction dir) {
 		if (animation instanceof EntityAnimationByDirection dam) {
 			return dam.get(dir).isRunning() ? "" : "(Stopped) ";
 		} else if (animation instanceof SingleEntityAnimation<?> ssa) {
@@ -94,7 +121,7 @@ public class GuysInfo {
 		}
 	}
 
-	private String fmtGhostInfo(Ghost ghost) {
+	private String ghostInfo(Ghost ghost) {
 		var game = hostScene.ctx.game();
 		String name = ghost.id == Ghost.RED_GHOST && game.cruiseElroyState > 0 ? "Elroy " + game.cruiseElroyState
 				: ghost.name;
@@ -108,24 +135,24 @@ public class GuysInfo {
 		var selectedAnim = ghost.animation();
 		if (selectedAnim.isPresent()) {
 			var key = ghost.animationSet().get().selectedKey();
-			var animState = fmtAnimationState(selectedAnim.get(), ghost.wishDir());
+			var animState = animationStateInfo(selectedAnim.get(), ghost.wishDir());
 			return "%s%n%s%n%s%n%s %s%s".formatted(name, locationInfo(ghost), movementInfo(ghost), stateText, animState, key);
 		}
 		return "%s%n%s%n%s%n%s".formatted(name, locationInfo(ghost), movementInfo(ghost), stateText);
 	}
 
-	private String fmtPacInfo(Pac pac) {
+	private String pacInfo(Pac pac) {
 		var selectedAnim = pac.animation();
 		if (selectedAnim.isPresent()) {
 			var key = pac.animationSet().get().selectedKey();
-			var animState = fmtAnimationState(selectedAnim.get(), pac.moveDir());
+			var animState = animationStateInfo(selectedAnim.get(), pac.moveDir());
 			return "%s%n%s%n%s%n%s%s".formatted(pac.name, locationInfo(pac), movementInfo(pac), animState, key);
 		} else {
 			return "%s%n%s%n%s".formatted(pac.name, locationInfo(pac), movementInfo(pac));
 		}
 	}
 
-	private String fmtBonusInfo(Bonus bonus) {
+	private String bonusInfo(Bonus bonus) {
 		var game = hostScene.ctx.game();
 		var bonusName = switch (game.variant) {
 		case MS_PACMAN -> MS_PACMAN_BONUS_NAMES[bonus.index()];
@@ -133,41 +160,5 @@ public class GuysInfo {
 		};
 		var symbolText = bonus.state() == BonusState.INACTIVE ? "INACTIVE" : bonusName;
 		return "%s%n%s".formatted(symbolText, game.bonus().state());
-	}
-
-	private void updateTextView(int i, String text, Entity entity) {
-		var textView = texts.get(i);
-		var pane = panes.get(i);
-		textView.setText(text);
-		var textSize = textView.getBoundsInLocal();
-		var scaling = hostScene.getScaling();
-		pane.setTranslateX((entity.getPosition().x() + World.HTS) * scaling - textSize.getWidth() / 2);
-		pane.setTranslateY(entity.getPosition().y() * scaling - textSize.getHeight());
-		pane.setVisible(entity.isVisible());
-	}
-
-	private void updateTextView(int i, String text, Bonus bonus) {
-		var textView = texts.get(i);
-		var pane = panes.get(i);
-		textView.setText(text);
-		var textSize = textView.getBoundsInLocal();
-		var scaling = hostScene.getScaling();
-		pane.setTranslateX((bonus.entity().getPosition().x() + World.HTS) * scaling - textSize.getWidth() / 2);
-		pane.setTranslateY(bonus.entity().getPosition().y() * scaling - textSize.getHeight());
-		pane.setVisible(bonus.state() != BonusState.INACTIVE);
-	}
-
-	public void update() {
-		var game = hostScene.ctx.game();
-		for (int i = 0; i < texts.size(); ++i) {
-			if (i < texts.size() - 2) {
-				var ghost = game.theGhosts[i];
-				updateTextView(i, fmtGhostInfo(ghost), ghost);
-			} else if (i == texts.size() - 2) {
-				updateTextView(i, fmtPacInfo(game.pac), game.pac);
-			} else {
-				updateTextView(i, fmtBonusInfo(game.bonus()), game.bonus());
-			}
-		}
 	}
 }
