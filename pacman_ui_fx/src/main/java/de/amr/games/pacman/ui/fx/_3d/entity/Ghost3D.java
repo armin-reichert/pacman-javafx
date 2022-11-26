e8fdc774b6ba54ae7291b616f330e4dc6d207c1a
Armin Reichert <armin.reichert@web.de>
2022-11-26 15:16:52 +0100

    refac

--- pacman_ui_fx/src/main/java/de/amr/games/pacman/ui/fx/_3d/entity/Ghost3D.java
+++ pacman_ui_fx/src/main/java/de/amr/games/pacman/ui/fx/_3d/entity/Ghost3D.java
@@ -156,7 +156,7 @@ public class Ghost3D extends Group {
 	}
 
 	private Look normalOrFrightenedOrFlashingLook(GameModel game) {
-		if (game.powerTimer.isRunning() && game.killedIndex[ghost.id] == -1) {
+		if (game.powerTimer().isRunning() && game.killedIndex[ghost.id] == -1) {
 			return frightenedOrFlashingLook(game);
 		}
 		return Look.NORMAL;
