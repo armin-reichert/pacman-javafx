177d0c02e466b08542c40db46aa8648f95920ee3
Armin Reichert <armin.reichert@web.de>
2023-01-14 11:50:30 +0100

    clean

--- pacman-ui-fx/src/main/java/de/amr/games/pacman/ui/fx/_2d/scene/common/PlayScene2D.java
+++ pacman-ui-fx/src/main/java/de/amr/games/pacman/ui/fx/_2d/scene/common/PlayScene2D.java
@@ -25,41 +25,19 @@ package de.amr.games.pacman.ui.fx._2d.scene.common;
 
 import static de.amr.games.pacman.model.common.world.World.TS;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import de.amr.games.pacman.controller.common.GameState;
 import de.amr.games.pacman.event.GameEvent;
-import de.amr.games.pacman.lib.anim.EntityAnimation;
-import de.amr.games.pacman.lib.anim.EntityAnimationByDirection;
 import de.amr.games.pacman.lib.anim.EntityAnimationMap;
-import de.amr.games.pacman.lib.anim.SingleEntityAnimation;
-import de.amr.games.pacman.lib.steering.Direction;
 import de.amr.games.pacman.model.common.GameLevel;
 import de.amr.games.pacman.model.common.GameSound;
-import de.amr.games.pacman.model.common.actors.Bonus;
-import de.amr.games.pacman.model.common.actors.BonusState;
-import de.amr.games.pacman.model.common.actors.Creature;
-import de.amr.games.pacman.model.common.actors.Entity;
 import de.amr.games.pacman.model.common.actors.Ghost;
-import de.amr.games.pacman.model.common.actors.GhostState;
-import de.amr.games.pacman.model.common.actors.Pac;
 import de.amr.games.pacman.model.common.world.ArcadeWorld;
-import de.amr.games.pacman.model.common.world.World;
-import de.amr.games.pacman.ui.fx.Env;
 import de.amr.games.pacman.ui.fx._2d.rendering.Rendering2D;
 import de.amr.games.pacman.ui.fx.shell.Actions;
 import de.amr.games.pacman.ui.fx.util.Keyboard;
 import de.amr.games.pacman.ui.fx.util.Modifier;
-import de.amr.games.pacman.ui.fx.util.Ufx;
-import javafx.beans.property.BooleanProperty;
-import javafx.beans.property.SimpleBooleanProperty;
 import javafx.scene.input.KeyCode;
-import javafx.scene.layout.Pane;
-import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
-import javafx.scene.text.Text;
-import javafx.scene.text.TextAlignment;
 
 /**
  * 2D scene displaying the maze and the game play.
@@ -68,11 +46,8 @@ import javafx.scene.text.TextAlignment;
  */
 public class PlayScene2D extends GameScene2D {
 
-	private final ActorsInfo actorsInfo = new ActorsInfo();
-
 	@Override
 	public void update() {
-		actorsInfo.update();
 		setCreditVisible(!ctx.hasCredit() || ctx.state() == GameState.GAME_OVER);
 	}
 
@@ -161,125 +136,5 @@ public class PlayScene2D extends GameScene2D {
 	@Override
 	public void onPlayerGetsExtraLife(GameEvent e) {
 		ctx.sounds().play(GameSound.EXTRA_LIFE);
-	}
-
-	/** Nested class for displaying info about the actors in this play scene. */
-	public class ActorsInfo {
-
-		private static final String[] PACMAN_BONUS_NAMES = { "CHERRIES", "STRAWBERRY", "PEACH", "APPLE", "GRAPES",
-				"GALAXIAN", "BELL", "KEY" };
-
-		private static final String[] MS_PACMAN_BONUS_NAMES = { "CHERRIES", "STRAWBERRY", "PEACH", "PRETZEL", "APPLE",
-				"PEAR", "BANANA" };
-
-		// 0..3: ghost info, 4: Pac info, 5: bonus info
-		private static final int NUM_INFOS = 6;
-		private static final int PAC_INDEX = 4;
-		private static final int BONUS_INDEX = 5;
-
-		private final List<Pane> panes = new ArrayList<>();
-		private final List<Text> texts = new ArrayList<>();
-
-		public final BooleanProperty enabledPy = new SimpleBooleanProperty();
-
-		public ActorsInfo() {
-			for (int i = 0; i < NUM_INFOS; ++i) {
-				var text = new Text();
-				text.setTextAlignment(TextAlignment.CENTER);
-				text.setFill(Color.WHITE);
-				texts.add(text);
-				var pane = new VBox(text);
-				pane.setBackground(Ufx.colorBackground(Color.rgb(200, 200, 255, 0.5)));
-				panes.add(pane);
-			}
-			panes.forEach(PlayScene2D.this::addToOverlayPane);
-			enabledPy.bind(Env.showDebugInfoPy);
-		}
-
-		public void update() {
-			if (!enabledPy.get()) {
-				return;
-			}
-			ctx.level().ifPresent(level -> {
-				level.ghosts()
-						.forEach(ghost -> updateInfo(panes.get(ghost.id()), texts.get(ghost.id()), ghostInfo(level, ghost), ghost));
-				updateInfo(panes.get(PAC_INDEX), texts.get(PAC_INDEX), pacInfo(level.pac()), level.pac());
-				var bonus = level.bonus();
-				updateInfo(panes.get(BONUS_INDEX), texts.get(BONUS_INDEX), bonusInfo(bonus), bonus.entity());
-				panes.get(BONUS_INDEX).setVisible(bonus.state() != BonusState.INACTIVE);
-			});
-		}
-
-		private void updateInfo(Pane pane, Text text, String info, Entity entity) {
-			text.setText(info);
-			var textSize = text.getBoundsInLocal();
-			pane.setTranslateX((entity.position().x() + World.HTS) * scaling() - textSize.getWidth() / 2);
-			pane.setTranslateY(entity.position().y() * scaling() - textSize.getHeight());
-			pane.setVisible(entity.isVisible());
-		}
-
-		private String locationInfo(Creature guy) {
-			return "Tile: %s%s%s".formatted(guy.tile(), guy.offset(), guy.isStuck() ? " stuck" : "");
-		}
-
-		private String movementInfo(Creature guy) {
-			return "Velocity: %s%ndir:%s wish:%s".formatted(guy.velocity(), guy.moveDir(), guy.wishDir());
-		}
-
-		private String animationStateInfo(EntityAnimation animation, Direction dir) {
-			if (animation instanceof EntityAnimationByDirection anim) {
-				return anim.get(dir).isRunning() ? "" : "(Stopped @%d) ".formatted(animation.frameIndex());
-			}
-			if (animation instanceof SingleEntityAnimation<?> anim) {
-				return anim.isRunning() ? "" : "(Stopped @%d) ".formatted(animation.frameIndex());
-			}
-			return "";
-		}
-
-		private String ghostInfo(GameLevel level, Ghost ghost) {
-			String name = ghost.id() == Ghost.ID_RED_GHOST && level.cruiseElroyState() > 0
-					? "Elroy " + level.cruiseElroyState()
-					: ghost.name();
-			var stateText = ghost.state().name();
-			if (ghost.is(GhostState.HUNTING_PAC)) {
-				stateText += level.chasingPhase().isPresent() ? " (Chasing)" : " (Scattering)";
-			}
-			if (ghost.killedIndex() != -1) {
-				stateText += " killed: #%d".formatted(ghost.killedIndex());
-			}
-			var selectedAnim = ghost.animation();
-			if (selectedAnim.isPresent()) {
-				var ghostAnims = ghost.animations();
-				if (ghostAnims.isPresent()) {
-					var key = ghostAnims.get().selectedKey();
-					var animState = animationStateInfo(selectedAnim.get(), ghost.wishDir());
-					return "%s%n%s%n%s%n%s %s%s".formatted(name, locationInfo(ghost), movementInfo(ghost), stateText, animState,
-							key);
-				}
-			}
-			return "%s%n%s%n%s%n%s".formatted(name, locationInfo(ghost), movementInfo(ghost), stateText);
-		}
-
-		private String pacInfo(Pac pac) {
-			var pacAnims = pac.animations();
-			if (pacAnims.isPresent()) {
-				var selectedAnim = pacAnims.get().selectedAnimation();
-				if (selectedAnim.isPresent()) {
-					var animState = animationStateInfo(selectedAnim.get(), pac.moveDir());
-					return "%s%n%s%n%s%n%s%s".formatted(pac.name(), locationInfo(pac), movementInfo(pac), animState,
-							pacAnims.get().selectedKey());
-				}
-			}
-			return "%s%n%s%n%s".formatted(pac.name(), locationInfo(pac), movementInfo(pac));
-		}
-
-		private String bonusInfo(Bonus bonus) {
-			var bonusName = switch (ctx.gameVariant()) {
-			case MS_PACMAN -> MS_PACMAN_BONUS_NAMES[bonus.symbol()];
-			case PACMAN -> PACMAN_BONUS_NAMES[bonus.symbol()];
-			};
-			var text = bonus.state() == BonusState.INACTIVE ? "INACTIVE" : bonusName;
-			return "%s%n%s".formatted(text, bonus.state());
-		}
 	}
 }
