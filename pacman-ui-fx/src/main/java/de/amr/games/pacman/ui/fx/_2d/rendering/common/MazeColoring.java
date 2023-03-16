925ca9cf29deffc71cce1297e40adec56d1d67fe
Armin Reichert <armin.reichert@web.de>
2023-03-16 05:54:06 +0100

    copyright years

--- pacman-ui-fx/src/main/java/de/amr/games/pacman/ui/fx/_2d/rendering/common/MazeColoring.java
+++ pacman-ui-fx/src/main/java/de/amr/games/pacman/ui/fx/_2d/rendering/common/MazeColoring.java
@@ -1,7 +1,7 @@
 /*
 MIT License
 
-Copyright (c) 2022 Armin Reichert
+Copyright (c) 2021-2023 Armin Reichert
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
