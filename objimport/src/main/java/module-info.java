0599b21b8e77e4582fb72c133159b01c8d0a4209
Armin Reichert <armin.reichert@web.de>
2023-03-22 16:18:58 +0100

    dependencies

--- objimport/src/main/java/module-info.java
+++ objimport/src/main/java/module-info.java
@@ -1,8 +1,7 @@
 module de.amr.objimport {
 
 	requires transitive org.apache.logging.log4j;
-	requires transitive javafx.controls;
-	requires transitive javafx.fxml;
+	requires transitive javafx.graphics;
 
 	exports de.amr.objimport;
 }
