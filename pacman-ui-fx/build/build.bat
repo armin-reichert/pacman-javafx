94894580a790cc861b2e30f49d0d5ffda2fb71d4
Armin Reichert <armin.reichert@web.de>
2023-03-23 15:56:38 +0100

    move batch scripts into project folder

---
+++ pacman-ui-fx/build/build.bat
@@ -0,0 +1,12 @@
+@echo off
+rem Builds the Pac-Man Java FX implementation and deploys into a zip file
+rem Expects that the Git repositories "pacman-basic" and "pacman-javafx" are locally available
+pushd ..\pacman-basic\pacman-core
+call mvn clean install
+popd
+pushd pacman-ui-fx
+call mvn clean install
+popd
+pushd tentackle
+call mvn clean install
+popd
---
+++ pacman-ui-fx/build/run.bat
@@ -0,0 +1,2 @@
+@echo off
+start "Pac-Man Game" /min cmd /c tentackle\target\jlink\bin\run.cmd
