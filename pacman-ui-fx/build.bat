9b539eec650ea99e51d50e05c0587b9c3012eedd
Armin Reichert <armin.reichert@web.de>
2023-03-23 17:12:02 +0100

    deploy.xml, remove tentackle subproject

--- pacman-ui-fx/build.bat
+++ pacman-ui-fx/build.bat
@@ -1,4 +1,4 @@
-@echo off
+@echo on
 rem Builds the Pac-Man Java FX implementation and deploys into a zip file
 rem Expects that the Git repositories "pacman-basic" and "pacman-javafx" are locally available
 pushd ..\..\pacman-basic\pacman-core
@@ -6,6 +6,5 @@ call mvn clean install
 popd
 call mvn clean install
 popd
-pushd ..\tentackle
-call mvn clean install
+call mvn -f deploy.xml clean install
 popd
