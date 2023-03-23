78cbf774f5476595b9eee5885e770e9aa0367710
Armin Reichert <armin.reichert@web.de>
2023-03-23 17:19:26 +0100

    build

--- pacman-ui-fx/build.bat
+++ pacman-ui-fx/build.bat
@@ -1,10 +1,9 @@
 @echo off
-rem Builds the Pac-Man Java FX implementation and deploys into a zip file
-rem Expects that the Git repositories "pacman-basic" and "pacman-javafx" are locally available
+rem Builds the Pac-Man Java FX project
+rem Git repositories "pacman-basic" and "pacman-javafx" must be locally available
+rem Creates excutable jar file and deployable zip file
 pushd ..\..\pacman-basic\pacman-core
 call mvn clean install
 popd
 call mvn clean install
-popd
 call mvn -f deploy.xml clean install
-popd
