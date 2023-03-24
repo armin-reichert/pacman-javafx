b6803d32d3d61d36e6b51ad8f6b55e0f71ec0d7f
Armin Reichert <armin.reichert@web.de>
2023-03-24 15:52:10 +0100

    pellet position

--- pacman-ui-fx/src/main/java/de/amr/games/pacman/ui/fx/_3d/entity/Pellet3D.java
+++ pacman-ui-fx/src/main/java/de/amr/games/pacman/ui/fx/_3d/entity/Pellet3D.java
@@ -59,7 +59,7 @@ public class Pellet3D implements Eatable3D {
 		shape.setUserData(tile);
 		shape.setTranslateX(tile.x() * TS + HTS);
 		shape.setTranslateY(tile.y() * TS + HTS);
-		shape.setTranslateZ(-HTS + 1);
+		shape.setTranslateZ(-HTS);
 	}
 
 	@Override
