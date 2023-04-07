572dce1a37350bc8b53e17ab0d94b01682cabbc3
Armin Reichert <armin.reichert@web.de>
2023-04-07 07:57:27 +0200

    refac

--- pacman-ui-fx/src/main/java/de/amr/games/pacman/ui/fx/_3d/entity/Ghost3D.java
+++ pacman-ui-fx/src/main/java/de/amr/games/pacman/ui/fx/_3d/entity/Ghost3D.java
@@ -69,40 +69,6 @@ public class Ghost3D {
 
 	private static final Duration TURN_DURATION = Duration.seconds(0.15);
 
-	private static final byte L = 0;
-	private static final byte U = 1;
-	private static final byte R = 2;
-	private static final byte D = 3;
-
-	//@formatter:off
-	private static byte dirIndex(Direction dir) {
-		return switch (dir) {	case LEFT -> L;	case RIGHT -> R; case UP -> U; case DOWN -> D; default -> L; };
-	}
-	//@formatter:on
-
-	//@formatter:off
-	private static final byte[][][] TURNS = {
-		{ null,    {L, R}, {L, U},  {L, -U} }, // LEFT  -> *
-		{ {R, L},  null,   {R, U},  {R, D}  }, // RIGHT -> *
-		{ {U, L},  {U, R}, null,    {U, D}  }, // UP    -> *
-		{ {-U, L}, {D, R}, {-U, U}, null    }, // DOWN  -> *
-	};
-	//@formatter:on
-
-	private static double angle(byte dirIndex) {
-		return dirIndex * 90.0;
-	}
-
-	private static RotateTransition createBrakeAnimation(Node node) {
-		var animation = new RotateTransition(Duration.seconds(0.4), node);
-		animation.setAxis(Rotate.Y_AXIS);
-		animation.setFromAngle(0);
-		animation.setToAngle(-35);
-		animation.setAutoReverse(true);
-		animation.setCycleCount(2);
-		return animation;
-	}
-
 	private enum Look {
 		NORMAL, FRIGHTENED, FLASHING, EYES, NUMBER;
 	}
@@ -156,7 +122,7 @@ public class Ghost3D {
 		turnTargetDir = ghost.moveDir();
 		turnRotation.stop();
 		root.setRotationAxis(Rotate.Z_AXIS);
-		root.setRotate(angle(dirIndex(ghost.moveDir())));
+		root.setRotate(Turn.angle(Turn.dirIndex(ghost.moveDir())));
 		update();
 	}
 
@@ -192,10 +158,20 @@ public class Ghost3D {
 		root.setVisible(ghost.isVisible() && !outsideWorld());
 	}
 
+	private RotateTransition createBrakeAnimation(Node node) {
+		var animation = new RotateTransition(Duration.seconds(0.4), node);
+		animation.setAxis(Rotate.Y_AXIS);
+		animation.setFromAngle(0);
+		animation.setToAngle(-35);
+		animation.setAutoReverse(true);
+		animation.setCycleCount(2);
+		return animation;
+	}
+
 	private void playTurnAnimation(Direction fromDir, Direction toDir) {
-		var turn = TURNS[fromDir.ordinal()][toDir.ordinal()];
-		double fromAngle = angle(turn[0]);
-		double toAngle = angle(turn[1]);
+		var turn = Turn.TURNS[fromDir.ordinal()][toDir.ordinal()];
+		double fromAngle = Turn.angle(turn[0]);
+		double toAngle = Turn.angle(turn[1]);
 		turnRotation.stop();
 		turnRotation.setAxis(Rotate.Z_AXIS);
 		turnRotation.setFromAngle(fromAngle);
---
+++ pacman-ui-fx/src/main/java/de/amr/games/pacman/ui/fx/_3d/entity/Turn.java
@@ -0,0 +1,59 @@
+/*
+MIT License
+
+Copyright (c) 2023 Armin Reichert
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+package de.amr.games.pacman.ui.fx._3d.entity;
+
+import de.amr.games.pacman.lib.steering.Direction;
+
+/**
+ * @author Armin Reichert
+ */
+public class Turn {
+
+	private static final byte L = 0;
+	private static final byte U = 1;
+	private static final byte R = 2;
+	private static final byte D = 3;
+
+	//@formatter:off
+	public static final byte[][][] TURNS = {
+		{ null,    {L, R}, {L, U},  {L, -U} }, // LEFT  -> *
+		{ {R, L},  null,   {R, U},  {R, D}  }, // RIGHT -> *
+		{ {U, L},  {U, R}, null,    {U, D}  }, // UP    -> *
+		{ {-U, L}, {D, R}, {-U, U}, null    }, // DOWN  -> *
+	};
+
+	public static byte dirIndex(Direction dir) {
+		return switch (dir) {	case LEFT -> L;	case RIGHT -> R; case UP -> U; case DOWN -> D; default -> L; };
+	}
+	//@formatter:on
+
+	public static double angle(byte dirIndex) {
+		return dirIndex * 90.0;
+	}
+
+	public static double angle(Direction dir) {
+		return angle(dirIndex(dir)) * 90.0;
+	}
+}
