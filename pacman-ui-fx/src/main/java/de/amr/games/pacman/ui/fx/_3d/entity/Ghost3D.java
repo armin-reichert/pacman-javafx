7f5fdd2f331d1499761afd5b5294550cd03cd37c
Armin Reichert <armin.reichert@web.de>
2023-04-13 10:34:23 +0200

    reworked, turn animation must be implemented differenty (still learning)

--- pacman-ui-fx/src/main/java/de/amr/games/pacman/ui/fx/_3d/entity/Ghost3D.java
+++ pacman-ui-fx/src/main/java/de/amr/games/pacman/ui/fx/_3d/entity/Ghost3D.java
@@ -28,7 +28,6 @@ import java.util.Objects;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import de.amr.games.pacman.lib.steering.Direction;
 import de.amr.games.pacman.model.common.GameLevel;
 import de.amr.games.pacman.model.common.actors.Ghost;
 import de.amr.games.pacman.model.common.world.World;
@@ -36,8 +35,6 @@ import de.amr.games.pacman.ui.fx._2d.rendering.GhostColoring;
 import de.amr.games.pacman.ui.fx._3d.Model3D;
 import de.amr.games.pacman.ui.fx._3d.animation.Turn;
 import javafx.animation.Animation;
-import javafx.animation.Animation.Status;
-import javafx.animation.Interpolator;
 import javafx.animation.RotateTransition;
 import javafx.beans.property.ObjectProperty;
 import javafx.beans.property.SimpleObjectProperty;
@@ -48,6 +45,7 @@ import javafx.scene.paint.PhongMaterial;
 import javafx.scene.shape.Box;
 import javafx.scene.shape.DrawMode;
 import javafx.scene.transform.Rotate;
+import javafx.scene.transform.Translate;
 import javafx.util.Duration;
 
 /**
@@ -69,7 +67,6 @@ public class Ghost3D {
 	private static final Logger LOG = LogManager.getFormatterLogger();
 
 	private static final Duration BRAKE_DURATION = Duration.seconds(0.4);
-	private static final Duration TURN_DURATION = Duration.seconds(0.15);
 
 	private enum Look {
 		NORMAL, FRIGHTENED, FLASHING, EYES, NUMBER;
@@ -82,8 +79,8 @@ public class Ghost3D {
 	private final Group root = new Group();
 	private final ColoredGhost3D coloredGhost3D;
 	private Box numberCube;
-	private Direction turnTargetDir;
-	private final RotateTransition turnAnimation;
+	private final Translate position;
+	private final Rotate orientation;
 	private final RotateTransition brakeAnimation;
 	private final RotateTransition dressAnimation;
 	private Image numberImage;
@@ -92,6 +89,10 @@ public class Ghost3D {
 	public Ghost3D(GameLevel level, Ghost ghost, GhostColoring colors, Model3D model3D, double size) {
 		this.level = Objects.requireNonNull(level, "Game level must not be null");
 		this.ghost = Objects.requireNonNull(ghost, "Ghost must not be null");
+
+		position = new Translate();
+		orientation = new Rotate();
+		root.getTransforms().addAll(position, orientation);
 
 		coloredGhost3D = new ColoredGhost3D(model3D, colors, size);
 		coloredGhost3D.dress().drawModeProperty().bind(drawModePy);
@@ -102,13 +103,6 @@ public class Ghost3D {
 
 		root.getChildren().add(coloredGhost3D.getRoot());
 		root.getChildren().add(numberCube);
-
-		turnAnimation = new RotateTransition(TURN_DURATION, root);
-		turnAnimation.setAxis(Rotate.Z_AXIS);
-		turnAnimation.setInterpolator(Interpolator.EASE_BOTH);
-		turnAnimation.setOnFinished(e -> {
-			LOG.trace("%s: Turn rotation finished", ghost.name());
-		});
 
 		brakeAnimation = new RotateTransition(BRAKE_DURATION, coloredGhost3D.getRoot());
 		brakeAnimation.setAxis(Rotate.Y_AXIS);
@@ -132,31 +126,40 @@ public class Ghost3D {
 	}
 
 	public void init() {
-		turnTargetDir = ghost.moveDir();
-		turnAnimation.stop();
+		brakeAnimation.stop();
 		dressAnimation.stop();
-		root.setRotationAxis(Rotate.Z_AXIS);
-		root.setRotate(Turn.angle(ghost.moveDir()));
-		update();
+		updatePosition();
+		turnToMoveDirection();
+		updateVisbility();
+		updateLook();
 	}
 
 	public void update() {
+		updatePosition();
+		turnToMoveDirection();
+		updateVisbility();
 		updateLook();
+		updateAnimations();
+	}
 
-		root.setTranslateX(ghost.center().x());
-		root.setTranslateY(ghost.center().y());
-		root.setTranslateZ(-4.25);
+	private void updatePosition() {
+		position.setX(ghost.center().x());
+		position.setY(ghost.center().y());
+		position.setZ(-5);
+	}
+
+	private void turnToMoveDirection() {
+		orientation.setAngle(Turn.angle(ghost.moveDir()));
+	}
+
+	private void updateVisbility() {
 		root.setVisible(ghost.isVisible() && !outsideWorld());
+	}
 
+	private void updateAnimations() {
 		if (currentLook != Look.NUMBER) {
 			if (ghost.moveResult.tunnelEntered) {
 				brakeAnimation.playFromStart();
-			}
-			if (turnAnimation.getStatus() == Status.RUNNING) {
-				LOG.trace("%s: Turn animation is running", ghost.name());
-			} else if (turnTargetDir != ghost.moveDir()) { // move direction changed? TODO: store info in ghost.moveResult?
-				playTurnAnimation(turnTargetDir, ghost.moveDir());
-				turnTargetDir = ghost.moveDir();
 			}
 //			if (dressAnimation.getStatus() != Status.RUNNING) {
 //				dressAnimation.play();
@@ -164,15 +167,6 @@ public class Ghost3D {
 		} else {
 //			dressAnimation.stop();
 		}
-	}
-
-	private void playTurnAnimation(Direction fromDir, Direction toDir) {
-		var turnAngles = Turn.angles(fromDir, toDir);
-		turnAnimation.stop();
-		turnAnimation.setAxis(Rotate.Z_AXIS);
-		turnAnimation.setFromAngle(turnAngles.from());
-		turnAnimation.setToAngle(turnAngles.to());
-		turnAnimation.playFromStart();
 	}
 
 	private void showAsColoredGhost(boolean visible) {
