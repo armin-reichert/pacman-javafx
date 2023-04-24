e6ccae8f8545c8eb1ce8deaab56a6a6e47da1bd3
Armin Reichert <armin.reichert@web.de>
2023-04-24 08:59:10 +0200

    sometimes build fails if not cleaned initially

--- pacman-ui-fx/build.bat
+++ pacman-ui-fx/build.bat
@@ -2,6 +2,7 @@
 rem Builds the Pac-Man Java FX project
 rem Git repositories "pacman-basic" and "pacman-javafx" must be locally available
 rem Creates excutable jar file and deployable zip file
+call mvn clean
 pushd ..\..\pacman-basic\pacman-core
 call mvn clean install
 popd
