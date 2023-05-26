4e99aa367479b219a57de439daefb693f2db2d73
Armin Reichert <armin.reichert@web.de>
2023-05-26 18:04:50 +0200

    Cleanup

--- pacman-ui-fx-2d/src/main/java/de/amr/games/pacman/ui/fx/scene2d/BootScene.java
+++ pacman-ui-fx-2d/src/main/java/de/amr/games/pacman/ui/fx/scene2d/BootScene.java
@@ -27,7 +27,6 @@ package de.amr.games.pacman.ui.fx.scene2d;
 import static de.amr.games.pacman.lib.Globals.RND;
 
 import de.amr.games.pacman.ui.fx.rendering2d.ArcadeTheme;
-import de.amr.games.pacman.ui.fx.util.Spritesheet;
 import javafx.geometry.Rectangle2D;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
@@ -89,11 +88,9 @@ public class BootScene extends GameScene2D {
 	}
 
 	private void paintRandomHexCodes() {
-		var theme = context.ui().theme();
-		var font8 = theme.font("font.arcade", 8);
 		eraseContent();
 		ctx.setFill(ArcadeTheme.PALE);
-		ctx.setFont(font8);
+		ctx.setFont(sceneFont());
 		for (int row = 0; row < TILES_Y; ++row) {
 			for (int col = 0; col < TILES_X; ++col) {
 				var hexCode = Integer.toHexString(RND.nextInt(16));
@@ -104,25 +101,24 @@ public class BootScene extends GameScene2D {
 	}
 
 	private void paintRandomSprites() {
-		var ss = context.renderer().spritesheet();
 		eraseContent();
 		for (int row = 0; row < TILES_Y / 2; ++row) {
 			if (RND.nextInt(100) > 10) {
-				var region1 = randomSquare(ss);
-				var region2 = randomSquare(ss);
+				var region1 = randomSpritesheetTile();
+				var region2 = randomSpritesheetTile();
 				var splitX = TILES_X / 8 + RND.nextInt(TILES_X / 4);
 				for (int col = 0; col < TILES_X / 2; ++col) {
 					var region = col < splitX ? region1 : region2;
-					context.renderer().drawSprite(ctx, region, region.getWidth() * col, region.getHeight() * row);
+					r().drawSprite(ctx, region, region.getWidth() * col, region.getHeight() * row);
 				}
 			}
 		}
 		updateSceneImage();
 	}
 
-	private Rectangle2D randomSquare(Spritesheet ss) {
-		var source = ss.source();
-		var raster = ss.raster();
+	private Rectangle2D randomSpritesheetTile() {
+		var source = r().spritesheet().source();
+		var raster = r().spritesheet().raster();
 		double x = RND.nextDouble() * (source.getWidth() - raster);
 		double y = RND.nextDouble() * (source.getHeight() - raster);
 		return new Rectangle2D(x, y, raster, raster);
