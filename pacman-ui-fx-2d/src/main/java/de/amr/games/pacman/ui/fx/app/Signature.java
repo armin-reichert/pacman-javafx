1cab9b34c4d21ee060b254ccf12fc93bf55f93d1
Armin Reichert <armin.reichert@web.de>
2023-06-26 16:15:14 +0200

    Signature

--- pacman-ui-fx-2d/src/main/java/de/amr/games/pacman/ui/fx/app/Signature.java
+++ pacman-ui-fx-2d/src/main/java/de/amr/games/pacman/ui/fx/app/Signature.java
@@ -8,12 +8,11 @@ import javafx.animation.FadeTransition;
 import javafx.animation.Interpolator;
 import javafx.animation.SequentialTransition;
 import javafx.animation.Transition;
-import javafx.geometry.VPos;
 import javafx.scene.Node;
-import javafx.scene.layout.GridPane;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.scene.text.Text;
+import javafx.scene.text.TextFlow;
 import javafx.util.Duration;
 
 /**
@@ -21,37 +20,27 @@ import javafx.util.Duration;
  */
 public class Signature {
 
-	private Text remakeText;
-	private Text nameText;
-	// Does not work in GWT
-//	private final TextFlow sentence;
-	private final GridPane sentence;
-	private final FadeTransition fadeIn;
-	private final FadeTransition fadeOut;
+	private final Text madeByText;
+	private final Text nameText;
+	private final TextFlow sentence = new TextFlow();
 	private final Transition animation;
 
 	public Signature() {
-		remakeText = new Text("Remake (2023) by  ");
-		remakeText.setFill(Color.gray(0.6));
-		remakeText.setFont(Font.font("Helvetica", 14));
+		madeByText = new Text("Remake (2023) by ");
+		madeByText.setFill(Color.GOLD);
+		madeByText.setFont(Font.font("Helvetica", 14));
 
 		nameText = new Text("Armin Reichert");
-		nameText.setFill(Color.gray(0.6));
+		nameText.setFill(Color.GOLD);
 		nameText.setFont(Font.font("Serif", 14));
 
-		sentence = new GridPane();
-		sentence.setHgap(4); // no effect in GWT
-		sentence.add(remakeText, 0, 0);
-		GridPane.setValignment(remakeText, VPos.BASELINE);
-		sentence.add(nameText, 1, 0);
-		GridPane.setValignment(nameText, VPos.BASELINE);
+		sentence.getChildren().addAll(madeByText, nameText);
 
-		fadeIn = new FadeTransition(Duration.seconds(5), sentence);
+		var fadeIn = new FadeTransition(Duration.seconds(5), sentence);
 		fadeIn.setFromValue(0);
 		fadeIn.setToValue(1);
-		fadeIn.setInterpolator(Interpolator.EASE_IN);
 
-		fadeOut = new FadeTransition(Duration.seconds(1), sentence);
+		var fadeOut = new FadeTransition(Duration.seconds(1), sentence);
 		fadeOut.setFromValue(1);
 		fadeOut.setToValue(0);
 
@@ -64,7 +53,7 @@ public class Signature {
 
 	public void setMadeByFont(Font font)
 	{
-		remakeText.setFont(font);
+		madeByText.setFont(font);
 	}
 
 	public Node root() {
