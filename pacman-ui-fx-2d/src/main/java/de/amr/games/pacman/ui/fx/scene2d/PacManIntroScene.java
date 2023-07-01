57c3737b3150532c9b43df7199d286550a5838d1
Armin Reichert <armin.reichert@web.de>
2023-07-01 10:57:00 +0200

    Cleanup

--- pacman-ui-fx-2d/src/main/java/de/amr/games/pacman/ui/fx/scene2d/PacManIntroScene.java
+++ pacman-ui-fx-2d/src/main/java/de/amr/games/pacman/ui/fx/scene2d/PacManIntroScene.java
@@ -8,8 +8,6 @@ import de.amr.games.pacman.controller.PacManIntro;
 import de.amr.games.pacman.controller.PacManIntro.State;
 import de.amr.games.pacman.ui.fx.app.ActionHandler;
 import de.amr.games.pacman.ui.fx.app.PacManGames2d;
-import de.amr.games.pacman.ui.fx.app.PacManGames2dUI;
-import de.amr.games.pacman.ui.fx.app.Signature;
 import de.amr.games.pacman.ui.fx.input.Keyboard;
 import de.amr.games.pacman.ui.fx.rendering2d.ArcadeTheme;
 import de.amr.games.pacman.ui.fx.rendering2d.pacman.GhostAnimationsPacManGame;
@@ -28,12 +26,12 @@ import static de.amr.games.pacman.lib.Globals.TS;
  */
 public class PacManIntroScene extends GameScene2D {
 
-	private static final String QUOTE = "\"";
+	private static final char QUOTE = '\"';
 
 	private PacManIntro intro;
-	private PacManIntro.Context ic;
 
-	public PacManIntroScene() {
+	private PacManIntro.Context context() {
+		return intro.context();
 	}
 
 	@Override
@@ -44,11 +42,10 @@ public class PacManIntroScene extends GameScene2D {
 		setScoreVisible(true);
 
 		intro = new PacManIntro();
-		ic = intro.context();
 
-		ic.pacMan.setAnimations(new PacAnimationsPacManGame(ic.pacMan, ss));
-		ic.ghosts().forEach(ghost -> ghost.setAnimations(new GhostAnimationsPacManGame(ghost, ss)));
-		ic.blinking.reset();
+		context().pacMan.setAnimations(new PacAnimationsPacManGame(context().pacMan, ss));
+		context().ghosts().forEach(ghost -> ghost.setAnimations(new GhostAnimationsPacManGame(ghost, ss)));
+		context().blinking.reset();
 
 		intro.changeState(State.START);
 	}
@@ -56,11 +53,6 @@ public class PacManIntroScene extends GameScene2D {
 	@Override
 	public void update() {
 		intro.update();
-	}
-
-	@Override
-	public void end() {
-		//ui().soundHandler().stopVoice();
 	}
 
 	@Override
@@ -83,34 +75,24 @@ public class PacManIntroScene extends GameScene2D {
 		switch (intro.state()) {
 		case SHOWING_POINTS: {
 			drawPoints();
-		}
 			break;
-
+		}
 		case CHASING_PAC: {
 			drawPoints();
 			drawBlinkingEnergizer();
 			drawGuys(flutter(timer.tick()));
 			drawMidwayCopyright(t(4), t(32));
-		}
 			break;
-
-		case CHASING_GHOSTS: {
-			drawPoints();
-			drawGuys(0);
-			drawMidwayCopyright(t(4), t(32));
 		}
-			break;
-
+		case CHASING_GHOSTS:
 		case READY_TO_PLAY: {
 			drawPoints();
 			drawGuys(0);
 			drawMidwayCopyright(t(4), t(32));
-		}
 			break;
-
-		default: {
-			// nothing to do
 		}
+		default:
+			break;
 		}
 		drawLevelCounter(t(24), t(34), game().levelCounter());
 	}
@@ -127,61 +109,63 @@ public class PacManIntroScene extends GameScene2D {
 
 	private void drawGallery() {
 		var ss = (SpritesheetPacManGame) spritesheet;
+		var font = sceneFont();
 
-		int tx = ic.leftTileX;
-		if (ic.titleVisible) {
-			drawText("CHARACTER / NICKNAME", ArcadeTheme.PALE, sceneFont(), t(tx + 3), t(6));
+		int tx = context().leftTileX;
+		if (context().titleVisible) {
+			drawText("CHARACTER / NICKNAME", ArcadeTheme.PALE, font, t(tx + 3), t(6));
 		}
 		for (int id = 0; id < 4; ++id) {
-			if (!ic.ghostInfo[id].pictureVisible) {
+			var ghostInfo = context().ghostInfo[id];
+			if (!ghostInfo.pictureVisible) {
 				continue;
 			}
 			int ty = 7 + 3 * id;
 			drawSpriteOverBoundingBox(ss.ghostFacingRight(id), t(tx) + 4, t(ty));
-			if (ic.ghostInfo[id].characterVisible) {
-				var text = "-" + ic.ghostInfo[id].character;
-				var color = theme.color(String.format("ghost.%d.color", id));
-				drawText(text, color, sceneFont(), t(tx + 3), t(ty + 1));
+			if (ghostInfo.characterVisible) {
+				var text = "-" + ghostInfo.character;
+				var color = theme.color("ghost." + id + ".color");
+				drawText(text, color, font, t(tx + 3), t(ty + 1));
 			}
-			if (ic.ghostInfo[id].nicknameVisible) {
-				var text = QUOTE + ic.ghostInfo[id].ghost.name() + QUOTE;
-				var color = theme.color(String.format("ghost.%d.color", id));
-				drawText(text, color, sceneFont(), t(tx + 14), t(ty + 1));
+			if (ghostInfo.nicknameVisible) {
+				var text = QUOTE + ghostInfo.ghost.name() + QUOTE;
+				var color = theme.color("ghost." + id + ".color");
+				drawText(text, color, font, t(tx + 14), t(ty + 1));
 			}
 		}
 	}
 
 	private void drawBlinkingEnergizer() {
-		if (ic.blinking.on()) {
+		if (context().blinking.on()) {
 			g.setFill(theme.color("pacman.maze.foodColor"));
-			g.fillOval(s(t(ic.leftTileX)), s(t(20)), s(TS), s(TS));
+			g.fillOval(s(t(context().leftTileX)), s(t(20)), s(TS), s(TS));
 		}
 	}
 
 	private void drawGuys(int shakingAmount) {
 		if (shakingAmount == 0) {
-			ic.ghosts().forEach(ghost -> drawGhostSprite(ghost));
+			context().ghosts().forEach(ghost -> drawGhostSprite(ghost));
 		} else {
-			drawGhostSprite(ic.ghost(0));
-			drawGhostSprite(ic.ghost(3));
+			drawGhostSprite(context().ghost(0));
+			drawGhostSprite(context().ghost(3));
 			// shaking ghosts effect, not quite as in original game
 			g.save();
 			g.translate(shakingAmount, 0);
-			drawGhostSprite(ic.ghost(1));
-			drawGhostSprite(ic.ghost(2));
+			drawGhostSprite(context().ghost(1));
+			drawGhostSprite(context().ghost(2));
 			g.restore();
 		}
-		drawPacSprite(ic.pacMan);
+		drawPacSprite(context().pacMan);
 	}
 
 	private void drawPoints() {
 		var font8 = sceneFont();
 		var font6 = theme.font("font.arcade", s(6));
-		int tx = ic.leftTileX + 6;
+		int tx = context().leftTileX + 6;
 		int ty = 25;
 		g.setFill(theme.color("pacman.maze.foodColor"));
 		g.fillRect(s(t(tx) + 4), s(t(ty - 1) + 4), s(2), s(2));
-		if (ic.blinking.on()) {
+		if (context().blinking.on()) {
 			g.fillOval(s(t(tx)), s(t(ty + 1)), s(TS), s(TS));
 		}
 		drawText("10",  ArcadeTheme.PALE, font8, t(tx + 2), t(ty));
