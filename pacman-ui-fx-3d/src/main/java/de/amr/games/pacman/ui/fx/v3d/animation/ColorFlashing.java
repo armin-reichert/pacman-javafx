4ffdcb3a11c69e7d50c9ecd3807cbafdf1c7b14e
Armin Reichert <armin.reichert@web.de>
2023-10-01 10:12:27 +0200

    Parameter validation

--- pacman-ui-fx-3d/src/main/java/de/amr/games/pacman/ui/fx/v3d/animation/ColorFlashing.java
+++ pacman-ui-fx-3d/src/main/java/de/amr/games/pacman/ui/fx/v3d/animation/ColorFlashing.java
@@ -4,6 +4,7 @@ See file LICENSE in repository root directory for details.
 */
 package de.amr.games.pacman.ui.fx.v3d.animation;
 
+import de.amr.games.pacman.lib.Globals;
 import javafx.animation.Interpolator;
 import javafx.animation.Transition;
 import javafx.beans.property.ObjectProperty;
@@ -11,20 +12,30 @@ import javafx.beans.property.SimpleObjectProperty;
 import javafx.scene.paint.Color;
 import javafx.util.Duration;
 
+import static de.amr.games.pacman.lib.Globals.checkNotNull;
+
 /**
  * @author Armin Reichert
  */
 public class ColorFlashing extends Transition {
 
-	public final ObjectProperty<Color> colorPy = new SimpleObjectProperty<>();
+	public final ObjectProperty<Color> colorPy;
 
 	private final Color startColor;
 	private final Color endColor;
 
 	public ColorFlashing(Color startColor, Color endColor, double seconds, int numFlashes) {
+		checkNotNull(startColor);
+		checkNotNull(endColor);
+		if (seconds < 0) {
+			throw new IllegalArgumentException("Flashing time seconds must be non-negative but is " + seconds);
+		}
+		if (numFlashes <= 0) {
+			throw new IllegalArgumentException("Flashing number must be positive but is " + numFlashes);
+		}
 		this.startColor = startColor;
 		this.endColor = endColor;
-		colorPy.set(startColor);
+		colorPy = new SimpleObjectProperty<>(startColor);
 		setCycleCount(INDEFINITE);
 		setCycleDuration(Duration.seconds(seconds / numFlashes));
 		setAutoReverse(true);
