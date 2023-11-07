a52d7b173e10f6e2a98f3235bc3682f14448feae
Armin Reichert <armin.reichert@web.de>
2023-11-07 13:26:52 +0100

    Use linear interpolation, check null args

--- pacman-ui-fx-3d/src/main/java/de/amr/games/pacman/ui/fx/v3d/animation/ColorChangeTransition.java
+++ pacman-ui-fx-3d/src/main/java/de/amr/games/pacman/ui/fx/v3d/animation/ColorChangeTransition.java
@@ -4,10 +4,14 @@ See file LICENSE in repository root directory for details.
 */
 package de.amr.games.pacman.ui.fx.v3d.animation;
 
+import de.amr.games.pacman.lib.Globals;
+import javafx.animation.Interpolator;
 import javafx.animation.Transition;
 import javafx.beans.property.ObjectProperty;
 import javafx.scene.paint.Color;
 import javafx.util.Duration;
+
+import static de.amr.games.pacman.lib.Globals.checkNotNull;
 
 /**
  * @author Armin Reichert
@@ -19,11 +23,16 @@ public class ColorChangeTransition extends Transition {
 	private final ObjectProperty<Color> targetColorPy;
 
 	public ColorChangeTransition(Duration duration, Color fromColor, Color toColor, ObjectProperty<Color> targetColorPy) {
+		checkNotNull(duration);
+		checkNotNull(fromColor);
+		checkNotNull(toColor);
+		checkNotNull(targetColorPy);
 		this.fromColor = fromColor;
 		this.toColor = toColor;
 		this.targetColorPy = targetColorPy;
 		setCycleCount(1);
 		setCycleDuration(duration);
+		setInterpolator(Interpolator.LINEAR);
 	}
 
 	@Override
