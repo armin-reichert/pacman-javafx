d08a1f03d0145808b4aa46aea8011edd384f3b50
Armin Reichert <armin.reichert@web.de>
2024-01-24 13:39:44 +0100

    Cleanup

--- pacman-ui-fx-3d/src/main/java/de/amr/games/pacman/ui/fx/v3d/PacManGames3dUI.java
+++ pacman-ui-fx-3d/src/main/java/de/amr/games/pacman/ui/fx/v3d/PacManGames3dUI.java
@@ -53,37 +53,41 @@ public class PacManGames3dUI extends PacManGames2dUI implements ActionHandler3D 
 	public static final ResourceBundle MSG_BUNDLE = ResourceBundle.getBundle(
 		"de.amr.games.pacman.ui.fx.v3d.texts.messages", PacManGames3dUI.class.getModule());
 
-	public static final float           PIP_MIN_HEIGHT           = 0.75f * GameModel.TILES_Y * TS;
-	public static final float           PIP_MAX_HEIGHT           = 2.00f * GameModel.TILES_Y * TS;
-	public static final DoubleProperty PY_PIP_OPACITY           = new SimpleDoubleProperty(0.66);
-	public static final DoubleProperty  PY_PIP_HEIGHT            = new SimpleDoubleProperty(GameModel.TILES_Y * TS);
-	public static final BooleanProperty PY_PIP_ON                = new SimpleBooleanProperty(false);
-	public static final IntegerProperty PY_SIMULATION_STEPS      = new SimpleIntegerProperty(1);
-	public static final BooleanProperty PY_3D_AXES_VISIBLE       = new SimpleBooleanProperty(false);
-	public static final ObjectProperty<DrawMode> PY_3D_DRAW_MODE = new SimpleObjectProperty<>(DrawMode.FILL);
-	public static final BooleanProperty PY_3D_ENABLED            = new SimpleBooleanProperty(true);
-	public static final ObjectProperty<Color> PY_3D_FLOOR_COLOR  = new SimpleObjectProperty<>(Color.grayRgb(0x33));
-	public static final StringProperty  PY_3D_FLOOR_TEXTURE      = new SimpleStringProperty("knobs");
-	public static final BooleanProperty PY_3D_FLOOR_TEXTURE_RND  = new SimpleBooleanProperty(false);
-	public static final ObjectProperty<Color> PY_3D_LIGHT_COLOR  = new SimpleObjectProperty<>(Color.GHOSTWHITE);
-	public static final BooleanProperty PY_3D_NIGHT_MODE         = new SimpleBooleanProperty(false);
-	public static final DoubleProperty  PY_3D_WALL_HEIGHT        = new SimpleDoubleProperty(1.75);
-	public static final DoubleProperty  PY_3D_WALL_THICKNESS     = new SimpleDoubleProperty(1.25);
-	public static final BooleanProperty PY_3D_PAC_LIGHT_ENABLED  = new SimpleBooleanProperty(true);
+	public static final float           PIP_MIN_HEIGHT                = 0.75f * GameModel.TILES_Y * TS;
+	public static final float           PIP_MAX_HEIGHT                = 2.00f * GameModel.TILES_Y * TS;
+
+	public static final DoubleProperty  PY_PIP_OPACITY                = new SimpleDoubleProperty(0.66);
+	public static final DoubleProperty  PY_PIP_HEIGHT                 = new SimpleDoubleProperty(GameModel.TILES_Y * TS);
+	public static final BooleanProperty PY_PIP_ON                     = new SimpleBooleanProperty(false);
+	public static final IntegerProperty PY_SIMULATION_STEPS           = new SimpleIntegerProperty(1);
+	public static final BooleanProperty PY_3D_AXES_VISIBLE            = new SimpleBooleanProperty(false);
+	public static final ObjectProperty<DrawMode> PY_3D_DRAW_MODE      = new SimpleObjectProperty<>(DrawMode.FILL);
+	public static final BooleanProperty PY_3D_ENABLED                 = new SimpleBooleanProperty(true);
+	public static final ObjectProperty<Color> PY_3D_FLOOR_COLOR       = new SimpleObjectProperty<>(Color.grayRgb(0x33));
+	public static final StringProperty  PY_3D_FLOOR_TEXTURE           = new SimpleStringProperty("knobs");
+	public static final BooleanProperty PY_3D_FLOOR_TEXTURE_RND       = new SimpleBooleanProperty(false);
+	public static final ObjectProperty<Color> PY_3D_LIGHT_COLOR       = new SimpleObjectProperty<>(Color.GHOSTWHITE);
+	public static final BooleanProperty PY_3D_NIGHT_MODE              = new SimpleBooleanProperty(false);
+	public static final DoubleProperty  PY_3D_WALL_HEIGHT             = new SimpleDoubleProperty(1.75);
+	public static final DoubleProperty  PY_3D_WALL_THICKNESS          = new SimpleDoubleProperty(1.25);
+	public static final BooleanProperty PY_3D_PAC_LIGHT_ENABLED       = new SimpleBooleanProperty(true);
 	public static final ObjectProperty<Perspective> PY_3D_PERSPECTIVE = new SimpleObjectProperty<>(Perspective.NEAR_PLAYER);
-	public static final BooleanProperty PY_3D_ENERGIZER_EXPLODES = new SimpleBooleanProperty(true);
-	public static final BooleanProperty PY_WOKE_PUSSY            = new SimpleBooleanProperty(false);
-	public static final KeyCodeCombination[] KEYS_TOGGLE_DASHBOARD = { just(KeyCode.F1), alt(KeyCode.B) };
-	public static final KeyCodeCombination   KEY_TOGGLE_PIP_VIEW   = just(KeyCode.F2);
-	public static final KeyCodeCombination   KEY_TOGGLE_2D_3D      = alt(KeyCode.DIGIT3);
-	public static final KeyCodeCombination   KEY_PREV_PERSPECTIVE  = alt(KeyCode.LEFT);
-	public static final KeyCodeCombination   KEY_NEXT_PERSPECTIVE  = alt(KeyCode.RIGHT);
-	public static final Picker<String> PICKER_READY_PACMAN    = Picker.fromBundle(MSG_BUNDLE, "pacman.ready");
-	public static final Picker<String> PICKER_READY_MS_PACMAN = Picker.fromBundle(MSG_BUNDLE, "mspacman.ready");
-	public static final Picker<String> PICKER_CHEATING        = Picker.fromBundle(MSG_BUNDLE, "cheating");
-	public static final Picker<String> PICKER_LEVEL_COMPLETE  = Picker.fromBundle(MSG_BUNDLE, "level.complete");
-	public static final Picker<String> PICKER_GAME_OVER       = Picker.fromBundle(MSG_BUNDLE, "game.over");
-	public static final String NO_TEXTURE = "No Texture";
+	public static final BooleanProperty PY_3D_ENERGIZER_EXPLODES      = new SimpleBooleanProperty(true);
+	public static final BooleanProperty PY_WOKE_PUSSY                 = new SimpleBooleanProperty(false);
+
+	public static final KeyCodeCombination[] KEYS_TOGGLE_DASHBOARD    = { just(KeyCode.F1), alt(KeyCode.B) };
+	public static final KeyCodeCombination   KEY_TOGGLE_PIP_VIEW      = just(KeyCode.F2);
+	public static final KeyCodeCombination   KEY_TOGGLE_2D_3D         = alt(KeyCode.DIGIT3);
+	public static final KeyCodeCombination   KEY_PREV_PERSPECTIVE     = alt(KeyCode.LEFT);
+	public static final KeyCodeCombination   KEY_NEXT_PERSPECTIVE     = alt(KeyCode.RIGHT);
+
+	public static final Picker<String> PICKER_READY_PACMAN            = Picker.fromBundle(MSG_BUNDLE, "pacman.ready");
+	public static final Picker<String> PICKER_READY_MS_PACMAN         = Picker.fromBundle(MSG_BUNDLE, "mspacman.ready");
+	public static final Picker<String> PICKER_CHEATING                = Picker.fromBundle(MSG_BUNDLE, "cheating");
+	public static final Picker<String> PICKER_LEVEL_COMPLETE          = Picker.fromBundle(MSG_BUNDLE, "level.complete");
+	public static final Picker<String> PICKER_GAME_OVER               = Picker.fromBundle(MSG_BUNDLE, "game.over");
+
+	public static final String NO_TEXTURE                             = "No Texture";
 
 	public PacManGames3dUI(Stage stage, Settings settings) {
 		super(stage, settings);
