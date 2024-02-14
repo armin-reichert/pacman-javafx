f1b2090469924d0ec407dcee4570867c741e0af4
Armin Reichert <armin.reichert@web.de>
2024-02-14 17:38:05 +0100

    Improve Maven build

--- mvn_build.bat
+++ mvn_build.bat
@@ -1,10 +1,2 @@
 @echo off
-pushd pacman-core
-call mvn clean install
-popd
-pushd pacman-ui-fx-2d
 call mvn clean install -Pbuild-for-windows
-popd
-pushd pacman-ui-fx-3d
-call mvn clean install -Pbuild-for-windows
-popd
--- mvn_build_linux.bat
+++ mvn_build_linux.bat
@@ -1,10 +1,2 @@
 @echo off
-pushd pacman-core
-call mvn clean install
-popd
-pushd pacman-ui-fx-2d
 call mvn install -Pbuild-for-linux
-popd
-pushd pacman-ui-fx-3d
-call mvn install -Pbuild-for-linux
-popd
