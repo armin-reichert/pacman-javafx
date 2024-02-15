6dc410ab3d31472fef0a1eba5b4b63799da3c963
Armin Reichert <armin.reichert@web.de>
2024-02-15 16:48:56 +0100

    open module

--- pacman-ui-fx-3d/src/main/java/module-info.java
+++ pacman-ui-fx-3d/src/main/java/module-info.java
@@ -2,7 +2,7 @@
 Copyright (c) 2021-2023 Armin Reichert (MIT License)
 See file LICENSE in repository root directory for details.
 */
-module de.amr.games.pacman.ui.fx.v3d {
+open module de.amr.games.pacman.ui.fx.v3d {
 
 	requires javafx.graphics;
 	requires transitive javafx.controls;
@@ -17,10 +17,4 @@ module de.amr.games.pacman.ui.fx.v3d {
 	exports de.amr.games.pacman.ui.fx.v3d.entity;
 	exports de.amr.games.pacman.ui.fx.v3d.model;
 	exports de.amr.games.pacman.ui.fx.v3d.scene3d;
-
-	// give access for loading module (non-class) resources, see Class.getResource()
-	opens de.amr.games.pacman.ui.fx.v3d.graphics;
-	opens de.amr.games.pacman.ui.fx.v3d.graphics.icons;
-	opens de.amr.games.pacman.ui.fx.v3d.graphics.textures;
-	opens de.amr.games.pacman.ui.fx.v3d.model3D;
 }
