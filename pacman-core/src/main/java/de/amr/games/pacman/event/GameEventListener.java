141c6ea58f980e810b528709e663d025135022a7
Armin Reichert <armin.reichert@web.de>
2024-02-27 07:50:08 +0100

    Refac

--- pacman-core/src/main/java/de/amr/games/pacman/event/GameEventListener.java
+++ pacman-core/src/main/java/de/amr/games/pacman/event/GameEventListener.java
@@ -18,68 +18,27 @@ public interface GameEventListener {
      */
     default void onGameEvent(GameEvent event) {
         switch (event.type) {
-            case BONUS_ACTIVATED:
-                onBonusActivated(event);
-                break;
-            case BONUS_EATEN:
-                onBonusEaten(event);
-                break;
-            case BONUS_EXPIRED:
-                onBonusExpired(event);
-                break;
-            case CREDIT_ADDED:
-                onCreditAdded(event);
-                break;
-            case EXTRA_LIFE_WON:
-                onExtraLifeWon(event);
-                break;
-            case GAME_STATE_CHANGED:
-                onGameStateChange((GameStateChangeEvent) event);
-                break;
-            case GHOST_EATEN:
-                onGhostEaten(event);
-                break;
-            case GHOST_ENTERS_HOUSE:
-                onGhostEntersHouse(event);
-                break;
-            case GHOST_STARTS_RETURNING_HOME:
-                onGhostStartsReturningHome(event);
-                break;
-            case HUNTING_PHASE_STARTED:
-                onHuntingPhaseStarted(event);
-                break;
-            case LEVEL_CREATED:
-                onLevelCreated(event);
-                break;
-            case LEVEL_STARTED:
-                onLevelStarted(event);
-                break;
-            case PAC_DIED:
-                onPacDied(event);
-                break;
-            case PAC_FOUND_FOOD:
-                onPacFoundFood(event);
-                break;
-            case PAC_GETS_POWER:
-                onPacGetsPower(event);
-                break;
-            case PAC_LOST_POWER:
-                onPacLostPower(event);
-                break;
-            case PAC_STARTS_LOSING_POWER:
-                onPacStartsLosingPower(event);
-                break;
-            case INTERMISSION_STARTED:
-                onIntermissionStarted(event);
-                break;
-            case STOP_ALL_SOUNDS:
-                onStopAllSounds(event);
-                break;
-            case UNSPECIFIED_CHANGE:
-                onUnspecifiedChange(event);
-                break;
-            default:
-                throw new IllegalArgumentException("Unknown event type: " + event);
+            case BONUS_ACTIVATED -> onBonusActivated(event);
+            case BONUS_EATEN -> onBonusEaten(event);
+            case BONUS_EXPIRED -> onBonusExpired(event);
+            case CREDIT_ADDED -> onCreditAdded(event);
+            case EXTRA_LIFE_WON -> onExtraLifeWon(event);
+            case GAME_STATE_CHANGED -> onGameStateChange((GameStateChangeEvent) event);
+            case GHOST_EATEN -> onGhostEaten(event);
+            case GHOST_ENTERS_HOUSE -> onGhostEntersHouse(event);
+            case GHOST_STARTS_RETURNING_HOME -> onGhostStartsReturningHome(event);
+            case HUNTING_PHASE_STARTED -> onHuntingPhaseStarted(event);
+            case LEVEL_CREATED -> onLevelCreated(event);
+            case LEVEL_STARTED -> onLevelStarted(event);
+            case PAC_DIED -> onPacDied(event);
+            case PAC_FOUND_FOOD -> onPacFoundFood(event);
+            case PAC_GETS_POWER -> onPacGetsPower(event);
+            case PAC_LOST_POWER -> onPacLostPower(event);
+            case PAC_STARTS_LOSING_POWER -> onPacStartsLosingPower(event);
+            case INTERMISSION_STARTED -> onIntermissionStarted(event);
+            case STOP_ALL_SOUNDS -> onStopAllSounds(event);
+            case UNSPECIFIED_CHANGE -> onUnspecifiedChange(event);
+            default -> throw new IllegalArgumentException("Unknown event type: " + event);
         }
     }
 
