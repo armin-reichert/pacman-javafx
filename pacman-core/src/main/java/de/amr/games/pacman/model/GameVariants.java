4f1dde3e83481d040108f984cf8b1263e612125e
Armin Reichert <armin.reichert@web.de>
2024-04-21 09:16:52 +0200

    Refac

--- pacman-core/src/main/java/de/amr/games/pacman/model/GameVariants.java
+++ pacman-core/src/main/java/de/amr/games/pacman/model/GameVariants.java
@@ -39,6 +39,11 @@ public enum GameVariants implements GameModel {
 
     MS_PACMAN {
 
+        {
+            initialLives = 3;
+            reset();
+        }
+
         /**
          * These numbers are from a conversation with user "damselindis" on Reddit. I am not sure if they are correct.
          *
@@ -234,6 +239,11 @@ public enum GameVariants implements GameModel {
      * <a href="https://pacman.holenet.info">Jamey Pittman: The Pac-Man Dossier</a>.
      */
     PACMAN {
+
+        {
+            initialLives = 3;
+            reset();
+        }
 
         final int[] HUNTING_TICKS_1 =      {7 * FPS, 20 * FPS, 7 * FPS, 20 * FPS, 5 * FPS,   20 * FPS, 5 * FPS, -1};
         final int[] HUNTING_TICKS_2_TO_4 = {7 * FPS, 20 * FPS, 7 * FPS, 20 * FPS, 5 * FPS, 1033 * FPS, 1,       -1};
@@ -446,11 +456,6 @@ public enum GameVariants implements GameModel {
     Ghost[] ghosts;
     Bonus bonus;
     World world;
-
-    GameVariants() {
-        initialLives = 3;
-        reset();
-    }
 
     @Override
     public void reset() {
