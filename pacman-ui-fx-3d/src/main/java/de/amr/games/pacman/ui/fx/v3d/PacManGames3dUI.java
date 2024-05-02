b93e8b314224b6231000cfcfdffebe41ead8f96b
Armin Reichert <armin.reichert@web.de>
2024-05-02 10:18:59 +0200

    Font

--- pacman-ui-fx-3d/src/main/java/de/amr/games/pacman/ui/fx/v3d/PacManGames3dUI.java
+++ pacman-ui-fx-3d/src/main/java/de/amr/games/pacman/ui/fx/v3d/PacManGames3dUI.java
@@ -215,8 +215,7 @@ public class PacManGames3dUI extends PacManGames2dUI implements ActionHandler3D 
         theme.set("infobox.min_label_width",       120);
         theme.set("infobox.text_color",            Color.WHITE);
         theme.set("infobox.label_font",            Font.font("Sans", 12));
-        //theme.set("infobox.text_font",             Font.font("Monospaced", 12));
-        theme.set("infobox.text_font",             rm.loadFont("fonts/SplineSansMono-Regular.ttf", 11));
+        theme.set("infobox.text_font",             rm.loadFont("fonts/SplineSansMono-Regular.ttf", 12));
     }
 
     static {
