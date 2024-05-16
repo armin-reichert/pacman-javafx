9a2ff0d307af69b169d08faea5842bc4e77517f8
Armin Reichert <armin.reichert@web.de>
2024-05-16 16:49:08 +0200

    oops

--- pacman-ui-fx-2d/src/main/java/de/amr/games/pacman/ui/fx/rendering2d/TerrainMapRenderer.java
+++ pacman-ui-fx-2d/src/main/java/de/amr/games/pacman/ui/fx/rendering2d/TerrainMapRenderer.java
@@ -10,6 +10,7 @@ import de.amr.games.pacman.model.world.Tiles;
 import javafx.geometry.Point2D;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.paint.Color;
+import javafx.scene.shape.ArcType;
 
 import java.util.ArrayList;
 import java.util.HashSet;
@@ -55,6 +56,13 @@ public class TerrainMapRenderer implements TileMapRenderer {
     public void drawTile(GraphicsContext g, Vector2i tile, byte content) {
         g.save();
         switch (content) {
+            case Tiles.WALL_H -> drawWallH(g, tile);
+            case Tiles.WALL_V -> drawWallV(g, tile);
+            case Tiles.DWALL_H -> drawDWallH(g, tile);
+            case Tiles.DWALL_V -> drawDWallV(g, tile);
+            case Tiles.CORNER_NW, Tiles.CORNER_NE, Tiles.CORNER_SW, Tiles.CORNER_SE -> drawCorner(g, tile, content);
+            case Tiles.DCORNER_NW, Tiles.DCORNER_NE, Tiles.DCORNER_SW, Tiles.DCORNER_SE ->
+                drawDCorner(g, tile, content);
             case Tiles.DOOR -> drawDoor(g, tile, Color.PINK);
             case Tiles.TUNNEL -> drawTunnel(g, tile);
             default -> {}
@@ -64,6 +72,75 @@ public class TerrainMapRenderer implements TileMapRenderer {
 
     public void drawTunnel(GraphicsContext g, Vector2i tile) {
         // overridden by design-time renderer
+    }
+
+    public void drawWallH(GraphicsContext g, Vector2i tile) {
+        double x = tile.x() * s(8), y = tile.y() * s(8);
+        g.setFill(wallStrokeColor);
+        // add 1 pixel to avoid gaps
+        g.fillRect(x, y + s(3.5f), s(8) + 1, s(1));
+    }
+
+    public void drawDWallH(GraphicsContext g, Vector2i tile) {
+        double x = tile.x() * s(8), y = tile.y() * s(8);
+        g.setFill(wallStrokeColor);
+        // add 1 pixel to avoid gaps
+        g.fillRect(x, y + s(2.5f), s(8) + 1, s(1));
+        g.fillRect(x, y + s(4.5f), s(8) + 1, s(1));
+    }
+
+    public void drawWallV(GraphicsContext g, Vector2i tile) {
+        double x = tile.x() * s(8), y = tile.y() * s(8);
+        g.setFill(wallStrokeColor);
+        // add 1 pixel to avoid gaps
+        g.fillRect(x + s(3.5f), y, s(1), s(8) + 1);
+    }
+
+    public void drawDWallV(GraphicsContext g, Vector2i tile) {
+        double x = tile.x() * s(8), y = tile.y() * s(8);
+        g.setFill(wallStrokeColor);
+        // add 1 pixel to avoid gaps
+        g.fillRect(x + s(2.5f), y, s(1), s(8) + 1);
+        g.fillRect(x + s(4.5f), y, s(1), s(8) + 1);
+    }
+
+    public void drawCorner(GraphicsContext g, Vector2i tile, byte cornerType) {
+        double x = tile.x() * s(8), y = tile.y() * s(8);
+        g.setStroke(wallStrokeColor);
+        g.setLineWidth(s(1));
+        switch (cornerType) {
+            case Tiles.CORNER_NW -> g.strokeArc(x + s(4), y + s(4), s(8), s(8), 90, 90,  ArcType.OPEN);
+            case Tiles.CORNER_NE -> g.strokeArc(x - s(4), y + s(4), s(8), s(8), 0, 90,   ArcType.OPEN);
+            case Tiles.CORNER_SE -> g.strokeArc(x - s(4), y - s(4), s(8), s(8), 270, 90, ArcType.OPEN);
+            case Tiles.CORNER_SW -> g.strokeArc(x + s(4), y - s(4), s(8), s(8), 180, 90, ArcType.OPEN);
+            default -> {}
+        }
+    }
+
+    public void drawDCorner(GraphicsContext g, Vector2i tile, byte cornerType) {
+        double x = tile.x() * s(8), y = tile.y() * s(8);
+        double s10 = 10 * scaling;
+        g.setStroke(wallStrokeColor);
+        g.setLineWidth(s(1));
+        switch (cornerType) {
+            case Tiles.DCORNER_NW -> {
+                g.strokeArc(x + s(5), y + s(5), s(6), s(6), 90, 90, ArcType.OPEN);
+                g.strokeArc(x + s(3), y + s(3), s10, s10, 90, 90, ArcType.OPEN);
+            }
+            case Tiles.DCORNER_NE -> {
+                g.strokeArc(x - s(3), y + s(5), s(6), s(6), 0, 90, ArcType.OPEN);
+                g.strokeArc(x - s(5), y + s(3), s10, s10, 0, 90, ArcType.OPEN);
+            }
+            case Tiles.DCORNER_SE -> {
+                g.strokeArc(x - s(3), y - s(3), s(6), s(6), 270, 90, ArcType.OPEN);
+                g.strokeArc(x - s(5), y - s(5), s10, s10, 270, 90, ArcType.OPEN);
+            }
+            case Tiles.DCORNER_SW -> {
+                g.strokeArc(x + s(5), y - s(3), s(6), s(6), 180, 90, ArcType.OPEN);
+                g.strokeArc(x + s(3), y - s(5), s10, s10, 180, 90, ArcType.OPEN);
+            }
+            default -> {}
+        }
     }
 
     public void drawDoor(GraphicsContext g, Vector2i tile, Color color) {
@@ -138,10 +215,19 @@ public class TerrainMapRenderer implements TileMapRenderer {
         double r = s(TILE_SIZE / 2f);
         g.beginPath();
         for (int i = 0; i < path.size(); ++i) {
-            Vector2i tile = path.get(i);
+            Vector2i tile = path.get(i), prevTile = (i == 0) ? null : path.get(i - 1);
             Point2D p = center(tile);
             double x = p.getX(), y = p.getY();
-            Vector2i prevTile = i == 0 ? null : path.get(i - 1);
+
+            //TODO how to avoid these?
+            if (i == 0 && tile.x() == 0 && map.get(tile) == Tiles.DWALL_H) {
+                g.moveTo(0, y);
+                g.lineTo(r, y);
+            }
+            if (i == path.size() - 1 && tile.x() == 0 && map.get(tile) == Tiles.DWALL_H) {
+                g.lineTo(0, y);
+            }
+
             switch (map.get(tile)) {
                 case Tiles.WALL_H, Tiles.DWALL_H -> g.lineTo(x + r, y);
                 case Tiles.WALL_V, Tiles.DWALL_V -> g.lineTo(x, y + r);
