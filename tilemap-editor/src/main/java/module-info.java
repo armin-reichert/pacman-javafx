b3ac5e828f81a88c429201a4da49b1e34ccc9b6d
Armin Reichert <armin.reichert@web.de>
2024-05-24 14:14:57 +0200

    Gradle bs

---
+++ tilemap-editor/src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module de.amr.games.pacman.tilemapeditor {
+    requires javafx.controls;
+    requires org.tinylog.api;
+    requires de.amr.games.pacman;
+    requires de.amr.games.pacman.lib;
+    requires de.amr.games.pacman.ui.fx;
+    exports de.amr.games.pacman.tilemapeditor;
+}
