9dd8b12422e77eae9e7d12082b9741db13c1436a
Armin Reichert <armin.reichert@web.de>
2024-05-24 14:43:29 +0200

    Cleanup

--- pacman-ui-fx-3d/src/main/java/de/amr/games/pacman/ui/fx/v3d/objimport/ObjImporter.java
+++ pacman-ui-fx-3d/src/main/java/de/amr/games/pacman/ui/fx/v3d/objimport/ObjImporter.java
@@ -45,10 +45,8 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.net.MalformedURLException;
 import java.net.URI;
 import java.net.URISyntaxException;
-import java.net.URL;
 import java.util.*;
 
 
