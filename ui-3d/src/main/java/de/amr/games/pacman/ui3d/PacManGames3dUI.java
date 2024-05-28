7c1847bad2eeda372b70bbd4735222ffaf718c66
Armin Reichert <armin.reichert@web.de>
2024-05-28 17:03:40 +0200

    Refac

--- ui-3d/src/main/java/de/amr/games/pacman/ui3d/PacManGames3dUI.java
+++ ui-3d/src/main/java/de/amr/games/pacman/ui3d/PacManGames3dUI.java
@@ -225,19 +225,19 @@ public class PacManGames3dUI extends PacManGames2dUI implements ActionHandler3D 
         // preload maps
         editor.addPredefinedMap(
             "Pac-Man",
-            GameModel.loadMap("/de/amr/games/pacman/maps/pacman.world", PacManGame.class));
+            GameModel.loadMap("/de/amr/games/pacman/maps/pacman.world", GameModel.class));
 
         editor.menuLoadMap().getItems().add(new SeparatorMenuItem());
 
-        rangeClosed(1, 6).forEach(i -> editor.addPredefinedMap(
-            "Ms. Pac-Man " + i,
-            GameModel.loadMap("/de/amr/games/pacman/maps/mspacman/mspacman_" + i + ".world", MsPacManGame.class)));
+        rangeClosed(1, 6).forEach(mapNumber -> editor.addPredefinedMap(
+            "Ms. Pac-Man " + mapNumber,
+            GameModel.loadMap("/de/amr/games/pacman/maps/mspacman/mspacman_" + mapNumber + ".world", GameModel.class)));
 
         editor.menuLoadMap().getItems().add(new SeparatorMenuItem());
 
-        rangeClosed(1, 8).forEach(i -> editor.addPredefinedMap(
-            "Pac-Man XXL " + i,
-            GameModel.loadMap("/de/amr/games/pacman/maps/masonic/masonic_" + i + ".world", PacManXXLGame.class))
+        rangeClosed(1, 8).forEach(mapNumber -> editor.addPredefinedMap(
+            "Pac-Man XXL " + mapNumber,
+            GameModel.loadMap("/de/amr/games/pacman/maps/masonic/masonic_" + mapNumber + ".world", GameModel.class))
         );
     }
 
