2c05602061e44d74a985a09133b417e866df1f9b
Armin Reichert <armin.reichert@web.de>
2024-06-03 10:55:44 +0200

    InfoBox

--- ui-3d/src/main/java/de/amr/games/pacman/ui3d/dashboard/InfoBoxGeneral.java
+++ ui-3d/src/main/java/de/amr/games/pacman/ui3d/dashboard/InfoBoxGeneral.java
@@ -11,7 +11,7 @@ import de.amr.games.pacman.ui3d.PacManGames3dUI;
 import javafx.scene.control.*;
 import javafx.scene.image.ImageView;
 
-import static de.amr.games.pacman.ui2d.PacManGames2dUI.PY_CANVAS_DECORATION;
+import static de.amr.games.pacman.ui2d.PacManGames2dUI.PY_CANVAS_DECORATED;
 import static de.amr.games.pacman.ui2d.PacManGames2dUI.PY_SHOW_DEBUG_INFO;
 
 /**
@@ -88,7 +88,7 @@ public class InfoBoxGeneral extends InfoBox {
         sliderTargetFPS.valueProperty().addListener(
             (py, ov, nv) -> sceneContext.gameClock().setTargetFrameRate(nv.intValue()));
         cbUsePlayScene3D.setOnAction(e -> actionHandler().toggle2D3D());
-        cbCanvasDecoration.selectedProperty().bindBidirectional(PY_CANVAS_DECORATION);
+        cbCanvasDecoration.selectedProperty().bindBidirectional(PY_CANVAS_DECORATED);
         cbDebugUI.setOnAction(e -> Ufx.toggle(PY_SHOW_DEBUG_INFO));
         cbTimeMeasured.setOnAction(e -> Ufx.toggle(sceneContext.gameClock().timeMeasuredPy));
     }
