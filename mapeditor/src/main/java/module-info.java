80be5098f6b09952bfb73ce74a5bfb458b947929
Armin Reichert <armin.reichert@web.de>
2024-06-06 14:27:36 +0200

    Dependencies

--- mapeditor/src/main/java/module-info.java
+++ mapeditor/src/main/java/module-info.java
@@ -3,7 +3,6 @@ open module de.amr.games.pacman.mapeditor {
     requires javafx.graphics;
     requires javafx.controls;
     requires org.tinylog.api;
-    requires de.amr.games.pacman;
     requires de.amr.games.pacman.lib;
 
     exports de.amr.games.pacman.mapeditor;
