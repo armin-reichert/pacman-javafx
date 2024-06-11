024b18043e87a266acc7422435bd6d5b78ef40e0
Armin Reichert <armin.reichert@web.de>
2024-06-11 10:35:03 +0200

    javadoc

--- ui-2d/src/main/java/de/amr/games/pacman/ui2d/rendering/SpriteGameRenderer.java
+++ ui-2d/src/main/java/de/amr/games/pacman/ui2d/rendering/SpriteGameRenderer.java
@@ -204,8 +204,14 @@ public class SpriteGameRenderer {
         }
     }
 
+    /**
+     * Draws a moving bonus entity at its current position (including jump offset).
+     * TODO reconsider this way of implementing the jumping bonus
+     *
+     * @param g     graphics context
+     * @param movingBonus moving bonus entity
+     */
     public void drawMovingBonus(GraphicsContext g, MovingBonus movingBonus) {
-        //TODO reconsider this way of implementing the jumping bonus
         g.save();
         g.translate(0, movingBonus.elongationY());
         if (movingBonus.state() == Bonus.STATE_EDIBLE) {
@@ -216,6 +222,12 @@ public class SpriteGameRenderer {
         g.restore();
     }
 
+    /**
+     * Draws a static bonus entity.
+     *
+     * @param g     graphics context
+     * @param bonus bonus containing entity
+     */
     public void drawStaticBonus(GraphicsContext g, Bonus bonus)
     {
         if (bonus.state() == Bonus.STATE_EDIBLE) {
@@ -225,10 +237,27 @@ public class SpriteGameRenderer {
         }
     }
 
+    /**
+     * Draws a sprite (section of the sprite sheet source) at the given (scaled) position.
+     *
+     * @param g             graphics context
+     * @param sprite        the sprite sheet section to draw
+     * @param x             scaled x-coordinate
+     * @param y             scaled y-coordinate
+     */
     public void drawSprite(GraphicsContext g, Rectangle2D sprite, double x, double y) {
         drawSubImage(g,  spriteSheet.source(), sprite, x, y);
     }
 
+    /**
+     * Draws a section of an image at the given (scaled) position.
+     *
+     * @param g             graphics context
+     * @param sourceImage   the source image e.g. a spritesheet source
+     * @param sprite        the sprite sheet section to draw
+     * @param x             scaled x-coordinate
+     * @param y             scaled y-coordinate
+     */
     public void drawSubImage(GraphicsContext g, Image sourceImage, Rectangle2D sprite, double x, double y) {
         if (sprite != null) {
             g.drawImage(sourceImage,
@@ -240,9 +269,10 @@ public class SpriteGameRenderer {
     /**
      * Draws a sprite using the current scene scaling.
      *
-     * @param sprite sprite sheet region ("sprite")
-     * @param x      UNSCALED x position
-     * @param y      UNSCALED y position
+     * @param g         graphics context
+     * @param sprite    sprite sheet region ("sprite")
+     * @param x         UNSCALED x position
+     * @param y         UNSCALED y position
      */
     public void drawSpriteScaled(GraphicsContext g, Rectangle2D sprite, double x, double y) {
         if (sprite != null) {
@@ -257,9 +287,10 @@ public class SpriteGameRenderer {
      * left-upper corner of the bounding box. Note that the sprites for Pac-Man and the ghosts are 16 pixels wide but the
      * bounding box is only 8 pixels (one square tile) wide.
      *
-     * @param sprite sprite sheet region (can be null)
-     * @param x      x coordinate of left-upper corner of bounding box
-     * @param y      y coordinate of left-upper corner of bounding box
+     * @param g         graphics context
+     * @param sprite    sprite sheet region (can be null)
+     * @param x         x-coordinate of left-upper corner of bounding box
+     * @param y         y-coordinate of left-upper corner of bounding box
      */
     public void drawSpriteCenteredOverBox(GraphicsContext g, Rectangle2D sprite, double x, double y) {
         drawSpriteScaled(g, sprite, x + HTS - 0.5 * sprite.getWidth(), y + HTS - 0.5 * sprite.getHeight());
@@ -268,8 +299,9 @@ public class SpriteGameRenderer {
     /**
      * Draws the sprite over the bounding box of the given entity (if visible).
      *
-     * @param entity an entity like Pac-Man or a ghost
-     * @param sprite the sprite
+     * @param g         graphics context
+     * @param entity    an entity like Pac-Man or a ghost
+     * @param sprite    sprite sheet region (can be null)
      */
     public void drawEntitySprite(GraphicsContext g, Entity entity, Rectangle2D sprite) {
         if (entity.isVisible()) {
@@ -278,18 +310,29 @@ public class SpriteGameRenderer {
     }
 
     /**
-     * Draws the given image scaled into this scene.
+     * Draws the given image scaled by the current scaling value.
      *
-     * @param image  image
-     * @param x      unscaled x
-     * @param y      unscaled y
-     * @param width  unscaled width
-     * @param height unscaled height
+     * @param g         graphics context
+     * @param image     image
+     * @param x         unscaled x-coordinate
+     * @param y         unscaled y-coordinate
+     * @param width     unscaled width
+     * @param height    unscaled height
      */
     public void drawImageScaled(GraphicsContext g, Image image, double x, double y, double width, double height) {
         g.drawImage(image, s(x), s(y), s(width), s(height));
     }
 
+    /**
+     * Draws a text with the given style at the given (unscaled) position.
+     *
+     * @param g     graphics context
+     * @param text  text
+     * @param color text color
+     * @param font  text font
+     * @param x     unscaled x-position
+     * @param y     unscaled y-position (baseline)
+     */
     public void drawText(GraphicsContext g, String text, Color color, Font font, double x, double y) {
         g.setFont(font);
         g.setFill(color);
