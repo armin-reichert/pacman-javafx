4b07ee216da28c3d12fb52c30f2dfb3a5f444d5a
Armin Reichert <armin.reichert@web.de>
2024-06-30 17:17:12 +0200

    Refac

--- core/src/main/java/de/amr/games/pacman/model/PacManXXLGame.java
+++ core/src/main/java/de/amr/games/pacman/model/PacManXXLGame.java
@@ -17,7 +17,8 @@ import static de.amr.games.pacman.lib.Globals.*;
 import static de.amr.games.pacman.model.GameModel.checkLevelNumber;
 
 /**
- * Extension of Arcade Pac-Man with 8 additional mazes (thanks to the one and only Sean Williams!).
+ * Extension of Arcade Pac-Man with 8 additional mazes (thanks to the one and only
+ * <a href="https://github.com/masonicGIT/pacman">Sean Williams</a>).
  */
 public class PacManXXLGame extends PacManGame {
 
@@ -33,27 +34,22 @@ public class PacManXXLGame extends PacManGame {
     }
 
     @Override
-    public void reset() {
-        super.reset();
-    }
-
-    @Override
     public void buildRegularLevel(int levelNumber) {
         this.levelNumber = checkLevelNumber(levelNumber);
         var customMaps = GameController.it().getCustomMaps();
         var world = switch (levelNumber) {
-            case 1 -> customMaps.isEmpty() ? createPacManWorld() : createModernWorld(customMaps.get(0));
+            case 1 -> customMaps.isEmpty() ? createPacManWorld() : createWorld(customMaps.get(0));
             case 2, 3, 4, 5, 6, 7, 8, 9 -> {
                 int mapNumber = levelNumber - 1;
                 URL mapURL = getClass().getResource(String.format("/de/amr/games/pacman/maps/masonic/masonic_%d.world", mapNumber));
                 var map = new WorldMap(mapURL);
-                yield createModernWorld(map);
+                yield createWorld(map);
             }
             default -> {
                 int mapNumber = randomInt(1, 9);
                 URL mapURL = getClass().getResource(String.format("/de/amr/games/pacman/maps/masonic/masonic_%d.world", mapNumber));
                 var map = new WorldMap(mapURL);
-                yield createModernWorld(map);
+                yield createWorld(map);
             }
         };
         setWorldAndCreatePopulation(world);
@@ -67,15 +63,15 @@ public class PacManXXLGame extends PacManGame {
         return 0;
     }
 
-    private World createModernWorld(WorldMap map) {
-        var modernWorld = new World(map);
+    private World createWorld(WorldMap map) {
+        var world = new World(map);
         int topLeftX = map.numCols() / 2 - 4;
         int topLeftY = map.numRows() / 2 - 3;
         House house = House.createArcadeHouse(v2i(topLeftX, topLeftY));
         house.setPacPositionFromMap(map);
         house.setGhostPositionsFromMap(map);
-        modernWorld.addHouse(house);
-        modernWorld.setBonusPosition(halfTileRightOf(13, 20)); // TODO get position from map?
-        return modernWorld;
+        world.addHouse(house);
+        world.setBonusPosition(halfTileRightOf(13, 20)); // TODO get position from map?
+        return world;
     }
 }
