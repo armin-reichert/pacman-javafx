de5a9c00183f6802a57d4f71b8de0a9c8dbf156c
Armin Reichert <armin.reichert@web.de>
2024-07-01 08:08:32 +0200

    Door

--- ui-3d/src/main/java/de/amr/games/pacman/ui3d/entity/Door3D.java
+++ ui-3d/src/main/java/de/amr/games/pacman/ui3d/entity/Door3D.java
@@ -12,10 +12,14 @@ import javafx.beans.property.ObjectProperty;
 import javafx.beans.property.SimpleObjectProperty;
 import javafx.scene.Group;
 import javafx.scene.paint.Color;
+import javafx.scene.paint.Material;
+import javafx.scene.paint.PhongMaterial;
+import javafx.scene.shape.Cylinder;
 import javafx.scene.shape.DrawMode;
+import javafx.scene.transform.Rotate;
 import javafx.util.Duration;
 
-import static de.amr.games.pacman.ui3d.PacManGames3dUI.PY_3D_DRAW_MODE;
+import static de.amr.games.pacman.lib.Globals.TS;
 
 /**
  * @author Armin Reichert
@@ -33,14 +37,12 @@ public class Door3D extends Group {
         this.animatedColorPy.set(normalColor);
         this.fadedOutColorPy.bind(fadedOutColorPy.map(color -> Ufx.opaqueColor(color, 0.1)));
 
-        var leftWing = new DoorWing3D(leftWingTile);
-        leftWing.colorPy.bind(animatedColorPy);
-        leftWing.drawModePy.bind(drawModePy);
+        var material = new PhongMaterial();
+        material.diffuseColorProperty().bind(animatedColorPy);
+        material.specularColorProperty().bind(material.diffuseColorProperty().map(Color::brighter));
 
-        var rightWing = new DoorWing3D(rightWingTile);
-        rightWing.colorPy.bind(animatedColorPy);
-        rightWing.drawModePy.bind(PY_3D_DRAW_MODE);
-
+        var leftWing = createDoorWing(leftWingTile, material, drawModePy);
+        var rightWing = createDoorWing(rightWingTile, material, drawModePy);
         getChildren().addAll(leftWing, rightWing);
     }
 
@@ -53,5 +55,35 @@ public class Door3D extends Group {
         );
         fadeIn.setDelay(Duration.seconds(0.25));
         new SequentialTransition(fadeOut, fadeIn).play();
+    }
+
+    private static Group createDoorWing(Vector2i tile, Material material, ObjectProperty<DrawMode> drawModePy) {
+        var group = new Group();
+
+        group.setTranslateX(tile.x() * TS);
+        group.setTranslateY(tile.y() * TS);
+
+        for (int i = 0; i < 2; ++i) {
+            var verticalBar = new Cylinder(0.75, GameLevel3D.HOUSE_HEIGHT);
+            verticalBar.setMaterial(material);
+            verticalBar.setTranslateX(i * 4 + 2);
+            verticalBar.setTranslateY(4);
+            verticalBar.setTranslateZ(-4);
+            verticalBar.setRotationAxis(Rotate.X_AXIS);
+            verticalBar.setRotate(90);
+            verticalBar.drawModeProperty().bind(drawModePy);
+            group.getChildren().add(verticalBar);
+        }
+
+        var horizontalBar = new Cylinder(0.5, 14);
+        horizontalBar.setMaterial(material);
+        horizontalBar.setTranslateX(4);
+        horizontalBar.setTranslateY(4);
+        horizontalBar.setTranslateZ(0.25 - GameLevel3D.HOUSE_HEIGHT * 0.5);
+        horizontalBar.setRotationAxis(Rotate.Z_AXIS);
+        horizontalBar.setRotate(90);
+        group.getChildren().add(horizontalBar);
+
+        return group;
     }
 }
