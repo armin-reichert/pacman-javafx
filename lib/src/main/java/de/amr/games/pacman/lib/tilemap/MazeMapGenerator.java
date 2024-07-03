bdfe0cbb98dda5e320a39a912f701b9d6728fb0a
Armin Reichert <armin.reichert@web.de>
2024-07-03 12:41:20 +0200

    Maze map experiment

--- lib/src/main/java/de/amr/games/pacman/lib/tilemap/MazeMapGenerator.java
+++ lib/src/main/java/de/amr/games/pacman/lib/tilemap/MazeMapGenerator.java
@@ -9,19 +9,14 @@ import de.amr.games.pacman.lib.graph.GridGraphImpl;
 import org.tinylog.Logger;
 
 import java.io.File;
-import java.util.ArrayList;
-import java.util.BitSet;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 import java.util.stream.IntStream;
 
 public class MazeMapGenerator {
 
     public static void createMazeByWilsonAlgorithm(GridGraph grid) {
         List<Integer> vertices = new ArrayList<>(IntStream.range(0, grid.numVertices()).boxed().toList());
-        Logger.info(vertices);
         Collections.shuffle(vertices);
-        Logger.info(vertices);
         BitSet inTree = new BitSet();
         inTree.set(vertices.get(0));
         DirMap lastWalkDir = new DirMap();
@@ -54,13 +49,60 @@ public class MazeMapGenerator {
         }
     }
 
+    // ---
+
+    public static void createMazeByRecursiveDivision(GridGraph grid) {
+        for (int row = 0; row < grid.numRows(); ++row) {
+            for (int col = 0; col < grid.numCols(); ++col) {
+                int vertex = grid.vertex(row, col);
+                if (row > 0) {
+                    grid.connect(vertex, Dir.N);
+                }
+                if (col > 0) {
+                    grid.connect(vertex, Dir.W);
+                }
+            }
+        }
+        divide(grid, new Random(), 0, 0, grid.numCols(), grid.numRows());
+    }
+
+    private static void divide(GridGraph grid, Random rnd, int x0, int y0, int w, int h) {
+        if (w <= 1 && h <= 1) {
+            return;
+        }
+        if (w < h || (w == h && rnd.nextBoolean())) {
+            // Build "horizontal wall" at random y from [y0 + 1, y0 + h - 1], keep random door
+            int y = y0 + 1 + rnd.nextInt(h - 1);
+            int door = x0 + rnd.nextInt(w);
+            for (int x = x0; x < x0 + w; ++x) {
+                if (x != door) {
+                    grid.disconnect(grid.vertex(y - 1, x), Dir.S);
+                }
+            }
+            divide(grid, rnd, x0, y0, w, y - y0);
+            divide(grid, rnd, x0, y, w, h - (y - y0));
+        } else {
+            // Build "vertical wall" at random x from [x0 + 1, x0 + w - 1], keep random door
+            int x = x0 + 1 + rnd.nextInt(w - 1);
+            int door = y0 + rnd.nextInt(h);
+            for (int y = y0; y < y0 + h; ++y) {
+                if (y != door) {
+                    grid.disconnect(grid.vertex(y, x - 1), Dir.E);
+                }
+            }
+            divide(grid, rnd, x0, y0, x - x0, h);
+            divide(grid, rnd, x, y0, w - (x - x0), h);
+        }
+    }
+
+
     private static final byte FREE = Tiles.EMPTY;
     private static final byte BLOCKED = Tiles.TUNNEL;
     private static final int EMPTY_ROWS_ABOVE = 3, EMPTY_ROWS_BELOW = 2;
 
     public WorldMap createMazeMap(int numRows, int numCols) {
         GridGraphImpl grid = new GridGraphImpl(numRows, numCols);
-        createMazeByWilsonAlgorithm(grid);
+        createMazeByRecursiveDivision(grid);
         var worldMap = new WorldMap(3 * numRows + EMPTY_ROWS_ABOVE + EMPTY_ROWS_BELOW, 3 * numCols);
         var terrain = worldMap.terrain();
         for (int v = 0; v < grid.numVertices(); ++v) {
@@ -153,18 +195,22 @@ public class MazeMapGenerator {
             return;
         }
         map.set(row, col, value);
-        Logger.info("x={} y={}: {} move {}", col, row, value, moveDir);
+        Logger.debug("x={} y={}: {} move {}", col, row, value, moveDir);
     }
 
     public static void main(String[] args)  {
         MazeMapGenerator gen = new MazeMapGenerator();
-        WorldMap mazeMap = gen.createMazeMap(40, 20);
         //WorldMap mazeMap = new WorldMap(new File("maze.world"));
-        try {
-            gen.setContour(mazeMap);
-        } catch (Exception x) {
-            Logger.error(x);
+        for (int i = 0; i < 20; ++i) {
+            try {
+                WorldMap mazeMap = gen.createMazeMap(40, 20);
+                gen.setContour(mazeMap);
+                File file = new File("maze_%d.world".formatted(i));
+                mazeMap.save(file);
+                Logger.info("Maze map {} saved", file);
+            } catch (Exception x) {
+                Logger.error(x);
+            }
         }
-        mazeMap.save(new File("maze_refined.world"));
     }
 }
