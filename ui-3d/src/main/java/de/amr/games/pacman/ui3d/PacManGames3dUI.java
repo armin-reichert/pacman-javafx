f10cd1039065ba1d600591e6f3f3b97dd978dbc2
Armin Reichert <armin.reichert@web.de>
2024-07-07 07:47:49 +0200

    Floor color

--- ui-3d/src/main/java/de/amr/games/pacman/ui3d/PacManGames3dUI.java
+++ ui-3d/src/main/java/de/amr/games/pacman/ui3d/PacManGames3dUI.java
@@ -59,7 +59,7 @@ public class PacManGames3dUI extends PacManGames2dUI {
     public static final ObjectProperty<DrawMode>    PY_3D_DRAW_MODE          = new SimpleObjectProperty<>(DrawMode.FILL);
     public static final BooleanProperty             PY_3D_ENABLED            = new SimpleBooleanProperty(true);
     public static final BooleanProperty             PY_3D_ENERGIZER_EXPLODES = new SimpleBooleanProperty(true);
-    public static final ObjectProperty<Color>       PY_3D_FLOOR_COLOR        = new SimpleObjectProperty<>(Color.grayRgb(33));
+    public static final ObjectProperty<Color>       PY_3D_FLOOR_COLOR        = new SimpleObjectProperty<>(Color.web("#333"));
     public static final StringProperty              PY_3D_FLOOR_TEXTURE      = new SimpleStringProperty("knobs");
     public static final ObjectProperty<Color>       PY_3D_LIGHT_COLOR        = new SimpleObjectProperty<>(Color.GHOSTWHITE);
     public static final BooleanProperty             PY_3D_NIGHT_MODE         = new SimpleBooleanProperty(false);
