3fdb4b997468af5ed0d679a25b5edc786e0c4602
Armin Reichert <armin.reichert@web.de>
2024-07-13 09:21:10 +0200

    Change maps used in Pac-Man XXL

--- core/src/main/java/de/amr/games/pacman/model/PacManXXLGame.java
+++ core/src/main/java/de/amr/games/pacman/model/PacManXXLGame.java
@@ -4,6 +4,7 @@ See file LICENSE in repository root directory for details.
 */
 package de.amr.games.pacman.model;
 
+import de.amr.games.pacman.lib.Globals;
 import de.amr.games.pacman.lib.tilemap.WorldMap;
 import de.amr.games.pacman.model.world.House;
 import de.amr.games.pacman.model.world.World;
@@ -11,6 +12,8 @@ import de.amr.games.pacman.steering.RuleBasedPacSteering;
 
 import java.io.File;
 import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
 
 import static de.amr.games.pacman.lib.Globals.*;
 import static de.amr.games.pacman.model.GameModel.checkLevelNumber;
@@ -21,10 +24,17 @@ import static de.amr.games.pacman.model.GameModel.checkLevelNumber;
  */
 public class PacManXXLGame extends PacManGame {
 
+    private WorldMap[] worldMapByLevelNumber = new WorldMap[8];
+
     @Override
     public void init() {
         initialLives = 3;
         highScoreFile = new File(GAME_DIR,"highscore-pacman_xxl.xml");
+        String mapPath = "/de/amr/games/pacman/maps/masonic/";
+        for (int number = 1; number <= 8; ++number) {
+            URL url = getClass().getResource(mapPath + "masonic_%d.world".formatted(number));
+            worldMapByLevelNumber[number-1] = new WorldMap(url);
+        }
     }
 
     @Override
@@ -35,33 +45,11 @@ public class PacManXXLGame extends PacManGame {
     @Override
     public void buildRegularLevel(int levelNumber) {
         this.levelNumber = checkLevelNumber(levelNumber);
-        var customMaps = controller().getCustomMaps();
-        if (customMaps.isEmpty()) {
-            var world = switch (levelNumber) {
-                case 1 -> createPacManWorld();
-                case 2, 3, 4, 5, 6, 7, 8, 9 -> {
-                    int mapNumber = levelNumber - 1;
-                    URL mapURL = getClass().getResource("/de/amr/games/pacman/maps/masonic/masonic_%d.world".formatted(mapNumber));
-                    var map = new WorldMap(mapURL);
-                    yield createWorld(map);
-                }
-                default -> {
-                    int mapNumber = randomInt(1, 9);
-                    URL mapURL = getClass().getResource("/de/amr/games/pacman/maps/masonic/masonic_%d.world".formatted(mapNumber));
-                    var map = new WorldMap(mapURL);
-                    yield createWorld(map);
-                }
-            };
-            setWorldAndCreatePopulation(world);
+        if (levelNumber <= worldMapByLevelNumber.length) {
+            setWorldAndCreatePopulation(createWorld(worldMapByLevelNumber[levelNumber-1]));
         } else {
-            if (levelNumber <= customMaps.size()) {
-                world = createWorld(customMaps.get(levelNumber - 1));
-            } else {
-                int mapNumber = randomInt(1, 9);
-                URL mapURL = getClass().getResource("/de/amr/games/pacman/maps/masonic/masonic_%d.world".formatted(mapNumber));
-                world = createWorld(new WorldMap(mapURL));
-            }
-            setWorldAndCreatePopulation(world);
+            int randomIndex = Globals.randomInt(0, 8);
+            setWorldAndCreatePopulation(createWorld(worldMapByLevelNumber[randomIndex]));
         }
         pac.setName("Pac-Man");
         pac.setAutopilot(new RuleBasedPacSteering(this));
