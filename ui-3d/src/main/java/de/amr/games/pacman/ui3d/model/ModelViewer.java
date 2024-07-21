106e3ad316b8f7d58e57d777dc7ef6dc757c3ed1
Armin Reichert <armin.reichert@web.de>
2024-07-21 10:06:35 +0200

    Model viewer

--- ui-3d/src/main/java/de/amr/games/pacman/ui3d/model/ModelViewer.java
+++ ui-3d/src/main/java/de/amr/games/pacman/ui3d/model/ModelViewer.java
@@ -2,13 +2,14 @@ package de.amr.games.pacman.ui3d.model;
 
 import de.amr.games.pacman.ui3d.entity.PacModel3D;
 import javafx.application.Application;
+import javafx.geometry.Point3D;
 import javafx.scene.*;
 import javafx.scene.control.Menu;
 import javafx.scene.control.MenuBar;
 import javafx.scene.control.MenuItem;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.paint.Color;
-import javafx.scene.transform.Rotate;
+import javafx.scene.shape.MeshView;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 import org.tinylog.Logger;
@@ -20,13 +21,12 @@ public class ModelViewer extends Application {
     static final String DIR = "C:\\Users\\armin\\git\\pacman-javafx\\ui-3d\\src\\main\\resources\\de\\amr\\games\\pacman\\ui3d\\model3D";
 
     private Stage stage;
-    private Scene scene;
     private SubScene previewArea;
     private PerspectiveCamera cam;
-    private BorderPane layoutPane = new BorderPane();
+    private final BorderPane layoutPane = new BorderPane();
     private FileChooser fileChooser;
-    private Model3D model3D;
-    private Group pacMan3D;
+    private double lastX = -1;
+    private double lastY = -1;
 
     @Override
     public void start(Stage stage) throws Exception {
@@ -37,7 +37,7 @@ public class ModelViewer extends Application {
         fileChooser.getExtensionFilters().addAll(objFilter, allFilter);
         fileChooser.setSelectedExtensionFilter(objFilter);
         fileChooser.setInitialDirectory(new File(DIR));
-        scene = new Scene(createSceneContent(), 800, 600);
+        Scene scene = new Scene(createSceneContent(), 800, 600);
         cam = new PerspectiveCamera(true);
         cam.setTranslateZ(-100);
         //cam.setTranslateY(-100);
@@ -61,8 +61,26 @@ public class ModelViewer extends Application {
 
         MenuBar menuBar = new MenuBar(fileMenu);
         layoutPane.setTop(menuBar);
+
         previewArea = new SubScene(new Group(), 800, 600, true, SceneAntialiasing.BALANCED);
         previewArea.setFill(Color.BLACK);
+        previewArea.setOnMouseClicked(e -> Logger.info(e));
+        previewArea.setOnMouseDragged(e -> {
+            if (e.getTarget() instanceof MeshView) {
+                if (lastX != -1) {
+                    double dx = e.getX() - lastX;
+                    double dy = e.getY() - lastY;
+                    Point3D axis = new Point3D(0, 0, cam.getTranslateZ());
+                    cam.setRotationAxis(axis);
+                    cam.setRotate(cam.getRotate() + dx);
+                    Logger.info("dx {0.00} dy {0.00}", dx, dy);
+                    Logger.info("Cam rotate: {}", cam.getRotate());
+                }
+                lastX = e.getX();
+                lastY = e.getY();
+            }
+        });
+
         layoutPane.setCenter(previewArea);
         return layoutPane;
     }
@@ -71,8 +89,8 @@ public class ModelViewer extends Application {
         File selectedFile = fileChooser.showOpenDialog(stage);
         if (selectedFile != null) {
             Logger.info("File {} selected", selectedFile);
-            model3D = new Model3D(selectedFile);
-            pacMan3D = PacModel3D.createPacShape(model3D, 16, Color.YELLOW, Color.BLACK, Color.PINK);
+            Model3D model3D = new Model3D(selectedFile);
+            Group pacMan3D = PacModel3D.createPacShape(model3D, 16, Color.YELLOW, Color.BLACK, Color.PINK);
             Group root = (Group) previewArea.getRoot();
             root.getChildren().setAll(pacMan3D);
         }
