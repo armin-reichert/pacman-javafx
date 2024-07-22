e4a2da7f8bc9a149fa408d1775ee091a7e971620
Armin Reichert <armin.reichert@web.de>
2024-07-22 08:04:36 +0200

    Size

--- ui-3d/src/main/java/de/amr/games/pacman/ui3d/entity/GameLevel3D.java
+++ ui-3d/src/main/java/de/amr/games/pacman/ui3d/entity/GameLevel3D.java
@@ -60,8 +60,8 @@ public class GameLevel3D extends Group {
     static final float HOUSE_HEIGHT          = 12.0f;
     static final float HOUSE_OPACITY         = 0.4f;
     static final float HOUSE_SENSITIVITY     = 1.5f * TS;
-    static final float PAC_SIZE              = 13.5f;
-    static final float GHOST_SIZE            = 12.5f;
+    static final float PAC_SIZE              = 14.0f;
+    static final float GHOST_SIZE            = 13f;
     static final float ENERGIZER_RADIUS      = 3.5f;
     static final float PELLET_RADIUS         = 1.0f;
 
