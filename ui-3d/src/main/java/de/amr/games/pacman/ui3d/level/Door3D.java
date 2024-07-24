e031bd430acf1c8508270e84173778ed14f1dc48
Armin Reichert <armin.reichert@web.de>
2024-07-24 09:07:20 +0200

    Refac

--- ui-3d/src/main/java/de/amr/games/pacman/ui3d/level/Door3D.java
+++ ui-3d/src/main/java/de/amr/games/pacman/ui3d/level/Door3D.java
@@ -2,7 +2,7 @@
 Copyright (c) 2021-2024 Armin Reichert (MIT License)
 See file LICENSE in repository root directory for details.
 */
-package de.amr.games.pacman.ui3d.entity;
+package de.amr.games.pacman.ui3d.level;
 
 import de.amr.games.pacman.lib.Vector2i;
 import de.amr.games.pacman.ui2d.util.Ufx;
--- ui-3d/src/main/java/de/amr/games/pacman/ui3d/level/NumberCube3D.java
+++ ui-3d/src/main/java/de/amr/games/pacman/ui3d/level/NumberCube3D.java
@@ -2,7 +2,7 @@
 Copyright (c) 2021-2024 Armin Reichert (MIT License)
 See file LICENSE in repository root directory for details.
 */
-package de.amr.games.pacman.ui3d.entity;
+package de.amr.games.pacman.ui3d.level;
 
 import javafx.animation.Interpolator;
 import javafx.animation.RotateTransition;
