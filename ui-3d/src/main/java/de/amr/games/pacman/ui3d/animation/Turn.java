e20ee014927b789fdfb869482708849b48a1cca7
Armin Reichert <armin.reichert@web.de>
2024-07-26 09:49:32 +0200

    Cleanup

--- ui-3d/src/main/java/de/amr/games/pacman/ui3d/animation/Turn.java
+++ ui-3d/src/main/java/de/amr/games/pacman/ui3d/animation/Turn.java
@@ -11,33 +11,20 @@ import de.amr.games.pacman.lib.Direction;
  */
 public class Turn {
 
-    public record Angles(double from, double to) {
-    }
+    private static final byte L = 0, U = 1, R = 2, D = 3;
 
-    private static final byte L = 0;
-    private static final byte U = 1;
-    private static final byte R = 2;
-    private static final byte D = 3;
-
-    //@formatter:off
-	private static final byte[][][] TURNS = {
-		{ null,    {L, R}, {L, U},  {L, -U} }, // LEFT  -> *
-		{ {R, L},  null,   {R, U},  {R, D}  }, // RIGHT -> *
-		{ {U, L},  {U, R}, null,    {U, D}  }, // UP    -> *
-		{ {-U, L}, {D, R}, {-U, U}, null    }, // DOWN  -> *
-	};
+    public record Angles(double from, double to) {}
 
 	private static byte dirIndex(Direction dir) {
-		return switch (dir) {	case LEFT -> L;	case RIGHT -> R; case UP -> U; case DOWN -> D; default -> L; };
+		return switch (dir) {
+            case LEFT  -> L;
+            case RIGHT -> R;
+            case UP    -> U;
+            case DOWN  -> D;
+        };
 	}
-	//@formatter:on
 
     public static double angle(Direction dir) {
         return dirIndex(dir) * 90;
-    }
-
-    public static Angles angles(Direction fromDir, Direction toDir) {
-        var turn = TURNS[fromDir.ordinal()][toDir.ordinal()];
-        return new Angles(turn[0] * 90, turn[1] * 90);
     }
 }
