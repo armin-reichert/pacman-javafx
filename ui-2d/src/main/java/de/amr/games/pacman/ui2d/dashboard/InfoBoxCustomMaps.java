ced27f3b0f9f5f238bf09c91fca24ff2e20f2c06
Armin Reichert <armin.reichert@web.de>
2024-08-07 08:58:43 +0200

    Use TableView

--- ui-2d/src/main/java/de/amr/games/pacman/ui2d/dashboard/InfoBoxCustomMaps.java
+++ ui-2d/src/main/java/de/amr/games/pacman/ui2d/dashboard/InfoBoxCustomMaps.java
@@ -7,41 +7,60 @@ package de.amr.games.pacman.ui2d.dashboard;
 import de.amr.games.pacman.lib.tilemap.WorldMap;
 import de.amr.games.pacman.ui2d.GameContext;
 import de.amr.games.pacman.ui2d.GameParameters;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 
 import java.io.File;
-
 
 /**
  * @author Armin Reichert
  */
 public class InfoBoxCustomMaps extends InfoBox {
 
+    record MapInfo(String fileName, int numRows, int numCols) {}
+
+    private TableView<MapInfo> mapTable;
+
     @Override
     public void init(GameContext context) {
         this.context = context;
-        reloadCustomMaps();
-    }
 
-    private void reloadCustomMaps() {
-        context.actionHandler().updateCustomMaps();
-        clearGrid();
-        var customMapsByFile = context.gameController().customMapsByFile();
-        if (customMapsByFile.isEmpty()) {
-            addTextRow("No custom maps found.", "");
-            return;
-        }
         var cbCustomMapEnabled = checkBox("Use Custom Maps", context.actionHandler()::updateCustomMaps);
         cbCustomMapEnabled.selectedProperty().bindBidirectional(GameParameters.PY_CUSTOM_MAPS_ENABLED);
 
         var btnReload = new Button("Reload");
-        btnReload.setOnAction(e -> reloadCustomMaps());
+        btnReload.setOnAction(e -> reloadCustomMapsAndUpdateTableView());
         addRow(cbCustomMapEnabled, btnReload);
 
-        for (File mapFile : customMapsByFile.keySet().stream().sorted().toList()) {
-            WorldMap map = customMapsByFile.get(mapFile);
-            String mapSize = "(%dx%d)".formatted(map.terrain().numRows(), map.terrain().numCols());
-            addTextRow(mapFile.getName(), mapSize);
+        mapTable = new TableView<>();
+        mapTable.setPrefHeight(200);
+        var col1 = new TableColumn<MapInfo, String>("Map File");
+        col1.setCellValueFactory(p -> new SimpleStringProperty(p.getValue().fileName));
+        var col2 = new TableColumn<MapInfo, Integer>("Rows");
+        col2.setCellValueFactory(p -> new SimpleIntegerProperty(p.getValue().numRows).asObject());
+        var col3 = new TableColumn<MapInfo, Integer>("Cols");
+        col3.setCellValueFactory(p -> new SimpleIntegerProperty(p.getValue().numCols).asObject());
+        mapTable.getColumns().add(col1);
+        mapTable.getColumns().add(col2);
+        mapTable.getColumns().add(col3);
+        addRow(mapTable);
+
+        reloadCustomMapsAndUpdateTableView();
+    }
+
+    private void reloadCustomMapsAndUpdateTableView() {
+        ObservableList<MapInfo> items = FXCollections.observableArrayList();
+        context.actionHandler().updateCustomMaps();
+        var maps = context.gameController().customMapsByFile();
+        for (File file : maps.keySet().stream().sorted().toList()) {
+            WorldMap map = maps.get(file);
+            items.add(new MapInfo(file.getName(), map.terrain().numRows(), map.terrain().numCols()));
         }
+        mapTable.setItems(items);
     }
 }
