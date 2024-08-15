97a82c961c6f25d4fd5d0d042539083aaee409f1
Armin Reichert <armin.reichert@web.de>
2024-08-15 12:31:25 +0200

    Refac

--- ui-2d/src/main/java/de/amr/games/pacman/ui2d/scene/CreditScene.java
+++ ui-2d/src/main/java/de/amr/games/pacman/ui2d/scene/CreditScene.java
@@ -8,6 +8,7 @@ import de.amr.games.pacman.event.GameEvent;
 import de.amr.games.pacman.ui2d.ActionHandler;
 import de.amr.games.pacman.ui2d.GameKey;
 import de.amr.games.pacman.ui2d.GameSounds;
+import javafx.geometry.Rectangle2D;
 
 import static de.amr.games.pacman.lib.Globals.t;
 
@@ -49,26 +50,27 @@ public class CreditScene extends GameScene2D {
     public void drawSceneContent() {
         var font8 = sceneFont(8);
         var font6 = sceneFont(6);
+        var cyan = context.assets().color("palette.cyan");
+        var orange = context.assets().color("palette.orange");
+        var rose = context.assets().color("palette.rose");
         switch (context.game().variant()) {
             case MS_PACMAN -> {
-                var color = context.assets().color("palette.orange");
-                spriteRenderer.drawText(g, "PUSH START BUTTON", color, font8, t(6), t(16));
-                spriteRenderer.drawText(g, "1 PLAYER ONLY", color, font8, t(8), t(18));
-                spriteRenderer.drawText(g, "ADDITIONAL    AT 10000", color, font8, t(2), t(25));
-                spriteRenderer.drawSpriteScaled(g, spriteRenderer.spriteSheet().livesCounterSprite(), t(13), t(23) + 1);
-                spriteRenderer.drawText(g, "PTS", color, font6, t(25), t(25));
+                Rectangle2D livesCounterSprite = spriteRenderer.spriteSheet().livesCounterSprite();
+                spriteRenderer.drawText(g, "PUSH START BUTTON", orange, font8, t(6), t(16));
+                spriteRenderer.drawText(g, "1 PLAYER ONLY", orange, font8, t(8), t(18));
+                spriteRenderer.drawText(g, "ADDITIONAL    AT 10000", orange, font8, t(2), t(25));
+                spriteRenderer.drawSpriteScaled(g, livesCounterSprite, t(13), t(23) + 1);
+                spriteRenderer.drawText(g, "PTS", orange, font6, t(25), t(25));
                 drawMsPacManCopyright(t(6), t(28));
-                drawLevelCounter(g);
             }
             case PACMAN, PACMAN_XXL -> {
-                spriteRenderer.drawText(g, "PUSH START BUTTON", context.assets().color("palette.orange"), font8, t(6), t(17));
-                spriteRenderer.drawText(g, "1 PLAYER ONLY", context.assets().color("palette.cyan"), font8, t(8), t(21));
-                spriteRenderer.drawText(g, "BONUS PAC-MAN FOR 10000", context.assets().color("palette.rose"), font8, t(1), t(25));
-                spriteRenderer.drawText(g, "PTS", context.assets().color("palette.rose"), font6, t(25), t(25));
+                spriteRenderer.drawText(g, "PUSH START BUTTON", orange, font8, t(6), t(17));
+                spriteRenderer.drawText(g, "1 PLAYER ONLY", cyan, font8, t(8), t(21));
+                spriteRenderer.drawText(g, "BONUS PAC-MAN FOR 10000", rose, font8, t(1), t(25));
+                spriteRenderer.drawText(g, "PTS", rose, font6, t(25), t(25));
                 drawMidwayCopyright(t(4), t(29));
-                drawLevelCounter(g);
             }
-
         }
+        drawLevelCounter(g);
     }
 }
