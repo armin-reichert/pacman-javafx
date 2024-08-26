306ba3afaaf2993cd1227862056dae1870bca7f0
Armin Reichert <armin.reichert@web.de>
2024-08-26 06:59:46 +0200

    fmt

--- pacman-ui-3d/src/main/java/de/amr/games/pacman/ui3d/level/PacShape3D.java
+++ pacman-ui-3d/src/main/java/de/amr/games/pacman/ui3d/level/PacShape3D.java
@@ -147,7 +147,7 @@ public class PacShape3D extends Group {
             new KeyValue(jaw.rotateProperty(), 0, Interpolator.LINEAR)
         };
         Timeline animation = new Timeline(
-            new KeyFrame(Duration.ZERO,           "Open on Start", open),
+            new KeyFrame(Duration.ZERO,        "Open on Start", open),
             new KeyFrame(Duration.millis(100), "Start Closing", open),
             new KeyFrame(Duration.millis(130), "Closed",        closed),
             new KeyFrame(Duration.millis(200), "Start Opening", closed),
