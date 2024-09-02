7f6873cef7338b41e4f04dd086b03df40c31183d
Armin Reichert <armin.reichert@web.de>
2024-09-02 06:17:46 +0200

    Refac

--- pacman-ui-2d/src/main/java/de/amr/games/pacman/ui2d/scene/PacManCutScene3.java
+++ pacman-ui-2d/src/main/java/de/amr/games/pacman/ui2d/scene/PacManCutScene3.java
@@ -6,13 +6,11 @@ package de.amr.games.pacman.ui2d.scene;
 
 import de.amr.games.pacman.controller.GameState;
 import de.amr.games.pacman.lib.Direction;
-import de.amr.games.pacman.model.actors.Animations;
 import de.amr.games.pacman.model.actors.Ghost;
 import de.amr.games.pacman.model.actors.Pac;
 import de.amr.games.pacman.ui2d.GameSounds;
 import de.amr.games.pacman.ui2d.rendering.PacManGameGhostAnimations;
 import de.amr.games.pacman.ui2d.rendering.PacManGamePacAnimations;
-import de.amr.games.pacman.ui2d.rendering.PacManGameSpriteSheet;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 
@@ -24,7 +22,8 @@ import static de.amr.games.pacman.lib.Globals.v2i;
  */
 public class PacManCutScene3 extends GameScene2D {
 
-    private int initialDelay;
+    static final short ANIMATION_START = 120;
+
     private int frame;
     private Pac pac;
     private Ghost blinky;
@@ -37,25 +36,15 @@ public class PacManCutScene3 extends GameScene2D {
     @Override
     public void init() {
         super.init();
-        frame = -1;
-        initialDelay = 120;
         context.setScoreVisible(true);
+
         pac = new Pac();
-        pac.setAnimations(new PacManGamePacAnimations(pac, (PacManGameSpriteSheet) spriteRenderer.spriteSheet()));
-        pac.selectAnimation(Pac.ANIM_MUNCHING);
-        pac.animations().ifPresent(Animations::startSelected);
-        pac.centerOverTile(v2i(29, 20));
-        pac.setMoveDir(Direction.LEFT);
-        pac.setSpeed(1.25f);
-        pac.show();
+        pac.setAnimations(new PacManGamePacAnimations(pac, spriteRenderer.spriteSheet()));
+
         blinky = Ghost.red();
-        blinky.setAnimations(new PacManGameGhostAnimations(blinky, (PacManGameSpriteSheet) spriteRenderer.spriteSheet()));
-        blinky.selectAnimation(Ghost.ANIM_BLINKY_PATCHED);
-        blinky.startAnimation();
-        blinky.centerOverTile(v2i(35, 20));
-        blinky.setMoveAndWishDir(Direction.LEFT);
-        blinky.setSpeed(1.25f);
-        blinky.show();
+        blinky.setAnimations(new PacManGameGhostAnimations(blinky, spriteRenderer.spriteSheet()));
+
+        frame = -1;
     }
 
     private void startMusic() {
@@ -67,28 +56,36 @@ public class PacManCutScene3 extends GameScene2D {
 
     @Override
     public void update() {
-        if (initialDelay > 0) {
-            --initialDelay;
-            if (initialDelay == 0) {
+        ++frame;
+        if (frame >= ANIMATION_START) {
+            pac.move();
+            blinky.move();
+        }
+        switch (frame) {
+            case ANIMATION_START -> {
                 startMusic();
+                pac.centerOverTile(v2i(29, 20));
+                pac.setMoveDir(Direction.LEFT);
+                pac.setSpeed(1.25f);
+                pac.show();
+                pac.selectAnimation(Pac.ANIM_MUNCHING);
+                pac.startAnimation();
+                blinky.centerOverTile(v2i(35, 20));
+                blinky.setMoveAndWishDir(Direction.LEFT);
+                blinky.setSpeed(1.25f);
+                blinky.show();
+                blinky.selectAnimation(Ghost.ANIM_BLINKY_PATCHED);
+                blinky.startAnimation();
             }
-            return;
-        }
-        if (context.gameState().timer().hasExpired()) {
-            return;
-        }
-        switch (++frame) {
-            case 400 -> {
+            case ANIMATION_START + 400 -> {
                 blinky.centerOverTile(v2i(-1, 20));
                 blinky.setMoveAndWishDir(Direction.RIGHT);
                 blinky.selectAnimation(Ghost.ANIM_BLINKY_NAKED);
                 blinky.startAnimation();
             }
-            case 700 -> context.gameState().timer().expire();
+            case ANIMATION_START + 700 -> context.gameState().timer().expire();
             default -> {}
         }
-        pac.move();
-        blinky.move();
     }
 
     @Override
@@ -101,7 +98,7 @@ public class PacManCutScene3 extends GameScene2D {
     @Override
     protected void drawSceneInfo() {
         drawTileGrid();
-        var text = initialDelay > 0 ? String.format("Wait %d", initialDelay) : String.format("Frame %d", frame);
+        var text = frame < ANIMATION_START ? String.format("Wait %d", ANIMATION_START - frame) : String.format("Frame %d", frame);
         spriteRenderer.drawText(g, text, Color.YELLOW, Font.font("Sans", 16), t(1), t(5));
     }
 }
