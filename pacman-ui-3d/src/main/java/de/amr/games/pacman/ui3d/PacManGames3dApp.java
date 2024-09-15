d304c81ed64c5a2469d2a909321b2f4855518dd2
Armin Reichert <armin.reichert@web.de>
2024-09-15 07:00:25 +0200

    Refac

--- pacman-ui-3d/src/main/java/de/amr/games/pacman/ui3d/PacManGames3dApp.java
+++ pacman-ui-3d/src/main/java/de/amr/games/pacman/ui3d/PacManGames3dApp.java
@@ -96,20 +96,20 @@ public class PacManGames3dApp extends Application {
         assets.store("ms_pacman.color.hairbow",         Color.rgb(255, 0, 0));
         assets.store("ms_pacman.color.hairbow.pearls",  Color.rgb(33, 33, 255));
 
-        assets.store("tengen.color.head",            Color.rgb(255, 255, 0));
-        assets.store("tengen.color.eyes",            Color.rgb(33, 33, 33));
-        assets.store("tengen.color.palate",          Color.rgb(240, 180, 160));
-        assets.store("tengen.color.boobs",           Color.rgb(255, 255, 0).deriveColor(0, 1.0, 0.96, 1.0));
-        assets.store("tengen.color.hairbow",         Color.rgb(255, 0, 0));
-        assets.store("tengen.color.hairbow.pearls",  Color.rgb(33, 33, 255));
+        assets.store("tengen.color.head",               Color.rgb(255, 255, 0));
+        assets.store("tengen.color.eyes",               Color.rgb(33, 33, 33));
+        assets.store("tengen.color.palate",             Color.rgb(240, 180, 160));
+        assets.store("tengen.color.boobs",              Color.rgb(255, 255, 0).deriveColor(0, 1.0, 0.96, 1.0));
+        assets.store("tengen.color.hairbow",            Color.rgb(255, 0, 0));
+        assets.store("tengen.color.hairbow.pearls",     Color.rgb(33, 33, 255));
 
         assets.store("pacman.color.head",               Color.rgb(255, 255, 0));
         assets.store("pacman.color.eyes",               Color.rgb(33, 33, 33));
         assets.store("pacman.color.palate",             Color.rgb(240, 180, 160));
 
-        assets.store("pacman_xxl.color.head",               Color.rgb(255, 255, 0));
-        assets.store("pacman_xxl.color.eyes",               Color.rgb(33, 33, 33));
-        assets.store("pacman_xxl.color.palate",             Color.rgb(240, 180, 160));
+        assets.store("pacman_xxl.color.head",           Color.rgb(255, 255, 0));
+        assets.store("pacman_xxl.color.eyes",           Color.rgb(33, 33, 33));
+        assets.store("pacman_xxl.color.palate",         Color.rgb(240, 180, 160));
 
         GameSounds.setAssets(assets);
     }
