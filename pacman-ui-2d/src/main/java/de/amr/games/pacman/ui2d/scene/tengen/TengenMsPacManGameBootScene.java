009d63aa3100b33f9dd359b12407e302f1fcfd6f
Armin Reichert <armin.reichert@web.de>
2024-10-11 16:40:28 +0200

    Improve timing a bit, need exact data

--- pacman-ui-2d/src/main/java/de/amr/games/pacman/ui2d/scene/tengen/TengenMsPacManGameBootScene.java
+++ pacman-ui-2d/src/main/java/de/amr/games/pacman/ui2d/scene/tengen/TengenMsPacManGameBootScene.java
@@ -32,27 +32,27 @@ public class TengenMsPacManGameBootScene extends GameScene2D {
 
     @Override
     public void update() {
-        if (0 <= t && t < 100) {
+        if (0 <= t && t < 90) {
             if (t % 4 == 0 && tengenPresentsY > 17 * TS) {
                 tengenPresentsY -= 2*TS;
             }
         }
-        else if (t == 100) {
+        else if (t == 90) {
             ghost.setPosition(t(31), t(25));
             ghost.setMoveAndWishDir(Direction.LEFT);
             ghost.setSpeed(8); // TODO check speed
             ghost.setVisible(true);
         }
-        else if (100 < t && t < 140) {
+        else if (90 < t && t < 150) {
             ghost.move();
         }
-        else if (140 <= t && t < 180) {
-            if (t % 2 == 0) { tengenPresentsY += 2 * TS; }
+        else if (150 <= t && t < 180) {
+            if (t % 2 == 0) { tengenPresentsY += TS; }
         }
         else if (t == 180) {
             grayScreen = true;
         }
-        else if (t == 184) {
+        else if (t == 185) {
             context.gameController().changeState(GameState.INTRO);
         }
         t += 1;
