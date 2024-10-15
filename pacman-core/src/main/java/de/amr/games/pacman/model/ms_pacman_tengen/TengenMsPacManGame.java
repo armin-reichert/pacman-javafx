823f4140b5804e6554586d23fe8a1c52c38e73a8
Armin Reichert <armin.reichert@web.de>
2024-10-15 15:32:18 +0200

    Colors corrected

--- pacman-core/src/main/java/de/amr/games/pacman/model/ms_pacman_tengen/TengenMsPacManGame.java
+++ pacman-core/src/main/java/de/amr/games/pacman/model/ms_pacman_tengen/TengenMsPacManGame.java
@@ -65,20 +65,20 @@ public class TengenMsPacManGame extends GameModel {
         BLACK_DARKBLUE("000000", "00298c", "00298c", "ffffff"),
         BLUE_WHITE_YELLOW("4242ff", "ffffff", "ffffff", "bdbd00"),
         BLUE2_WHITE("3900a5", "ffffff", "ffffff", "ffffff"),
-        BLUE_YELLOW("00298c", "e7e794", "e7e794", "ffffff"),
+        BLUE_YELLOW("002a88", "e4e594", "e4e594", "ffffff"),
         BROWN_WHITE("522100", "ffffff", "ffffff", "ffffff"),
         BROWN2_WHITE("9c4a00", "ffffff", "ffffff", "ffffff"),
         GRAY_WHITE_YELLOW("adadad", "ffffff", "ffffff", "bdbd00"),
         GREEN_WHITE_YELLOW("109400", "ffffff", "ffffff", "bdbd00"),
         GREEN_WHITE_WHITE("398400", "ffffff", "ffffff", "ffffff"),
         GREEN_WHITE_VIOLET("00424a", "ffffff", "ffffff", "9c18ce"),
-        LIGHTBLUE_WHITE_YELLOW("63adff", "ffffff", "ffffff", "bdbd00"),
+        LIGHTBLUE_WHITE_YELLOW("64b0ff", "ffffff", "ffffff", "bcbe00"),
         KHAKI_WHITE("6b6b00", "ffffff", "ffffff", "ffffff"),
         PINK_ROSE("b5217b", "ff6bce", "ff6bce", "ffffff"),
         PINK_DARKRED("feccc5", "b71e7b", "b71e7b", "ffffff"),
         PINK_WHITE("ff6bce", "ffffff", "ffffff", "ffffff"),
         PINK_YELLOW("ffc6e7", "bdbd00", "bdbd00", "ffffff"),
-        ORANGE_WHITE("b53121", "ffffff", "ffffff", "b5217b"),
+        ORANGE_WHITE("b53120", "ffffff", "ffffff", "b71e7b"),
         DARKBLUE_YELLOW("00298c", "e7e794", "e7e794", "ffffff"),
         RED_WHITE("b5217b", "ffffff", "ffffff", "ffffff"),
         RED_PINK("b5217b", "ff6bce", "ff6bce", "ffc6e7"),
@@ -110,7 +110,7 @@ public class TengenMsPacManGame extends GameModel {
         useMap(34, nonArcadeMaps, miniMaps, MapColoring.PINK_DARKRED);
         useMap(35, nonArcadeMaps, miniMaps, MapColoring.LIGHTBLUE_WHITE_YELLOW);
         useMap(34, nonArcadeMaps, miniMaps, MapColoring.ORANGE_WHITE);
-        useMap(35, nonArcadeMaps, miniMaps, MapColoring.DARKBLUE_YELLOW);
+        useMap(35, nonArcadeMaps, miniMaps, MapColoring.BLUE_YELLOW);
         useMap(36, nonArcadeMaps, miniMaps, MapColoring.PINK_YELLOW);
 
         // 6-10
