27ac3989deca17baab0000e666cf195f492aca66
Armin Reichert <armin.reichert@web.de>
2024-11-13 22:12:48 +0100

    order

--- pacman-ui-2d/src/main/java/module-info.java
+++ pacman-ui-2d/src/main/java/module-info.java
@@ -4,25 +4,25 @@
 open module de.amr.games.pacman.ui2d {
     // module is open to allow access to (non-class) resources
 
-    requires org.tinylog.api;
-    requires de.amr.games.pacman;
-    requires de.amr.games.pacman.lib;
-    requires de.amr.games.pacman.mapeditor;
     requires javafx.base;
     requires javafx.controls;
     requires javafx.graphics;
     requires javafx.media;
+    requires org.tinylog.api;
+    requires de.amr.games.pacman;
+    requires de.amr.games.pacman.lib;
+    requires de.amr.games.pacman.mapeditor;
 
     exports de.amr.games.pacman.ui2d;
     exports de.amr.games.pacman.ui2d.dashboard;
+    exports de.amr.games.pacman.ui2d.input;
     exports de.amr.games.pacman.ui2d.page;
     exports de.amr.games.pacman.ui2d.rendering;
     exports de.amr.games.pacman.ui2d.scene.common;
     exports de.amr.games.pacman.ui2d.scene.ms_pacman;
+    exports de.amr.games.pacman.ui2d.scene.ms_pacman_tengen;
     exports de.amr.games.pacman.ui2d.scene.pacman;
     exports de.amr.games.pacman.ui2d.scene.pacman_xxl;
-    exports de.amr.games.pacman.ui2d.scene.ms_pacman_tengen;
     exports de.amr.games.pacman.ui2d.sound;
     exports de.amr.games.pacman.ui2d.util;
-    exports de.amr.games.pacman.ui2d.input;
 }
