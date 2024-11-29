d64309c162355938f4e0b7c7d766241dbc59c5fb
Armin Reichert <armin.reichert@web.de>
2024-11-29 10:52:13 +0100

    Simplified

--- pacman-ui-2d/src/main/java/de/amr/games/pacman/ui2d/scene/ms_pacman_tengen/LevelCompleteAnimationTengen.java
+++ pacman-ui-2d/src/main/java/de/amr/games/pacman/ui2d/scene/ms_pacman_tengen/LevelCompleteAnimationTengen.java
@@ -24,14 +24,14 @@ public class LevelCompleteAnimationTengen extends LevelCompleteAnimation {
     public LevelCompleteAnimationTengen(Map<String, Object> mapConfig, int numFlashes, int highlightPhaseDuration) {
         super(numFlashes, highlightPhaseDuration);
         var nesColorScheme = (NES_ColorScheme) mapConfig.get("nesColorScheme");
-        var randomColorScheme = (boolean) mapConfig.get("randomColorScheme");
+        var randomize = (boolean) mapConfig.get("randomColorScheme");
         for (int i = 0; i < numFlashes; ++i) {
             NES_ColorScheme previous = i > 0 ? colorSchemes.get(i-1) : null;
-            colorSchemes.add(randomColorScheme ? chooseRandomColorScheme(previous) : nesColorScheme);
+            colorSchemes.add(randomize ? randomColorSchemeNotRepeating(previous) : nesColorScheme);
         }
     }
 
-    private NES_ColorScheme chooseRandomColorScheme(NES_ColorScheme previous) {
+    private NES_ColorScheme randomColorSchemeNotRepeating(NES_ColorScheme previous) {
         NES_ColorScheme nesColorScheme = NES_ColorScheme.random();
         // avoid repetitions and ignore color schemes with black fill color
         while (nesColorScheme == previous || nesColorScheme.fillColor().equals(NES.Palette.color(0x0f))) {
