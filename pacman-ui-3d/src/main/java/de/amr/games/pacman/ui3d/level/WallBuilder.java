db86411bbf5e619e69abd32f1668108e91abb3fd
Armin Reichert <armin.reichert@web.de>
2024-12-02 07:42:17 +0100

    Refac

--- pacman-ui-3d/src/main/java/de/amr/games/pacman/ui3d/level/WallBuilder.java
+++ pacman-ui-3d/src/main/java/de/amr/games/pacman/ui3d/level/WallBuilder.java
@@ -25,42 +25,42 @@ import static de.amr.games.pacman.ui3d.PacManGames3dApp.PY_3D_DRAW_MODE;
 public interface WallBuilder {
 
     static Node createWall(
-        Vector2i tile1, Vector2i tile2,
+        Vector2i beginTile, Vector2i endTile,
         double thickness, DoubleProperty wallHeightPy, double coatHeight,
         Property<PhongMaterial> fillMaterialPy, Property<PhongMaterial> strokeMaterialPy)
     {
-        if (tile1.y() == tile2.y()) { // horizontal wall
-            Vector2i left  = tile1.x() < tile2.x() ? tile1 : tile2;
-            Vector2i right = tile1.x() < tile2.x() ? tile2 : tile1;
-            Vector2i origin = left.plus(right).scaled(HTS).plus(HTS, HTS);
+        if (beginTile.y() == endTile.y()) { // horizontal wall
+            Vector2i left  = beginTile.x() < endTile.x() ? beginTile : endTile;
+            Vector2i right = beginTile.x() < endTile.x() ? endTile : beginTile;
+            Vector2i center = left.plus(right).scaled(HTS).plus(HTS, HTS);
             int length = right.minus(left).scaled(TS).x();
-            return createWall(origin, length + thickness, thickness, wallHeightPy, coatHeight, fillMaterialPy, strokeMaterialPy);
+            return createWall(center, length + thickness, thickness, wallHeightPy, coatHeight, fillMaterialPy, strokeMaterialPy);
         }
-        else if (tile1.x() == tile2.x()) { // vertical wall
-            Vector2i top    = tile1.y() < tile2.y() ? tile1 : tile2;
-            Vector2i bottom = tile1.y() < tile2.y() ? tile2 : tile1;
-            Vector2i origin = top.plus(bottom).scaled(HTS).plus(HTS, HTS);
+        else if (beginTile.x() == endTile.x()) { // vertical wall
+            Vector2i top    = beginTile.y() < endTile.y() ? beginTile : endTile;
+            Vector2i bottom = beginTile.y() < endTile.y() ? endTile : beginTile;
+            Vector2i center = top.plus(bottom).scaled(HTS).plus(HTS, HTS);
             int length = bottom.minus(top).scaled(TS).y();
-            return createWall(origin, thickness, length, wallHeightPy, coatHeight, fillMaterialPy, strokeMaterialPy);
+            return createWall(center, thickness, length, wallHeightPy, coatHeight, fillMaterialPy, strokeMaterialPy);
         }
-        throw new IllegalArgumentException(String.format("Cannot build wall between tiles %s and %s", tile1, tile2));
+        throw new IllegalArgumentException("Cannot build wall between tiles %s and %s".formatted(beginTile, endTile));
     }
 
     static Node createWall(
-        Vector2i origin, double sizeX, double sizeY, DoubleProperty wallHeightPy, double coatHeight,
+        Vector2i center, double sizeX, double sizeY, DoubleProperty wallHeightPy, double coatHeight,
         Property<PhongMaterial> fillMaterialPy, Property<PhongMaterial> strokeMaterialPy) {
 
         var base = new Box(sizeX, sizeY, wallHeightPy.get());
-        base.setTranslateX(origin.x());
-        base.setTranslateY(origin.y());
+        base.setTranslateX(center.x());
+        base.setTranslateY(center.y());
         base.translateZProperty().bind(wallHeightPy.multiply(-0.5).add(2*coatHeight));
         base.depthProperty().bind(wallHeightPy);
         base.materialProperty().bind(fillMaterialPy);
         base.drawModeProperty().bind(PY_3D_DRAW_MODE);
 
         var top = new Box(sizeX, sizeY, coatHeight);
-        top.translateXProperty().bind(base.translateXProperty());
-        top.translateYProperty().bind(base.translateYProperty());
+        top.setTranslateX(center.x());
+        top.setTranslateY(center.y());
         top.translateZProperty().bind(wallHeightPy.add(0.5*coatHeight).multiply(-1).add(2*coatHeight));
         top.materialProperty().bind(strokeMaterialPy);
         top.drawModeProperty().bind(PY_3D_DRAW_MODE);
@@ -69,11 +69,9 @@ public interface WallBuilder {
     }
 
     static void createCircularWall(
-        Group parent, TileMapPath path,
+        Group parent, Vector2i center,
         DoubleProperty wallHeightPy, double coatHeight,
         Property<PhongMaterial> wallFillMaterialPy, Property<PhongMaterial> wallStrokeMaterialPy) {
-
-        Vector2i center = path.startTile().plus(1, 1).scaled(TS);
 
         Cylinder base = new Cylinder(HTS, wallHeightPy.get());
         base.setRotationAxis(Rotate.X_AXIS);
@@ -103,7 +101,8 @@ public interface WallBuilder {
         Property<PhongMaterial> wallFillMaterialPy, Property<PhongMaterial> wallStrokeMaterialPy)
     {
         if (path.size() == 4 && path.isClosed()) {
-            createCircularWall(parent, path, wallHeightPy, coatHeight, wallFillMaterialPy, wallStrokeMaterialPy);
+            Vector2i center = path.startTile().plus(1, 1).scaled(TS);
+            createCircularWall(parent, center, wallHeightPy, coatHeight, wallFillMaterialPy, wallStrokeMaterialPy);
             return;
         }
 
