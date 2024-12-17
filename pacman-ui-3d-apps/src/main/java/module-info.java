203744331e9d292cdd5524eadf13e0c4041532d1
Armin Reichert <armin.reichert@web.de>
2024-12-17 12:37:13 +0100

    Put 3D apps in separate module

---
+++ pacman-ui-3d-apps/src/main/java/module-info.java
@@ -0,0 +1,14 @@
+module de.amr.games.pacman.ui3d.apps {
+    requires org.tinylog.api;
+    requires javafx.graphics;
+    requires de.amr.games.pacman;
+    requires de.amr.games.pacman.lib;
+    requires de.amr.games.pacman.ui3d;
+    requires de.amr.games.pacman.ui2d;
+    requires de.amr.games.pacman.arcade.ms_pacman;
+    requires de.amr.games.pacman.arcade.pacman;
+    requires de.amr.games.pacman.arcade.pacman_xxl;
+    requires de.amr.games.pacman.tengen.ms_pacman;
+
+    exports de.amr.games.pacman.ui3d.apps;
+}
