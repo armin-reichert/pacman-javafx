6b5d6f46c8e99c51035697d6e89af14093d41c47
Armin Reichert <armin.reichert@web.de>
2024-12-19 07:37:52 +0100

    Refac

--- pacman-ui-2d/src/main/java/de/amr/games/pacman/ui2d/GlobalProperties2d.java
+++ pacman-ui-2d/src/main/java/de/amr/games/pacman/ui2d/GlobalProperties2d.java
@@ -10,16 +10,16 @@ import javafx.beans.property.*;
 import javafx.scene.paint.Color;
 
 public interface GlobalProperties2d {
-    BooleanProperty PY_AUTOPILOT = new SimpleBooleanProperty(false);
-    BooleanProperty PY_IMMUNITY  = new SimpleBooleanProperty(false);
-    ObjectProperty<Color> PY_CANVAS_BG_COLOR      = new SimpleObjectProperty<>(Color.BLACK);
-    BooleanProperty PY_CANVAS_IMAGE_SMOOTHING     = new SimpleBooleanProperty(false);
-    BooleanProperty PY_CANVAS_FONT_SMOOTHING      = new SimpleBooleanProperty(false);
-    BooleanProperty PY_DEBUG_INFO_VISIBLE         = new SimpleBooleanProperty(false);
-    ObjectProperty<NightMode> PY_NIGHT_MODE       = new SimpleObjectProperty<>(NightMode.AUTO);
-    IntegerProperty PY_PIP_HEIGHT                 = new SimpleIntegerProperty(8*36);
-    BooleanProperty PY_PIP_ON                     = new SimpleBooleanProperty(false);
-    IntegerProperty PY_PIP_OPACITY_PERCENT        = new SimpleIntegerProperty(100);
-    IntegerProperty PY_SIMULATION_STEPS           = new SimpleIntegerProperty(1);
+    BooleanProperty PY_AUTOPILOT               = new SimpleBooleanProperty(false);
+    ObjectProperty<Color> PY_CANVAS_BG_COLOR   = new SimpleObjectProperty<>(Color.BLACK);
+    BooleanProperty PY_CANVAS_FONT_SMOOTHING   = new SimpleBooleanProperty(false);
+    BooleanProperty PY_CANVAS_IMAGE_SMOOTHING  = new SimpleBooleanProperty(false);
+    BooleanProperty PY_DEBUG_INFO_VISIBLE      = new SimpleBooleanProperty(false);
+    BooleanProperty PY_IMMUNITY                = new SimpleBooleanProperty(false);
     ObjectProperty<MapSelectionMode> PY_MAP_SELECTION_MODE = new SimpleObjectProperty<>(MapSelectionMode.CUSTOM_MAPS_FIRST);
+    ObjectProperty<NightMode> PY_NIGHT_MODE    = new SimpleObjectProperty<>(NightMode.AUTO);
+    IntegerProperty PY_PIP_HEIGHT              = new SimpleIntegerProperty(8*36);
+    BooleanProperty PY_PIP_ON                  = new SimpleBooleanProperty(false);
+    IntegerProperty PY_PIP_OPACITY_PERCENT     = new SimpleIntegerProperty(100);
+    IntegerProperty PY_SIMULATION_STEPS        = new SimpleIntegerProperty(1);
 }
