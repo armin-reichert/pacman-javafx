0157f5c6473eb940f9f377720a1efb7317275031
Armin Reichert <armin.reichert@web.de>
2024-12-25 07:14:28 +0100

    Refac

--- pacman-ui-2d-app-allgames/src/main/java/de/amr/games/pacman/ui2d/appsuite/Main.java
+++ pacman-ui-2d-app-allgames/src/main/java/de/amr/games/pacman/ui2d/appsuite/Main.java
@@ -4,13 +4,13 @@ See file LICENSE in repository root directory for details.
 */
 package de.amr.games.pacman.ui2d.appsuite;
 
-import static de.amr.games.pacman.ui2d.lib.Ufx.startApp;
+import de.amr.games.pacman.ui2d.lib.Ufx;
 
 /**
  * @author Armin Reichert
  */
 public class Main {
     public static void main(String[] args) {
-        startApp(PacManGames2dApp.class, args);
+        Ufx.startApp(PacManGames2dApp.class, args);
     }
 }
