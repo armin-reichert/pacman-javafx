762af927ea3d7458292fcd272e2c6fcad2bb2223
Armin Reichert <armin.reichert@web.de>
2024-12-25 06:32:49 +0100

    Cleanup

--- pacman-arcade-pacmanxxl/src/main/java/de/amr/games/pacman/arcade/pacman_xxl/ArcadePacManXXLStartPage.java
+++ pacman-arcade-pacmanxxl/src/main/java/de/amr/games/pacman/arcade/pacman_xxl/ArcadePacManXXLStartPage.java
@@ -1,3 +1,7 @@
+/*
+Copyright (c) 2021-2024 Armin Reichert (MIT License)
+See file LICENSE in repository root directory for details.
+*/
 package de.amr.games.pacman.arcade.pacman_xxl;
 
 import de.amr.games.pacman.model.GameVariant;
@@ -10,7 +14,7 @@ public class ArcadePacManXXLStartPage {
 
     public ArcadePacManXXLStartPage() {
         ResourceManager rm = this::getClass;
-        flyer = new Flyer(rm.loadImage("graphics/pacman_xxl_logo.jpg"));
+        flyer = new Flyer(rm.loadImage("graphics/pacman_xxl_startpage.jpg"));
         flyer.setUserData(GameVariant.PACMAN_XXL);
         flyer.selectFlyerPage(0);
         flyer.setLayoutMode(0, Flyer.LayoutMode.FILL);
