a7d5386bdb72d4b85a8418c3f935154aad32b56c
Armin Reichert <armin.reichert@web.de>
2025-01-16 11:31:01 +0100

    Rename (some) subprojects

--- experiments/src/main/java/module-info.java
+++ experiments/src/main/java/module-info.java
@@ -2,7 +2,6 @@ module experiments {
     requires javafx.graphics;
 
     requires de.amr.games.pacman;
-    requires de.amr.games.pacman.ui2d;
     requires de.amr.games.pacman.ui3d;
     requires javafx.controls;
     requires org.tinylog.api;
