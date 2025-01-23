5d00bb94364ad7f3c5a5f74d432ae97d54411a56
Armin Reichert <armin.reichert@web.de>
2025-01-23 23:11:28 +0100

    Refac

--- pacman-core/src/main/java/de/amr/games/pacman/lib/tilemap/ObstaclePartitioning.java
+++ pacman-core/src/main/java/de/amr/games/pacman/lib/tilemap/ObstaclePartitioning.java
@@ -6,60 +6,69 @@ package de.amr.games.pacman.lib.tilemap;
 
 import de.amr.games.pacman.lib.RectArea;
 import de.amr.games.pacman.lib.Vector2i;
+import org.tinylog.Logger;
 
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Stream;
 
 import static de.amr.games.pacman.lib.Globals.vec_2i;
 
 public interface ObstaclePartitioning {
 
-    static List<RectArea> computePartition(Obstacle obstacle) {
-        List<RectArea> rectangleList = new ArrayList<>();
+    static List<RectArea> computeRectangularPartition(Obstacle obstacle) {
         List<Vector2i> polygon = computeInnerPolygon(obstacle);
+        List<RectArea> rectangles = new ArrayList<>();
         while (!polygon.isEmpty()) {
-            Vector2i p_k = min_point(polygon), p_l = min_point_except(polygon, p_k), p_m = min_point_between(polygon, p_k, p_l);
-            rectangleList.add( spanned_rect(p_k, vec_2i(p_l.x(), p_m.y())) );
+            Vector2i p_k = minPoint(polygon.stream()),
+                    p_l = minPointExcluding(polygon.stream(), p_k),
+                    p_m = minPointBetween(polygon.stream(), p_k, p_l);
+            rectangles.add( spannedRectangle(p_k, vec_2i(p_l.x(), p_m.y())) );
             toggle(polygon, p_k);
             toggle(polygon, p_l);
             toggle(polygon, vec_2i(p_k.x(), p_m.y()));
             toggle(polygon, vec_2i(p_l.x(), p_m.y()));
         }
-        return rectangleList;
+        return rectangles;
     }
 
-    static RectArea spanned_rect(Vector2i p_min, Vector2i p_max) {
-        return new RectArea(p_min.x(), p_min.y(), p_max.x() - p_min.x(), p_max.y() - p_min.y());
+    static RectArea spannedRectangle(Vector2i pMin, Vector2i pMax) {
+        return new RectArea(pMin.x(), pMin.y(), pMax.x() - pMin.x(), pMax.y() - pMin.y());
     }
 
     static void toggle(List<Vector2i> polygon, Vector2i p) {
         if (polygon.contains(p)) polygon.remove(p); else polygon.add(p);
     }
 
-    static Vector2i min_point_between(List<Vector2i> polygon, Vector2i left, Vector2i right) {
-        List<Vector2i> points_between = polygon.stream().filter(p -> left.x() <= p.x() && p.x() < right.x() && p.y() > left.y()).toList();
-        return min_point(points_between);
+    static Vector2i minPointBetween(Stream<Vector2i> polygon, Vector2i left, Vector2i right) {
+        Stream<Vector2i> points = polygon.filter(p -> left.x() <= p.x() && p.x() < right.x() && p.y() > left.y());
+        return minPoint(points);
     }
 
-    static Vector2i min_point_except(List<Vector2i> polygon, Vector2i p) {
-        polygon.remove(p);
-        Vector2i min = min_point(polygon);
-        polygon.add(p);
-        return min;
+    static Vector2i minPointExcluding(Stream<Vector2i> polygon, Vector2i excludedPoint) {
+        return minPoint(polygon.filter(p -> p != excludedPoint));
     }
 
-    static Vector2i min_point(List<Vector2i> polygon) {
-        return polygon.stream().min(Comparator.comparingInt(Vector2i::y).thenComparingInt(Vector2i::x)).orElseThrow();
+    static Vector2i minPoint(Stream<Vector2i> polygon) {
+        return polygon.min(Comparator.comparingInt(Vector2i::y).thenComparingInt(Vector2i::x)).orElseThrow();
     }
 
+    //TODO
     static List<Vector2i> computeInnerPolygon(Obstacle obstacle) {
-/*
-        return new ArrayList<>(List.of(vec_2i(8, 0), vec_2i(8, 24), vec_2i(0, 24), vec_2i(0, 32), vec_2i(8, 32), vec_2i(8, 48),
+        return new ArrayList<>(
+            List.of(
+                vec_2i(8, 0), vec_2i(8, 24), vec_2i(0, 24), vec_2i(0, 32), vec_2i(8, 32), vec_2i(8, 48),
                 vec_2i(16, 48), vec_2i(16, 16), vec_2i(24, 16), vec_2i(24, 8), vec_2i(16, 8), vec_2i(16, 0)
         ));
- */
-        Vector2i p = obstacle.startPoint();
-        return List.of();
+        //Vector2i p = obstacle.startPoint();
+        //return List.of();
+    }
+
+    public static void main(String[] args) {
+        List<RectArea> rects = computeRectangularPartition(null);
+        for (var r : rects) {
+            Logger.info(r);
+        }
     }
 }
