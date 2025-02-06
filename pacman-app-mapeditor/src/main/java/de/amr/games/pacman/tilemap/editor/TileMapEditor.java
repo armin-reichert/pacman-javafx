58d2acf7562d06b2d50f7e29fa76a714da41841d
Armin Reichert <armin.reichert@web.de>
2025-02-06 13:48:54 +0100

    Preset scatter tiles also in blank map

--- pacman-app-mapeditor/src/main/java/de/amr/games/pacman/tilemap/editor/TileMapEditor.java
+++ pacman-app-mapeditor/src/main/java/de/amr/games/pacman/tilemap/editor/TileMapEditor.java
@@ -1484,6 +1484,12 @@ public class TileMapEditor {
         terrain.setProperty(PROPERTY_COLOR_WALL_STROKE, MS_PACMAN_COLOR_WALL_STROKE);
         terrain.setProperty(PROPERTY_COLOR_WALL_FILL, MS_PACMAN_COLOR_WALL_FILL);
         terrain.setProperty(PROPERTY_COLOR_DOOR, MS_PACMAN_COLOR_DOOR);
+        if (tilesX >= 3 && tilesY >= 2) {
+            terrain.setProperty(PROPERTY_POS_SCATTER_RED_GHOST, formatTile(vec_2i(tilesX - 3, 0)));
+            terrain.setProperty(PROPERTY_POS_SCATTER_PINK_GHOST, formatTile(vec_2i(2, 0)));
+            terrain.setProperty(PROPERTY_POS_SCATTER_CYAN_GHOST, formatTile(vec_2i(tilesX - 1, tilesY - 2)));
+            terrain.setProperty(PROPERTY_POS_SCATTER_ORANGE_GHOST, formatTile(vec_2i(0, tilesY - 2)));
+        }
         blankMap.food().setProperty(PROPERTY_COLOR_FOOD, MS_PACMAN_COLOR_FOOD);
         setWorldMap(blankMap);
     }
