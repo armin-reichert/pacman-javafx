886d7ef7fd12d6af4a45d1687829b83443477c57
Armin Reichert <armin.reichert@web.de>
2025-02-27 21:37:59 +0100

    Refac

--- pacman-app-arcade-pacmanxxl/src/main/java/de/amr/games/pacman/arcade/pacman_xxl/PacManXXL_OptionsMenu.java
+++ pacman-app-arcade-pacmanxxl/src/main/java/de/amr/games/pacman/arcade/pacman_xxl/PacManXXL_OptionsMenu.java
@@ -1,3 +1,7 @@
+/*
+Copyright (c) 2021-2025 Armin Reichert (MIT License)
+See file LICENSE in repository root directory for details.
+*/
 package de.amr.games.pacman.arcade.pacman_xxl;
 
 import de.amr.games.pacman.arcade.pacman_xxl.ms_pacman.PacManXXL_MsPacMan_GameConfig3D;
