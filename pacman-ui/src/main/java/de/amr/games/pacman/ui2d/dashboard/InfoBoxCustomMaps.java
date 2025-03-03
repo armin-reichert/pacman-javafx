658506ac00832070969e2bd1dbf82c820071197c
Armin Reichert <armin.reichert@web.de>
2025-03-03 09:58:12 +0100

    Refac

--- pacman-ui/src/main/java/de/amr/games/pacman/ui2d/dashboard/InfoBoxCustomMaps.java
+++ pacman-ui/src/main/java/de/amr/games/pacman/ui2d/dashboard/InfoBoxCustomMaps.java
@@ -4,6 +4,7 @@ See file LICENSE in repository root directory for details.
 */
 package de.amr.games.pacman.ui2d.dashboard;
 
+import de.amr.games.pacman.lib.tilemap.WorldMap;
 import de.amr.games.pacman.model.MapSelectionMode;
 import de.amr.games.pacman.ui2d.GameContext;
 import javafx.beans.property.SimpleIntegerProperty;
@@ -14,15 +15,12 @@ import javafx.scene.control.TableView;
 
 import static de.amr.games.pacman.ui2d.GlobalProperties2d.PY_MAP_SELECTION_MODE;
 
-
 /**
  * @author Armin Reichert
  */
 public class InfoBoxCustomMaps extends InfoBox {
 
-    record MapInfo(String fileName, int numRows, int numCols) {}
-
-    private TableView<MapInfo> mapTable;
+    private TableView<WorldMap> mapsTableView;
     private ComboBox<MapSelectionMode> comboMapSelectionMode;
 
     @Override
@@ -33,38 +31,29 @@ public class InfoBoxCustomMaps extends InfoBox {
         comboMapSelectionMode.setOnAction(e -> PY_MAP_SELECTION_MODE.set(comboMapSelectionMode.getValue()));
         comboMapSelectionMode.getSelectionModel().select(PY_MAP_SELECTION_MODE.get());
 
-        mapTable = new TableView<>();
-        mapTable.setPrefHeight(200);
-        addRow(mapTable);
+        mapsTableView = new TableView<>();
+        mapsTableView.setPrefHeight(200);
+        addRow(mapsTableView);
 
-        var tcMapFile = new TableColumn<MapInfo, String>("Map File");
-        tcMapFile.setCellValueFactory(p -> new SimpleStringProperty(p.getValue().fileName));
+        var tcMapFile = new TableColumn<WorldMap, String>("Map File");
+        tcMapFile.setCellValueFactory(p -> new SimpleStringProperty(p.getValue().url().toString()));
 
-        var tcMapRowCount = new TableColumn<MapInfo, Integer>("Rows");
-        tcMapRowCount.setCellValueFactory(p -> new SimpleIntegerProperty(p.getValue().numRows).asObject());
+        var tcMapRowCount = new TableColumn<WorldMap, Integer>("Rows");
+        tcMapRowCount.setCellValueFactory(p -> new SimpleIntegerProperty(p.getValue().numRows()).asObject());
 
-        var tcMapColCount = new TableColumn<MapInfo, Integer>("Cols");
-        tcMapColCount.setCellValueFactory(p -> new SimpleIntegerProperty(p.getValue().numCols).asObject());
+        var tcMapColCount = new TableColumn<WorldMap, Integer>("Cols");
+        tcMapColCount.setCellValueFactory(p -> new SimpleIntegerProperty(p.getValue().numCols()).asObject());
 
-        mapTable.getColumns().add(tcMapFile);
-        mapTable.getColumns().add(tcMapRowCount);
-        mapTable.getColumns().add(tcMapColCount);
-        mapTable.getColumns().forEach(column -> {
+        mapsTableView.getColumns().add(tcMapFile);
+        mapsTableView.getColumns().add(tcMapRowCount);
+        mapsTableView.getColumns().add(tcMapColCount);
+        mapsTableView.getColumns().forEach(column -> {
             column.setSortable(false);
             column.setReorderable(false);
         });
     }
 
-    //TODO reimplement and this time right
-    /*
-    public void updateTableView() {
-        ObservableList<MapInfo> items = FXCollections.observableArrayList();
-        Map<File, WorldMap> customMaps = context.game().customMapsByFile();
-        for (File file : customMaps.keySet().stream().sorted().toList()) {
-            WorldMap worldMap = customMaps.get(file);
-            items.add(new MapInfo(file.getName(), worldMap.numRows(), worldMap.numCols()));
-        }
-        mapTable.setItems(items);
+    public TableView<WorldMap> getMapsTableView() {
+        return mapsTableView;
     }
-     */
 }
