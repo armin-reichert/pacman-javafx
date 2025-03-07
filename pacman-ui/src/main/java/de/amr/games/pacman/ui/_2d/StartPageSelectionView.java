131afff8630fbb0e7c8503854551b49a972e9301
Armin Reichert <armin.reichert@web.de>
2025-03-07 13:15:57 +0100

    Simplified

--- pacman-ui/src/main/java/de/amr/games/pacman/ui/_2d/StartPageSelectionView.java
+++ pacman-ui/src/main/java/de/amr/games/pacman/ui/_2d/StartPageSelectionView.java
@@ -4,26 +4,19 @@ See file LICENSE in repository root directory for details.
 */
 package de.amr.games.pacman.ui._2d;
 
-import de.amr.games.pacman.lib.arcade.Arcade;
-import de.amr.games.pacman.lib.nes.NES_JoypadButton;
 import de.amr.games.pacman.model.GameVariant;
-import de.amr.games.pacman.ui.GameContext;
 import de.amr.games.pacman.ui.GameAction;
 import de.amr.games.pacman.ui.GameActionProvider;
-import de.amr.games.pacman.ui.input.ArcadeKeyBinding;
-import de.amr.games.pacman.ui.input.JoypadKeyBinding;
+import de.amr.games.pacman.ui.GameContext;
 import de.amr.games.pacman.uilib.Carousel;
 import javafx.scene.Node;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyCodeCombination;
-import javafx.scene.layout.Background;
-import javafx.scene.paint.Color;
 import org.tinylog.Logger;
 
 import java.util.HashMap;
 import java.util.Map;
 
-import static de.amr.games.pacman.lib.Globals.assertNotNull;
 import static de.amr.games.pacman.ui.input.Keyboard.naked;
 
 /**
@@ -44,10 +37,8 @@ public class StartPageSelectionView extends Carousel implements GameActionProvid
     };
 
     private final Map<KeyCodeCombination, GameAction> actionBindings = new HashMap<>();
-    private final GameContext context;
 
     public StartPageSelectionView(GameContext context) {
-        this.context = assertNotNull(context);
         setOnPrevSlideSelected(startPage -> {
             var variant = (GameVariant) startPage.getUserData();
             context.selectGameVariant(variant);
@@ -59,6 +50,19 @@ public class StartPageSelectionView extends Carousel implements GameActionProvid
             startPage.requestFocus();
         });
         bindGameActions();
+    }
+
+    @Override
+    public Map<KeyCodeCombination, GameAction> actionBindings() {
+        return actionBindings;
+    }
+
+    @Override
+    public void bindGameActions() {
+        bind(context -> showPreviousSlide(), naked(KeyCode.LEFT));
+        bind(context -> showNextSlide(), naked(KeyCode.RIGHT));
+        bind(actionSelectGamePage, naked(KeyCode.ENTER));
+        bind(GameActions2D.TOGGLE_PAUSED, KeyCode.P);
     }
 
     public void addStartPage(GameVariant gameVariant, StartPage startPage) {
@@ -75,28 +79,5 @@ public class StartPageSelectionView extends Carousel implements GameActionProvid
         selectedIndexProperty().set(0);
 
         Logger.info("Start page {} added for game variant {}", startPage, gameVariant);
-    }
-
-    @Override
-    public void bindGameActions() {
-        if (context.gameVariant() == GameVariant.MS_PACMAN_TENGEN) {
-            JoypadKeyBinding joypad = context.currentJoypadKeyBinding();
-            bind(context -> showPreviousSlide(), joypad.key(NES_JoypadButton.BTN_LEFT));
-            bind(context -> showNextSlide(), joypad.key(NES_JoypadButton.BTN_RIGHT));
-            //bind(actionSelectGamePage, joypad.key(NES_JoypadButton.BTN_START));
-        } else {
-            ArcadeKeyBinding arcadeKeys = context.arcadeKeys();
-            bind(context -> showPreviousSlide(), arcadeKeys.keyLeft());
-            bind(context -> showNextSlide(), arcadeKeys.keyRight());
-            // START key is "1" which might be unclear on start page, so add ENTER
-            bind(actionSelectGamePage, context.arcadeKeys().key(Arcade.Button.START), naked(KeyCode.ENTER));
-        }
-        // in case clock has been paused and start page selector got called, allow pause toggle
-        bind(GameActions2D.TOGGLE_PAUSED, KeyCode.P);
-    }
-
-    @Override
-    public Map<KeyCodeCombination, GameAction> actionBindings() {
-        return actionBindings;
     }
 }
