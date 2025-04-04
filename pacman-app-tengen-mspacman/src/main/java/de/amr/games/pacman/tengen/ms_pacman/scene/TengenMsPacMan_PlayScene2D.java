dcd5604cfc915a69d5feab12fd383f8459d0fa90
Armin Reichert <armin.reichert@web.de>
2025-04-04 09:21:44 +0200

    Refac

--- pacman-app-tengen-mspacman/src/main/java/de/amr/games/pacman/tengen/ms_pacman/scene/TengenMsPacMan_PlayScene2D.java
+++ pacman-app-tengen-mspacman/src/main/java/de/amr/games/pacman/tengen/ms_pacman/scene/TengenMsPacMan_PlayScene2D.java
@@ -466,7 +466,7 @@ public class TengenMsPacMan_PlayScene2D extends GameScene2D implements CameraCon
             //TODO in the original game, the message is draw under the maze image but over the pellets!
             tr.drawWorld(level, 0,  3 * TS);
             tr.drawFood(level);
-            tr.drawLevelMessage(level, game.isDemoLevel());
+            tr.drawLevelMessage(level, game.isDemoLevel(), tr.getMessagePosition());
         }
 
         level.bonus().ifPresent(tr::drawBonus);
