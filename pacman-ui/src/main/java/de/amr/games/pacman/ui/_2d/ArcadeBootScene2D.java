a86dc6fe5ca3d98c0a49eeef1f28e93a57efecaf
Armin Reichert <armin.reichert@web.de>
2025-04-08 06:50:09 +0200

    Refac

--- pacman-ui/src/main/java/de/amr/games/pacman/ui/_2d/ArcadeBootScene2D.java
+++ pacman-ui/src/main/java/de/amr/games/pacman/ui/_2d/ArcadeBootScene2D.java
@@ -8,7 +8,6 @@ import de.amr.games.pacman.lib.RectArea;
 import de.amr.games.pacman.lib.Vector2f;
 import de.amr.games.pacman.lib.Vector2i;
 import de.amr.games.pacman.lib.arcade.Arcade;
-import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.image.Image;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
@@ -20,6 +19,11 @@ import static de.amr.games.pacman.ui.Globals.*;
 
 public class ArcadeBootScene2D extends GameScene2D {
 
+    public static final Color WHITE = Color.web(Arcade.Palette.WHITE);
+
+    private boolean isTickMultipleOf4;
+    private boolean isTickMultipleOf8;
+
     @Override
     public void doInit() {
         game().setScoreVisible(false);
@@ -27,6 +31,9 @@ public class ArcadeBootScene2D extends GameScene2D {
 
     @Override
     public void update() {
+        long tick = gameState().timer().tickCount();
+        isTickMultipleOf4 = tick % 4 == 0;
+        isTickMultipleOf8 = tick % 8 == 0;
         if (gameState().timer().atSecond(4)) {
             THE_GAME_CONTROLLER.terminateCurrentState();
         }
@@ -39,28 +46,26 @@ public class ArcadeBootScene2D extends GameScene2D {
 
     @Override
     public void draw() {
-        Vector2f sceneSize = sizeInPx();
+        long tick = gameState().timer().tickCount();
         gr.setScaling(scaling());
-        var timer = gameState().timer();
-        if (timer.tickCount() == 1) {
+        if (tick == 1) {
             gr.fillCanvas(backgroundColor());
-        } else if (timer.betweenSeconds(1, 2) && timer.tickCount() % 8 == 0) {
-            paintRandomHexCodes(sceneSize);
-        } else if (timer.betweenSeconds(2, 3.5) && timer.tickCount() % 4 == 0) {
-            paintRandomSprites(THE_UI_CONFIGS.current().spriteSheet().sourceImage(), sceneSize);
-        } else if (timer.atSecond(3.5)) {
-            paintScreenTestGrid(sceneSize);
+        } else if (gameState().timer().betweenSeconds(1, 2) && isTickMultipleOf8) {
+            paintRandomHexCodes(sizeInPx());
+        } else if (gameState().timer().betweenSeconds(2, 3.5) && isTickMultipleOf4) {
+            paintRandomSprites(sizeInPx());
+        } else if (gameState().timer().atSecond(3.5)) {
+            paintScreenTestGrid(sizeInPx());
         }
     }
 
     @Override
-    protected void drawSceneContent() {
-    }
+    protected void drawSceneContent() {}
 
     private void paintRandomHexCodes(Vector2f sceneSize) {
         Font font = THE_ASSETS.arcadeFontAtSize(scaled(TS));
         gr.fillCanvas(backgroundColor());
-        gr.ctx().setFill(Color.web(Arcade.Palette.WHITE));
+        gr.ctx().setFill(WHITE);
         gr.ctx().setFont(font);
         int numRows = (int) (sceneSize.y() / TS), numCols = (int) (sceneSize.x() / TS);
         for (int row = 0; row < numRows; ++row) {
@@ -71,13 +76,14 @@ public class ArcadeBootScene2D extends GameScene2D {
         }
     }
 
-    private void paintRandomSprites(Image spriteImage, Vector2f sceneSize) {
+    private void paintRandomSprites(Vector2f sceneSize) {
+        Image spriteImage = THE_UI_CONFIGS.current().spriteSheet().sourceImage();
         gr.fillCanvas(backgroundColor());
         int numRows = (int) (sceneSize.y() / TS), numCols = (int) (sceneSize.x() / TS);
         for (int row = 0; row < numRows / 2; ++row) {
             if (RND.nextInt(100) > 20) {
-                var region1 = randomSpriteSheetTile(spriteImage);
-                var region2 = randomSpriteSheetTile(spriteImage);
+                var region1 = randomImageRegion(spriteImage, 16);
+                var region2 = randomImageRegion(spriteImage, 16);
                 var splitX = numCols / 8 + RND.nextInt(numCols / 4);
                 for (int col = 0; col < numCols / 2; ++col) {
                     var region = col < splitX ? region1 : region2;
@@ -87,27 +93,26 @@ public class ArcadeBootScene2D extends GameScene2D {
         }
     }
 
-    private RectArea randomSpriteSheetTile(Image spriteImage) {
-        int x = (int) (RND.nextDouble() * (spriteImage.getWidth() - 16));
-        int y = (int) (RND.nextDouble() * (spriteImage.getHeight() - 16));
-        return new RectArea(x, y, 16, 16);
+    private RectArea randomImageRegion(Image image, int size) {
+        int x = (int) (RND.nextDouble() * (image.getWidth() - size));
+        int y = (int) (RND.nextDouble() * (image.getHeight() - size));
+        return new RectArea(x, y, size, size);
     }
 
     // was probably used to correct screen geometry
     private void paintScreenTestGrid(Vector2f sceneSize) {
-        GraphicsContext g = gr.ctx();
         gr.fillCanvas(backgroundColor());
         Vector2i sizeInTiles = levelSizeInTilesOrElse(ARCADE_MAP_SIZE_IN_TILES);
         int numRows = sizeInTiles.y() / 2, numCols = sizeInTiles.y() / 2;
-        g.setStroke(Color.web(Arcade.Palette.WHITE));
-        g.setLineWidth(scaled(2.0));
+        gr.ctx().setStroke(WHITE);
+        gr.ctx().setLineWidth(scaled(2.0));
         for (int row = 0; row <= numRows; ++row) {
-            g.setLineWidth(row == 0 || row == numRows ? scaled(4.0) : scaled(2.0));
-            g.strokeLine(0, scaled(row * 16), scaled(sceneSize.x()), scaled(row * 16));
+            gr.ctx().setLineWidth(row == 0 || row == numRows ? scaled(4.0) : scaled(2.0));
+            gr.ctx().strokeLine(0, scaled(row * 16), scaled(sceneSize.x()), scaled(row * 16));
         }
         for (int col = 0; col <= numCols; ++col) {
-            g.setLineWidth(col == 0 || col == numCols ? scaled(4.0) : scaled(2.0));
-            g.strokeLine(scaled(col * 16), 0, scaled(col * 16), scaled(sceneSize.y()));
+            gr.ctx().setLineWidth(col == 0 || col == numCols ? scaled(4.0) : scaled(2.0));
+            gr.ctx().strokeLine(scaled(col * 16), 0, scaled(col * 16), scaled(sceneSize.y()));
         }
     }
 }
