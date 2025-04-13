72acb8c78160a20be2735724887f8cb303f5c151
Armin Reichert <armin.reichert@web.de>
2025-04-13 07:04:07 +0200

    Refac

--- pacman-core/src/main/java/de/amr/games/pacman/model/Score.java
+++ pacman-core/src/main/java/de/amr/games/pacman/model/Score.java
@@ -4,6 +4,10 @@ See file LICENSE in repository root directory for details.
 */
 package de.amr.games.pacman.model;
 
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleObjectProperty;
 import org.tinylog.Logger;
 
 import java.io.*;
@@ -16,42 +20,42 @@ import java.util.Properties;
  */
 public class Score {
 
-    private int points;
-    private int levelNumber;
-    private LocalDate date;
+    private final IntegerProperty pointsPy = new SimpleIntegerProperty();
+    private final IntegerProperty levelNumberPy = new SimpleIntegerProperty();
+    private final ObjectProperty<LocalDate> datePy = new SimpleObjectProperty<>();
 
     public Score() {
         reset();
     }
 
     public void reset() {
-        points = 0;
-        levelNumber = 1;
-        date = LocalDate.now();
+        setPoints(0);
+        setLevelNumber(1);
+        setDate(LocalDate.now());
     }
 
-    public void setPoints(int points) {
-        this.points = points;
-    }
+    public IntegerProperty pointsProperty() { return pointsPy; }
+
+    public void setPoints(int points) { pointsProperty().set(points);  }
 
     public int points() {
-        return points;
+        return pointsProperty().get();
     }
 
-    public void setLevelNumber(int levelNumber) {
-        this.levelNumber = levelNumber;
-    }
+    public IntegerProperty levelNumberProperty() { return levelNumberPy; }
+
+    public void setLevelNumber(int levelNumber) { levelNumberProperty().set(levelNumber); }
 
     public int levelNumber() {
-        return levelNumber;
+        return levelNumberProperty().get();
     }
 
-    public void setDate(LocalDate date) {
-        this.date = date;
-    }
+    public ObjectProperty<LocalDate> dateProperty() { return datePy; }
+
+    public void setDate(LocalDate date) { dateProperty().set(date); }
 
     public LocalDate date() {
-        return date;
+        return dateProperty().get();
     }
 
     public void read(File file) {
