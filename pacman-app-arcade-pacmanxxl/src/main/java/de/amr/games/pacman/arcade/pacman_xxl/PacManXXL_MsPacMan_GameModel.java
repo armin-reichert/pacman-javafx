48314afa25f3289861982895e2ab37ea4805b358
Armin Reichert <armin.reichert@web.de>
2025-04-26 12:41:50 +0200

    Use random mazes in demo level modes of XXL game variants

--- pacman-app-arcade-pacmanxxl/src/main/java/de/amr/games/pacman/arcade/pacman_xxl/PacManXXL_MsPacMan_GameModel.java
+++ pacman-app-arcade-pacmanxxl/src/main/java/de/amr/games/pacman/arcade/pacman_xxl/PacManXXL_MsPacMan_GameModel.java
@@ -6,14 +6,32 @@ package de.amr.games.pacman.arcade.pacman_xxl;
 
 import de.amr.games.pacman.Globals;
 import de.amr.games.pacman.arcade.ms_pacman.ArcadeMsPacMan_GameModel;
+import de.amr.games.pacman.model.MapSelectionMode;
 import de.amr.games.pacman.model.MapSelector;
 
 import java.io.File;
+
+import static de.amr.games.pacman.Globals.THE_RNG;
 
 public class PacManXXL_MsPacMan_GameModel extends ArcadeMsPacMan_GameModel {
 
     public PacManXXL_MsPacMan_GameModel(MapSelector mapSelector) {
         super(mapSelector);
         scoreManager.setHighScoreFile(new File(Globals.HOME_DIR, "highscore-mspacman_xxl.xml"));
+    }
+
+    @Override
+    public void buildDemoLevel() {
+        // Select random (standard) level with different map and map color scheme for each choice
+        int[] levelNumbers = { 1, 3, 6, 10, 14, 18 };
+        int levelNumber = levelNumbers[THE_RNG.nextInt(levelNumbers.length)];
+        MapSelectionMode mapSelectionMode = mapSelector.mapSelectionMode();
+        mapSelector.setMapSelectionMode(MapSelectionMode.NO_CUSTOM_MAPS);
+        buildLevel(levelNumber, createLevelData(1));
+        mapSelector.setMapSelectionMode(mapSelectionMode);
+        level.setDemoLevel(true);
+        assignDemoLevelBehavior(level.pac());
+        demoLevelSteering.init();
+        levelCounter.setEnabled(false);
     }
 }
--- pacman-app-arcade-pacmanxxl/src/main/java/de/amr/games/pacman/arcade/pacman_xxl/PacManXXL_PacMan_GameModel.java
+++ pacman-app-arcade-pacmanxxl/src/main/java/de/amr/games/pacman/arcade/pacman_xxl/PacManXXL_PacMan_GameModel.java
@@ -6,11 +6,13 @@ package de.amr.games.pacman.arcade.pacman_xxl;
 
 import de.amr.games.pacman.Globals;
 import de.amr.games.pacman.arcade.ArcadePacMan_GameModel;
+import de.amr.games.pacman.model.MapSelectionMode;
 import de.amr.games.pacman.model.MapSelector;
-import de.amr.games.pacman.steering.RouteBasedSteering;
 import de.amr.games.pacman.steering.RuleBasedPacSteering;
 
 import java.io.File;
+
+import static de.amr.games.pacman.Globals.THE_RNG;
 
 /**
  * Extension of Arcade Pac-Man with 8 new builtin mazes (thanks to the one and only
@@ -24,5 +26,20 @@ public class PacManXXL_PacMan_GameModel extends ArcadePacMan_GameModel {
         scoreManager.setHighScoreFile(new File(Globals.HOME_DIR, "highscore-pacman_xxl.xml"));
         // Demo level map could be custom map, so use generic automatic steering
         demoLevelSteering = new RuleBasedPacSteering(this);
+    }
+
+    @Override
+    public void buildDemoLevel() {
+        // Select random (standard) level with different map and map color scheme for each choice
+        int[] levelNumbers = { 1, 3, 6, 10, 14, 18 };
+        int levelNumber = levelNumbers[THE_RNG.nextInt(levelNumbers.length)];
+        MapSelectionMode mapSelectionMode = mapSelector.mapSelectionMode();
+        mapSelector.setMapSelectionMode(MapSelectionMode.NO_CUSTOM_MAPS);
+        buildLevel(levelNumber, createLevelData(1));
+        mapSelector.setMapSelectionMode(mapSelectionMode);
+        level.setDemoLevel(true);
+        assignDemoLevelBehavior(level.pac());
+        demoLevelSteering.init();
+        levelCounter.setEnabled(false);
     }
 }
