34a5c486269ff900d133fa893eb75e0b01afa840
Armin Reichert <armin.reichert@web.de>
2025-05-03 17:12:46 +0200

    Refac

--- pacman-core/src/main/java/de/amr/games/pacman/model/actors/ActorSpeedControl.java
+++ pacman-core/src/main/java/de/amr/games/pacman/model/actors/ActorSpeedControl.java
@@ -1,9 +1,14 @@
+/*
+Copyright (c) 2021-2025 Armin Reichert (MIT License)
+See file LICENSE in repository root directory for details.
+*/
 package de.amr.games.pacman.model.actors;
 
 import de.amr.games.pacman.model.GameLevel;
 
 public interface ActorSpeedControl {
     void setActorBaseSpeed(GameLevel level);
+
     float ghostAttackSpeed(GameLevel level, Ghost ghost);
     float ghostFrightenedSpeed(GameLevel level, Ghost ghost);
     float ghostSpeedInsideHouse(GameLevel level, Ghost ghost);
