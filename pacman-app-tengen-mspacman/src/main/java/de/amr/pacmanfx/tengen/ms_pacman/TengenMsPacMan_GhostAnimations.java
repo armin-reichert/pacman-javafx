03337b55661dd5a9551b540850b3c138782d727e
Armin Reichert <armin.reichert@web.de>
2025-05-18 09:10:07 +0200

    Refac

--- pacman-app-tengen-mspacman/src/main/java/de/amr/pacmanfx/tengen/ms_pacman/TengenMsPacMan_GhostAnimations.java
+++ pacman-app-tengen-mspacman/src/main/java/de/amr/pacmanfx/tengen/ms_pacman/TengenMsPacMan_GhostAnimations.java
@@ -11,82 +11,50 @@ import de.amr.pacmanfx.model.actors.Ghost;
 import de.amr.pacmanfx.model.actors.GhostAnimations;
 import de.amr.pacmanfx.ui._2d.GameSpriteSheet;
 import de.amr.pacmanfx.ui._2d.SpriteAnimationSet;
-import de.amr.pacmanfx.uilib.animation.SpriteAnimation;
 import de.amr.pacmanfx.uilib.assets.SpriteSheet;
+import javafx.animation.Animation;
 
 import java.util.Map;
+import java.util.stream.Stream;
 
+import static de.amr.pacmanfx.uilib.animation.SpriteAnimation.from;
 import static java.util.Objects.requireNonNull;
 
-/**
- * @author Armin Reichert
- */
 public class TengenMsPacMan_GhostAnimations extends SpriteAnimationSet implements GhostAnimations {
 
-    public static final int NORMAL_FRAME_TICKS = 8;  // TODO check this in emulator
-    public static final int FRIGHTENED_FRAME_TICKS = 8;  // TODO check this in emulator
-    public static final int FLASH_FRAME_TICKS = 7;  // TODO check this in emulator
+    public static final int NORMAL_TICKS = 8;  // TODO check this in emulator
+    public static final int FRIGHTENED_TICKS = 8;  // TODO check this in emulator
+    public static final int FLASH_TICKS = 7;  // TODO check this in emulator
 
-    public TengenMsPacMan_GhostAnimations(GameSpriteSheet spriteSheet, byte personality) {
-        requireNonNull(spriteSheet);
-
-        var normal = SpriteAnimation
-            .from(spriteSheet)
-            .take(spriteSheet.ghostNormalSprites(personality, Direction.LEFT))
-            .frameTicks(NORMAL_FRAME_TICKS)
-            .endless();
-
-        var frightened = SpriteAnimation
-            .from(spriteSheet)
-            .take(spriteSheet.ghostFrightenedSprites())
-            .frameTicks(FRIGHTENED_FRAME_TICKS)
-            .endless();
-
-        var flashing = SpriteAnimation
-            .from(spriteSheet)
-            .take(spriteSheet.ghostFlashingSprites())
-            .frameTicks(FLASH_FRAME_TICKS)
-            .endless();
-
-        var eyes = SpriteAnimation
-            .from(spriteSheet)
-            .take(spriteSheet.ghostEyesSprites(Direction.LEFT))
-            .end();
-
-        var number = SpriteAnimation
-            .from(spriteSheet)
-            .take(spriteSheet.ghostNumberSprites())
-            .end();
-
+    public TengenMsPacMan_GhostAnimations(GameSpriteSheet ss, byte personality) {
+        requireNonNull(ss);
         add(Map.of(
-            GhostAnimations.ANIM_GHOST_NORMAL, normal,
-            GhostAnimations.ANIM_GHOST_FRIGHTENED, frightened,
-            GhostAnimations.ANIM_GHOST_FLASHING, flashing,
-            GhostAnimations.ANIM_GHOST_EYES, eyes,
-            GhostAnimations.ANIM_GHOST_NUMBER, number));
-
-        // TODO check this
-        eyes.play();
-        frightened.play();
-        flashing.play();
+            ANIM_GHOST_NORMAL,     from(ss).take(ss.ghostNormalSprites(personality, Direction.LEFT)).frameTicks(NORMAL_TICKS).endless(),
+            ANIM_GHOST_FRIGHTENED, from(ss).take(ss.ghostFrightenedSprites()).frameTicks(FRIGHTENED_TICKS).endless(),
+            ANIM_GHOST_FLASHING,   from(ss).take(ss.ghostFlashingSprites()).frameTicks(FLASH_TICKS).endless(),
+            ANIM_GHOST_EYES,       from(ss).take(ss.ghostEyesSprites(Direction.LEFT)).end(),
+            ANIM_GHOST_NUMBER,     from(ss).take(ss.ghostNumberSprites()).end()
+        ));
+        // TODO start animations when selected
+        Stream.of(ANIM_GHOST_EYES, ANIM_GHOST_FRIGHTENED, ANIM_GHOST_FLASHING).map(this::animation).forEach(Animation::play);
     }
 
     @Override
     public void select(String id, int frameIndex) {
         super.select(id, frameIndex);
-        if (GhostAnimations.ANIM_GHOST_NUMBER.equals(id)) {
-            animation(GhostAnimations.ANIM_GHOST_NUMBER).setFrameIndex(frameIndex);
+        if (ANIM_GHOST_NUMBER.equals(id)) {
+            animation(ANIM_GHOST_NUMBER).setFrameIndex(frameIndex);
         }
     }
 
     @Override
     protected RectArea[] selectedSprites(SpriteSheet spriteSheet, Actor actor) {
-        GameSpriteSheet gss = (GameSpriteSheet) spriteSheet;
         if (actor instanceof Ghost ghost) {
-            if (isCurrentAnimationID(GhostAnimations.ANIM_GHOST_NORMAL)) {
+            GameSpriteSheet gss = (GameSpriteSheet) spriteSheet;
+            if (isCurrentAnimationID(ANIM_GHOST_NORMAL)) {
                 return gss.ghostNormalSprites(ghost.personality(), ghost.wishDir());
             }
-            if (isCurrentAnimationID(GhostAnimations.ANIM_GHOST_EYES)) {
+            if (isCurrentAnimationID(ANIM_GHOST_EYES)) {
                 return gss.ghostEyesSprites(ghost.wishDir());
             }
         }
