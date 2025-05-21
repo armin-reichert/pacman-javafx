2888e1b48905381d8864ee464701cd4ae8d7b79c
Armin Reichert <armin.reichert@web.de>
2025-05-21 10:15:06 +0200

    Unit test for Cruise Elroy mode in Arcade Pac-Man

--- pacman-app-arcade-pacman/src/test/java/TestCruiseElroy.java
+++ pacman-app-arcade-pacman/src/test/java/TestCruiseElroy.java
@@ -3,7 +3,6 @@ import de.amr.pacmanfx.model.GameVariant;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.tinylog.Logger;
 
 import static de.amr.pacmanfx.Globals.*;
 import static java.util.function.Predicate.not;
@@ -14,59 +13,53 @@ public class TestCruiseElroy {
     @BeforeAll
     static void setup() {
         theGameController().register(GameVariant.PACMAN, new ArcadePacMan_GameModel());
-        Logger.info("Pac-Man game model registered");
         theGameController().select(GameVariant.PACMAN);
-        Logger.info("Pac-Man game selected");
         theGame().buildNormalLevel(1);
-        Logger.info("Level 1 built");
     }
 
+    private ArcadePacMan_GameModel pacManGame() { return (ArcadePacMan_GameModel) theGame(); }
+
     private void eatNextPellet() {
-        var arcadeGame = (ArcadePacMan_GameModel) theGame();
         theGameLevel().worldMap().tiles()
             .filter(theGameLevel()::hasFoodAt)
             .filter(not(theGameLevel()::isEnergizerPosition))
             .findFirst().ifPresent(tile -> {
                 theGameLevel().registerFoodEatenAt(tile);
-                arcadeGame.onPelletEaten();
-                //Logger.info("Food remaining: {}, cruise elroy: {}", theGameLevel().uneatenFoodCount(), arcadeGame.cruiseElroy());
+                pacManGame().onPelletEaten();
             });
     }
 
     private void eatNextEnergizer() {
-        var arcadeGame = (ArcadePacMan_GameModel) theGame();
         theGameLevel(). energizerTiles()
             .filter(theGameLevel()::hasFoodAt)
             .findFirst().ifPresent(tile -> {
                 theGameLevel().registerFoodEatenAt(tile);
-                arcadeGame.onEnergizerEaten();
-                //Logger.info("Food remaining: {}, cruise elroy: {}", theGameLevel().uneatenFoodCount(), arcadeGame.cruiseElroy());
+                pacManGame().onEnergizerEaten();
             });
     }
 
     @Test
     @DisplayName("Test Cruise Elroy Mode 1")
     public void testCruiseElroyMode1() {
-        var arcadeGame = (ArcadePacMan_GameModel) theGame();
         while (theGameLevel().uneatenFoodCount() > theGameLevel().data().elroy1DotsLeft()) {
-            assertEquals(0, arcadeGame.cruiseElroy());
+            assertEquals(0, pacManGame().cruiseElroy());
             eatNextPellet();
         }
-        assertEquals(1, arcadeGame.cruiseElroy());
+        assertEquals(1, pacManGame().cruiseElroy());
         while (theGameLevel().uneatenFoodCount() > theGameLevel().data().elroy2DotsLeft()) {
-            assertEquals(1, arcadeGame.cruiseElroy());
+            assertEquals(1, pacManGame().cruiseElroy());
             eatNextPellet();
         }
-        assertEquals(2, arcadeGame.cruiseElroy());
+        assertEquals(2, pacManGame().cruiseElroy());
         while (theGameLevel().uneatenFoodCount() > 4) {
-            assertEquals(2, arcadeGame.cruiseElroy());
+            assertEquals(2, pacManGame().cruiseElroy());
             eatNextPellet();
         }
-        assertEquals(2, arcadeGame.cruiseElroy());
+        assertEquals(2, pacManGame().cruiseElroy());
         while (theGameLevel().uneatenFoodCount() > 0) {
-            assertEquals(2, arcadeGame.cruiseElroy());
+            assertEquals(2, pacManGame().cruiseElroy());
             eatNextEnergizer();
         }
-        assertEquals(2, arcadeGame.cruiseElroy());
+        assertEquals(2, pacManGame().cruiseElroy());
     }
 }
