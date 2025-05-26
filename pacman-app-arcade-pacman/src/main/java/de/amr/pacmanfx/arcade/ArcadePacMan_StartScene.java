c1cbe4bb4bd6014835ed1089166789a41c45f879
Armin Reichert <armin.reichert@web.de>
2025-05-26 21:50:56 +0200

    Refac

--- pacman-app-arcade-pacman/src/main/java/de/amr/pacmanfx/arcade/ArcadePacMan_StartScene.java
+++ pacman-app-arcade-pacman/src/main/java/de/amr/pacmanfx/arcade/ArcadePacMan_StartScene.java
@@ -10,7 +10,6 @@ import de.amr.pacmanfx.ui._2d.GameScene2D;
 import javafx.scene.text.Font;
 
 import static de.amr.pacmanfx.Globals.*;
-import static de.amr.pacmanfx.lib.UsefulFunctions.tiles_to_px;
 import static de.amr.pacmanfx.lib.arcade.Arcade.ARCADE_MAP_SIZE_IN_PIXELS;
 import static de.amr.pacmanfx.ui.GameAssets.*;
 import static de.amr.pacmanfx.ui.PacManGamesEnv.theAssets;
@@ -40,10 +39,10 @@ public class ArcadePacMan_StartScene extends GameScene2D {
     public void drawSceneContent() {
         final Font smallFont = theAssets().arcadeFontAtSize(scaled(6));
         gr().drawScores(theGame().scoreManager(), scoreColor(), defaultSceneFont());
-        gr().fillTextAtScaledPosition("PUSH START BUTTON", ARCADE_ORANGE, defaultSceneFont(), tiles_to_px(6), tiles_to_px(17));
-        gr().fillTextAtScaledPosition("1 PLAYER ONLY", ARCADE_CYAN, defaultSceneFont(), tiles_to_px(8), tiles_to_px(21));
-        gr().fillTextAtScaledPosition("BONUS PAC-MAN FOR 10000", ARCADE_ROSE, defaultSceneFont(), tiles_to_px(1), tiles_to_px(25));
-        gr().fillTextAtScaledPosition("PTS", ARCADE_ROSE, smallFont, tiles_to_px(25), tiles_to_px(25));
+        gr().fillTextAtScaledTilePosition("PUSH START BUTTON", ARCADE_ORANGE, defaultSceneFont(), 6, 17);
+        gr().fillTextAtScaledTilePosition("1 PLAYER ONLY", ARCADE_CYAN, defaultSceneFont(), 8, 21);
+        gr().fillTextAtScaledTilePosition("BONUS PAC-MAN FOR 10000", ARCADE_ROSE, defaultSceneFont(), 1, 25);
+        gr().fillTextAtScaledTilePosition("PTS", ARCADE_ROSE, smallFont, 25, 25);
         gr().fillTextAtScaledTilePosition("© 1980 MIDWAY MFG.CO.", ARCADE_PINK, defaultSceneFont(), 4, 29);
         gr().fillTextAtScaledPosition("CREDIT %2d".formatted(theCoinMechanism().numCoins()),
                 scoreColor(), defaultSceneFont(), 2 * TS, sizeInPx().y() - 2);
