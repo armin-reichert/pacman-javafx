3afb88db25de55dc6d40d0c4228f8ba2c98bf6e2
Armin Reichert <armin.reichert@web.de>
2025-06-16 09:56:00 +0200

    Strip down

--- pacman-app-allgames/src/main/java/module-info.java
+++ pacman-app-allgames/src/main/java/module-info.java
@@ -3,11 +3,8 @@ Copyright (c) 2021-2025 Armin Reichert (MIT License)
 See file LICENSE in repository root directory for details.
 */
 
-open module x.de.amr.pacmanfx.allgames {
-    requires javafx.controls;
+module x.de.amr.pacmanfx.allgames {
     requires javafx.graphics;
-    requires javafx.media;
-    requires org.tinylog.api;
     requires de.amr.pacmanfx.core;
     requires de.amr.pacmanfx.uilib;
     requires de.amr.pacmanfx.ui;
