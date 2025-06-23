42a6fa082015349a3d1b191e3d5dca5a6f7f5a82
Armin Reichert <armin.reichert@web.de>
2025-06-23 15:25:18 +0200

    Refac

--- pacman-ui/src/main/java/de/amr/pacmanfx/ui/PacManGames_UI_Impl.java
+++ pacman-ui/src/main/java/de/amr/pacmanfx/ui/PacManGames_UI_Impl.java
@@ -52,23 +52,6 @@ public class PacManGames_UI_Impl implements PacManGames_UI {
         public static String PACMAN = "PACMAN";
         public static String PACMAN_XXL = "PACMAN_XXL";
 
-        private final Map<String, GameModel> models = new HashMap<>();
-        private final Map<String, Class<? extends PacManGames_UIConfig>> uiConfigClasses = new HashMap<>();
-        private StartPage[] startPages;
-        private int selectedStartPageIndex;
-        private DashboardID[] dashboardIDs = new DashboardID[0];
-        private final Stage stage;
-        private double width = 800;
-        private double height = 600;
-
-        public Builder(Stage stage, double width, double height) {
-            checkUserDirsExistingAndWritable();
-            validateStage(stage, width, height);
-            this.stage = stage;
-            this.width = width;
-            this.height = height;
-        }
-
         private static void checkUserDirsExistingAndWritable() {
             String homeDirDesc = "Pac-Man FX home directory";
             String customMapDirDesc = "Pac-Man FX custom map directory";
@@ -100,35 +83,44 @@ public class PacManGames_UI_Impl implements PacManGames_UI {
             return true;
         }
 
+        private final Stage stage;
+        private final double width;
+        private final double height;
+        private final Map<String, GameModel> models = new HashMap<>();
+        private final Map<String, Class<? extends PacManGames_UIConfig>> uiConfigClasses = new HashMap<>();
+        private StartPage[] startPages;
+        private int selectedStartPageIndex;
+        private DashboardID[] dashboardIDs = new DashboardID[0];
+
+        public Builder(Stage stage, double width, double height) {
+            this.stage = stage;
+            this.width = width;
+            this.height = height;
+        }
+
         public Builder game(String variant, GameModel model, Class<? extends PacManGames_UIConfig> configClass) {
-            validateGameVariant(variant);
-            validateGameModel(model);
-            validateUIConfigClass(configClass);
             models.put(variant, model);
             uiConfigClasses.put(variant, configClass);
             return this;
         }
 
         public Builder startPages(StartPage... startPages) {
-            validateStartPageList(startPages);
             this.startPages = startPages;
             return this;
         }
 
         public Builder selectStartPage(int index) {
-            // is checked after start page list has been specified
             this.selectedStartPageIndex = index;
             return this;
         }
 
         public Builder dashboardEntries(DashboardID... dashboardIDs) {
-            validateDashboardEntryList(dashboardIDs);
             this.dashboardIDs = dashboardIDs;
             return this;
         }
 
         public PacManGames_UI build() {
-            validateAll();
+            validate();
             theUI = new PacManGames_UI_Impl();
             theUI.configure(uiConfigClasses);
             theUI.build(stage, width, height, dashboardIDs);
@@ -142,42 +134,46 @@ public class PacManGames_UI_Impl implements PacManGames_UI {
             return theUI;
         }
 
-        private void validateAll() {
-            validateGameModels();
-            validateDashboardEntryList(dashboardIDs);
-            validateStartPageList(startPages);
-            if (selectedStartPageIndex < 0 || selectedStartPageIndex >= startPages.length) {
-                error("Selected start page index (%d) is out of range 0..%d".formatted(selectedStartPageIndex, startPages.length - 1));
+        private void validate() {
+            checkUserDirsExistingAndWritable();
+            if (stage == null) {
+                error("Stage is null");
             }
-            validateStage(stage, width, height);
-        }
-
-        private void validateGameModels() {
+            if (width <= 0) {
+                error("Stage width (%.2f) must be a positive number".formatted(width));
+            }
+            if (height <= 0) {
+                error("Stage height (%.2f) must be a positive number".formatted(height));
+            }
             if (models.isEmpty()) {
                 error("No game models specified");
             }
             models.forEach((variant, model) -> {
                 validateGameVariant(variant);
-                validateGameModel(model);
+                if (model == null) {
+                    error("Game model is null");
+                }
             });
             if (uiConfigClasses.isEmpty()) {
                 error("No UI configurations specified");
             }
             uiConfigClasses.forEach((variant, configClass) -> {
                 validateGameVariant(variant);
-                validateUIConfigClass(configClass);
+                if (configClass == null) {
+                    error("UI configuration class is null");
+                }
             });
-        }
-
-        private void validateGameModel(GameModel model) {
-            if (model == null) {
-                error("Game model is null");
+            if (dashboardIDs == null) {
+                error("Dashboard entry list is null");
             }
-        }
-
-        private void validateUIConfigClass(Class<?> configClass) {
-            if (configClass == null) {
-                error("UI configuration class is null");
+            if (startPages == null) {
+                error("Start pages list is null");
+            }
+            if (startPages.length == 0) {
+                error("Start pages list is empty");
+            }
+            if (selectedStartPageIndex < 0 || selectedStartPageIndex >= startPages.length) {
+                error("Selected start page index (%d) is out of range 0..%d".formatted(selectedStartPageIndex, startPages.length - 1));
             }
         }
 
@@ -190,33 +186,6 @@ public class PacManGames_UI_Impl implements PacManGames_UI {
             }
             if (!variant.matches("[a-zA-Z_$][a-zA-Z_$0-9]*")) {
                 error("Game variant ('%s') is not a valid identifier".formatted(variant));
-            }
-        }
-
-        private void validateStartPageList(StartPage[] startPages) {
-            if (startPages == null) {
-                error("Start pages list is null");
-            }
-            if (startPages.length == 0) {
-                error("Start pages list is empty");
-            }
-        }
-
-        private void validateDashboardEntryList(DashboardID[] dashboardIDs) {
-            if (dashboardIDs == null) {
-                error("Dashboard entry list is null");
-            }
-        }
-
-        private void validateStage(Stage stage, double width, double height) {
-            if (stage == null) {
-                error("Stage is null");
-            }
-            if (width <= 0) {
-                error("Stage width (%.2f) must be a positive number".formatted(width));
-            }
-            if (height <= 0) {
-                error("Stage height (%.2f) must be a positive number".formatted(height));
             }
         }
 
