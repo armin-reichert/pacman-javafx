4cdc8dbcc2224db1611dc197aaffc30c2420ce70
Armin Reichert <armin.reichert@web.de>
2025-07-03 09:01:42 +0200

    Simplified

--- pacman-ui/src/main/java/de/amr/pacmanfx/ui/layout/MiniGameView.java
+++ pacman-ui/src/main/java/de/amr/pacmanfx/ui/layout/MiniGameView.java
@@ -9,6 +9,7 @@ import de.amr.pacmanfx.model.GameLevel;
 import de.amr.pacmanfx.model.actors.Actor;
 import de.amr.pacmanfx.model.actors.Bonus;
 import de.amr.pacmanfx.ui._2d.GameRenderer;
+import javafx.animation.Animation;
 import javafx.animation.Interpolator;
 import javafx.animation.TranslateTransition;
 import javafx.beans.binding.Bindings;
@@ -16,7 +17,10 @@ import javafx.beans.property.*;
 import javafx.geometry.Insets;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
-import javafx.scene.layout.*;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
+import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.scene.text.TextAlignment;
@@ -31,21 +35,24 @@ import static de.amr.pacmanfx.ui.PacManGames.theUI;
 
 public class MiniGameView extends VBox {
 
-    private final DoubleProperty aspectProperty = new SimpleDoubleProperty(28.0/36.0);
-    private final ObjectProperty<Color> backgroundColorPy = new SimpleObjectProperty<>(Color.BLACK);
-    private final BooleanProperty debugPy = new SimpleBooleanProperty(false);
-    private final DoubleProperty canvasHeightProperty = new SimpleDoubleProperty(400);
-    private final FloatProperty scalingPy = new SimpleFloatProperty(1.0f);
-    private final ObjectProperty<Vector2f> worldSizePy = new SimpleObjectProperty<>(Vector2f.of(28*TS,36*TS));
+    private static final Vector2f ARCADE_SIZE = Vector2f.of(28, 36);
+
+    private final FloatProperty aspectProperty                  = new SimpleFloatProperty(ARCADE_SIZE.x() / ARCADE_SIZE.y());
+    private final ObjectProperty<Color> backgroundColorProperty = new SimpleObjectProperty<>(Color.BLACK);
+    private final BooleanProperty debugProperty                 = new SimpleBooleanProperty(false);
+    private final DoubleProperty canvasHeightProperty           = new SimpleDoubleProperty(400);
+    private final FloatProperty scalingProperty                 = new SimpleFloatProperty(1.0f);
+    private final ObjectProperty<Vector2f> worldSizeProperty    = new SimpleObjectProperty<>(ARCADE_SIZE.scaled(TS));
 
     private final Canvas canvas;
     private final HBox canvasContainer;
-    private long drawCount;
     private GameRenderer gr;
+    private long drawCallCount;
+
+    private final Animation moveIntoScreenAnimation;
 
     public MiniGameView() {
         canvas = new Canvas();
-        // Keep canvas at current aspect which depends on the current game level's world size
         canvas.heightProperty().bind(canvasHeightProperty());
         canvas.widthProperty().bind(Bindings.createDoubleBinding(
             () -> aspectProperty.get() * canvas.getHeight(),
@@ -60,21 +67,21 @@ public class MiniGameView extends VBox {
         VBox.setVgrow(canvasContainer, Priority.NEVER);
         getChildren().add(canvasContainer);
 
-        scalingPy.bind(Bindings.createFloatBinding(
-            () -> (float) canvas.getHeight() / worldSizePy.get().y(),
-            canvas.heightProperty(), worldSizePy
+        scalingProperty.bind(Bindings.createFloatBinding(
+            () -> (float) canvas.getHeight() / worldSizeProperty.get().y(),
+            canvas.heightProperty(), worldSizeProperty
         ));
-        aspectProperty.bind(worldSizePy.map(size -> size.x() / size.y()));
+        aspectProperty.bind(worldSizeProperty.map(size -> size.x() / size.y()));
 
-        worldSizePy.set(Vector2f.of(28*TS,36*TS));
+        moveIntoScreenAnimation = createMoveIntoScreenAnimation();
     }
 
     public ObjectProperty<Color> backgroundColorProperty() {
-        return backgroundColorPy;
+        return backgroundColorProperty;
     }
 
     public BooleanProperty debugProperty() {
-        return debugPy;
+        return debugProperty;
     }
 
     public DoubleProperty canvasHeightProperty() {
@@ -82,52 +89,45 @@ public class MiniGameView extends VBox {
     }
 
     public void onLevelCreated(GameLevel gameLevel) {
-        worldSizePy.set(gameLevel.worldSizePx());
+        worldSizeProperty.set(gameLevel.worldSizePx());
         gr = theUI().configuration().createGameRenderer(canvas);
         gr.applyRenderingHints(gameLevel);
-        gr.setScaling(scalingPy.floatValue());
-        moveDownIntoScreen();
+        gr.setScaling(scalingProperty.floatValue());
+        moveIntoScreenAnimation.play();
     }
 
     public void onLevelCompleted(GameLevel level) {
-        moveUpOffScreen();
+        Animation moveOffAnimation = createMoveOffScreenAnimation();
+        moveOffAnimation.play();
     }
 
-    private void moveDownIntoScreen() {
+    private Animation createMoveIntoScreenAnimation() {
         var transition = new TranslateTransition(Duration.seconds(1), this);
         transition.setToY(0);
         transition.setByY(10);
-        transition.setDelay(Duration.seconds(2));
+        transition.setDelay(Duration.seconds(1));
         transition.setInterpolator(Interpolator.EASE_OUT);
-        transition.play();
+        return transition;
     }
 
-    private void moveUpOffScreen() {
+    private Animation createMoveOffScreenAnimation() {
         var transition = new TranslateTransition(Duration.seconds(2), this);
         transition.setToY(-canvasContainer.getHeight());
         transition.setByY(10);
         transition.setDelay(Duration.seconds(2));
         transition.setInterpolator(Interpolator.EASE_IN);
-        transition.play();
+        return transition;
     }
 
     public void draw(GameLevel gameLevel) {
         GraphicsContext ctx = canvas.getGraphicsContext2D();
-        float scaling = scalingPy.floatValue();
+        float scaling = scalingProperty.floatValue();
         ctx.setFill(backgroundColorProperty().get());
         ctx.fillRect(0, 0, canvas.getWidth(), canvas.getHeight());
         if (gr == null) {
             Logger.warn("Cannot draw game scene without game renderer");
             return;
         }
-        drawGameScene(gameLevel, scaling);
-        if (debugProperty().get()) {
-            drawInfo(scaling);
-        }
-        drawCount += 1;
-    }
-
-    private void drawGameScene(GameLevel gameLevel, float scaling) {
         gr.setScaling(scaling);
         gr.drawLevel(gameLevel, backgroundColorProperty().get(), false, gameLevel.blinking().isOn());
         var actors = new ArrayList<Actor>();
@@ -135,15 +135,14 @@ public class MiniGameView extends VBox {
         actors.add(gameLevel.pac());
         Stream.of(ORANGE_GHOST_POKEY, CYAN_GHOST_BASHFUL, PINK_GHOST_SPEEDY, RED_GHOST_SHADOW).map(gameLevel::ghost).forEach(actors::add);
         actors.forEach(gr::drawActor);
-    }
-
-    private void drawInfo(float scaling) {
-        GraphicsContext ctx = canvas.getGraphicsContext2D();
-        ctx.save();
-        ctx.setFill(Color.WHITE);
-        ctx.setFont(Font.font(14 * scaling));
-        ctx.setTextAlign(TextAlignment.CENTER);
-        ctx.fillText("scaling: %.2f, draw calls: %d".formatted(scaling, drawCount), canvas.getWidth() * 0.5, 16 * scaling);
-        ctx.restore();
+        if (debugProperty().get()) {
+            ctx.save();
+            ctx.setFill(Color.WHITE);
+            ctx.setFont(Font.font(14 * scaling));
+            ctx.setTextAlign(TextAlignment.CENTER);
+            ctx.fillText("scaling: %.2f, draw calls: %d".formatted(scaling, drawCallCount), canvas.getWidth() * 0.5, 16 * scaling);
+            ctx.restore();
+        }
+        drawCallCount += 1;
     }
 }
