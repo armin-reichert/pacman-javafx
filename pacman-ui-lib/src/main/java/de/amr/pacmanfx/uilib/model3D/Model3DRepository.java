b791a323d75331963719df819edc1fa71a2dd30d
Armin Reichert <armin.reichert@web.de>
2025-07-03 16:25:30 +0200

    Refac

--- pacman-ui-lib/src/main/java/de/amr/pacmanfx/uilib/model3D/Model3DRepository.java
+++ pacman-ui-lib/src/main/java/de/amr/pacmanfx/uilib/model3D/Model3DRepository.java
@@ -26,9 +26,9 @@ import static java.util.Objects.requireNonNull;
 
 public final class Model3DRepository {
 
-    public static final String PAC_MAN_MODEL = "PacMan";
-    public static final String GHOST_MODEL   = "Ghost";
-    public static final String PELLET_MODEL  = "Pellet";
+    public static final String ID_PAC_MAN_MODEL = "PacMan";
+    public static final String ID_GHOST_MODEL = "Ghost";
+    public static final String ID_PELLET_MODEL = "Pellet";
 
     private static final String PAC_MAN_MESH_ID_EYES   = "PacMan.Eyes";
     private static final String PAC_MAN_MESH_ID_HEAD   = "PacMan.Head";
@@ -46,27 +46,19 @@ public final class Model3DRepository {
 
     public Model3D getModel(String modelID) {
         requireNonNull(modelID);
-        return switch (modelID) {
-            case PAC_MAN_MODEL -> {
-                if (!modelMap.containsKey(modelID)) {
-                    modelMap.put(modelID, loadModel("/de/amr/pacmanfx/uilib/model3D/pacman.obj"));
-                }
-                yield modelMap.get(modelID);
-            }
-            case GHOST_MODEL -> {
-                if (!modelMap.containsKey(modelID)) {
-                    modelMap.put(modelID, loadModel("/de/amr/pacmanfx/uilib/model3D/ghost.obj"));
-                }
-                yield modelMap.get(modelID);
-            }
-            case PELLET_MODEL -> {
-                if (!modelMap.containsKey(modelID)) {
-                    modelMap.put(modelID, loadModel("/de/amr/pacmanfx/uilib/model3D/pellet.obj"));
-                }
-                yield modelMap.get(modelID);
-            }
+        return getOrLoadModel(modelID, switch (modelID) {
+            case ID_PAC_MAN_MODEL -> "/de/amr/pacmanfx/uilib/model3D/pacman.obj";
+            case ID_GHOST_MODEL   -> "/de/amr/pacmanfx/uilib/model3D/ghost.obj";
+            case ID_PELLET_MODEL  -> "/de/amr/pacmanfx/uilib/model3D/pellet.obj";
             default -> throw new IllegalArgumentException("Illegal 3D model ID %s".formatted(modelID));
-        };
+        });
+    }
+
+    private Model3D getOrLoadModel(String modelID, String path) {
+        if (!modelMap.containsKey(modelID)) {
+            modelMap.put(modelID, loadModel(path));
+        }
+        return modelMap.get(modelID);
     }
 
     private Model3D loadModel(String path) {
@@ -78,22 +70,22 @@ public final class Model3DRepository {
         }
     }
 
-    public Mesh ghostDressMesh()   { return getModel(GHOST_MODEL).mesh("Sphere.004_Sphere.034_light_blue_ghost"); }
-    public Mesh ghostPupilsMesh()  { return getModel(GHOST_MODEL).mesh("Sphere.010_Sphere.039_grey_wall"); }
-    public Mesh ghostEyeballsMesh() { return getModel(GHOST_MODEL).mesh("Sphere.009_Sphere.036_white"); }
+    public Mesh ghostDressMesh()   { return getModel(ID_GHOST_MODEL).mesh("Sphere.004_Sphere.034_light_blue_ghost"); }
+    public Mesh ghostPupilsMesh()  { return getModel(ID_GHOST_MODEL).mesh("Sphere.010_Sphere.039_grey_wall"); }
+    public Mesh ghostEyeballsMesh() { return getModel(ID_GHOST_MODEL).mesh("Sphere.009_Sphere.036_white"); }
 
-    public Mesh pelletMesh() { return getModel(PELLET_MODEL).mesh(PELLET_MESH_ID); }
+    public Mesh pelletMesh() { return getModel(ID_PELLET_MODEL).mesh(PELLET_MESH_ID); }
 
     public Group createPacShape(double size, Color headColor, Color eyesColor, Color palateColor) {
-        var head = new MeshView(getModel(PAC_MAN_MODEL).mesh(PAC_MAN_MESH_ID_HEAD));
+        var head = new MeshView(getModel(ID_PAC_MAN_MODEL).mesh(PAC_MAN_MESH_ID_HEAD));
         head.setId(toCSS_ID(PAC_MAN_MESH_ID_HEAD));
         head.setMaterial(Ufx.coloredPhongMaterial(headColor));
 
-        var eyes = new MeshView(getModel(PAC_MAN_MODEL).mesh(PAC_MAN_MESH_ID_EYES));
+        var eyes = new MeshView(getModel(ID_PAC_MAN_MODEL).mesh(PAC_MAN_MESH_ID_EYES));
         eyes.setId(toCSS_ID(PAC_MAN_MESH_ID_EYES));
         eyes.setMaterial(Ufx.coloredPhongMaterial(eyesColor));
 
-        var palate = new MeshView(getModel(PAC_MAN_MODEL).mesh(PAC_MAN_MESH_ID_PALATE));
+        var palate = new MeshView(getModel(ID_PAC_MAN_MODEL).mesh(PAC_MAN_MESH_ID_PALATE));
         palate.setId(toCSS_ID(PAC_MAN_MESH_ID_PALATE));
         palate.setMaterial(Ufx.coloredPhongMaterial(palateColor));
 
@@ -115,11 +107,11 @@ public final class Model3DRepository {
     }
 
     public Group createPacSkull(double size, Color headColor, Color palateColor) {
-        var head = new MeshView(getModel(PAC_MAN_MODEL).mesh(PAC_MAN_MESH_ID_HEAD));
+        var head = new MeshView(getModel(ID_PAC_MAN_MODEL).mesh(PAC_MAN_MESH_ID_HEAD));
         head.setId(toCSS_ID(PAC_MAN_MESH_ID_HEAD));
         head.setMaterial(Ufx.coloredPhongMaterial(headColor));
 
-        var palate = new MeshView(getModel(PAC_MAN_MODEL).mesh(PAC_MAN_MESH_ID_PALATE));
+        var palate = new MeshView(getModel(ID_PAC_MAN_MODEL).mesh(PAC_MAN_MESH_ID_PALATE));
         palate.setId(toCSS_ID(PAC_MAN_MESH_ID_PALATE));
         palate.setMaterial(Ufx.coloredPhongMaterial(palateColor));
 
@@ -183,16 +175,16 @@ public final class Model3DRepository {
     }
 
     public Node createGhostShape3D(double size, Color dressColor, double rotate) {
-        MeshView dress = new MeshView(getModel(GHOST_MODEL).mesh(GHOST_MESH_ID_DRESS));
+        MeshView dress = new MeshView(getModel(ID_GHOST_MODEL).mesh(GHOST_MESH_ID_DRESS));
         dress.setMaterial(Ufx.coloredPhongMaterial(dressColor));
         Bounds dressBounds = dress.getBoundsInLocal();
         var centeredOverOrigin = new Translate(-dressBounds.getCenterX(), -dressBounds.getCenterY(), -dressBounds.getCenterZ());
         dress.getTransforms().add(centeredOverOrigin);
 
-        MeshView pupils = new MeshView(getModel(GHOST_MODEL).mesh(GHOST_MESH_ID_PUPILS));
+        MeshView pupils = new MeshView(getModel(ID_GHOST_MODEL).mesh(GHOST_MESH_ID_PUPILS));
         pupils.setMaterial(Ufx.coloredPhongMaterial(Color.BLUE));
 
-        MeshView eyeballs = new MeshView(getModel(GHOST_MODEL).mesh(GHOST_MESH_ID_EYEBALLS));
+        MeshView eyeballs = new MeshView(getModel(ID_GHOST_MODEL).mesh(GHOST_MESH_ID_EYEBALLS));
         eyeballs.setMaterial(Ufx.coloredPhongMaterial(Color.WHITE));
         var eyesGroup = new Group(pupils, eyeballs);
         eyesGroup.getTransforms().add(centeredOverOrigin);
