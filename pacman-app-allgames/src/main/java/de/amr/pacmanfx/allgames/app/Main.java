4bdd63bcd42051da85b1e97c1c196f4d2be7a011
Armin Reichert <armin.reichert@web.de>
2025-07-12 09:05:20 +0200

    Refac

--- pacman-app-allgames/src/main/java/de/amr/pacmanfx/allgames/app/Main.java
+++ pacman-app-allgames/src/main/java/de/amr/pacmanfx/allgames/app/Main.java
@@ -6,12 +6,6 @@ package de.amr.pacmanfx.allgames.app;
 
 import javafx.application.Application;
 
-/**
- * Wraps application start, if an exception occurs, the stacktrace will be stored in file <code>oh_shit.txt</code>.
- */
 public class Main {
-    public static void main(String[] args) {
-        //Ufx.startApplication(PacManGames3dApp.class, args);
-        Application.launch(PacManGames3dApp.class, args);
-    }
+    public static void main(String[] args) { Application.launch(PacManGames3dApp.class, args); }
 }
