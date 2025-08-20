1bfdc63e8b23d73b4af8d502208840296cffc4d6
Armin Reichert <armin.reichert@web.de>
2025-08-20 17:13:14 +0200

    Refac

--- pacman-app-tengen-mspacman/src/main/java/de/amr/pacmanfx/tengen/ms_pacman/scenes/TengenMsPacMan_IntroScene.java
+++ pacman-app-tengen-mspacman/src/main/java/de/amr/pacmanfx/tengen/ms_pacman/scenes/TengenMsPacMan_IntroScene.java
@@ -18,13 +18,9 @@ import de.amr.pacmanfx.tengen.ms_pacman.rendering.*;
 import de.amr.pacmanfx.ui._2d.GameScene2D;
 import de.amr.pacmanfx.ui.api.GameUI;
 import de.amr.pacmanfx.ui.api.GameUI_Config;
-import javafx.beans.property.DoubleProperty;
-import javafx.beans.property.SimpleDoubleProperty;
-import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.paint.Color;
 import org.tinylog.Logger;
 
-import java.util.BitSet;
 import java.util.List;
 
 import static de.amr.pacmanfx.Globals.*;
@@ -44,7 +40,6 @@ public class TengenMsPacMan_IntroScene extends GameScene2D {
     private static final int ACTOR_Y = MARQUEE_Y + 72;
     private static final int GHOST_STOP_X = MARQUEE_X - 18;
     private static final int MS_PAC_MAN_STOP_X = MARQUEE_X + 62;
-    private static final int NUM_BULBS = 96;
     private static final float SPEED = 2.2f; //TODO check exact speed
 
     private final StateMachine<SceneState, TengenMsPacMan_IntroScene> sceneController;
@@ -90,6 +85,7 @@ public class TengenMsPacMan_IntroScene extends GameScene2D {
         actionBindings.assign(ACTION_TOGGLE_JOYPAD_BINDINGS_DISPLAY, tengenActionBindings);
 
         marquee = new Marquee();
+        marquee.setPosition(MARQUEE_X, MARQUEE_Y);
         marquee.scalingProperty().bind(scaling);
 
         presentsText = new Actor();
@@ -123,9 +119,8 @@ public class TengenMsPacMan_IntroScene extends GameScene2D {
 
     @Override
     public void drawSceneContent() {
+        long tick = sceneController.state().timer().tickCount();
         ctx().setFont(scaledArcadeFont8());
-        TickTimer timer = sceneController.state().timer;
-        long tick = timer.tickCount();
         switch (sceneController.state()) {
             case WAITING_FOR_START -> {
                 if (!dark) {
@@ -166,47 +161,6 @@ public class TengenMsPacMan_IntroScene extends GameScene2D {
 
         if (PROPERTY_JOYPAD_BINDINGS_DISPLAYED.get()) {
             scenesRenderer.drawJoypadKeyBinding(ui.joypad().currentKeyBinding());
-        }
-    }
-
-    private static class Marquee {
-        private final DoubleProperty scaling = new SimpleDoubleProperty(1.0);
-        private final BitSet bulbState = new BitSet(NUM_BULBS);
-        private long marqueeProgress;
-
-        public DoubleProperty scalingProperty() {
-            return scaling;
-        }
-
-        public void update(long tick) {
-            if (tick % 4 == 0) {
-                marqueeProgress += 2;
-                bulbState.clear();
-                for (int b = 0; b < 6; ++b) {
-                    bulbState.set((int) (b * 16 + marqueeProgress) % NUM_BULBS);
-                }
-            }
-        }
-
-        public void draw(GraphicsContext ctx) {
-            double xMin = MARQUEE_X, xMax = xMin + 132, yMin = MARQUEE_Y, yMax = yMin + 60;
-            for (int i = 0; i < NUM_BULBS; ++i) {
-                ctx.setFill(bulbState.get(i) ? nesColor(0x20) : nesColor(0x15));
-                if (i <= 33) { // lower border left-to-right
-                    drawBulb(ctx, xMin + 4 * i, yMax);
-                } else if (i <= 48) { // right border bottom-to-top
-                    drawBulb(ctx, xMax, yMax - 4 * (i - 33));
-                } else if (i <= 81) { // upper border right-to-left
-                    drawBulb(ctx, xMax - 4 * (i - 48), yMin);
-                } else { // left border top-to-bottom
-                    drawBulb(ctx, xMin, yMin + 4 * (i - 81));
-                }
-            }
-        }
-
-        private void drawBulb(GraphicsContext ctx, double x, double y) {
-            double s = scaling.get();
-            ctx.fillRect(s*x, s*y, s*2, s*2);
         }
     }
 
