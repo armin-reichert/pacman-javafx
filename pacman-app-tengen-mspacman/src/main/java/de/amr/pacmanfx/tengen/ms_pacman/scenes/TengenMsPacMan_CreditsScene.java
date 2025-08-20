69356b8e4fe39197f641f305275cd7860ba53c7b
Armin Reichert <armin.reichert@web.de>
2025-08-20 14:39:34 +0200

    Fade effect für Arme

--- pacman-app-tengen-mspacman/src/main/java/de/amr/pacmanfx/tengen/ms_pacman/scenes/TengenMsPacMan_CreditsScene.java
+++ pacman-app-tengen-mspacman/src/main/java/de/amr/pacmanfx/tengen/ms_pacman/scenes/TengenMsPacMan_CreditsScene.java
@@ -27,6 +27,7 @@ public class TengenMsPacMan_CreditsScene extends GameScene2D {
     private static final float DISPLAY_SECONDS = 16;
 
     private int y;
+    private float fadeProgress = 0;
 
     private TengenMsPacMan_ScenesRenderer scenesRenderer;
 
@@ -54,6 +55,10 @@ public class TengenMsPacMan_CreditsScene extends GameScene2D {
     public void update() {
         if (gameContext().gameState().timer().atSecond(DISPLAY_SECONDS)) {
             gameContext().gameController().letCurrentGameStateExpire();
+            return;
+        }
+        if (gameContext().gameState().timer().betweenSeconds(0.5 * DISPLAY_SECONDS, DISPLAY_SECONDS)) {
+            fadeProgress = Math.min(fadeProgress + 0.005f, 1f); // Clamp to 1.0
         }
     }
 
@@ -74,35 +79,39 @@ public class TengenMsPacMan_CreditsScene extends GameScene2D {
         if (gameContext().gameState().timer().betweenSeconds(0, 0.5 * DISPLAY_SECONDS)) {
             drawOriginalCreditsText();
         } else {
+            ctx().setGlobalAlpha(fadeProgress);
             drawRemakeCreditsText();
+            ctx().setGlobalAlpha(1);
+
         }
     }
+
     private void drawOriginalCreditsText() {
-        printAndAdvanceTiles(4, "CREDITS FOR MS PAC-MAN",    0x20,  3);
-        printAndAdvanceTiles(2, "GAME PROGRAMMER:",          0x23,  4);
-        printAndAdvanceTiles(3, "FRANZ LANZINGER",           0x23, 10);
-        printAndAdvanceTiles(2, "SPECIAL THANKS:",           0x23,  4);
-        printAndAdvanceTiles(1, "JEFF YONAN",                0x23, 10);
-        printAndAdvanceTiles(4, "DAVE O'RIVA",               0x23, 10);
-        printAndAdvanceTiles(1, "MS PAC-MAN TM NAMCO LTD",   0x19,  5);
-        printAndAdvanceTiles(1, "©1990 TENGEN INC",          0x19,  7);
-        printAndAdvanceTiles(0, "ALL RIGHTS RESERVED",       0x19,  6);
+        print(4, "CREDITS FOR MS PAC-MAN",    0x20,  3);
+        print(2, "GAME PROGRAMMER:",          0x23,  4);
+        print(3, "FRANZ LANZINGER",           0x23, 10);
+        print(2, "SPECIAL THANKS:",           0x23,  4);
+        print(1, "JEFF YONAN",                0x23, 10);
+        print(4, "DAVE O'RIVA",               0x23, 10);
+        print(1, "MS PAC-MAN TM NAMCO LTD",   0x19,  5);
+        print(1, "©1990 TENGEN INC",          0x19,  7);
+        print(0, "ALL RIGHTS RESERVED",       0x19,  6);
     }
 
     private void drawRemakeCreditsText() {
-        printAndAdvanceTiles(4, "CREDITS FOR JAVAFX REMAKE", 0x20,  3);
-        printAndAdvanceTiles(2, "GAME PROGRAMMER:",          0x23,  4);
-        printAndAdvanceTiles(3, "ARMIN REICHERT",            0x23, 10);
-        printAndAdvanceTiles(2, "SPECIAL THANKS:",           0x23,  4);
-        printAndAdvanceTiles(1, "@RUSSIANMANSMWC",           0x23, 10);
-        printAndAdvanceTiles(1, "@FLICKY1211",               0x23, 10);
-        printAndAdvanceTiles(3, "ANDYANA JONSEPH",           0x23, 10);
-        printAndAdvanceTiles(1, "GITHUB.COM/ARMIN-REICHERT", 0x19,  3);
-        printAndAdvanceTiles(1, "©2024 MIT LICENSE",         0x19,  6);
-        printAndAdvanceTiles(0, "ALL RIGHTS GRANTED",        0x19,  5);
+        print(4, "CREDITS FOR JAVAFX REMAKE", 0x20,  3);
+        print(2, "GAME PROGRAMMER:",          0x23,  4);
+        print(3, "ARMIN REICHERT",            0x23, 10);
+        print(2, "SPECIAL THANKS:",           0x23,  4);
+        print(1, "@RUSSIANMANSMWC",           0x23, 10);
+        print(1, "@FLICKY1211",               0x23, 10);
+        print(3, "ANDYANA JONSEPH",           0x23, 10);
+        print(1, "GITHUB.COM/ARMIN-REICHERT", 0x19,  3);
+        print(1, "©2024 MIT LICENSE",         0x19,  6);
+        print(0, "ALL RIGHTS GRANTED",        0x19,  5);
     }
 
-    private void printAndAdvanceTiles(int numTiles, String text, int colorIndex, int tilesX) {
+    private void print(int numTiles, String text, int colorIndex, int tilesX) {
         scenesRenderer.fillText(text, nesColor(colorIndex), TS(tilesX), y);
         y += numTiles * TS;
     }
