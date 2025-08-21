9eb9f12ef130a39c961df2db94784600134c3654
Armin Reichert <armin.reichert@web.de>
2025-08-21 12:26:27 +0200

    Refac

--- pacman-app-arcade-pacman/src/main/java/de/amr/pacmanfx/arcade/pacman/scenes/ArcadePacMan_BootScene2D.java
+++ pacman-app-arcade-pacman/src/main/java/de/amr/pacmanfx/arcade/pacman/scenes/ArcadePacMan_BootScene2D.java
@@ -14,8 +14,6 @@ import de.amr.pacmanfx.ui.sound.SoundID;
 import de.amr.pacmanfx.uilib.assets.SpriteSheet;
 import de.amr.pacmanfx.uilib.rendering.SpriteRenderer;
 
-import java.util.Random;
-
 import static de.amr.pacmanfx.Globals.ARCADE_MAP_SIZE_IN_PIXELS;
 import static de.amr.pacmanfx.Globals.TS;
 import static de.amr.pacmanfx.lib.RandomNumberSupport.randomFloat;
@@ -29,7 +27,7 @@ import static de.amr.pacmanfx.ui._2d.ArcadePalette.ARCADE_WHITE;
  */
 public class ArcadePacMan_BootScene2D extends GameScene2D {
 
-    private static final int FRAGMENT_SIZE = 16;
+    private static final int RASTER_SIZE = 16;
 
     private SpriteRenderer spriteRenderer;
 
@@ -58,7 +56,7 @@ public class ArcadePacMan_BootScene2D extends GameScene2D {
         double width = spriteSheet.sourceImage().getWidth(), height = spriteSheet.sourceImage().getHeight();
         // ignore left half of sprite sheet image containing maze images
         minPoint = Vector2f.of(width / 2, 0);
-        maxPoint = Vector2f.of(width - FRAGMENT_SIZE, height - FRAGMENT_SIZE);
+        maxPoint = Vector2f.of(width - RASTER_SIZE, height - RASTER_SIZE);
         ui.soundManager().playVoice(SoundID.VOICE_EXPLAIN, 0);
     }
 
@@ -93,10 +91,13 @@ public class ArcadePacMan_BootScene2D extends GameScene2D {
     @Override
     public void drawSceneContent() {
         TickTimer timer = context().gameState().timer();
+        // during first second, nothing happens...
         if (timer.betweenSeconds(1, 2) && timer.tickCount() % 4 == 0) {
+            // change pattern every 4th tick
             clear();
             drawRandomHexDigits();
         } else if (timer.betweenSeconds(2, 3.5) && timer.tickCount() % 4 == 0) {
+            // change pattern every 4th tick
             clear();
             drawRandomSpriteFragments();
         } else if (timer.atSecond(3.5)) {
@@ -106,7 +107,6 @@ public class ArcadePacMan_BootScene2D extends GameScene2D {
     }
 
     private void drawRandomHexDigits() {
-        var random = new Random();
         int numRows = (int) (ARCADE_MAP_SIZE_IN_PIXELS.y() / TS);
         int numCols = (int) (ARCADE_MAP_SIZE_IN_PIXELS.x() / TS);
         ctx().setFill(ARCADE_WHITE);
@@ -114,21 +114,21 @@ public class ArcadePacMan_BootScene2D extends GameScene2D {
         for (int row = 0; row < numRows; ++row) {
             double y = scaled(TS(row + 1));
             for (int col = 0; col < numCols; ++col) {
-                int hexDigit = random.nextInt(16);
+                int hexDigit = randomInt(0, 16);
                 ctx().fillText(Integer.toHexString(hexDigit), scaled(TS(col)), y);
             }
         }
     }
 
     private void drawRandomSpriteFragments() {
-        int numRows = (int) (ARCADE_MAP_SIZE_IN_PIXELS.y() / FRAGMENT_SIZE);
-        int numCols = (int) (ARCADE_MAP_SIZE_IN_PIXELS.x() / FRAGMENT_SIZE);
+        int numRows = (int) (ARCADE_MAP_SIZE_IN_PIXELS.y() / RASTER_SIZE);
+        int numCols = (int) (ARCADE_MAP_SIZE_IN_PIXELS.x() / RASTER_SIZE);
         for (int row = 0; row < numRows; ++row) {
             if (randomInt(0, 100) < 20) continue;
             RectShort fragment1 = randomSpriteFragment(), fragment2 = randomSpriteFragment();
             int split = numCols / 8 + randomInt(0, numCols / 4);
             for (int col = 0; col < numCols; ++col) {
-                spriteRenderer.drawSprite(col < split ? fragment1 : fragment2, FRAGMENT_SIZE * col, FRAGMENT_SIZE * row, true);
+                spriteRenderer.drawSprite(col < split ? fragment1 : fragment2, RASTER_SIZE * col, RASTER_SIZE * row, true);
             }
         }
     }
@@ -137,24 +137,24 @@ public class ArcadePacMan_BootScene2D extends GameScene2D {
         return new RectShort(
             (int) lerp(minPoint.x(), maxPoint.x(), randomFloat(0, 1)),
             (int) lerp(minPoint.y(), maxPoint.y(), randomFloat(0, 1)),
-            FRAGMENT_SIZE, FRAGMENT_SIZE);
+                RASTER_SIZE, RASTER_SIZE);
     }
 
     private void drawGridLines() {
         double gridWidth = scaled(ARCADE_MAP_SIZE_IN_PIXELS.x());
         double gridHeight = scaled(ARCADE_MAP_SIZE_IN_PIXELS.y());
-        int numRows = (int) (ARCADE_MAP_SIZE_IN_PIXELS.y() / 16);
-        int numCols = (int) (ARCADE_MAP_SIZE_IN_PIXELS.x() / 16);
+        int numRows = (int) (ARCADE_MAP_SIZE_IN_PIXELS.y() / RASTER_SIZE);
+        int numCols = (int) (ARCADE_MAP_SIZE_IN_PIXELS.x() / RASTER_SIZE);
         double thin = scaled(2), thick = scaled(4);
         ctx().setStroke(ARCADE_WHITE);
         for (int row = 0; row <= numRows; ++row) {
             ctx().setLineWidth(row == 0 || row == numRows ? thick : thin);
-            double y = scaled(row * 16);
+            double y = scaled(row * RASTER_SIZE);
             ctx().strokeLine(0, y, gridWidth, y);
         }
         for (int col = 0; col <= numCols; ++col) {
             ctx().setLineWidth(col == 0 || col == numCols ? thick : thin);
-            double x = scaled(col * 16);
+            double x = scaled(col * RASTER_SIZE);
             ctx().strokeLine(x, 0, x, gridHeight);
         }
     }
