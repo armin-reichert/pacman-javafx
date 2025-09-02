bb683210671a2b237e7f238cd4c7550947eaf2d6
Armin Reichert <armin.reichert@web.de>
2025-09-02 12:40:13 +0200

    Improved

--- pacman-app-mapeditor/src/main/java/de/amr/pacmanfx/tilemap/editor/actions/Action_IdentifyObstacle.java
+++ pacman-app-mapeditor/src/main/java/de/amr/pacmanfx/tilemap/editor/actions/Action_IdentifyObstacle.java
@@ -2,8 +2,8 @@ package de.amr.pacmanfx.tilemap.editor.actions;
 
 import de.amr.pacmanfx.lib.Vector2i;
 import de.amr.pacmanfx.lib.tilemap.*;
-import de.amr.pacmanfx.tilemap.editor.MessageType;
 import de.amr.pacmanfx.tilemap.editor.EditorUI;
+import de.amr.pacmanfx.tilemap.editor.MessageType;
 import javafx.scene.input.Clipboard;
 import javafx.scene.input.ClipboardContent;
 
@@ -23,27 +23,59 @@ public class Action_IdentifyObstacle extends AbstractEditorUIAction<String> {
     @Override
     public String execute() {
         WorldMap worldMap = editor.currentWorldMap();
+        byte terrainCode = worldMap.content(LayerID.TERRAIN, tile);
+        byte foodCode = worldMap.content(LayerID.FOOD, tile);
+        String info = "";
+        if (terrainCode != TerrainTile.EMPTY.code())
+            info = "Terrain 0x%02X (%s)".formatted(terrainCode, terrainTileName(terrainCode));
+        if (foodCode != FoodTile.EMPTY.code())
+            info = "Food 0x%02X (%s)".formatted(foodCode, foodTileName(foodCode));
+        ui.messageDisplay().showMessage(info, 4, MessageType.INFO);
+
+        return identifyObstacleStartingAtTile(worldMap);
+    }
+
+    private String identifyObstacleStartingAtTile(WorldMap worldMap) {
         Obstacle obstacleStartingAtTile = worldMap.obstacles().stream()
             .filter(obstacle -> tileAt(obstacle.startPoint().minus(HTS, 0).toVector2f()).equals(tile))
             .findFirst().orElse(null);
-        if (obstacleStartingAtTile != null) {
-            String encoding = obstacleStartingAtTile.encoding();
-            Clipboard clipboard = Clipboard.getSystemClipboard();
-            ClipboardContent content = new ClipboardContent();
-            content.putString(encoding);
-            clipboard.setContent(content);
-            ui.messageDisplay().showMessage("Obstacle (copied to clipboard)", 5, MessageType.INFO);
-            return encoding;
-        } else {
-            byte terrainCode = worldMap.content(LayerID.TERRAIN, tile);
-            byte foodCode = worldMap.content(LayerID.FOOD, tile);
-            String info = "";
-            if (terrainCode != TerrainTile.EMPTY.code())
-                info = "Terrain #%02X (%s)".formatted(terrainCode, terrainCode);
-            if (foodCode != FoodTile.EMPTY.code())
-                info = "Food #%02X (%s)".formatted(foodCode, foodCode);
-            ui.messageDisplay().showMessage(info, 4, MessageType.INFO);
-            return null;
-        }
+        if (obstacleStartingAtTile == null) return null;
+        String encoding = obstacleStartingAtTile.encoding();
+        Clipboard clipboard = Clipboard.getSystemClipboard();
+        ClipboardContent content = new ClipboardContent();
+        content.putString(encoding);
+        clipboard.setContent(content);
+        return encoding;
+    }
+
+    private String foodTileName(byte code) {
+        return switch (code) {
+            case 0x00 -> "EMPTY";
+            case 0x01 -> "PELLET";
+            case 0x02 -> "ENERGIZER";
+            default -> "UNKNOWN";
+        };
+    }
+    private String terrainTileName(byte code) {
+        return switch (code) {
+            case 0x00 -> "EMPTY";
+            case 0x01 -> "WALL_H";
+            case 0x02 -> "WALL_V";
+            case 0x03 -> "ARC_NW";
+            case 0x04 -> "ARC_NE";
+            case 0x05 -> "ARC_SE";
+            case 0x06 -> "ARC_SW";
+            case 0x07 -> "TUNNEL";
+            case 0x0e -> "DOOR";
+            case 0x10 -> "DARC_NW";
+            case 0x11 -> "DARC_NE";
+            case 0x12 -> "DARC_SE";
+            case 0x13 -> "DARC_SW";
+            case 0x14 -> "ONE_WAY_UP";
+            case 0x15 -> "ONE_WAY_RIGHT";
+            case 0x16 -> "ONE_WAY_DOWN";
+            case 0x17 -> "ONE_WAY_LEFT";
+            default   -> "UNKNOWN";
+        };
     }
 }
