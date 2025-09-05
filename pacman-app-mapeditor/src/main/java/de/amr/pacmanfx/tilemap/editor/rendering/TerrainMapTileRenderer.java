11e6fdba5aeae8f0498746ace9353e9661ab3bd7
Armin Reichert <armin.reichert@web.de>
2025-09-05 10:40:05 +0200

    Refac

--- pacman-app-mapeditor/src/main/java/de/amr/pacmanfx/tilemap/editor/rendering/TerrainMapTileRenderer.java
+++ pacman-app-mapeditor/src/main/java/de/amr/pacmanfx/tilemap/editor/rendering/TerrainMapTileRenderer.java
@@ -88,8 +88,8 @@ public class TerrainMapTileRenderer extends BaseCanvasRenderer implements Terrai
 
     @Override
     public void draw(WorldMap worldMap, Set<Obstacle> obstacles) {
-        ctx().save();
-        ctx().scale(scaling(), scaling());
+        ctx.save();
+        ctx.scale(scaling(), scaling());
         worldMap.tiles().forEach(tile -> {
             byte code = worldMap.content(LayerID.TERRAIN, tile);
             drawTileUnscaled(tile, code);
@@ -104,7 +104,7 @@ public class TerrainMapTileRenderer extends BaseCanvasRenderer implements Terrai
         if (obstacleInnerAreaDisplayed) {
             drawObstacleInnerAreas(worldMap, obstacles);
         }
-        ctx().restore();
+        ctx.restore();
     }
 
     private void drawObstacleInnerAreas(WorldMap worldMap, Set<Obstacle> obstacles) {
@@ -116,24 +116,24 @@ public class TerrainMapTileRenderer extends BaseCanvasRenderer implements Terrai
             List<RectShort> rectangles = obstacle.innerAreaRectangles();
             for (int i = 0; i < rectangles.size(); ++i) {
                 RectShort rect = rectangles.get(i);
-                ctx().setFill(RANDOM_COLORS[i % RANDOM_COLORS.length]);
-                ctx().fillRect(rect.x(), rect.y(), rect.width(), rect.height());
+                ctx.setFill(RANDOM_COLORS[i % RANDOM_COLORS.length]);
+                ctx.fillRect(rect.x(), rect.y(), rect.width(), rect.height());
             }
-            ctx().setFill(Color.grayRgb(200));
-            ctx().setStroke(Color.grayRgb(200));
-            ctx().setLineWidth(0.5);
+            ctx.setFill(Color.grayRgb(200));
+            ctx.setStroke(Color.grayRgb(200));
+            ctx.setLineWidth(0.5);
             for (Vector2i p : obstacle.computeInnerPolygon()) {
-                ctx().fillOval(p.x() - r, p.y() - r, 2*r, 2*r);
+                ctx.fillOval(p.x() - r, p.y() - r, 2*r, 2*r);
                 if (prev != null) {
-                    ctx().strokeLine(prev.x(), prev.y(), p.x(), p.y());
+                    ctx.strokeLine(prev.x(), prev.y(), p.x(), p.y());
                 }
                 prev = p;
             }
             for (int i = 0; i < rectangles.size(); ++i) {
                 RectShort rect = rectangles.get(i);
-                ctx().setFill(Color.WHITE);
-                ctx().setFont(Font.font("Sans", FontWeight.BOLD, 3.5));
-                ctx().fillText("R" + (i+1), rect.x() + rect.width() * 0.5 - 3, rect.y() + rect.height() * 0.5 + 1);
+                ctx.setFill(Color.WHITE);
+                ctx.setFont(Font.font("Sans", FontWeight.BOLD, 3.5));
+                ctx.fillText("R" + (i+1), rect.x() + rect.width() * 0.5 - 3, rect.y() + rect.height() * 0.5 + 1);
             }
         });
     }
@@ -145,22 +145,22 @@ public class TerrainMapTileRenderer extends BaseCanvasRenderer implements Terrai
                 Vector2f start = segment.startPoint().toVector2f();
                 Vector2f end = segment.endPoint().toVector2f();
                 Vector2f  middle = start.midpoint(end);
-                ctx().setFont(SEGMENT_NUMBER_FONT);
-                ctx().setFill(SEGMENT_NUMBER_FILL_COLOR);
-                ctx().setStroke(SEGMENT_NUMBER_STROKE_COLOR);
-                ctx().setLineWidth(0.1);
-                ctx().fillText(String.valueOf(i), middle.x() - 0.5 * SEGMENT_NUMBER_FONT_SIZE, middle.y());
-                ctx().strokeText(String.valueOf(i), middle.x() - 0.5 * SEGMENT_NUMBER_FONT_SIZE, middle.y());
+                ctx.setFont(SEGMENT_NUMBER_FONT);
+                ctx.setFill(SEGMENT_NUMBER_FILL_COLOR);
+                ctx.setStroke(SEGMENT_NUMBER_STROKE_COLOR);
+                ctx.setLineWidth(0.1);
+                ctx.fillText(String.valueOf(i), middle.x() - 0.5 * SEGMENT_NUMBER_FONT_SIZE, middle.y());
+                ctx.strokeText(String.valueOf(i), middle.x() - 0.5 * SEGMENT_NUMBER_FONT_SIZE, middle.y());
             }
         });
     }
 
     @Override
     public void drawTile(Vector2i tile, byte content) {
-        ctx().save();
-        ctx().scale(scaling(), scaling());
+        ctx.save();
+        ctx.scale(scaling(), scaling());
         drawTileUnscaled(tile, content);
-        ctx().restore();
+        ctx.restore();
     }
 
     private Optional<Vector2i> specialTile(WorldMap worldMap, String propertyName) {
@@ -185,53 +185,53 @@ public class TerrainMapTileRenderer extends BaseCanvasRenderer implements Terrai
     private void drawDoor(Vector2i tile, Color color) {
         double x = tile.x() * TS, y = tile.y() * TS;
         double height = TS * 0.25;
-        ctx().setFill(color);
-        ctx().fillRect(x, y + 0.5 * (TS - height), TS, height);
+        ctx.setFill(color);
+        ctx.fillRect(x, y + 0.5 * (TS - height), TS, height);
     }
 
     public void drawScatterTarget(Vector2i tile, Color color) {
         double x = tile.x() * TS, y = tile.y() * TS;
-        ctx().setFill(color);
-        ctx().fillRect(x, y, TS, TS);
-        ctx().setStroke(Color.WHITE);
-        ctx().setLineWidth(0.5);
-        ctx().strokeOval(x + 2, y + 2, TS - 4, TS - 4);
-        ctx().strokeLine(x + 0.5 * TS, y, x + 0.5 * TS, y + TS);
-        ctx().strokeLine(x, y + 0.5 * TS, x + TS, y + 0.5 * TS);
+        ctx.setFill(color);
+        ctx.fillRect(x, y, TS, TS);
+        ctx.setStroke(Color.WHITE);
+        ctx.setLineWidth(0.5);
+        ctx.strokeOval(x + 2, y + 2, TS - 4, TS - 4);
+        ctx.strokeLine(x + 0.5 * TS, y, x + 0.5 * TS, y + TS);
+        ctx.strokeLine(x, y + 0.5 * TS, x + TS, y + 0.5 * TS);
     }
 
     private void drawOneWaySign(Vector2i tile, byte code) {
         double x = tile.x() * TS, y = tile.y() * TS;
-        ctx().save();
-        ctx().beginPath();
+        ctx.save();
+        ctx.beginPath();
         switch (code) {
             case 0x14 -> {
-                ctx().moveTo(x, y+8);
-                ctx().lineTo(x+4, y);
-                ctx().lineTo(x+8,y+8);
+                ctx.moveTo(x, y+8);
+                ctx.lineTo(x+4, y);
+                ctx.lineTo(x+8,y+8);
             }
             case 0x15 -> {
-                ctx().moveTo(x, y);
-                ctx().lineTo(x+8, y+4);
-                ctx().lineTo(x,y+8);
+                ctx.moveTo(x, y);
+                ctx.lineTo(x+8, y+4);
+                ctx.lineTo(x,y+8);
             }
             case 0x16 -> {
-                ctx().moveTo(x, y);
-                ctx().lineTo(x+4, y+8);
-                ctx().lineTo(x+8,y);
+                ctx.moveTo(x, y);
+                ctx.lineTo(x+4, y+8);
+                ctx.lineTo(x+8,y);
             }
             case 0x17 -> {
-                ctx().moveTo(x+8, y);
-                ctx().lineTo(x, y+4);
-                ctx().lineTo(x+8,y+8);
+                ctx.moveTo(x+8, y);
+                ctx.lineTo(x, y+4);
+                ctx.lineTo(x+8,y+8);
             }
         }
-        ctx().closePath();
-        ctx().setStroke(Color.grayRgb(222));
-        ctx().setLineWidth(0.4);
-        ctx().setLineDashes(0.5, 1);
-        ctx().stroke();
-        ctx().restore();
+        ctx.closePath();
+        ctx.setStroke(Color.grayRgb(222));
+        ctx.setLineWidth(0.4);
+        ctx.setLineDashes(0.5, 1);
+        ctx.stroke();
+        ctx.restore();
     }
 
     private void drawTunnel(Vector2i tile) {
@@ -243,25 +243,25 @@ public class TerrainMapTileRenderer extends BaseCanvasRenderer implements Terrai
 
     private void drawWallH(Vector2i tile) {
         double x = tile.x() * TS, y = tile.y() * TS;
-        ctx().setFill(colorScheme().wallStrokeColor());
-        ctx().fillRect(x, y + 3.5f, TS, 1);
+        ctx.setFill(colorScheme().wallStrokeColor());
+        ctx.fillRect(x, y + 3.5f, TS, 1);
     }
 
     private void drawWallV(Vector2i tile) {
         double x = tile.x() * TS, y = tile.y() * TS;
-        ctx().setFill(colorScheme().wallStrokeColor());
+        ctx.setFill(colorScheme().wallStrokeColor());
         // add 1 pixel to avoid gaps
-        ctx().fillRect(x + 3.5f, y, 1, TS);
+        ctx.fillRect(x + 3.5f, y, 1, TS);
     }
 
     private void drawArc(Vector2i tile, byte cornerType) {
         double x = tile.x() * TS, y = tile.y() * TS;
-        ctx().setStroke(colorScheme().wallStrokeColor());
-        ctx().setLineWidth(1);
-        if (cornerType == ARC_NW.$) ctx().strokeArc(x + 4, y + 4, TS, TS, 90, 90,  ArcType.OPEN);
-        if (cornerType == ARC_NE.$) ctx().strokeArc(x - 4, y + 4, TS, TS, 0, 90,   ArcType.OPEN);
-        if (cornerType == ARC_SE.$) ctx().strokeArc(x - 4, y - 4, TS, TS, 270, 90, ArcType.OPEN);
-        if (cornerType == ARC_SW.$) ctx().strokeArc(x + 4, y - 4, TS, TS, 180, 90, ArcType.OPEN);
+        ctx.setStroke(colorScheme().wallStrokeColor());
+        ctx.setLineWidth(1);
+        if (cornerType == ARC_NW.$) ctx.strokeArc(x + 4, y + 4, TS, TS, 90, 90,  ArcType.OPEN);
+        if (cornerType == ARC_NE.$) ctx.strokeArc(x - 4, y + 4, TS, TS, 0, 90,   ArcType.OPEN);
+        if (cornerType == ARC_SE.$) ctx.strokeArc(x - 4, y - 4, TS, TS, 270, 90, ArcType.OPEN);
+        if (cornerType == ARC_SW.$) ctx.strokeArc(x + 4, y - 4, TS, TS, 180, 90, ArcType.OPEN);
     }
 
     private void drawDCorner(Vector2i tile, byte code, double[] xp, double[] yp) {
@@ -269,60 +269,60 @@ public class TerrainMapTileRenderer extends BaseCanvasRenderer implements Terrai
         double cx = x + HTS, cy = y + HTS;
         double rightEdge = x + TS, bottomEdge = y + TS;
         double d = 1;
-        ctx().setStroke(colorScheme().wallStrokeColor());
-        ctx().setLineWidth(1);
+        ctx.setStroke(colorScheme().wallStrokeColor());
+        ctx.setLineWidth(1);
         switch (code) {
             case 0x10 -> {
                 xp[0] = xp[1] = cx - d;
                 xp[2] = rightEdge;
                 yp[0] = bottomEdge;
                 yp[1] = yp[2] = cy - d;
-                ctx().strokePolyline(xp, yp, xp.length);
+                ctx.strokePolyline(xp, yp, xp.length);
 
                 xp[0] = xp[1] = cx + d;
                 xp[2] = rightEdge;
                 yp[0] = bottomEdge;
                 yp[1] = yp[2] = cy + d;
-                ctx().strokePolyline(xp, yp, xp.length);
+                ctx.strokePolyline(xp, yp, xp.length);
             }
             case 0x11 -> {
                 xp[0] = x;
                 xp[1] = xp[2] = cx + d;
                 yp[0] = yp[1] = cy - d;
                 yp[2] = bottomEdge;
-                ctx().strokePolyline(xp, yp, xp.length);
+                ctx.strokePolyline(xp, yp, xp.length);
 
                 xp[0] = x;
                 xp[1] = xp[2] = cx - d;
                 yp[0] = yp[1] = cy + d;
                 yp[2] = bottomEdge;
-                ctx().strokePolyline(xp, yp, xp.length);
+                ctx.strokePolyline(xp, yp, xp.length);
             }
             case 0x12 -> {
                 xp[0] = x;
                 xp[1] = xp[2] = cx - d;
                 yp[0] = yp[1] = cy - d;
                 yp[2] = y;
-                ctx().strokePolyline(xp, yp, xp.length);
+                ctx.strokePolyline(xp, yp, xp.length);
 
                 xp[0] = x;
                 xp[1] = xp[2] = cx + d;
                 yp[0] = yp[1] = cy + d;
                 yp[2] = y;
-                ctx().strokePolyline(xp, yp, xp.length);
+                ctx.strokePolyline(xp, yp, xp.length);
             }
             case 0x13 -> {
                 xp[0] = xp[1] = cx - d;
                 xp[2] = rightEdge;
                 yp[0] = y;
                 yp[1] = yp[2] = cy + d;
-                ctx().strokePolyline(xp, yp, xp.length);
+                ctx.strokePolyline(xp, yp, xp.length);
 
                 xp[0] = xp[1] = cx + d;
                 xp[2] = rightEdge;
                 yp[0] = y;
                 yp[1] = yp[2] = cy - d;
-                ctx().strokePolyline(xp, yp, xp.length);
+                ctx.strokePolyline(xp, yp, xp.length);
             }
         }
     }
