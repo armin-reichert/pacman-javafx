c3dfd8f190424d6e1bd8812934add85e08cd4b62
Armin Reichert <armin.reichert@web.de>
2025-09-26 10:28:04 +0200

    Refac

--- pacman-ui-lib/src/main/java/de/amr/pacmanfx/uilib/rendering/TerrainMapVectorRenderer.java
+++ pacman-ui-lib/src/main/java/de/amr/pacmanfx/uilib/rendering/TerrainMapVectorRenderer.java
@@ -95,8 +95,8 @@ public class TerrainMapVectorRenderer extends BaseRenderer implements TerrainMap
     }
 
     public void draw(WorldMap worldMap) {
-        ctx().save();
-        ctx().scale(scaling(), scaling());
+        ctx.save();
+        ctx.scale(scaling(), scaling());
         for (Obstacle obstacle : worldMap.obstacles()) {
             if (obstacle.borderObstacle()) {
                 drawDoubleStripedObstacle(obstacle);
@@ -104,7 +104,7 @@ public class TerrainMapVectorRenderer extends BaseRenderer implements TerrainMap
                 drawSingleStripedObstacle(obstacle);
             }
         }
-        ctx().restore();
+        ctx.restore();
     }
 
     private void drawDoubleStripedObstacle(Obstacle obstacle) {
@@ -120,57 +120,57 @@ public class TerrainMapVectorRenderer extends BaseRenderer implements TerrainMap
         int r = HTS;
         int x = obstacle.startPoint().x();
         int y = obstacle.startPoint().y();
-        ctx().beginPath();
-        ctx().moveTo(x, y);
+        ctx.beginPath();
+        ctx.moveTo(x, y);
         for (ObstacleSegment segment : obstacle.segments()) {
             x += segment.vector().x();
             y += segment.vector().y();
             if (segment.isStraightLine()) {
-                ctx().lineTo(x, y);
+                ctx.lineTo(x, y);
             } else {
                 boolean counterClockwise = segment.ccw();
                 TerrainTile terrainTile = TerrainTile.values()[segment.encoding()];
                 switch (terrainTile) {
                     case ARC_NW -> {
-                        if (counterClockwise) ctx().arc(x+r, y, r, r,  90, 90); else ctx().arc(x, y+r, r, r, 180, -90);
+                        if (counterClockwise) ctx.arc(x+r, y, r, r,  90, 90); else ctx.arc(x, y+r, r, r, 180, -90);
                     }
                     case ARC_SW -> {
-                        if (counterClockwise) ctx().arc(x, y-r, r, r, 180, 90); else ctx().arc(x+r, y, r, r, 270, -90);
+                        if (counterClockwise) ctx.arc(x, y-r, r, r, 180, 90); else ctx.arc(x+r, y, r, r, 270, -90);
                     }
                     case ARC_SE -> {
-                        if (counterClockwise) ctx().arc(x-r, y, r, r, 270, 90); else ctx().arc(x, y-r, r, r, 0, -90);
+                        if (counterClockwise) ctx.arc(x-r, y, r, r, 270, 90); else ctx.arc(x, y-r, r, r, 0, -90);
                     }
                     case ARC_NE -> {
-                        if (counterClockwise) ctx().arc(x, y+r, r, r, 0, 90); else ctx().arc(x-r, y, r, r, 90, -90);
+                        if (counterClockwise) ctx.arc(x, y+r, r, r, 0, 90); else ctx.arc(x-r, y, r, r, 90, -90);
                     }
                     case ANG_ARC_NW -> {
-                        if (counterClockwise) ctx().lineTo(x,y-r); else ctx().lineTo(x-r, y);
-                        ctx().lineTo(x, y);
+                        if (counterClockwise) ctx.lineTo(x,y-r); else ctx.lineTo(x-r, y);
+                        ctx.lineTo(x, y);
                     }
                     case ANG_ARC_SW -> {
-                        if (counterClockwise) ctx().lineTo(x-r, y); else ctx().lineTo(x, y+r);
-                        ctx().lineTo(x, y);
+                        if (counterClockwise) ctx.lineTo(x-r, y); else ctx.lineTo(x, y+r);
+                        ctx.lineTo(x, y);
                     }
                     case ANG_ARC_SE -> {
-                        if (counterClockwise) ctx().lineTo(x,y+r); else ctx().lineTo(x-r, y);
-                        ctx().lineTo(x, y);
+                        if (counterClockwise) ctx.lineTo(x,y+r); else ctx.lineTo(x-r, y);
+                        ctx.lineTo(x, y);
                     }
                     case ANG_ARC_NE -> {
-                        if (counterClockwise) ctx().lineTo(x+r, y); else ctx().lineTo(x-r, y-r);
-                        ctx().lineTo(x, y);
+                        if (counterClockwise) ctx.lineTo(x+r, y); else ctx.lineTo(x-r, y-r);
+                        ctx.lineTo(x, y);
                     }
                 }
             }
         }
         if (obstacle.isClosed()) {
-            ctx().closePath();
+            ctx.closePath();
             if (fillInside) {
-                ctx().setFill(fillColor);
-                ctx().fill();
+                ctx.setFill(fillColor);
+                ctx.fill();
             }
         }
-        ctx().setLineWidth(lineWidth);
-        ctx().setStroke(strokeColor);
-        ctx().stroke();
+        ctx.setLineWidth(lineWidth);
+        ctx.setStroke(strokeColor);
+        ctx.stroke();
     }
 }
