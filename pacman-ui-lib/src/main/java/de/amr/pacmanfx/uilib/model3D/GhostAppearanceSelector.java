2eec2c6c69726c3a1f911a838a771f329cd8a0e3
Armin Reichert <armin.reichert@web.de>
2025-09-30 12:34:55 +0200

    Refac

--- pacman-ui-lib/src/main/java/de/amr/pacmanfx/uilib/model3D/GhostAppearanceSelector.java
+++
@@ -1,9 +0,0 @@
-/*
-Copyright (c) 2021-2025 Armin Reichert (MIT License)
-See file LICENSE in repository root directory for details.
-*/
-package de.amr.pacmanfx.uilib.model3D;
-
-public interface GhostAppearanceSelector {
-
-}
