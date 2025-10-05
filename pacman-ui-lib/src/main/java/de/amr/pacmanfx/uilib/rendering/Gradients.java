8ebe70f7fa8c47ff355632d82df59a76c92cbc4e
Armin Reichert <armin.reichert@web.de>
2025-10-05 17:31:00 +0200

    Gradients

--- pacman-ui-lib/src/main/java/de/amr/pacmanfx/uilib/rendering/Gradients.java
+++ pacman-ui-lib/src/main/java/de/amr/pacmanfx/uilib/rendering/Gradients.java
@@ -12,34 +12,39 @@ import javafx.scene.paint.*;
  */
 public interface Gradients {
 
-    enum Progress {
-        LEFT_TO_RIGHT, TOP_TO_BOTTOM, TOP_LEFT_TO_BOTTOM_RIGHT;
+    enum Axis {
+        /** Left to Right */
+        HORIZONTAL,
+        /** Top to Bottom */
+        VERTICAL,
+        /** Top-Left to Bottom-Right */
+        DIAGONAL;
+
+        public Vector2i start() {
+            return Vector2i.ZERO;
+        }
+
+        public Vector2i end() {
+            return switch (this) {
+                case HORIZONTAL -> Vector2i.of(1, 0);
+                case VERTICAL   -> Vector2i.of(0, 1);
+                case DIAGONAL   -> Vector2i.of(1, 1);
+            };
+        }
     }
 
-    static Paint createGradient(String startColorCode, String stopColorCode, Progress progress) {
-        Vector2i start = switch (progress) {
-            case LEFT_TO_RIGHT, TOP_TO_BOTTOM, TOP_LEFT_TO_BOTTOM_RIGHT -> Vector2i.ZERO;
-        };
-        Vector2i stop = switch (progress) {
-            case LEFT_TO_RIGHT -> Vector2i.of(1, 0);
-            case TOP_TO_BOTTOM -> Vector2i.of(0, 1);
-            case TOP_LEFT_TO_BOTTOM_RIGHT -> Vector2i.of(1, 1);
-        };
-        Stop[] stops = new Stop[] {
-            new Stop(0, Color.web(startColorCode)),
-            new Stop(1, Color.web(stopColorCode))
-        };
+    static Paint createGradient(String startColorCode, String stopColorCode, Axis axis) {
         return new LinearGradient(
-            start.x(), start.y(), stop.x(), stop.y(),
+            axis.start().x(), axis.end().y(), axis.end().x(), axis.end().y(),
             true, // proportional coordinates
             CycleMethod.NO_CYCLE,
-            stops
+            new Stop(0, Color.web(startColorCode)), new Stop(1, Color.web(stopColorCode))
         );
     }
 
-    Paint BLUE_BELL_DREAMS = createGradient("#6495ed", "#7c9ec3", Progress.LEFT_TO_RIGHT);
-    Paint FINDING_NEMO = createGradient("#0047ab", "#1ca9c9", Progress.LEFT_TO_RIGHT);
-    Paint PRINCE_TO_KING = createGradient("#4169e1", "#89CFF0", Progress.LEFT_TO_RIGHT);
-    Paint STARRY_NIGHT = createGradient("#003366", "#0f52ba", Progress.LEFT_TO_RIGHT);
-    Paint TOP_GUN_FLYOVER = createGradient("#000080", "#00bfff", Progress.LEFT_TO_RIGHT);
+    Paint BLUE_BELL_DREAMS = createGradient("#6495ed", "#7c9ec3", Axis.HORIZONTAL);
+    Paint FINDING_NEMO = createGradient("#0047ab", "#1ca9c9", Axis.HORIZONTAL);
+    Paint PRINCE_TO_KING = createGradient("#4169e1", "#89CFF0", Axis.HORIZONTAL);
+    Paint STARRY_NIGHT = createGradient("#003366", "#0f52ba", Axis.HORIZONTAL);
+    Paint TOP_GUN_FLYOVER = createGradient("#000080", "#00bfff", Axis.HORIZONTAL);
 }
