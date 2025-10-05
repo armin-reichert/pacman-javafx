de47c03edd6af9c1a5b01651090074f1d8457efb
Armin Reichert <armin.reichert@web.de>
2025-10-05 18:29:55 +0200

    Refac

--- pacman-ui-lib/src/main/java/de/amr/pacmanfx/uilib/rendering/Gradients.java
+++ pacman-ui-lib/src/main/java/de/amr/pacmanfx/uilib/rendering/Gradients.java
@@ -4,7 +4,6 @@ See file LICENSE in repository root directory for details.
 */
 package de.amr.pacmanfx.uilib.rendering;
 
-import de.amr.pacmanfx.lib.Vector2i;
 import javafx.scene.paint.*;
 
 /**
@@ -14,28 +13,28 @@ public interface Gradients {
 
     enum Axis {
         /** Left to Right */
-        HORIZONTAL,
+        HORIZONTAL(1, 0),
         /** Top to Bottom */
-        VERTICAL,
+        VERTICAL(0, 1),
         /** Top-Left to Bottom-Right */
-        DIAGONAL;
+        DIAGONAL(1, 1);
 
-        public Vector2i start() {
-            return Vector2i.ZERO;
-        }
+        public final int endX() { return endX; }
 
-        public Vector2i end() {
-            return switch (this) {
-                case HORIZONTAL -> Vector2i.of(1, 0);
-                case VERTICAL   -> Vector2i.of(0, 1);
-                case DIAGONAL   -> Vector2i.of(1, 1);
-            };
+        public final int endY() { return endY; };
+
+        private final byte endX;
+        private final byte endY;
+
+        Axis(int endX, int endY) {
+            this.endX = (byte) endX;
+            this.endY = (byte) endY;
         }
     }
 
     static Paint createGradient(String startColorCode, String stopColorCode, Axis axis) {
         return new LinearGradient(
-            axis.start().x(), axis.end().y(), axis.end().x(), axis.end().y(),
+            0, 0, axis.endX(), axis.endY(),
             true, // proportional coordinates
             CycleMethod.NO_CYCLE,
             new Stop(0, Color.web(startColorCode)), new Stop(1, Color.web(stopColorCode))
@@ -43,9 +42,9 @@ public interface Gradients {
     }
 
     Paint BLUE_BELL_DREAMS = createGradient("#6495ed", "#7c9ec3", Axis.HORIZONTAL);
-    Paint FINDING_NEMO = createGradient("#0047ab", "#1ca9c9", Axis.HORIZONTAL);
-    Paint MOON_SPOT = createGradient("#8c92ac", "#f5f5f5", Axis.HORIZONTAL);
-    Paint PRINCE_TO_KING = createGradient("#4169e1", "#89CFF0", Axis.HORIZONTAL);
-    Paint STARRY_NIGHT = createGradient("#003366", "#0f52ba", Axis.HORIZONTAL);
-    Paint TOP_GUN_FLYOVER = createGradient("#000080", "#00bfff", Axis.HORIZONTAL);
+    Paint FINDING_NEMO     = createGradient("#0047ab", "#1ca9c9", Axis.HORIZONTAL);
+    Paint MOON_SPOT        = createGradient("#8c92ac", "#f5f5f5", Axis.HORIZONTAL);
+    Paint PRINCE_TO_KING   = createGradient("#4169e1", "#89CFF0", Axis.HORIZONTAL);
+    Paint STARRY_NIGHT     = createGradient("#003366", "#0f52ba", Axis.HORIZONTAL);
+    Paint TOP_GUN_FLYOVER  = createGradient("#000080", "#00bfff", Axis.HORIZONTAL);
 }
