4b311dbfdacbb249affbc4e21aba5d72301531d2
Armin Reichert <armin.reichert@web.de>
2025-10-19 07:41:10 +0200

    Refac

--- pacman-app-tengen-mspacman/src/main/java/de/amr/pacmanfx/tengen/ms_pacman/scenes/Bag.java
+++ pacman-app-tengen-mspacman/src/main/java/de/amr/pacmanfx/tengen/ms_pacman/scenes/Bag.java
@@ -12,19 +12,22 @@ import de.amr.pacmanfx.uilib.animation.SpriteAnimationManager;
 
 public class Bag extends Actor {
 
+    private static final String ANIM_ID_BAG = "bag";
+    private static final String ANIM_ID_JUNIOR = "junior";
+
     private boolean open;
 
     public Bag(TengenMsPacMan_SpriteSheet spriteSheet) {
         var spriteAnimationManager = new SpriteAnimationManager<>(spriteSheet);
-        spriteAnimationManager.setAnimation("junior", SpriteAnimation.builder().ofSprite(spriteSheet.sprite(SpriteID.JUNIOR_PAC)).once());
-        spriteAnimationManager.setAnimation("bag", SpriteAnimation.builder().ofSprite(spriteSheet.sprite(SpriteID.BLUE_BAG)).once());
+        spriteAnimationManager.setAnimation(ANIM_ID_BAG, SpriteAnimation.builder().ofSprite(spriteSheet.sprite(SpriteID.BLUE_BAG)).once());
+        spriteAnimationManager.setAnimation(ANIM_ID_JUNIOR, SpriteAnimation.builder().ofSprite(spriteSheet.sprite(SpriteID.JUNIOR_PAC)).once());
         setAnimationManager(spriteAnimationManager);
         setOpen(false);
     }
 
     public void setOpen(boolean open) {
         this.open = open;
-        animationManager.select(open ? "junior" : "bag");
+        animationManager.select(open ? ANIM_ID_JUNIOR : ANIM_ID_BAG);
     }
 
     public boolean isOpen() {
