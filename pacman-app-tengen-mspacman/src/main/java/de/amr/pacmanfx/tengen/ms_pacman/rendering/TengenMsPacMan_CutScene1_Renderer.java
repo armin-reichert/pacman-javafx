5113c5c4c5abe9eefbd654b0a90f088c85fe11a5
Armin Reichert <armin.reichert@web.de>
2025-11-23 08:25:54 +0100

    License

--- pacman-app-tengen-mspacman/src/main/java/de/amr/pacmanfx/tengen/ms_pacman/rendering/TengenMsPacMan_CutScene1_Renderer.java
+++ pacman-app-tengen-mspacman/src/main/java/de/amr/pacmanfx/tengen/ms_pacman/rendering/TengenMsPacMan_CutScene1_Renderer.java
@@ -1,3 +1,7 @@
+/*
+Copyright (c) 2021-2025 Armin Reichert (MIT License)
+See file LICENSE in repository root directory for details.
+*/
 package de.amr.pacmanfx.tengen.ms_pacman.rendering;
 
 import de.amr.pacmanfx.tengen.ms_pacman.scenes.TengenMsPacMan_CutScene1;
