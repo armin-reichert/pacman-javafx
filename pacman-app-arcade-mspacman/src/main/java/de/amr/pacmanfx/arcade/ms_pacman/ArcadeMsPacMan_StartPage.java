ea4ea841933625f2378a7c7e3632c3e9734b02fc
Armin Reichert <armin.reichert@web.de>
2025-12-11 21:44:20 +0100

    Refac

--- pacman-app-arcade-mspacman/src/main/java/de/amr/pacmanfx/arcade/ms_pacman/ArcadeMsPacMan_StartPage.java
+++ pacman-app-arcade-mspacman/src/main/java/de/amr/pacmanfx/arcade/ms_pacman/ArcadeMsPacMan_StartPage.java
@@ -7,7 +7,6 @@ package de.amr.pacmanfx.arcade.ms_pacman;
 import de.amr.pacmanfx.model.StandardGameVariant;
 import de.amr.pacmanfx.ui.api.GameUI;
 import de.amr.pacmanfx.ui.api.StartPage;
-import de.amr.pacmanfx.ui.layout.StartPagesCarousel;
 import de.amr.pacmanfx.ui.sound.SoundID;
 import de.amr.pacmanfx.uilib.assets.ResourceManager;
 import de.amr.pacmanfx.uilib.widgets.Flyer;
@@ -18,19 +17,22 @@ import javafx.scene.layout.Region;
 import javafx.scene.layout.StackPane;
 
 import static de.amr.pacmanfx.ui.action.CommonGameActions.ACTION_BOOT_SHOW_PLAY_VIEW;
+import static de.amr.pacmanfx.ui.layout.StartPagesCarousel.createDefaultStartButton;
 import static java.util.Objects.requireNonNull;
 
 public class ArcadeMsPacMan_StartPage extends StackPane implements StartPage {
+
+    private static final ResourceManager LOCAL_RESOURCES = () -> ArcadeMsPacMan_StartPage.class;
 
     private final Flyer flyer;
 
     public ArcadeMsPacMan_StartPage(GameUI ui) {
         requireNonNull(ui);
 
-        final ResourceManager rm = () -> ArcadeMsPacMan_StartPage.class;
         flyer = new Flyer(
-            rm.loadImage("graphics/flyer-page-1.jpg"),
-            rm.loadImage("graphics/flyer-page-2.jpg"));
+            LOCAL_RESOURCES.loadImage("graphics/flyer-page-1.jpg"),
+            LOCAL_RESOURCES.loadImage("graphics/flyer-page-2.jpg")
+        );
 
         addEventHandler(KeyEvent.KEY_PRESSED, e -> {
             switch (e.getCode()) {
@@ -47,7 +49,7 @@ public class ArcadeMsPacMan_StartPage extends StackPane implements StartPage {
             }
         });
 
-        Node startButton = StartPagesCarousel.createDefaultStartButton(ui, () -> {
+        Node startButton = createDefaultStartButton(ui, () -> {
             ui.soundManager().stopVoice();
             ACTION_BOOT_SHOW_PLAY_VIEW.executeIfEnabled(ui);
         });
@@ -58,8 +60,8 @@ public class ArcadeMsPacMan_StartPage extends StackPane implements StartPage {
 
     @Override
     public void onEnter(GameUI ui) {
-        ui.selectGameVariant(StandardGameVariant.MS_PACMAN.name());
         flyer.selectPage(0);
+        ui.selectGameVariant(StandardGameVariant.MS_PACMAN.name());
         ui.soundManager().playVoiceAfterSec(1, SoundID.VOICE_FLYER_TEXT);
     }
 
