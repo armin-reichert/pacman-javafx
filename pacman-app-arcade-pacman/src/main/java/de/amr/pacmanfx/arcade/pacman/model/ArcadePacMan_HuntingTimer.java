2c77bcc482008d02814c6e65d57332d35230097f
Armin Reichert <armin.reichert@web.de>
2025-12-26 16:35:57 +0100

    Improved

--- pacman-app-arcade-pacman/src/main/java/de/amr/pacmanfx/arcade/pacman/model/ArcadePacMan_HuntingTimer.java
+++ pacman-app-arcade-pacman/src/main/java/de/amr/pacmanfx/arcade/pacman/model/ArcadePacMan_HuntingTimer.java
@@ -4,27 +4,36 @@ See file LICENSE in repository root directory for details.
 */
 package de.amr.pacmanfx.arcade.pacman.model;
 
+import de.amr.pacmanfx.Validations;
 import de.amr.pacmanfx.lib.TickTimer;
 import de.amr.pacmanfx.model.AbstractHuntingTimer;
 
 public class ArcadePacMan_HuntingTimer extends AbstractHuntingTimer {
 
-    // Ticks of scatter and chasing phases, -1 = INFINITE
-    static final int[] TICKS_LEVEL_1 = {420, 1200, 420, 1200, 300, 1200, 300, -1};
-    static final int[] TICKS_LEVEL_2_3_4 = {420, 1200, 420, 1200, 300, 61980, 1, -1};
-    static final int[] TICKS_LEVEL_5_ON = {300, 1200, 300, 1200, 300, 62262, 1, -1};
+    private static final int NUM_PHASES = 8;
+
+    // Ticks of scatter (index 0, 2, 4, 6) and chasing (1, 3, 5, 7) phases, -1 = forever
+    private static final int[] TICKS_LEVEL_1     = { 420, 1200, 420, 1200, 300,  1200, 300, -1 };
+    private static final int[] TICKS_LEVEL_2_3_4 = { 420, 1200, 420, 1200, 300, 61980,   1, -1 };
+    private static final int[] TICKS_LEVEL_5_ON  = { 300, 1200, 300, 1200, 300, 62262,   1, -1 };
 
     public ArcadePacMan_HuntingTimer() {
-        super("ArcadePacMan-HuntingTimer", 8);
+        super("ArcadePacMan-HuntingTimer", NUM_PHASES);
     }
 
     @Override
     public long phaseDuration(int levelNumber, int phaseIndex) {
-        long ticks = switch (levelNumber) {
-            case 1 -> TICKS_LEVEL_1[phaseIndex];
-            case 2, 3, 4 -> TICKS_LEVEL_2_3_4[phaseIndex];
-            default -> TICKS_LEVEL_5_ON[phaseIndex];
-        };
-        return ticks != -1 ? ticks : TickTimer.INDEFINITE;
+        Validations.requireValidLevelNumber(levelNumber);
+        if (Validations.inClosedRange(phaseIndex, 0, NUM_PHASES - 1)) {
+            long ticks = switch (levelNumber) {
+                case 1 -> TICKS_LEVEL_1[phaseIndex];
+                case 2, 3, 4 -> TICKS_LEVEL_2_3_4[phaseIndex];
+                default -> TICKS_LEVEL_5_ON[phaseIndex];
+            };
+            return ticks != -1 ? ticks : TickTimer.INDEFINITE;
+        }
+        else {
+            throw new IllegalArgumentException("Phase index " + phase() + " is invalid");
+        }
     }
 }
