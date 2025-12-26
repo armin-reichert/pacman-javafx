fe9ae6ccfda6e202846768effd9c3fdbb15cbcdb
Armin Reichert <armin.reichert@web.de>
2025-12-26 16:42:21 +0100

    Improved

--- pacman-app-tengen-mspacman/src/main/java/de/amr/pacmanfx/tengenmspacman/model/TengenMsPacMan_HuntingTimer.java
+++ pacman-app-tengen-mspacman/src/main/java/de/amr/pacmanfx/tengenmspacman/model/TengenMsPacMan_HuntingTimer.java
@@ -4,6 +4,7 @@ See file LICENSE in repository root directory for details.
 */
 package de.amr.pacmanfx.tengenmspacman.model;
 
+import de.amr.pacmanfx.Validations;
 import de.amr.pacmanfx.lib.TickTimer;
 import de.amr.pacmanfx.model.AbstractHuntingTimer;
 
@@ -23,6 +24,13 @@ public class TengenMsPacMan_HuntingTimer extends AbstractHuntingTimer {
 
     @Override
     public long phaseDuration(int levelNumber, int phaseIndex) {
-        return levelNumber <= 4 ? TICKS_LEVEL_1_TO_4[phaseIndex] : TICKS_LEVEL_5_PLUS[phaseIndex];
+        Validations.requireValidLevelNumber(levelNumber);
+        if (Validations.inClosedRange(phaseIndex, 0, NUM_PHASES - 1)) {
+            return levelNumber <= 4
+                ? TICKS_LEVEL_1_TO_4[phaseIndex]
+                : TICKS_LEVEL_5_PLUS[phaseIndex];        }
+        else {
+            throw new IllegalArgumentException("Phase index " + phaseIndex + " is invalid");
+        }
     }
 }
