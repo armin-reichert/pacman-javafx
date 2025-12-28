4067b0c4c7c158b8ed5e5eb6c1a3300c165d250e
Armin Reichert <armin.reichert@web.de>
2025-12-28 07:31:39 +0100

    Refac

--- pacman-app-allgames/src/main/java/module-info.java
+++ pacman-app-allgames/src/main/java/module-info.java
@@ -3,7 +3,7 @@ Copyright (c) 2021-2026 Armin Reichert (MIT License)
 See file LICENSE in repository root directory for details.
 */
 
-module pacmanfx.all {
+module de.amr.pacmanfx.allgames {
     requires javafx.graphics;
     requires de.amr.pacmanfx.core;
     requires de.amr.pacmanfx.uilib;
