1fdab76239518785d49c0648e9bf2277b9b32849
Armin Reichert <armin.reichert@web.de>
2025-12-31 10:33:52 +0100

    Modify test to use new siren player

--- pacman-app-arcade-mspacman/src/main/java/de/amr/pacmanfx/arcade/ms_pacman/SoundTest.java
+++ pacman-app-arcade-mspacman/src/main/java/de/amr/pacmanfx/arcade/ms_pacman/SoundTest.java
@@ -1,13 +1,17 @@
 package de.amr.pacmanfx.arcade.ms_pacman;
 
+import de.amr.pacmanfx.ui.sound.SirenPlayer;
 import de.amr.pacmanfx.uilib.assets.ResourceManager;
 import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
 import javafx.application.Platform;
-import javafx.scene.media.AudioClip;
 import javafx.util.Duration;
 
+import java.net.URL;
+
 public class SoundTest implements ResourceManager {
+
+    static final int SINGLE_TEST_DURATION_SECONDS = 5;
 
     static final String SOUND_PATH = "/de/amr/pacmanfx/arcade/ms_pacman/sound/";
 
@@ -16,46 +20,39 @@ public class SoundTest implements ResourceManager {
         return SoundTest.class;
     }
 
-    public static void main(String[] args) {
-        SoundTest test = new SoundTest();
-        Platform.startup(test::runAllTests);
+    static void main() {
+        Platform.startup(() -> new SoundTest().runAllTests(1.0));
     }
 
-    static final int TEST_DURATION = 5;
-
     private final Timeline timeline = new Timeline();
-    private final AudioClip[] players = new AudioClip[4];
+    private final SirenPlayer sirenPlayer;
 
-    private void runAllTests() {
-        timeline.getKeyFrames().add(new KeyFrame(Duration.seconds(TEST_DURATION),  e -> testGhostNoise(1)));
-        timeline.getKeyFrames().add(new KeyFrame(Duration.seconds(2*TEST_DURATION),  e -> {
-            stopGhostNoise(1);
-            testGhostNoise(2);
+    public SoundTest() {
+        sirenPlayer = new SirenPlayer(sirenURL(1), sirenURL(2), sirenURL(3), sirenURL(4));
+    }
+
+    private URL sirenURL(int number) {
+        return url(SOUND_PATH + "GhostNoise%d.wav".formatted(number));
+    }
+
+    private void runAllTests(double volume) {
+        timeline.getKeyFrames().add(new KeyFrame(Duration.seconds(SINGLE_TEST_DURATION_SECONDS), _ -> sirenPlayer.playSiren(1, volume)));
+        timeline.getKeyFrames().add(new KeyFrame(Duration.seconds(2* SINGLE_TEST_DURATION_SECONDS), _ -> {
+            sirenPlayer.stopSiren(1);
+            sirenPlayer.playSiren(2, volume);
         }));
-        timeline.getKeyFrames().add(new KeyFrame(Duration.seconds(3*TEST_DURATION),  e -> {
-            stopGhostNoise(2);
-            testGhostNoise(3);
+        timeline.getKeyFrames().add(new KeyFrame(Duration.seconds(3* SINGLE_TEST_DURATION_SECONDS), _ -> {
+            sirenPlayer.stopSiren(2);
+            sirenPlayer.playSiren(3, volume);
         }));
-        timeline.getKeyFrames().add(new KeyFrame(Duration.seconds(4*TEST_DURATION),  e -> {
-            stopGhostNoise(3);
-            testGhostNoise(4);
+        timeline.getKeyFrames().add(new KeyFrame(Duration.seconds(4* SINGLE_TEST_DURATION_SECONDS), _ -> {
+            sirenPlayer.stopSiren(3);
+            sirenPlayer.playSiren(4, volume);
         }));
-        timeline.getKeyFrames().add(new KeyFrame(Duration.seconds(5*TEST_DURATION),  e -> {
-            stopGhostNoise(4);
+        timeline.getKeyFrames().add(new KeyFrame(Duration.seconds(5* SINGLE_TEST_DURATION_SECONDS), _ -> {
+            sirenPlayer.stopSiren(4);
             Platform.exit();
         }));
         timeline.play();
-    }
-
-    private void stopGhostNoise(int number) {
-        players[number-1].stop();
-    }
-
-    private void testGhostNoise(int number) {
-        String url = url(SOUND_PATH + "GhostNoise%d.wav".formatted(number)).toExternalForm();
-        int i = number - 1;
-        players[i] = new AudioClip(url);
-        players[i].setCycleCount(10);
-        players[i].play();
     }
 }
