4cec26d0bb3bd56c7dd9f94f91b987d164ac9e05
Armin Reichert <armin.reichert@web.de>
2026-01-02 07:31:41 +0100

    Refac

--- pacman-ui-lib/src/main/java/de/amr/pacmanfx/uilib/assets/UIPreferences.java
+++ pacman-ui-lib/src/main/java/de/amr/pacmanfx/uilib/assets/UIPreferences.java
@@ -15,23 +15,36 @@ import java.util.prefs.Preferences;
 
 import static de.amr.pacmanfx.uilib.Ufx.formatColorHex;
 
-public class UIPreferences {
+public abstract class UIPreferences {
 
     private final Map<String, Object> defaultValueMap = new HashMap<>();
     private final Preferences prefs;
 
-    public UIPreferences(Class<?> javaClass) {
-        prefs = Preferences.userNodeForPackage(javaClass);
-        defineDefaultValues();
+    public UIPreferences(Class<?> packageProvidingClass) {
+        prefs = Preferences.userNodeForPackage(packageProvidingClass);
+        storeDefaultValues();
         if (!isBackingStoreAccessible()) {
             Logger.error("User preferences could not be accessed, using default values!");
         } else {
-            addMissingValues();
+            final List<String> allKeys = defaultValueMap.keySet().stream().sorted().toList();
+            // add missing values
+            try {
+                final Set<String> prefKeys = new HashSet<>(Arrays.asList(prefs.keys()));
+                for (String key : allKeys) {
+                    if (!prefKeys.contains(key)) {
+                        final Object defaultValue = defaultValueMap.get(key);
+                        store(key, defaultValue);
+                        Logger.info("Added missing preference '{}'='{}'", key, defaultValue);
+                    }
+                }
+            } catch (BackingStoreException x) {
+                Logger.error("Could not access preferences store to add missing keys");
+                Logger.error(x);
+            }
         }
     }
 
-    protected void defineDefaultValues() {
-    }
+    protected abstract void storeDefaultValues();
 
     public boolean isBackingStoreAccessible() {
         try {
@@ -43,92 +56,75 @@ public class UIPreferences {
         }
     }
 
-    private void addMissingValues() {
-        try {
-            Set<String> prefKeys = new HashSet<>(Arrays.asList(prefs.keys()));
-            List<String> allKeys = defaultValueMap.keySet().stream().sorted().toList();
-            for (String key : allKeys) {
-                if (!prefKeys.contains(key)) {
-                    Object defaultValue = defaultValueMap.get(key);
-                    storeValue(key, defaultValue);
-                    Logger.info("Added missing preference '{}'='{}'", key, defaultValue);
-                }
-            }
-        } catch (BackingStoreException x) {
-            Logger.error("Could not access preferences store to add missing keys");
-            Logger.error(x);
-        }
-    }
-
-    public void storeValue(String key, Object defaultValue) {
+    public void store(String key, Object defaultValue) {
         switch (defaultValue) {
             case Float floatValue -> prefs.putFloat(key, floatValue);
             case Integer intValue -> prefs.putInt(key, intValue);
-            default -> prefs.put(key, String.valueOf(defaultValue));
+            default               -> prefs.put(key, String.valueOf(defaultValue));
         }
         defaultValueMap.put(key, defaultValue);
     }
 
-    public void storeDefaultValue(String key, Object defaultValue) {
+    public void storeDefault(String key, Object defaultValue) {
         defaultValueMap.put(key, defaultValue);
     }
 
     public void storeColor(String key, Color color) {
-        String colorSpec = formatColorHex(color);
+        final String colorSpec = formatColorHex(color);
         prefs.put(key, colorSpec);
         defaultValueMap.put(key, colorSpec);
     }
 
     public void storeDefaultColor(String key, Color color) {
-        String colorSpec = formatColorHex(color);
+        final String colorSpec = formatColorHex(color);
         defaultValueMap.put(key, colorSpec);
     }
 
     public Color getColor(String colorKey) {
-        String colorValue = prefs.get(colorKey, getDefaultValue(colorKey, String.class));
+        final String colorValue = prefs.get(colorKey, getDefaultValue(colorKey, String.class));
         return Color.valueOf(colorValue);
     }
 
     public void storeFont(String key, Font font) {
-        String familyKey = key + ".family";
+        final String familyKey = key + ".family";
         prefs.put(familyKey, font.getFamily());
         defaultValueMap.put(familyKey, font.getFamily());
 
-        String styleKey = key + ".style";
+        final String styleKey = key + ".style";
         prefs.put(styleKey, font.getStyle());
         defaultValueMap.put(styleKey, font.getStyle());
 
-        String sizeKey = key + ".size";
+        final String sizeKey = key + ".size";
         prefs.putFloat(sizeKey, (float) font.getSize());
         defaultValueMap.put(sizeKey, (float) font.getSize());
     }
 
     public void storeDefaultFont(String key, Font font) {
-        String familyKey = key + ".family";
+        final String familyKey = key + ".family";
         defaultValueMap.put(familyKey, font.getFamily());
 
-        String styleKey = key + ".style";
+        final String styleKey = key + ".style";
         defaultValueMap.put(styleKey, font.getStyle());
 
-        String sizeKey = key + ".size";
+        final String sizeKey = key + ".size";
         defaultValueMap.put(sizeKey, (float) font.getSize());
     }
 
     public Font getFont(String key) {
-        String familyKey = key + ".family";
-        String family = prefs.get(familyKey, getDefaultValue(familyKey, String.class));
+        final String familyKey = key + ".family";
+        final String family = prefs.get(familyKey, getDefaultValue(familyKey, String.class));
 
-        String styleKey = key + ".style";
-        String style = prefs.get(styleKey, getDefaultValue(styleKey, String.class));
+        final String styleKey = key + ".style";
+        final String style = prefs.get(styleKey, getDefaultValue(styleKey, String.class));
 
-        String sizeKey = key + ".size";
-        float size = prefs.getFloat(sizeKey, getDefaultValue(sizeKey, Float.class));
+        final String sizeKey = key + ".size";
+        final float size = prefs.getFloat(sizeKey, getDefaultValue(sizeKey, Float.class));
 
         return Font.font(family, FontWeight.findByName(style), size);
     }
 
     public <T> T getDefaultValue(String key, Class<T> type) {
-        Object defaultValue = defaultValueMap.get(key);
+        final Object defaultValue = defaultValueMap.get(key);
         if (type.isInstance(defaultValue)) {
             return type.cast(defaultValue);
         }
@@ -137,11 +133,11 @@ public class UIPreferences {
     }
 
     private Object getValue(String key) {
-        Object defaultValue = defaultValueMap.get(key);
+        final Object defaultValue = defaultValueMap.get(key);
         return switch (defaultValue) {
             case Float floatValue -> prefs.getFloat(key, floatValue);
             case Integer intValue -> prefs.getInt(key, intValue);
-            default -> prefs.get(key, String.valueOf(defaultValue));
+            default               -> prefs.get(key, String.valueOf(defaultValue));
         };
     }
 
--- pacman-ui/src/main/java/de/amr/pacmanfx/ui/api/GameUI_Preferences.java
+++ pacman-ui/src/main/java/de/amr/pacmanfx/ui/api/GameUI_Preferences.java
@@ -17,31 +17,31 @@ public class GameUI_Preferences extends UIPreferences {
         super(GameUI_Preferences.class);
     }
 
-    protected void defineDefaultValues() {
-        storeDefaultValue("3d.bonus.symbol.width", 8.0f);
-        storeDefaultValue("3d.bonus.points.width", 1.8f * 8.0f);
-        storeDefaultValue("3d.energizer.radius", 3.5f);
-        storeDefaultValue("3d.energizer.scaling.min", 0.2f);
-        storeDefaultValue("3d.energizer.scaling.max", 1.0f);
-        storeDefaultValue("3d.floor.padding", 5.0f);
-        storeDefaultValue("3d.floor.thickness", 0.5f);
-        storeDefaultValue("3d.ghost.size", 15.5f);
-        storeDefaultValue("3d.house.base_height", 12.0f);
-        storeDefaultValue("3d.house.opacity", 0.4f);
-        storeDefaultValue("3d.house.sensitivity", 1.5f * TS);
-        storeDefaultValue("3d.house.wall_thickness", 2.5f);
-        storeDefaultValue("3d.level_counter.symbol_size", 10.0f);
-        storeDefaultValue("3d.level_counter.elevation", 6f);
-        storeDefaultValue("3d.lives_counter.capacity", 5);
+    protected void storeDefaultValues() {
+        storeDefault("3d.bonus.symbol.width", 8.0f);
+        storeDefault("3d.bonus.points.width", 1.8f * 8.0f);
+        storeDefault("3d.energizer.radius", 3.5f);
+        storeDefault("3d.energizer.scaling.min", 0.2f);
+        storeDefault("3d.energizer.scaling.max", 1.0f);
+        storeDefault("3d.floor.padding", 5.0f);
+        storeDefault("3d.floor.thickness", 0.5f);
+        storeDefault("3d.ghost.size", 15.5f);
+        storeDefault("3d.house.base_height", 12.0f);
+        storeDefault("3d.house.opacity", 0.4f);
+        storeDefault("3d.house.sensitivity", 1.5f * TS);
+        storeDefault("3d.house.wall_thickness", 2.5f);
+        storeDefault("3d.level_counter.symbol_size", 10.0f);
+        storeDefault("3d.level_counter.elevation", 6f);
+        storeDefault("3d.lives_counter.capacity", 5);
         storeDefaultColor("3d.lives_counter.pillar_color", Color.grayRgb(120));
         storeDefaultColor("3d.lives_counter.plate_color",  Color.grayRgb(180));
-        storeDefaultValue("3d.lives_counter.shape_size", 12.0f);
-        storeDefaultValue("3d.obstacle.base_height", 4.0f);
-        storeDefaultValue("3d.obstacle.corner_radius", 4.0f);
-        storeDefaultValue("3d.obstacle.opacity", 1.0f);
-        storeDefaultValue("3d.obstacle.wall_thickness", 2.25f);
-        storeDefaultValue("3d.pac.size", 16.0f);
-        storeDefaultValue("3d.pellet.radius", 1.0f);
+        storeDefault("3d.lives_counter.shape_size", 12.0f);
+        storeDefault("3d.obstacle.base_height", 4.0f);
+        storeDefault("3d.obstacle.corner_radius", 4.0f);
+        storeDefault("3d.obstacle.opacity", 1.0f);
+        storeDefault("3d.obstacle.wall_thickness", 2.25f);
+        storeDefault("3d.pac.size", 16.0f);
+        storeDefault("3d.pellet.radius", 1.0f);
 
         // "Kornblumenblau, sind die Augen der Frauen beim Weine. Hicks!"
         storeDefaultColor("context_menu.title.fill", Color.CORNFLOWERBLUE);
@@ -51,6 +51,6 @@ public class GameUI_Preferences extends UIPreferences {
         storeDefaultColor("debug_text.stroke", Color.GRAY);
         storeDefaultFont("debug_text.font", Font.font("Sans", 16.0f));
 
-        storeDefaultValue("scene2d.max_scaling", 5.0f);
+        storeDefault("scene2d.max_scaling", 5.0f);
     }
 }
