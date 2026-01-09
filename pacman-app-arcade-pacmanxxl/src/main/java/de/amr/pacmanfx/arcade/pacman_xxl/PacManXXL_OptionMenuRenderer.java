91d4ea8af546ee88fd272de06adf2e29c59b13b0
Armin Reichert <armin.reichert@web.de>
2026-01-09 09:31:15 +0100

    Refac

--- pacman-app-arcade-pacmanxxl/src/main/java/de/amr/pacmanfx/arcade/pacman_xxl/PacManXXL_OptionMenuRenderer.java
+++ pacman-app-arcade-pacmanxxl/src/main/java/de/amr/pacmanfx/arcade/pacman_xxl/PacManXXL_OptionMenuRenderer.java
@@ -6,7 +6,6 @@ package de.amr.pacmanfx.arcade.pacman_xxl;
 
 import de.amr.pacmanfx.uilib.widgets.OptionMenu;
 import de.amr.pacmanfx.uilib.widgets.OptionMenuRenderer;
-import de.amr.pacmanfx.uilib.widgets.OptionMenuStyle;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.paint.Color;
 
@@ -14,49 +13,43 @@ import static de.amr.pacmanfx.Globals.TS;
 
 public class PacManXXL_OptionMenuRenderer extends OptionMenuRenderer {
 
+    public static final float LINE_SKIP = TS(2);
+
     public PacManXXL_OptionMenuRenderer(Canvas canvas) {
         super(canvas);
     }
 
+    private void drawText(String text, Color fill, double x, double y) {
+        ctx.setFill(fill);
+        ctx.fillText(text, x, y);
+    }
+
     @Override
     protected void drawUsageInfo(OptionMenu menu) {
-        final OptionMenuStyle style = menu.style();
-        final Color normal = style.hintTextFill(), bright = style.entryValueFill();
+        final Color textFill = menu.style().hintTextFill();
+        final Color valueFill = menu.style().entryValueFill();
 
-        ctx.setFont(style.textFont());
+        ctx.setFont(menu.style().textFont());
 
         double y = TS(menu.numTilesY() - 8);
-        ctx.setFill(normal);
-        ctx.fillText("SELECT OPTIONS WITH", TS(6), y);
-        ctx.setFill(bright);
-        ctx.fillText("UP", TS(26), y);
-        ctx.setFill(normal);
-        ctx.fillText("AND", TS(29), y);
-        ctx.setFill(bright);
-        ctx.fillText("DOWN", TS(33), y);
+        drawText("SELECT OPTIONS WITH", textFill, TS(6), y);
+        drawText("UP", valueFill, TS(26), y);
+        drawText("AND", textFill, TS(29), y);
+        drawText("DOWN", valueFill, TS(33), y);
 
-        y += TS(2);
-        ctx.setFill(normal);
-        ctx.fillText("PRESS", TS(8), y);
-        ctx.setFill(bright);
-        ctx.fillText("SPACE", TS(14), y);
-        ctx.setFill(normal);
-        ctx.fillText("TO CHANGE VALUE", TS(20), y);
+        y += LINE_SKIP;
+        drawText("PRESS", textFill, TS(8), y);
+        drawText("SPACE", valueFill, TS(14), y);
+        drawText("TO CHANGE VALUE", textFill, TS(20), y);
 
-        y += TS(2);
-        ctx.setFill(normal);
-        ctx.fillText("PRESS", TS(10), y);
-        ctx.setFill(bright);
-        ctx.fillText("E", TS(16), y);
-        ctx.setFill(normal);
-        ctx.fillText("TO OPEN EDITOR", TS(18), y);
+        y += LINE_SKIP;
+        drawText("PRESS", textFill, TS(10), y);
+        drawText("E", valueFill, TS(16), y);
+        drawText("TO OPEN EDITOR", textFill, TS(18), y);
 
-        y += TS(2);
-        ctx.setFill(normal);
-        ctx.fillText("PRESS", TS(11), y);
-        ctx.setFill(bright);
-        ctx.fillText("ENTER", TS(17), y);
-        ctx.setFill(normal);
-        ctx.fillText("TO START", TS(23), y);
+        y += LINE_SKIP;
+        drawText("PRESS", textFill, TS(11), y);
+        drawText("ENTER", valueFill, TS(17), y);
+        drawText("TO START", textFill, TS(23), y);
     }
 }
