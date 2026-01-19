1cfb7c2a31b581cf221be4dded8d2e4ac8a38b04
Armin Reichert <armin.reichert@web.de>
2026-01-19 07:41:46 +0100

    Refac

--- pacman-app-arcade-pacman/src/main/java/de/amr/pacmanfx/arcade/pacman/app/ArcadePacMan_App.java
+++ pacman-app-arcade-pacman/src/main/java/de/amr/pacmanfx/arcade/pacman/app/ArcadePacMan_App.java
@@ -7,14 +7,16 @@ package de.amr.pacmanfx.arcade.pacman.app;
 import de.amr.pacmanfx.arcade.pacman.ArcadePacMan_StartPage;
 import de.amr.pacmanfx.arcade.pacman.ArcadePacMan_UIConfig;
 import de.amr.pacmanfx.arcade.pacman.model.ArcadePacMan_GameModel;
+import de.amr.pacmanfx.model.Game;
+import de.amr.pacmanfx.model.StandardGameVariant;
 import de.amr.pacmanfx.ui.GameUI;
 import de.amr.pacmanfx.ui.GameUI_Builder;
-import de.amr.pacmanfx.ui.GameUI_Config;
 import de.amr.pacmanfx.ui.GameUI_Implementation;
 import de.amr.pacmanfx.ui.dashboard.CommonDashboardID;
 import de.amr.pacmanfx.ui.dashboard.DashboardID;
 import javafx.application.Application;
 import javafx.application.Platform;
+import javafx.geometry.Dimension2D;
 import javafx.stage.Screen;
 import javafx.stage.Stage;
 import org.tinylog.Logger;
@@ -24,16 +26,15 @@ import java.util.List;
 import java.util.Map;
 
 import static de.amr.pacmanfx.Globals.THE_GAME_BOX;
-import static de.amr.pacmanfx.model.StandardGameVariant.ARCADE_PACMAN;
 
 public class ArcadePacMan_App extends Application {
 
-    private static final String PACMAN_GAME = ARCADE_PACMAN.name();
+    private static final String ARCADE_PACMAN_GAME = StandardGameVariant.ARCADE_PACMAN.name();
 
     private static final float ASPECT_RATIO = 1.2f; // 12:10 aspect ratio
     private static final float USED_HEIGHT_FRACTION = 0.8f;  // 80% of available height
 
-    private static final DashboardID[] DASHBOARD_IDS = {
+    private static final List<DashboardID> DASHBOARD_IDS = List.of(
         CommonDashboardID.GENERAL,
         CommonDashboardID.GAME_CONTROL,
         CommonDashboardID.SETTINGS_3D,
@@ -43,25 +44,28 @@ public class ArcadePacMan_App extends Application {
         CommonDashboardID.KEYS_GLOBAL,
         CommonDashboardID.KEYS_LOCAL,
         CommonDashboardID.ABOUT
-    };
+    );
 
     private GameUI ui;
 
-    @Override
-    public void start(Stage primaryStage) {
+    private Dimension2D sceneSize() {
         final double screenHeight = Screen.getPrimary().getVisualBounds().getHeight();
         final int height = (int) Math.round(USED_HEIGHT_FRACTION * screenHeight);
         final int width  = Math.round(ASPECT_RATIO * height);
-        final String useBuilder = getParameters().getNamed().getOrDefault("use_builder", "true");
+        return new Dimension2D(width, height);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
         try {
-            ui = Boolean.parseBoolean(useBuilder)
-                ? createUI_WithBuilder(primaryStage, width, height)
-                : createUI_WithoutBuilder(primaryStage, width, height);
+            final String parameter = getParameters().getNamed().getOrDefault("use_builder", "true");
+            final boolean useBuilder = Boolean.parseBoolean(parameter);
+            ui = useBuilder ? createUI_WithBuilder(primaryStage, sceneSize()) : createUI(primaryStage, sceneSize());
             ui.show();
         }
         catch (RuntimeException x) {
+            Logger.error("An error occurred while UI creation.");
             Logger.error(x);
-            Logger.error("An error occurred starting the game.");
             Platform.exit();
         }
     }
@@ -71,25 +75,29 @@ public class ArcadePacMan_App extends Application {
         ui.terminate();
     }
 
-    private GameUI createUI_WithoutBuilder(Stage stage, double sceneWidth, double sceneHeight) {
-        final File highScoreFile = THE_GAME_BOX.highScoreFile(PACMAN_GAME);
-        THE_GAME_BOX.registerGame(PACMAN_GAME, new ArcadePacMan_GameModel(THE_GAME_BOX, highScoreFile));
-        final Map<String, Class<? extends GameUI_Config>> uiConfigMap = Map.of(PACMAN_GAME, ArcadePacMan_UIConfig.class);
-        final var ui = new GameUI_Implementation(uiConfigMap, THE_GAME_BOX, stage, sceneWidth, sceneHeight);
+    private GameUI createUI(Stage stage, Dimension2D size) {
+        final File highScoreFile = THE_GAME_BOX.highScoreFile(ARCADE_PACMAN_GAME);
+        final Game game = new ArcadePacMan_GameModel(THE_GAME_BOX, highScoreFile);
+        THE_GAME_BOX.registerGame(ARCADE_PACMAN_GAME, game);
+        final var ui = new GameUI_Implementation(
+            Map.of(ARCADE_PACMAN_GAME, ArcadePacMan_UIConfig.class),
+            THE_GAME_BOX,
+            stage,
+            size.getWidth(), size.getHeight());
         ui.views().startPagesView().addStartPage(new ArcadePacMan_StartPage());
-        ui.views().playView().dashboard().addSections(ui, List.of(DASHBOARD_IDS));
+        ui.views().playView().dashboard().addSections(ui, DASHBOARD_IDS);
         return ui;
     }
 
-    private GameUI createUI_WithBuilder(Stage stage, double sceneWidth, double sceneHeight) {
-        return GameUI_Builder.create(stage, sceneWidth, sceneHeight)
+    private GameUI createUI_WithBuilder(Stage stage, Dimension2D size) {
+        return GameUI_Builder.create(stage, size.getWidth(), size.getHeight())
             .game(
-                PACMAN_GAME,
+                ARCADE_PACMAN_GAME,
                 ArcadePacMan_GameModel.class,
                 ArcadePacMan_UIConfig.class
             )
-            .startPage(ArcadePacMan_StartPage.class, PACMAN_GAME)
-            .dashboard(DASHBOARD_IDS)
+            .startPage(ArcadePacMan_StartPage.class, ARCADE_PACMAN_GAME)
+            .dashboard(DASHBOARD_IDS.toArray(DashboardID[]::new))
             .build();
     }
 }
