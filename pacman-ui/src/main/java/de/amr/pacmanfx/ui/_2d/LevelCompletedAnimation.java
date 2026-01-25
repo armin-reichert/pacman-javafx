4ee75222850c9a302a25e94db38dd0eaf88d85d1
Armin Reichert <armin.reichert@web.de>
2026-01-25 12:29:42 +0100

    Javadoc (thank you Grok)

--- pacman-ui/src/main/java/de/amr/pacmanfx/ui/_2d/LevelCompletedAnimation.java
+++ pacman-ui/src/main/java/de/amr/pacmanfx/ui/_2d/LevelCompletedAnimation.java
@@ -19,25 +19,40 @@ import static de.amr.pacmanfx.uilib.animation.AnimationSupport.pauseSecThen;
 import static java.util.Objects.requireNonNull;
 
 /**
- * Animation played when level is complete. Consists of the following steps:
+ * Animation played when a level is complete.
+ * <p>
+ * Consists of the following steps:
  * <pre>
  *     Time (sec)         Action
  *     0.0                Start animation
  *     1.5                Hide ghosts
- *     2.0                Start n flashing cycles, each cycle takes 1/3 sec
+ *     2.0                Start n flashing cycles, each cycle takes ~1/3 sec
  *     2.0 + n * 1/3 sec  Wait 1 sec
  * </pre>
+ * </p>
+ * <p>
  * After each flashing cycle, the flashing index is incremented. This is used by the Tengen play scene renderer to
- * draw a different map color for each flashing cycle (only for the non-ARCADE maps starting at level 28)
+ * draw a different map color for each flashing cycle (only for the non-ARCADE maps starting at level 28).
+ * </p>
  */
 public class LevelCompletedAnimation {
 
+    /**
+     * State of the flashing part of the level-complete animation. Used by renderers to query the current flashing status.
+     */
     public interface FlashingState {
+
+        /** @return true if the map should be drawn highlighted (bright) in the current frame */
         boolean isHighlighted();
+
+        /** @return true if the flashing animation is currently running */
         boolean isFlashing();
+
+        /** @return the current flashing cycle index (0-based) */
         int flashingIndex();
     }
 
+    /** Default duration of a single flashing cycle in milliseconds (~1/3 sec). */
     public static final int DEFAULT_SINGLE_FLASH_MILLIS = 333;
 
     private static class FlashingAnimation implements FlashingState {
@@ -48,10 +63,10 @@ public class LevelCompletedAnimation {
 
         private FlashingAnimation(int numFlashes, long singleFlashMillis) {
             timeline = new Timeline(
-                new KeyFrame(Duration.ZERO,                             _ -> reset()),
-                new KeyFrame(Duration.millis(singleFlashMillis * 0.25), _ -> setHighlighted(true)),
-                new KeyFrame(Duration.millis(singleFlashMillis * 0.75), _ -> setHighlighted(false)),
-                new KeyFrame(Duration.millis(singleFlashMillis),        _ -> enterNextCycle(numFlashes))
+                    new KeyFrame(Duration.ZERO,                             _ -> reset()),
+                    new KeyFrame(Duration.millis(singleFlashMillis * 0.25), _ -> setHighlighted(true)),
+                    new KeyFrame(Duration.millis(singleFlashMillis * 0.75), _ -> setHighlighted(false)),
+                    new KeyFrame(Duration.millis(singleFlashMillis),        _ -> enterNextCycle(numFlashes))
             );
             timeline.setCycleCount(numFlashes);
         }
@@ -92,20 +107,39 @@ public class LevelCompletedAnimation {
     private FlashingAnimation flashingAnimation;
     private Animation animation;
 
+    /**
+     * Creates a level-complete animation for the given level using the default flashing duration.
+     *
+     * @param level      the game level
+     * @param onFinished callback executed when the animation finishes
+     */
     public LevelCompletedAnimation(GameLevel level, Runnable onFinished) {
         this(level, DEFAULT_SINGLE_FLASH_MILLIS, onFinished);
     }
 
+    /**
+     * Creates a level-complete animation for the given level.
+     *
+     * @param level             the game level
+     * @param singleFlashMillis duration of a single flashing cycle in milliseconds
+     * @param onFinished        callback executed when the animation finishes
+     */
     public LevelCompletedAnimation(GameLevel level, int singleFlashMillis, Runnable onFinished) {
         this.level = requireNonNull(level);
         this.singleFlashMillis = singleFlashMillis;
         this.onFinished = onFinished;
     }
 
+    /**
+     * Returns the current flashing state if flashing is active for this level.
+     *
+     * @return optional flashing state (empty if the level has no flashing)
+     */
     public Optional<FlashingState> flashingState() {
         return Optional.ofNullable(flashingAnimation);
     }
 
+    /** Starts (or restarts) the level-complete animation. */
     public void play() {
         if (animation == null) {
             createAnimation();
@@ -114,17 +148,16 @@ public class LevelCompletedAnimation {
     }
 
     private void createAnimation() {
-        final Animation hideGhosts = pauseSecThen(1.5, () -> hideGhosts(level));
+        final Animation hideGhosts = pauseSecThen(1.5, this::hideGhosts);
         if (level.numFlashes() > 0) {
             flashingAnimation = new FlashingAnimation(level.numFlashes(), singleFlashMillis);
             animation = new SequentialTransition(
-                hideGhosts,
-                pauseSec(0.5),
-                flashingAnimation.timeline,
-                pauseSec(1)
+                    hideGhosts,
+                    pauseSec(0.5),
+                    flashingAnimation.timeline,
+                    pauseSec(1)
             );
-        }
-        else {
+        } else {
             animation = new SequentialTransition(hideGhosts, pauseSec(1.5));
         }
         if (onFinished != null) {
@@ -132,7 +165,7 @@ public class LevelCompletedAnimation {
         }
     }
 
-    private void hideGhosts(GameLevel level) {
+    private void hideGhosts() {
         level.ghosts().forEach(Ghost::hide);
     }
 }
