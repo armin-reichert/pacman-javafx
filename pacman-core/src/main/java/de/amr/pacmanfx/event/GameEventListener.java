7d1e91fc3a8e1d29df725211f9ece0548a260112
Armin Reichert <armin.reichert@web.de>
2026-01-27 11:46:36 +0100

    New game event implementation

--- pacman-core/src/main/java/de/amr/pacmanfx/event/GameEventListener.java
+++ pacman-core/src/main/java/de/amr/pacmanfx/event/GameEventListener.java
@@ -1,3 +1,7 @@
+/*
+Copyright (c) 2021-2026 Armin Reichert (MIT License)
+See file LICENSE in repository root directory for details.
+*/
 package de.amr.pacmanfx.event;
 
 import org.tinylog.Logger;
