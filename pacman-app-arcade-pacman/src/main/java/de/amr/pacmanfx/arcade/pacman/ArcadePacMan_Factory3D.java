10dc918fe57a985769e79e8ba6df80d3538346eb
Armin Reichert <armin.reichert@web.de>
2026-03-13 12:25:04 +0100

    Refac

--- pacman-app-arcade-pacman/src/main/java/de/amr/pacmanfx/arcade/pacman/ArcadePacMan_Factory3D.java
+++ pacman-app-arcade-pacman/src/main/java/de/amr/pacmanfx/arcade/pacman/ArcadePacMan_Factory3D.java
@@ -3,18 +3,27 @@
  */
 package de.amr.pacmanfx.arcade.pacman;
 
+import de.amr.pacmanfx.lib.math.Vector2i;
 import de.amr.pacmanfx.model.actors.Ghost;
 import de.amr.pacmanfx.model.actors.Pac;
-import de.amr.pacmanfx.ui.config.EntityConfig;
-import de.amr.pacmanfx.ui.config.GhostConfig;
-import de.amr.pacmanfx.ui.config.PacConfig;
+import de.amr.pacmanfx.ui.config.*;
 import de.amr.pacmanfx.ui.d3.Factory3D;
+import de.amr.pacmanfx.ui.d3.Pellet3D;
 import de.amr.pacmanfx.uilib.animation.AnimationRegistry;
 import de.amr.pacmanfx.uilib.model3D.Models3D;
 import de.amr.pacmanfx.uilib.model3D.actor.MutableGhost3D;
 import de.amr.pacmanfx.uilib.model3D.actor.PacBody;
 import de.amr.pacmanfx.uilib.model3D.actor.PacMan3D;
+import de.amr.pacmanfx.uilib.model3D.world.Energizer3D;
+import javafx.geometry.Bounds;
+import javafx.scene.paint.PhongMaterial;
+import javafx.scene.shape.Mesh;
+import javafx.scene.shape.MeshView;
+import javafx.scene.shape.Sphere;
+import javafx.scene.transform.Rotate;
 
+import static de.amr.pacmanfx.Globals.HTS;
+import static de.amr.pacmanfx.Globals.TS;
 import static java.util.Objects.requireNonNull;
 
 public class ArcadePacMan_Factory3D implements Factory3D {
@@ -74,5 +83,39 @@ public class ArcadePacMan_Factory3D implements Factory3D {
             pacConfig.eyesColor(),
             pacConfig.palateColor()
         );
+    }
+
+    @Override
+    public Pellet3D createPellet3D(PelletConfig3D pelletConfig, PhongMaterial material, Vector2i tile) {
+        final Mesh pelletMesh = Models3D.PELLET_MODEL.mesh();
+        final var dummy = new MeshView(pelletMesh);
+        final Bounds bounds = dummy.getBoundsInLocal();
+        final double maxExtent = Math.max(Math.max(bounds.getWidth(), bounds.getHeight()), bounds.getDepth());
+        final double scaling = (2 * pelletConfig.radius()) / maxExtent;
+        final Mesh scaledPelletMesh = Models3D.createScaledMesh(pelletMesh, scaling);
+        final var pellet3D = new Pellet3D(scaledPelletMesh, tile);
+        pellet3D.setMaterial(material);
+        //TODO fix rotation in OBJ file
+        pellet3D.setRotationAxis(Rotate.Z_AXIS);
+        pellet3D.setRotate(90);
+
+        pellet3D.setTranslateX(tile.x() * TS + HTS);
+        pellet3D.setTranslateY(tile.y() * TS + HTS);
+
+        return pellet3D;
+    }
+
+    @Override
+    public Energizer3D createEnergizer3D(EnergizerConfig3D config, AnimationRegistry animationRegistry, PhongMaterial material, Vector2i tile) {
+        final var energizer3D = new Energizer3D(animationRegistry, tile);
+        energizer3D.setShapeFactory(() -> {
+            final var shape = new Sphere(config.radius(), 48);
+            shape.setMaterial(material);
+            return shape;
+        });
+        energizer3D.setPumpingFrequency(config.pumpingFrequency());
+        energizer3D.setInflatedSize(config.scalingInflated());
+        energizer3D.setExpandedSize(config.scalingExpanded());
+        return energizer3D;
     }
 }
