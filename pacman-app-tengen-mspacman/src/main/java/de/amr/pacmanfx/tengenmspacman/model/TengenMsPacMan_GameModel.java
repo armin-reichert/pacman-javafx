8e569f8076f7d07ece9f5ed7e3fe75f3e1198164
Armin Reichert <armin.reichert@web.de>
2026-03-19 08:45:11 +0100

    Refac

--- pacman-app-tengen-mspacman/src/main/java/de/amr/pacmanfx/tengenmspacman/model/TengenMsPacMan_GameModel.java
+++ pacman-app-tengen-mspacman/src/main/java/de/amr/pacmanfx/tengenmspacman/model/TengenMsPacMan_GameModel.java
@@ -5,6 +5,7 @@ package de.amr.pacmanfx.tengenmspacman.model;
 
 import de.amr.pacmanfx.event.*;
 import de.amr.pacmanfx.lib.TickTimer;
+import de.amr.pacmanfx.lib.math.Direction;
 import de.amr.pacmanfx.lib.math.Vector2b;
 import de.amr.pacmanfx.lib.math.Vector2f;
 import de.amr.pacmanfx.lib.math.Vector2i;
@@ -50,50 +51,86 @@ public class TengenMsPacMan_GameModel extends AbstractGameModel {
         return msPacMan;
     }
 
+    /**
+     * In Arcade Ms. Pac-Man, Blinky and Pinky move randomly during the *first* scatter phase. Some say,
+     * the original intention had been to randomize the scatter target of *all* ghosts but because of a bug,
+     * only the scatter target of Blinky and Pinky would have been affected. Who knows?
+     * <p>
+     * I use the same behavior here, however I do not know what the real Tengen implementation does.
+     * </p>
+     */
     public static Ghost createGhost(byte personality) {
         return switch (personality) {
-            case RED_GHOST_SHADOW -> modifyShadowBehavior(new RedGhostShadow("Blinky"));
-            case PINK_GHOST_SPEEDY -> modifyAmbushBehavior(new PinkGhostAmbusher("Pinky"));
+            case RED_GHOST_SHADOW -> applyModifiedShadowBehavior(new RedGhostShadow("Blinky"));
+            case PINK_GHOST_SPEEDY -> applyModifiedAmbushBehavior(new PinkGhostAmbusher("Pinky"));
             case CYAN_GHOST_BASHFUL -> new CyanGhostBashful("Inky");
             case ORANGE_GHOST_POKEY -> new OrangeGhostPokey("Sue");
             default -> throw new IllegalArgumentException();
         };
     }
 
-    private static Ghost modifyShadowBehavior(RedGhostShadow ghost) {
+    private static Ghost applyModifiedShadowBehavior(RedGhostShadow ghost) {
         ghost.setHuntingStrategy((GameLevel level, Float speed) -> {
-            final AbstractHuntingTimer huntingTimer = level.huntingTimer();
-            ghost.setSpeed(speed);
-            if (huntingTimer.phaseIndex() == 0) { // first scatter phase: move randomly
-                ghost.roam(level);
+            final TerrainLayer terrain = level.worldMap().terrainLayer();
+            final boolean firstScatterPhase = level.huntingTimer().phaseIndex() == 0;
+            final boolean takeRandomDir = ghost.isNewTileEntered() && terrain.isIntersection(ghost.tile());
+            if (firstScatterPhase && takeRandomDir) {
+                selectRandomWishDir(ghost, level);
+                ghost.setSpeed(speed);
+                ghost.moveThroughThisCruelWorld(level);
             } else {
-                // Copilot claims, there is no "cruise elroy" behavior in Tengen Ms. Pac-Man!
-                final boolean chaseMsPacMan = huntingTimer.phase() == HuntingPhase.CHASING;
-                final Vector2i targetTile = chaseMsPacMan
-                    ? ghost.chasingTargetTile(level)
-                    : level.worldMap().terrainLayer().ghostScatterTile(ghost.personality());
+                // Normal behavior of red ghost
+                final boolean chase = level.huntingTimer().phase() == HuntingPhase.CHASING || ghost.elroyState().enabled();
+                final Vector2i targetTile = chase ? ghost.chasingTargetTile(level) : terrain.ghostScatterTile(ghost.personality());
+                ghost.setSpeed(speed);
                 ghost.tryMovingTowardsTargetTile(level, targetTile);
             }
         });
         return ghost;
     }
 
-    private static Ghost modifyAmbushBehavior(Ghost ghost) {
+    private static Ghost applyModifiedAmbushBehavior(Ghost ghost) {
         ghost.setHuntingStrategy((GameLevel level, Float speed) -> {
-            final AbstractHuntingTimer huntingTimer = level.huntingTimer();
-            ghost.setSpeed(speed);
-            if (huntingTimer.phaseIndex() == 0) { // first scatter phase
-                ghost.roam(level);
+            final TerrainLayer terrain = level.worldMap().terrainLayer();
+            final boolean firstScatterPhase = level.huntingTimer().phaseIndex() == 0;
+            final boolean takeRandomDir = ghost.isNewTileEntered() && terrain.isIntersection(ghost.tile());
+            if (firstScatterPhase && takeRandomDir) {
+                selectRandomWishDir(ghost, level);
+                ghost.setSpeed(speed);
+                ghost.moveThroughThisCruelWorld(level);
             } else {
-                final Vector2i targetTile = huntingTimer.phase() == HuntingPhase.CHASING
-                    ? ghost.chasingTargetTile(level)
-                    : level.worldMap().terrainLayer().ghostScatterTile(ghost.personality());
+                final boolean chase = level.huntingTimer().phase() == HuntingPhase.CHASING;
+                final Vector2i targetTile = chase ? ghost.chasingTargetTile(level) : terrain.ghostScatterTile(ghost.personality());
+                ghost.setSpeed(speed);
                 ghost.tryMovingTowardsTargetTile(level, targetTile);
             }
         });
         return ghost;
     }
 
+    private static void selectRandomWishDir(Ghost ghost, GameLevel level) {
+        final Vector2i tile = ghost.tile();
+        final boolean teleporting = level.worldMap().terrainLayer().isTileInPortalSpace(tile);
+        if (teleporting) {
+            return;
+        }
+        int dirsTried = 0;
+        Direction dir = Direction.random();
+        while (++dirsTried <= 4) {
+            if (isAcceptableWishDir(level, ghost, dir)) {
+                ghost.setWishDir(dir);
+                Logger.info("{} selects random wish direction {}", ghost.name(), dir);
+                break;
+            }
+            Logger.debug("{} rejects wish dir {}", ghost.name(), dir);
+            dir = dir.nextClockwise();
+        }
+    }
+
+    private static boolean isAcceptableWishDir(GameLevel level, Ghost ghost, Direction dir) {
+        final Vector2i neighborTile = ghost.tile().plus(dir.vector());
+        return dir != ghost.moveDir().opposite() && ghost.canAccessTile(level, neighborTile);
+    }
 
     static final short TICK_SHOW_READY = 10;
     static final short TICK_NEW_GAME_SHOW_GUYS = 70;
