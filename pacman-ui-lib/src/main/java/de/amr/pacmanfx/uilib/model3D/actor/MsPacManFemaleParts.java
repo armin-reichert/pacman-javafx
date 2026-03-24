59a1b7b393ec36edc1d36f9e1d5ced43d026177a
Armin Reichert <armin.reichert@web.de>
2026-03-24 11:04:35 +0100

    Refac

--- pacman-ui-lib/src/main/java/de/amr/pacmanfx/uilib/model3D/actor/MsPacManFemaleParts.java
+++ pacman-ui-lib/src/main/java/de/amr/pacmanfx/uilib/model3D/actor/MsPacManFemaleParts.java
@@ -3,7 +3,7 @@
  */
 package de.amr.pacmanfx.uilib.model3D.actor;
 
-import de.amr.pacmanfx.lib.Disposable;
+import de.amr.pacmanfx.uilib.model3D.DisposableGraphicsObject;
 import javafx.scene.Group;
 import javafx.scene.paint.Color;
 import javafx.scene.paint.PhongMaterial;
@@ -12,59 +12,47 @@ import javafx.scene.transform.Translate;
 
 import static de.amr.pacmanfx.uilib.Ufx.coloredPhongMaterial;
 
-public class MsPacManFemaleParts extends Group implements Disposable {
-
-    private final Sphere bowLeft;
-    private final Sphere bowRight;
-    private final Sphere pearlLeft;
-    private final Sphere pearlRight;
-    private final Sphere boobLeft;
-    private final Sphere boobRight;
-    private final Sphere beautySpot;
-    private PhongMaterial bowMaterial;
-    private PhongMaterial pearlMaterial;
-    private PhongMaterial beautySpotMaterial;
-    private PhongMaterial silicone;
+public class MsPacManFemaleParts extends Group implements DisposableGraphicsObject {
 
     public MsPacManFemaleParts(double pacSize, MsPacManComponentColors colors) {
-        int divisions = 16; // 64 is default
+        final int sphereDivisions = 16; // 64 is default
 
-        bowMaterial = coloredPhongMaterial(colors.hairBow());
+        final PhongMaterial bowMaterial = coloredPhongMaterial(colors.hairBow());
 
-        bowLeft = new Sphere(1.2, divisions);
+        final Sphere bowLeft = new Sphere(1.2, sphereDivisions);
         bowLeft.setMaterial(bowMaterial);
         bowLeft.getTransforms().addAll(new Translate(3.0, 1.5, -pacSize * 0.55));
 
-        bowRight = new Sphere(1.2, divisions);
+        final Sphere bowRight = new Sphere(1.2, sphereDivisions);
         bowRight.setMaterial(bowMaterial);
         bowRight.getTransforms().addAll(new Translate(3.0, -1.5, -pacSize * 0.55));
 
-        pearlMaterial = coloredPhongMaterial(colors.hairBowPearls());
+        final PhongMaterial pearlMaterial = coloredPhongMaterial(colors.hairBowPearls());
 
-        pearlLeft = new Sphere(0.4, divisions);
+        final Sphere pearlLeft = new Sphere(0.4, sphereDivisions);
         pearlLeft.setMaterial(pearlMaterial);
         pearlLeft.getTransforms().addAll(new Translate(2, 0.5, -pacSize * 0.58));
 
-        pearlRight = new Sphere(0.4, divisions);
+        final Sphere pearlRight = new Sphere(0.4, sphereDivisions);
         pearlRight.setMaterial(pearlMaterial);
         pearlRight.getTransforms().addAll(new Translate(2, -0.5, -pacSize * 0.58));
 
-        beautySpotMaterial = coloredPhongMaterial(Color.rgb(120, 120, 120));
-        beautySpot = new Sphere(0.5, divisions);
+        final PhongMaterial beautySpotMaterial = coloredPhongMaterial(Color.rgb(120, 120, 120));
+        final Sphere beautySpot = new Sphere(0.5, sphereDivisions);
         beautySpot.getTransforms().addAll(new Translate(-0.33 * pacSize, -0.4 * pacSize, -0.14 * pacSize));
         beautySpot.setMaterial(beautySpotMaterial);
 
-        silicone = coloredPhongMaterial(colors.boobs());
+        final PhongMaterial silicone = coloredPhongMaterial(colors.boobs());
 
-        double bx = -0.2 * pacSize; // forward
-        double by = 1.6; // or - 1.6 // sidewards
-        double bz = 0.4 * pacSize; // up/down
+        final double bx = -0.2 * pacSize; // forward
+        final double by = 1.6; // or - 1.6 // sidewards
+        final double bz = 0.4 * pacSize; // up/down
 
-        boobLeft = new Sphere(1.8, divisions);
+        final Sphere boobLeft = new Sphere(1.8, sphereDivisions);
         boobLeft.setMaterial(silicone);
         boobLeft.getTransforms().addAll(new Translate(bx, -by, bz));
 
-        boobRight = new Sphere(1.8, divisions);
+        final Sphere boobRight = new Sphere(1.8, sphereDivisions);
         boobRight.setMaterial(silicone);
         boobRight.getTransforms().addAll(new Translate(bx, by, bz));
 
@@ -73,17 +61,6 @@ public class MsPacManFemaleParts extends Group implements Disposable {
 
     @Override
     public void dispose() {
-        getChildren().clear();
-        bowLeft.setMaterial(null);
-        bowRight.setMaterial(null);
-        pearlLeft.setMaterial(null);
-        pearlRight.setMaterial(null);
-        boobLeft.setMaterial(null);
-        boobRight.setMaterial(null);
-        beautySpot.setMaterial(null);
-        bowMaterial = null;
-        pearlMaterial = null;
-        beautySpotMaterial = null;
-        silicone = null;
+        cleanupGroup(this, true);
     }
 }
