8de74b521464cda0d1037c6dda345761bdbf21f5
Armin Reichert <armin.reichert@web.de>
2026-03-30 12:31:56 +0200

    Refac

--- pacman-ui-lib/src/main/java/de/amr/pacmanfx/uilib/model3D/actor/PacMan3D.java
+++ pacman-ui-lib/src/main/java/de/amr/pacmanfx/uilib/model3D/actor/PacMan3D.java
@@ -5,60 +5,22 @@ package de.amr.pacmanfx.uilib.model3D.actor;
 
 import de.amr.pacmanfx.model.actors.Pac;
 import de.amr.pacmanfx.uilib.animation.AnimationRegistry;
-import de.amr.pacmanfx.uilib.animation.AnimationSupport;
-import de.amr.pacmanfx.uilib.animation.ManagedAnimation;
 import de.amr.pacmanfx.uilib.model3D.Models3D;
 import de.amr.pacmanfx.uilib.model3D.animation.HeadBangingAnimation;
 import de.amr.pacmanfx.uilib.model3D.animation.Pac3DChewingAnimation;
-import javafx.animation.*;
-import javafx.scene.transform.Rotate;
-import javafx.util.Duration;
+import de.amr.pacmanfx.uilib.model3D.animation.PacMan3DDyingAnimation;
 
 public class PacMan3D extends Pac3D {
 
-    public PacMan3D(AnimationRegistry animationRegistry, Pac pac, PacConfig pacConfig) {
-        super(animationRegistry, pac);
+    public PacMan3D(AnimationRegistry animations, Pac pac, PacConfig pacConfig) {
+        super(animations, pac);
 
         setBody(Models3D.PAC_MAN_MODEL.createPacBody(pacConfig));
         setJaw(Models3D.PAC_MAN_MODEL.createBlindPacBody(pacConfig));
 
         animations.register(AnimationID.PAC_CHEWING, new Pac3DChewingAnimation(this));
-
-        final var dyingAnimation = new ManagedAnimation("PacMan_Dying");
-        dyingAnimation.setFactory(() -> {
-            Duration duration = Duration.seconds(1.5);
-            byte numSpins = 5;
-
-            var spinning = new RotateTransition(duration.divide(numSpins), PacMan3D.this);
-            spinning.setAxis(Rotate.Z_AXIS);
-            spinning.setByAngle(360);
-            spinning.setCycleCount(numSpins);
-            spinning.setInterpolator(Interpolator.LINEAR);
-
-            var shrinking = new ScaleTransition(duration.multiply(0.5), PacMan3D.this);
-            shrinking.setToX(0.25);
-            shrinking.setToY(0.25);
-            shrinking.setToZ(0.02);
-
-            var expanding = new ScaleTransition(duration.multiply(0.5), PacMan3D.this);
-            expanding.setToX(0.75);
-            expanding.setToY(0.75);
-
-            var sinking = new TranslateTransition(duration, PacMan3D.this);
-            sinking.setToZ(0);
-
-            return new SequentialTransition(
-                new ParallelTransition(spinning, new SequentialTransition(shrinking, expanding), sinking),
-                AnimationSupport.pauseSecThen(1.0, () -> {
-                    setVisible(false);
-                    setScaleX(1.0);
-                    setScaleY(1.0);
-                    setScaleZ(1.0);
-                })
-            );
-        });
-        animationRegistry.register(AnimationID.PAC_DYING, dyingAnimation);
-        animationRegistry.register(AnimationID.PAC_MOVING, new HeadBangingAnimation(PacMan3D.this));
+        animations.register(AnimationID.PAC_DYING,   new PacMan3DDyingAnimation(this));
+        animations.register(AnimationID.PAC_MOVING,  new HeadBangingAnimation(PacMan3D.this));
 
         setMovementAnimationPowerMode(false);
     }
---
+++ pacman-ui-lib/src/main/java/de/amr/pacmanfx/uilib/model3D/animation/PacMan3DDyingAnimation.java
@@ -0,0 +1,62 @@
+/*
+ * Copyright (c) 2021-2026 Armin Reichert (MIT License)
+ */
+
+package de.amr.pacmanfx.uilib.model3D.animation;
+
+import de.amr.pacmanfx.uilib.animation.ManagedAnimation;
+import de.amr.pacmanfx.uilib.model3D.actor.PacMan3D;
+import javafx.animation.*;
+import javafx.scene.transform.Rotate;
+import javafx.util.Duration;
+
+import static de.amr.pacmanfx.uilib.animation.AnimationSupport.doNow;
+import static de.amr.pacmanfx.uilib.animation.AnimationSupport.pauseSecThen;
+import static java.util.Objects.requireNonNull;
+
+public class PacMan3DDyingAnimation extends ManagedAnimation {
+
+    private final PacMan3D pacMan3D;
+
+    public PacMan3DDyingAnimation(PacMan3D pacMan3D) {
+        super("PacMan_Dying");
+        this.pacMan3D = requireNonNull(pacMan3D);
+        setFactory(this::createAnimation);
+    }
+
+    private Animation createAnimation() {
+        Duration duration = Duration.seconds(1.5);
+        byte numSpins = 5;
+
+        var spinning = new RotateTransition(duration.divide(numSpins), pacMan3D);
+        spinning.setAxis(Rotate.Z_AXIS);
+        spinning.setByAngle(360);
+        spinning.setCycleCount(numSpins);
+        spinning.setInterpolator(Interpolator.LINEAR);
+
+        var shrinking = new ScaleTransition(duration.multiply(0.5), pacMan3D);
+        shrinking.setToX(0.25);
+        shrinking.setToY(0.25);
+        shrinking.setToZ(0.02);
+
+        var expanding = new ScaleTransition(duration.multiply(0.5), pacMan3D);
+        expanding.setToX(0.75);
+        expanding.setToY(0.75);
+
+        var sinking = new TranslateTransition(duration, pacMan3D);
+        sinking.setToZ(0);
+
+        final var deathFight = new ParallelTransition(spinning, new SequentialTransition(shrinking, expanding), sinking);
+        return new SequentialTransition(
+            doNow(() -> pacMan3D.light().ifPresent(light -> light.setLightOn(false))),
+            deathFight,
+            pauseSecThen(1.0, () -> {
+                pacMan3D.setVisible(false);
+                //TODO check this:
+                pacMan3D.setScaleX(1.0);
+                pacMan3D.setScaleY(1.0);
+                pacMan3D.setScaleZ(1.0);
+            })
+        );
+    }
+}
