5de70c04773da8f5e8fc97931fe6970ce5654703
Armin Reichert <armin.reichert@web.de>
2026-04-05 21:32:22 +0200

    Refac

--- pacman-app-arcade-pacman/src/main/java/de/amr/pacmanfx/arcade/pacman/scenes/Arcade_BootScene2D.java
+++ pacman-app-arcade-pacman/src/main/java/de/amr/pacmanfx/arcade/pacman/scenes/Arcade_BootScene2D.java
@@ -8,8 +8,8 @@ import de.amr.pacmanfx.model.Game;
 import de.amr.pacmanfx.ui.d2.GameScene2D;
 
 /**
- * The boot screen is showing some strange screen patterns and eventually  a grid.
- * This scene tries to mimic that to a certain degree.
+ * The boot screen displays some strange hex codes, garbage from the graphics memory
+ * and eventually a grid (maybe used to calibrate the screen?). This scene tries to mimic that to a certain degree.
  */
 public class Arcade_BootScene2D extends GameScene2D {
 
