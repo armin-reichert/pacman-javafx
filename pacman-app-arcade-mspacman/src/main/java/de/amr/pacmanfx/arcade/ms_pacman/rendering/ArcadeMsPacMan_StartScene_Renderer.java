dc941272b44a26740d1bfe4dcf3395bc1c817a07
Armin Reichert <armin.reichert@web.de>
2026-04-10 18:31:06 +0200

    Refac

--- pacman-app-arcade-mspacman/src/main/java/de/amr/pacmanfx/arcade/ms_pacman/rendering/ArcadeMsPacMan_StartScene_Renderer.java
+++ pacman-app-arcade-mspacman/src/main/java/de/amr/pacmanfx/arcade/ms_pacman/rendering/ArcadeMsPacMan_StartScene_Renderer.java
@@ -4,13 +4,14 @@
 
 package de.amr.pacmanfx.arcade.ms_pacman.rendering;
 
+import de.amr.pacmanfx.lib.math.RectShort;
 import de.amr.pacmanfx.ui.GameUI;
 import de.amr.pacmanfx.ui.d2.GameScene2D;
 import de.amr.pacmanfx.ui.d2.GameScene2D_Renderer;
+import de.amr.pacmanfx.uilib.assets.AssetMap;
 import de.amr.pacmanfx.uilib.rendering.SpriteRenderer;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.image.Image;
-import javafx.scene.text.Font;
 
 import static de.amr.pacmanfx.Globals.TS;
 import static de.amr.pacmanfx.uilib.rendering.ArcadePalette.ARCADE_ORANGE;
@@ -32,30 +33,31 @@ public class ArcadeMsPacMan_StartScene_Renderer extends GameScene2D_Renderer imp
     public void draw(GameScene2D scene) {
         clearCanvas();
 
-        final Font font6 = arcadeFont6();
-        final Font font8 = arcadeFont8();
-        fillText("PUSH START BUTTON", ARCADE_ORANGE, font8, TS(6), TS(16));
-        fillText("1 PLAYER ONLY", ARCADE_ORANGE, font8, TS(8), TS(18));
-        fillText("ADDITIONAL    AT 10000", ARCADE_ORANGE, font8,TS(2), TS(25));
-        drawSprite(spriteSheet().sprite(SpriteID.LIVES_COUNTER_SYMBOL), TS(13), TS(23) + 1, true);
-        fillText("PTS", ARCADE_ORANGE, font6, TS(25), TS(25));
+        final double STS = scaled(TS);
+        final RectShort livesCounterSprite = spriteSheet().sprite(SpriteID.LIVES_COUNTER_SYMBOL);
 
-        Image logo = scene.ui().currentConfig().assets().image("logo.midway");
-        if (logo != null) {
-            drawMidwayCopyright(logo, TS * 6, TS * 28);
-        }
+        ctx.setFill(ARCADE_ORANGE);
+        ctx.setFont(arcadeFont8());
+        ctx.fillText("PUSH START BUTTON",      STS*6, STS*16);
+        ctx.fillText("1 PLAYER ONLY",          STS*8, STS*18);
+        ctx.fillText("ADDITIONAL    AT 10000", STS*2, STS*25);
+        ctx.setFont(arcadeFont6());
+        ctx.fillText("PTS", STS*25, STS*25);
+        drawSprite(livesCounterSprite, TS(13), TS(23) + 1, true);
+        drawMidwayCopyright(scene.ui().currentConfig().assets(), TS * 6, TS * 28);
 
         if (GameUI.PROPERTY_DEBUG_INFO_VISIBLE.get()) {
             debugRenderer.draw(scene);
         }
     }
 
-    public void drawMidwayCopyright(Image logo, double x, double y) {
-        ctx.drawImage(logo, scaled(x), scaled(y + 2), scaled(TS(4) - 2), scaled(TS(4)));
+    public void drawMidwayCopyright(AssetMap assets, double x, double y) {
+        final Image logo = assets.image("logo.midway");
         ctx.setFont(arcadeFont8());
         ctx.setFill(ARCADE_RED);
-        ctx.fillText("©", scaled(x + TS(5)), scaled(y + TS(2)) + 2);
+        ctx.drawImage(logo, scaled(x), scaled(y + 2), scaled(TS(4) - 2), scaled(TS(4)));
+        ctx.fillText("©",             scaled(x + TS(5)), scaled(y + TS(2)) + 2);
         ctx.fillText("MIDWAY MFG CO", scaled(x + TS(7)), scaled(y + TS(2)));
-        ctx.fillText("1980/1981", scaled(x + TS(8)), scaled(y + TS(4)));
+        ctx.fillText("1980/1981",     scaled(x + TS(8)), scaled(y + TS(4)));
     }
 }
