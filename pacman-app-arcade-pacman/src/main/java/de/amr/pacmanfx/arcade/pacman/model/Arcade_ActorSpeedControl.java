f99dceeb4c01129a7d229c738a72a3ce5c6809b0
Armin Reichert <armin.reichert@web.de>
2026-04-15 10:51:45 +0200

    Refac

--- pacman-app-arcade-pacman/src/main/java/de/amr/pacmanfx/arcade/pacman/model/Arcade_ActorSpeedControl.java
+++ pacman-app-arcade-pacman/src/main/java/de/amr/pacmanfx/arcade/pacman/model/Arcade_ActorSpeedControl.java
@@ -10,31 +10,32 @@ import de.amr.pacmanfx.model.actors.Ghost;
 import de.amr.pacmanfx.model.actors.RedGhostShadow;
 import de.amr.pacmanfx.model.world.TerrainLayer;
 
+import static de.amr.pacmanfx.arcade.pacman.model.Arcade_GameModel.levelData;
+
 public class Arcade_ActorSpeedControl implements ActorSpeedControl {
-    /**
-     * Base speed is 75 px per second (=1.25 px/tick).
-     */
+
+    /** Base speed is 75 px per second (=1.25 px/tick). */
     public static final float BASE_SPEED = 1.25f;
-    public static final float BASE_SPEED_1_PERCENT = 0.01f * BASE_SPEED;
+
+    /** 1 percent of base speed. */
+    public static final float BASE_SPEED_ONE_PERCENT = 0.01f * BASE_SPEED;
 
     @Override
     public float bonusSpeed(GameLevel level) {
-        //TODO clarify exact speed
+        //TODO clarify exact speed in emulator
         return 0.5f * pacSpeed(level);
     }
 
     @Override
     public float pacSpeed(GameLevel level) {
-        final LevelData data = Arcade_GameModel.levelData(level.number());
-        byte percentage = data.pctPacSpeed();
-        return percentage > 0 ? percentage * BASE_SPEED_1_PERCENT : BASE_SPEED;
+        final byte pct = levelData(level.number()).pctPacSpeed();
+        return pct > 0 ? pct * BASE_SPEED_ONE_PERCENT : BASE_SPEED;
     }
 
     @Override
     public float pacSpeedWhenHasPower(GameLevel level) {
-        final LevelData data = Arcade_GameModel.levelData(level.number());
-        byte percentage = data.pctPacSpeedPowered();
-        return percentage > 0 ? percentage * BASE_SPEED_1_PERCENT : pacSpeed(level);
+        final byte pct = levelData(level.number()).pctPacSpeedPowered();
+        return pct > 0 ? pct * BASE_SPEED_ONE_PERCENT : pacSpeed(level);
     }
 
     @Override
@@ -42,12 +43,12 @@ public class Arcade_ActorSpeedControl implements ActorSpeedControl {
         final int levelNumber = level.number();
         final TerrainLayer terrain = level.worldMap().terrainLayer();
         final boolean insideHouse = terrain.house().isVisitedBy(ghost);
-        final boolean insideTunnel = terrain.isTunnel(ghost.tile());
+        final boolean tunnelSlowdown = terrain.isTunnel(ghost.tile());
         return switch (ghost.state()) {
             case LOCKED -> insideHouse ? 0.5f : 0;
             case LEAVING_HOUSE -> 0.5f;
-            case HUNTING_PAC -> insideTunnel ? ghostSpeedTunnel(levelNumber) : ghostSpeedAttacking(level, ghost);
-            case FRIGHTENED -> insideTunnel ? ghostSpeedTunnel(levelNumber) : ghostSpeedFrightened(level);
+            case HUNTING_PAC -> tunnelSlowdown ? ghostSpeedTunnel(levelNumber) : ghostSpeedAttacking(level, ghost);
+            case FRIGHTENED -> tunnelSlowdown ? ghostSpeedTunnel(levelNumber) : ghostSpeedFrightened(level);
             case EATEN -> 0;
             case RETURNING_HOME, ENTERING_HOUSE -> 2;
         };
@@ -56,29 +57,27 @@ public class Arcade_ActorSpeedControl implements ActorSpeedControl {
     @Override
     public float ghostSpeedAttacking(GameLevel level, Ghost ghost) {
         final int levelNumber = level.number();
-        final LevelData data = Arcade_GameModel.levelData(levelNumber);
+        final LevelData data = levelData(levelNumber);
         if (ghost instanceof RedGhostShadow redGhostShadow) {
             return switch (redGhostShadow.elroyState().mode()) {
-                case ZERO -> data.pctGhostSpeed() * BASE_SPEED_1_PERCENT;
-                case ONE -> data.pctElroy1Speed() * BASE_SPEED_1_PERCENT;
-                case TWO -> data.pctElroy2Speed() * BASE_SPEED_1_PERCENT;
+                case ZERO -> data.pctGhostSpeed() * BASE_SPEED_ONE_PERCENT;
+                case ONE  -> data.pctElroy1Speed() * BASE_SPEED_ONE_PERCENT;
+                case TWO  -> data.pctElroy2Speed() * BASE_SPEED_ONE_PERCENT;
             };
         } else {
-            return data.pctGhostSpeed() * BASE_SPEED_1_PERCENT;
+            return data.pctGhostSpeed() * BASE_SPEED_ONE_PERCENT;
         }
     }
 
     @Override
     public float ghostSpeedFrightened(GameLevel level) {
         final int levelNumber = level.number();
-        final LevelData data = Arcade_GameModel.levelData(levelNumber);
-        float percentage = data.pctGhostSpeedFrightened();
-        return percentage > 0 ? percentage * BASE_SPEED_1_PERCENT : BASE_SPEED;
+        final float pct = levelData(levelNumber).pctGhostSpeedFrightened();
+        return pct > 0 ? pct * BASE_SPEED_ONE_PERCENT : BASE_SPEED;
     }
 
     @Override
     public float ghostSpeedTunnel(int levelNumber) {
-        final LevelData data = Arcade_GameModel.levelData(levelNumber);
-        return data.pctGhostSpeedTunnel() * BASE_SPEED_1_PERCENT;
+        return levelData(levelNumber).pctGhostSpeedTunnel() * BASE_SPEED_ONE_PERCENT;
     }
 }
