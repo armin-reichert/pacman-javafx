8520c58b24ae7cc110bef867edfa859cd1ecc86d
Armin Reichert <armin.reichert@web.de>
2026-04-18 15:41:24 +0200

    Improved

--- pacman-core/src/main/java/de/amr/pacmanfx/lib/ColorRGBA.java
+++ pacman-core/src/main/java/de/amr/pacmanfx/lib/ColorRGBA.java
@@ -11,4 +11,19 @@ public record ColorRGBA(byte red, byte green, byte blue, double alpha) {
     public static ColorRGBA of(int red, int green, int blue, double alpha) {
         return new ColorRGBA((byte) red, (byte) green, (byte) blue, alpha);
     }
+
+    private static void validateColorValue(int value, String message) {
+        if (value < 0 || value > 255) {
+            throw new IllegalArgumentException("Illegal %s: %d".formatted(message, value));
+        }
+    }
+
+    public ColorRGBA {
+        validateColorValue(red, "red");
+        validateColorValue(green, "green");
+        validateColorValue(blue, "blue");
+        if (alpha < 0.0 || alpha > 1.0) {
+            throw new IllegalArgumentException("Illegal alpha: %f".formatted(alpha));
+        }
+    }
 }
