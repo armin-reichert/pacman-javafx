11cdd7225af78aaba8f1f239a4be84a1430f42a9
Armin Reichert <armin.reichert@web.de>
2026-04-21 08:52:36 +0200

    Material file parser

--- pacman-ui-lib/src/main/java/de/amr/pacmanfx/uilib/objimport/MtlFileParser.java
+++ pacman-ui-lib/src/main/java/de/amr/pacmanfx/uilib/objimport/MtlFileParser.java
@@ -4,17 +4,47 @@
 
 package de.amr.pacmanfx.uilib.objimport;
 
+import javafx.scene.paint.Color;
 import javafx.scene.paint.PhongMaterial;
 import org.tinylog.Logger;
 
 import java.io.BufferedReader;
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.util.*;
 
 public class MtlFileParser {
 
-    public enum Command {
+    static final String[] FILE_PATHS = {
+        "/de/amr/pacmanfx/uilib/model3D/ghost.mtl",
+        "/de/amr/pacmanfx/uilib/model3D/pacman.mtl",
+        "/de/amr/pacmanfx/uilib/model3D/pellet.mtl"
+    };
+
+    static void main() {
+        for (String path : FILE_PATHS) {
+            final URL url = MtlFileParser.class.getResource(path);
+            if (url == null) {
+                Logger.error("Invalid OBJ file URL {}", url);
+                return;
+            }
+            Logger.info("Parsing material file '{}'", path);
+            final MtlFileParser parser = new MtlFileParser();
+            try {
+                try (InputStream is = url.openStream()) {
+                    final var reader = new BufferedReader(new InputStreamReader(is, StandardCharsets.UTF_8));
+                    parser.parse(reader);
+                }
+            } catch (IOException x) {
+                Logger.error(x, "Parsing error");
+            }
+        }
+    }
+
+    public enum Keyword {
         NEW_MATERIAL    ("newmtl"),
         SHININESS       ("Ns"),
         OPACITY         ("d"),
@@ -23,29 +53,212 @@ public class MtlFileParser {
         DIFFUSE_COLOR   ("Kd"),
         EMISSIVE_COLOR  ("Ke"),
         SPECULAR_COLOR  ("Ks"),
-        OPTICAL_DENSITY ("Ni"),
-        COMMENT         ("#"),
+        OPTICAL_DENSITY ("Ni");
 
-        $("");
+        private final String text;
 
-        private final String token;
+        Keyword(String text) {
+            this.text = text;
+        }
+    }
 
-        Command(String token) {
-            this.token = token;
+    private record RGB(double red, double green, double blue) {
+        static RGB BLACK = new RGB(0.0, 0.0, 0.0);
+    }
+
+    private static class ObjMaterial {
+        String name;
+        double ns = 0;
+        double d = 1;
+        int illum = 2;
+        double ni = 1;
+
+        RGB ka = RGB.BLACK;
+        RGB kd = RGB.BLACK;
+        RGB ks = RGB.BLACK;
+        RGB ke = RGB.BLACK;
+
+        String mapKd;
+        String mapKs;
+        String mapD;
+        String mapBump;
+
+
+        @Override
+        public String toString() {
+            return "ObjMaterial{" +
+                "name='" + name + '\'' +
+                ", ns=" + ns +
+                ", d=" + d +
+                ", illum=" + illum +
+                ", ni=" + ni +
+                ", ka=" + ka +
+                ", kd=" + kd +
+                ", ks=" + ks +
+                ", ke=" + ke +
+                ", mapKd='" + mapKd + '\'' +
+                ", mapKs='" + mapKs + '\'' +
+                ", mapD='" + mapD + '\'' +
+                ", mapBump='" + mapBump + '\'' +
+                '}';
         }
     }
 
     private final Map<String, PhongMaterial> materialMap = new HashMap<>();
 
-    private String line;
+    private final List<ObjMaterial> materialList = new ArrayList<>();
+    private ObjMaterial currentMaterial;
+
+    private int lineNo = 1;
 
     public void parse(BufferedReader reader) throws IOException {
-        while ((line = reader.readLine()) != null) {
-            if (line.isBlank() || line.startsWith("#")) {
+        String statement;
+        while ((statement = reader.readLine()) != null) {
+            if (statement.isBlank() || statement.startsWith("#")) {
                 Logger.trace("Blank or comment line, ignored");
             }
+            else if (matches(statement, Keyword.NEW_MATERIAL)) {
+                commitMaterial();
+                String parameters = parameters(statement, Keyword.NEW_MATERIAL);
+                currentMaterial = new ObjMaterial();
+                currentMaterial.name = parameters.strip();
+            }
+            else if (matches(statement, Keyword.SHININESS)) {
+                if (assertCurrentMaterial()) {
+                    String parameters = parameters(statement, Keyword.SHININESS);
+                    currentMaterial.ns = parseShininess(parameters);
+                }
+            }
+            else if (matches(statement, Keyword.OPACITY)) {
+                if (assertCurrentMaterial()) {
+                    String parameters = parameters(statement, Keyword.OPACITY);
+                    currentMaterial.d = parseOpacity(parameters, 1.0);
+                }
+            }
+            else if (matches(statement, Keyword.ILLUMINATION)) {
+                if (assertCurrentMaterial()) {
+                    String params = parameters(statement, Keyword.ILLUMINATION);
+                    currentMaterial.illum = parseIllumination(params, 2);
+                }
+            }
+            else if (matches(statement, Keyword.OPTICAL_DENSITY)) {
+                if (assertCurrentMaterial()) {
+                    String params = parameters(statement, Keyword.OPTICAL_DENSITY);
+                    currentMaterial.ni = Double.parseDouble(params);
+                }
+            }
+            else if (matches(statement, Keyword.AMBIENT_COLOR)) {
+                if (assertCurrentMaterial()) {
+                    String parameters = parameters(statement, Keyword.AMBIENT_COLOR);
+                    currentMaterial.ka = parseRGB(parameters, RGB.BLACK);
+                }
+            }
+            else if (matches(statement, Keyword.DIFFUSE_COLOR)) {
+                if (assertCurrentMaterial()) {
+                    String parameters = parameters(statement, Keyword.DIFFUSE_COLOR);
+                    currentMaterial.kd = parseRGB(parameters, RGB.BLACK);
+                }
+            }
+            else if (matches(statement, Keyword.EMISSIVE_COLOR)) {
+                if (assertCurrentMaterial()) {
+                    String parameters = parameters(statement, Keyword.EMISSIVE_COLOR);
+                    currentMaterial.ke = parseRGB(parameters, RGB.BLACK);
+                }
+            }
+            else if (matches(statement, Keyword.SPECULAR_COLOR)) {
+                if (assertCurrentMaterial()) {
+                    String parameters = parameters(statement, Keyword.SPECULAR_COLOR);
+                    currentMaterial.ks = parseRGB(parameters, RGB.BLACK);
+                }
+            }
+            ++lineNo;
+        }
+        commitMaterial();
+        for (ObjMaterial material : materialList) {
+            Logger.info(material);
+            PhongMaterial oldValue = materialMap.put(material.name, createPhongMaterial(material));
+            if (oldValue != null) {
+                Logger.warn("Duplicate material found: {}. Overwrites previous material.", material.name);
+            }
+        }
+        Logger.info("Found {} materials", materialMap.size());
+    }
 
+    private static Color fxColor(RGB rgb) {
+        return Color.color(rgb.red, rgb.green, rgb.blue);
+    }
+
+    private static PhongMaterial createPhongMaterial(ObjMaterial material) {
+        PhongMaterial phongMaterial = new PhongMaterial();
+        phongMaterial.setDiffuseColor(fxColor(material.kd));
+        phongMaterial.setSpecularColor(fxColor(material.ks));
+        phongMaterial.setSpecularPower(material.ns);
+        return phongMaterial;
+    }
+
+    private boolean assertCurrentMaterial() {
+        if (currentMaterial == null) {
+            Logger.error("{}: No material definition has been started", lineNo);
+            return false;
+        }
+        return true;
+    }
+
+    private void commitMaterial() {
+        if (currentMaterial != null) {
+            materialList.add(currentMaterial);
+            currentMaterial = null;
         }
     }
 
+    private static boolean matches(String statement, MtlFileParser.Keyword keyword) {
+        return statement.startsWith(keyword.text + " ");
+    }
+
+    private static String parameters(String statement, MtlFileParser.Keyword keyword) {
+        return statement.substring(keyword.text.length() + 1).strip();
+    }
+
+    // double, 0-1000
+    private double parseShininess(String s) {
+        double value = Double.parseDouble(s);
+        if (0 <= value && value <= 1000) {
+            return value;
+        }
+        Logger.error("Shininess value out-of-range 0..1000: {}", value);
+        return 500;
+    }
+
+    // integer, 0..10
+    private int parseIllumination(String s, int defaultIllumination) {
+        int value = Integer.parseInt(s);
+        if (0 <= value && value <= 10) {
+            return value;
+        }
+        Logger.error("Illumination value out-of-range 0..10: {}", value);
+        return defaultIllumination;
+    }
+
+    private double parseOpacity(String s, double defaultOpacity) {
+        double value = Double.parseDouble(s);
+        if (0 <= value && value <= 1) {
+            return value;
+        }
+        Logger.error("Opacity value out-of-range 0..1: {}", value);
+        return defaultOpacity;
+    }
+
+    private RGB parseRGB(String s, RGB defaultRGB) {
+        String[] comp = s.trim().split("\\s+");
+        if (comp.length == 3) {
+            double r = Double.parseDouble(comp[0]);
+            double g = Double.parseDouble(comp[1]);
+            double b = Double.parseDouble(comp[2]);
+            return new RGB(r, g, b);
+        }
+        else {
+            Logger.error("Invalid RGB color format: {}", s);
+            return defaultRGB;
+        }
+    }
 }
