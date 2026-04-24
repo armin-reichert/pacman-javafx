ffd946d790349963f1cc852f756b37764642fc07
Armin Reichert <armin.reichert@web.de>
2026-04-24 16:07:06 +0200

    Refac

--- pacman-ui-lib/src/main/java/de/amr/pacmanfx/uilib/objimport/ObjFileParserTest.java
+++ pacman-ui-lib/src/main/java/de/amr/pacmanfx/uilib/objimport/ObjFileParserTest.java
@@ -28,8 +28,7 @@ public class ObjFileParserTest {
             }
             try {
                 final ObjFileParser parser = new ObjFileParser(url, StandardCharsets.UTF_8);
-                final ObjModel objModel = parser.parse();
-                TriangleMeshBuilder builder = new TriangleMeshBuilder(objModel);
+                TriangleMeshBuilder builder = new TriangleMeshBuilder(parser.parse());
                 Map<String, MeshView> meshes = builder.buildMeshViewsByGroup();
                 Logger.info("Constructed {} mesh views from OBJ model: {}", meshes.size(), meshes);
             } catch (IOException x) {
