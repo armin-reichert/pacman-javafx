9717b517ed2ea3705af71f9de183ac301fe6abc2
Armin Reichert <armin.reichert@web.de>
2026-04-29 21:44:36 +0200

    fmt

--- meshviewer/src/main/java/de/amr/meshviewer/MeshViewerApp.java
+++ meshviewer/src/main/java/de/amr/meshviewer/MeshViewerApp.java
@@ -14,13 +14,16 @@ public class MeshViewerApp extends Application {
             "Teapot",
             getClass().getResource("/newell_teaset/teapot.obj"),
             new SampleState(-10, 0, 0, 0, true)),
+
         new SampleModel(
             "Alien Animal",
             getClass().getResource("/alien_animal/Alien Animal.obj"),
             new SampleState(-50, 0, 0, 0, true)),
+
         new SampleModel("Beagle",
             getClass().getResource("/beagle/13041_Beagle_v1_L1.obj"),
             new SampleState(-150, -90, 0, 0, false)),
+
         new SampleModel("Aya",
             getClass().getResource("/aya_japanese_girl/091_W_Aya_100K.obj"),
             new SampleState(-2800, 0, 0, 0, true))
