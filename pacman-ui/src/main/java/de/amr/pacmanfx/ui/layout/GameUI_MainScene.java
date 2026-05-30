8370a6b4d5bf2b60076340e10ae1b1da11f1c8c6
Armin Reichert <armin.reichert@web.de>
2026-05-30 08:15:29 +0200

    Improve user input handling

--- pacman-ui/src/main/java/de/amr/pacmanfx/ui/layout/GameUI_MainScene.java
+++ pacman-ui/src/main/java/de/amr/pacmanfx/ui/layout/GameUI_MainScene.java
@@ -10,13 +10,9 @@ import de.amr.pacmanfx.ui.action.ActionBindingsSet;
 import de.amr.pacmanfx.ui.action.CommonActions;
 import de.amr.pacmanfx.ui.action.GameActionBindingsSet;
 import de.amr.pacmanfx.ui.input.Input;
-import de.amr.pacmanfx.ui.input.Keyboard;
 import javafx.scene.Scene;
-import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.StackPane;
-
-import static de.amr.pacmanfx.ui.input.Keyboard.bare;
 
 public class GameUI_MainScene extends Scene {
 
@@ -30,28 +26,27 @@ public class GameUI_MainScene extends Scene {
     public void init(GameUI ui) {
         getStylesheets().add(GameUIConstants.STYLE_SHEET_PATH);
 
-        actionBindings.registerAnyBindingFromSet(CommonActions.ACTION_ENTER_FULLSCREEN,        GameUIConstants.COMMON_BINDINGS);
-        actionBindings.registerAnyBindingFromSet(CommonActions.ACTION_OPEN_EDITOR,             GameUIConstants.COMMON_BINDINGS);
-        actionBindings.registerAnyBindingFromSet(CommonActions.ACTION_TOGGLE_KEYBOARD_MONITOR, GameUIConstants.COMMON_BINDINGS);
-        actionBindings.registerAnyBindingFromSet(CommonActions.ACTION_TOGGLE_MUTED,            GameUIConstants.COMMON_BINDINGS);
-        actionBindings.activate();
-
-
         final Input userInput = Input.instance();
 
         addEventFilter(KeyEvent.KEY_PRESSED,  userInput.keyboard::onKeyPressed);
         addEventFilter(KeyEvent.KEY_RELEASED, userInput.keyboard::onKeyReleased);
 
-        // If a global action is bound to the key press, execute it; otherwise let the current view handle it.
-        setOnKeyPressed(e -> actionBindings.matchingAction(userInput.keyboard)
-            .ifPresentOrElse(action -> {
-                if (action.executeIfEnabled(ui)) e.consume();
-            },
-            () -> ui.viewManager().currentView().onInput(ui, userInput))
-        );
+        // If a global action can be executed, do it; otherwise let the current view handle it.
+        userInput.keyboard.addStateListener(_ -> {
+            actionBindings.matchingAction(userInput.keyboard).ifPresentOrElse(
+                action -> action.executeIfEnabled(ui),
+                () -> ui.viewManager().currentView().onInput(ui, userInput));
+        });
 
         // Delegate mouse scroll events to scene
         setOnScroll(e -> ui.gameSceneManager().optCurrentGameScene().ifPresent(gameScene -> gameScene.onScroll(e)));
+
+        // Global action bindings
+        actionBindings.registerAnyBindingFromSet(CommonActions.ACTION_ENTER_FULLSCREEN,        GameUIConstants.COMMON_BINDINGS);
+        actionBindings.registerAnyBindingFromSet(CommonActions.ACTION_OPEN_EDITOR,             GameUIConstants.COMMON_BINDINGS);
+        actionBindings.registerAnyBindingFromSet(CommonActions.ACTION_TOGGLE_KEYBOARD_MONITOR, GameUIConstants.COMMON_BINDINGS);
+        actionBindings.registerAnyBindingFromSet(CommonActions.ACTION_TOGGLE_MUTED,            GameUIConstants.COMMON_BINDINGS);
+        actionBindings.activate();
     }
 
     public StackPane rootPane() {
