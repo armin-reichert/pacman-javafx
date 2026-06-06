25418c93cceb485a7553d71d87f32c7771e8606e
Armin Reichert <armin.reichert@web.de>
2026-06-06 16:28:02 +0200

    Refac

--- pacman-ui/src/main/java/de/amr/pacmanfx/ui/input/Input.java
+++ pacman-ui/src/main/java/de/amr/pacmanfx/ui/input/Input.java
@@ -6,16 +6,16 @@ package de.amr.pacmanfx.ui.input;
 
 public record Input(Keyboard keyboard, Joypad joypad) {
 
-    private static Input create() {
+    public static Input instance() {
+        return LazyThreadSafeSingletonHolder.SINGLETON;
+    }
+
+    private static Input createSingleton() {
         final Keyboard kb = new Keyboard();
         return new Input(kb, new Joypad(kb));
     }
 
     private static class LazyThreadSafeSingletonHolder {
-        static final Input SINGLETON = create();
-    }
-
-    public static Input instance() {
-        return LazyThreadSafeSingletonHolder.SINGLETON;
+        static final Input SINGLETON = createSingleton();
     }
 }
