84a260e4e9ef3e6fc64105a22acf10a216200d6d
Armin Reichert <armin.reichert@web.de>
2026-06-07 13:44:27 +0200

    Refac

--- pacman-app-tengen-mspacman/src/main/java/de/amr/pacmanfx/tengenmspacman/flow/TengenMsPacMan_GameState.java
+++ pacman-app-tengen-mspacman/src/main/java/de/amr/pacmanfx/tengenmspacman/flow/TengenMsPacMan_GameState.java
@@ -7,6 +7,7 @@ package de.amr.pacmanfx.tengenmspacman.flow;
 import de.amr.pacmanfx.core.GameContext;
 import de.amr.pacmanfx.event.GameContinuedEvent;
 import de.amr.pacmanfx.event.GameStartedEvent;
+import de.amr.pacmanfx.flow.GameFlow;
 import de.amr.pacmanfx.gamestate.*;
 import de.amr.pacmanfx.model.GameModel;
 import de.amr.pacmanfx.model.actors.Ghost;
@@ -35,19 +36,20 @@ public enum TengenMsPacMan_GameState {
      * level ends, the credits screens are shown and then again the "PRESS START" scene.
      * </p>
      */
-    GAME_INTRO(new GameIntroState()),
+    GAME_INTRO (new GameIntroState()),
 
     /**
      * Corresponds to the "MS PAC-MAN OPTIONS" screen where difficulty, booster, map category
      * and start level can be set.
      */
-    GAME_PREPARATION(new GamePreparationState()),
+    GAME_PREPARATION (new GamePreparationState()),
 
     /**
      * Corresponds to the screen showing the people that have contributed to the game. Here, a seconds
      * screen with the contributors to the remake has been added.
      */
     SHOWING_HALL_OF_FAME (new GameState(TengenMsPacMan_GameStateID.SHOWING_HALL_OF_FAME) {
+        
         @Override
         public void onEnter(GameContext gameContext) {
             lock();
@@ -62,15 +64,18 @@ public enum TengenMsPacMan_GameState {
     }),
 
     GAME_OR_LEVEL_STARTING( new GameState(GameStateID.GAME_OR_LEVEL_STARTING) {
+        
         @Override
         public void onEnter(GameContext gameContext) {
             final TengenMsPacMan_GameModel gameModel = (TengenMsPacMan_GameModel) gameContext.gameModel();
             final TengenMsPacMan_HeadsUpDisplay hud = gameModel.hud();
+        
             hud.credit(false).score(true).levelCounter(true).livesCounter(true).show();
 
             // The rules vary between map categories so update the rules here:
             final TengenMsPacMan_GameRules gameRules = (TengenMsPacMan_GameRules) gameContext.gameRules();
             gameRules.setCurrentMapCategory(gameModel.mapCategory());
+        
             Logger.info("Using game rules for map category {}", gameRules.currentMapCategory());
         }
 
@@ -78,24 +83,28 @@ public enum TengenMsPacMan_GameState {
         public void onUpdate(GameContext gameContext) {
             final GameModel gameModel = gameContext.gameModel();
             final long tick = timer().tickCount();
+        
             if (gameModel.isPlaying()) {
                 gameContext.gameFlow().enterState(GameStateID.GAME_LEVEL_CONTINUE);
             }
             else if (gameModel.canStartNewGame(gameContext)) {
                 gameContext.gameFlow().enterState(GameStateID.GAME_STARTING);
-            } else {
+            } 
+            else {
                 gameModel.startDemoLevel(gameContext, tick);
             }
         }
     }),
 
-    GAME_STARTING(new GameState(GameStateID.GAME_STARTING) {
+    GAME_STARTING( new GameState(GameStateID.GAME_STARTING) {
 
         @Override
         public void onEnter(GameContext gameContext) {
             final GameModel gameModel = gameContext.gameModel();
+            
             gameModel.prepareNewGame();
-            gameModel.buildNormalLevel(gameContext, tengenGameModel(gameModel).startLevelNumber());
+            gameModel.buildNormalLevel(gameContext, tengenMsPacman(gameModel).startLevelNumber());
+
             gameContext.gameFlow().publishGameEvent(new GameStartedEvent(gameContext));
         }
 
@@ -103,11 +112,12 @@ public enum TengenMsPacMan_GameState {
         public void onUpdate(GameContext gameContext) {
             final GameModel gameModel = gameContext.gameModel();
             final long tick = timer().tickCount();
+
             if (tick == Timing.TICK_SHOW_READY) {
                 gameModel.startLevel(gameContext);
             }
             else if (tick == Timing.TICK_NEW_GAME_SHOW_GUYS) {
-                final GameLevel level = gameModel.optGameLevel().orElseThrow();
+                final GameLevel level = gameContext.requireGameLevel();
                 level.entities().pac().show();
                 level.entities().ghosts().forEach(Ghost::show);
             }
@@ -123,7 +133,7 @@ public enum TengenMsPacMan_GameState {
         @Override
         public void onEnter(GameContext gameContext) {
             final GameModel gameModel = gameContext.gameModel();
-            final GameLevel level = gameModel.optGameLevel().orElseThrow();
+            final GameLevel level = gameContext.requireGameLevel();
 
             gameModel.prepareLevelForPlaying(level);
             level.entities().pac().show();
@@ -135,6 +145,7 @@ public enum TengenMsPacMan_GameState {
         @Override
         public void onUpdate(GameContext gameContext) {
             final long tick = timer().tickCount();
+
             if (tick == Timing.TICK_RESUME_HUNTING) {
                 gameContext.gameFlow().enterState(GameStateID.GAME_LEVEL_PLAYING);
             }
@@ -144,85 +155,92 @@ public enum TengenMsPacMan_GameState {
     GAME_LEVEL_PLAYING( new GameLevelPlayingState()),
 
     GAME_LEVEL_COMPLETE( new GameState(GameStateID.GAME_LEVEL_COMPLETE) {
+
         @Override
         public void onEnter(GameContext gameContext) {
             final GameModel gameModel = gameContext.gameModel();
-            lock(); // UI triggers timeout
-            gameModel.onLevelCompleted(gameModel.optGameLevel().orElseThrow());
+
+            lock(); // Waits for UI to trigger timeout
+            gameModel.onLevelCompleted(gameContext.requireGameLevel());
         }
 
         @Override
         public void onUpdate(GameContext gameContext) {
-            final GameModel gameModel = gameContext.gameModel();
-            final GameLevel level = gameModel.optGameLevel().orElseThrow();
+            final GameFlow flow = gameContext.gameFlow();
+            final GameLevel level = gameContext.requireGameLevel();
 
             if (level.isDemoLevel()) {
-                gameContext.gameFlow().enterState(TengenMsPacMan_GameStateID.SHOWING_HALL_OF_FAME);
+                flow.enterState(TengenMsPacMan_GameStateID.SHOWING_HALL_OF_FAME);
                 return;
             }
 
             if (timer().hasExpired()) {
                 if (level.isDemoLevel()) {
                     // Just in case: if demo level is completed, go back to intro scene
-                    gameContext.gameFlow().enterState(GameStateID.GAME_INTRO);
+                    flow.enterState(GameStateID.GAME_INTRO);
                 }
-                else if (gameContext.gameFlow().cutScenesEnabled() && level.cutSceneNumber() != 0) {
-                    gameContext.gameFlow().enterState(GameStateID.GAME_LEVEL_INTERMISSION);
+                else if (flow.cutScenesEnabled() && level.cutSceneNumber() != 0) {
+                    flow.enterState(GameStateID.GAME_LEVEL_INTERMISSION);
                 }
                 else {
-                    gameContext.gameFlow().enterState(GameStateID.GAME_LEVEL_TRANSITION);
+                    flow.enterState(GameStateID.GAME_LEVEL_TRANSITION);
                 }
             }
         }
     }),
 
-    GAME_LEVEL_TRANSITION(new GameLevelTransitionState()),
+    GAME_LEVEL_TRANSITION( new GameLevelTransitionState() ),
 
-    GAME_LEVEL_EATING_GHOST(new GameLevelEatingGhostState()),
+    GAME_LEVEL_EATING_GHOST( new GameLevelEatingGhostState() ),
 
-    GAME_LEVEL_PACMAN_DYING(new GameLevelPacManDyingState()),
+    GAME_LEVEL_PACMAN_DYING( new GameLevelPacManDyingState() ),
 
-    GAME_OVER (new GameState(GameStateID.GAME_OVER) {
+    GAME_OVER ( new GameState(GameStateID.GAME_OVER) {
+
         @Override
         public void onEnter(GameContext gameContext) {
             final GameModel gameModel = gameContext.gameModel();
-            final GameLevel level = gameModel.optGameLevel().orElseThrow();
+            final GameLevel level = gameContext.requireGameLevel();
+
             timer().restartTicks(level.gameOverStateTicks());
             gameModel.onGameOver(gameContext, level);
         }
 
         @Override
         public void onUpdate(GameContext gameContext) {
+            final GameFlow flow = gameContext.gameFlow();
             final GameModel gameModel = gameContext.gameModel();
+            final GameLevel level = gameContext.requireGameLevel();
+
             if (timer().hasExpired()) {
-                final GameLevel level = gameModel.optGameLevel().orElseThrow();
                 gameModel.cheats().clear();
+                level.clearMessage();
+
                 if (level.isDemoLevel()) {
-                    gameContext.gameFlow().enterState(TengenMsPacMan_GameStateID.SHOWING_HALL_OF_FAME);
-                } else {
-                    level.clearMessage();
-                    gameContext.gameFlow().enterState(gameModel.canContinueOnGameOver()
-                        ? GameStateID.GAME_PREPARATION
-                        : GameStateID.GAME_INTRO);
+                    flow.enterState(TengenMsPacMan_GameStateID.SHOWING_HALL_OF_FAME);
+                }
+                else {
+                    flow.enterState(gameModel.canContinueOnGameOver() ? GameStateID.GAME_PREPARATION : GameStateID.GAME_INTRO);
                 }
             }
         }
     }),
 
-    GAME_LEVEL_INTERMISSION(new GameState(GameStateID.GAME_LEVEL_INTERMISSION) {
+    GAME_LEVEL_INTERMISSION( new GameState(GameStateID.GAME_LEVEL_INTERMISSION) {
 
         @Override
         public void onEnter(GameContext gameContext) {
             final GameModel gameModel = gameContext.gameModel();
-            final GameLevel level = gameModel.optGameLevel().orElseThrow();
+            final GameLevel level = gameContext.requireGameLevel();
             final var hud = (TengenMsPacMan_HeadsUpDisplay) gameModel.hud();
             final boolean isLastCutScene = level.cutSceneNumber() == gameContext.gameRules().lastCutSceneNumber();
 
             lock();
 
-            if (tengenGameModel(gameModel).mapCategory() == MapCategory.ARCADE || isLastCutScene) {
+            if (tengenMsPacman(gameModel).mapCategory() == MapCategory.ARCADE || isLastCutScene) {
                 hud.hide();
-            } else {
+            }
+            else {
                 hud.show();
                 hud.gameOptions(false).score(false).levelCounter(true).livesCounter(false).show();
             }
@@ -230,11 +248,11 @@ public enum TengenMsPacMan_GameState {
 
         @Override
         public void onUpdate(GameContext gameContext) {
+            final GameFlow flow = gameContext.gameFlow();
             final GameModel gameModel = gameContext.gameModel();
+
             if (timer().hasExpired()) {
-                gameContext.gameFlow().enterState(gameModel.isPlaying()
-                    ? GameStateID.GAME_LEVEL_TRANSITION
-                    : GameStateID.GAME_INTRO);
+                flow.enterState(gameModel.isPlaying() ? GameStateID.GAME_LEVEL_TRANSITION : GameStateID.GAME_INTRO);
             }
         }
 
@@ -242,14 +260,15 @@ public enum TengenMsPacMan_GameState {
         public void onExit(GameContext gameContext) {
             final GameModel gameModel = gameContext.gameModel();
             final var hud = (TengenMsPacMan_HeadsUpDisplay) gameModel.hud();
-            if (tengenGameModel(gameModel).mapCategory() == MapCategory.ARCADE) {
+
+            if (tengenMsPacman(gameModel).mapCategory() == MapCategory.ARCADE) {
                 hud.hide();
-            } else {
+            }
+            else {
                 hud.show();
                 hud.all(true).gameOptions(true).score(true).levelCounter(true).livesCounter(false).show();
             }
         }
-
     });
 
     final GameState state;
@@ -262,7 +281,7 @@ public enum TengenMsPacMan_GameState {
         return state;
     }
 
-    static TengenMsPacMan_GameModel tengenGameModel(GameModel game) {
+    static TengenMsPacMan_GameModel tengenMsPacman(GameModel game) {
         return (TengenMsPacMan_GameModel) game;
     }
 
@@ -274,7 +293,7 @@ public enum TengenMsPacMan_GameState {
         public static final short TICK_RESUME_HUNTING = 240;
         public static final short TICK_DEMO_LEVEL_START_HUNTING = 120;
 
-        //TODO check times in common game state
+        //TODO check times if times match with those common game state implementation, if not, provide own state!
         public static final short TICK_PACMAN_DYING_HIDE_GHOSTS = 60;
         public static final short TICK_PACMAN_DYING_START_PAC_ANIMATION = 90;
         public static final short TICK_PACMAN_DYING_HIDE_PAC = 190;
