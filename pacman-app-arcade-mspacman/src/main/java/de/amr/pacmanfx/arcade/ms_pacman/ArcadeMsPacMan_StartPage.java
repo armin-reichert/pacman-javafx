8b3b7ca087412820bd632037cbd54a48af2baabd
Armin Reichert <armin.reichert@web.de>
2026-06-09 16:40:35 +0200

    Refac

--- pacman-app-arcade-mspacman/src/main/java/de/amr/pacmanfx/arcade/ms_pacman/ArcadeMsPacMan_StartPage.java
+++ pacman-app-arcade-mspacman/src/main/java/de/amr/pacmanfx/arcade/ms_pacman/ArcadeMsPacMan_StartPage.java
@@ -7,6 +7,7 @@ import de.amr.pacmanfx.core.GameVariantID;
 import de.amr.pacmanfx.ui.game.Game;
 import de.amr.pacmanfx.ui.subviews.startpages.FlyerStartPage;
 import de.amr.pacmanfx.uilib.assets.ResourceManager;
+import javafx.scene.image.Image;
 import javafx.scene.media.Media;
 
 public class ArcadeMsPacMan_StartPage extends FlyerStartPage {
@@ -15,18 +16,20 @@ public class ArcadeMsPacMan_StartPage extends FlyerStartPage {
 
     private static final Media VOICE = RM.loadMedia("/de/amr/pacmanfx/arcade/ms_pacman/sound/flyer-text.mp3");
 
+    private static final Image[] FLYER_IMAGES = {
+        RM.loadImage("/de/amr/pacmanfx/arcade/ms_pacman/graphics/flyer-page-1.jpg"),
+        RM.loadImage("/de/amr/pacmanfx/arcade/ms_pacman/graphics/flyer-page-2.jpg")
+    };
+
     public ArcadeMsPacMan_StartPage() {
         super("Ms. Pac-Man (Arcade)");
-        flyer.setImages(
-            RM.loadImage("/de/amr/pacmanfx/arcade/ms_pacman/graphics/flyer-page-1.jpg"),
-            RM.loadImage("/de/amr/pacmanfx/arcade/ms_pacman/graphics/flyer-page-2.jpg")
-        );
+        flyer.setImages(FLYER_IMAGES);
     }
 
     @Override
     public void onEnterStartPage(Game game) {
         game.selectGameVariant(GameVariantID.ARCADE_MS_PACMAN.name());
-        flyer.selectPage(0);
         game.ui().sounds().playVoice(VOICE);
+        flyer.selectPage(0);
     }
 }
--- pacman-app-arcade-pacman/src/main/java/de/amr/pacmanfx/arcade/pacman/ArcadePacMan_StartPage.java
+++ pacman-app-arcade-pacman/src/main/java/de/amr/pacmanfx/arcade/pacman/ArcadePacMan_StartPage.java
@@ -8,6 +8,7 @@ import de.amr.pacmanfx.core.GameVariantID;
 import de.amr.pacmanfx.ui.game.Game;
 import de.amr.pacmanfx.ui.subviews.startpages.FlyerStartPage;
 import de.amr.pacmanfx.uilib.assets.ResourceManager;
+import javafx.scene.image.Image;
 import javafx.scene.media.Media;
 
 public class ArcadePacMan_StartPage extends FlyerStartPage {
@@ -16,19 +17,21 @@ public class ArcadePacMan_StartPage extends FlyerStartPage {
 
     private static final Media VOICE = RM.loadMedia("/de/amr/pacmanfx/arcade/pacman/sound/flyer-text.mp3");
 
+    private static final Image[] FLYER_IMAGES = {
+        RM.loadImage("/de/amr/pacmanfx/arcade/pacman/graphics/flyer-page-1.jpg"),
+        RM.loadImage("/de/amr/pacmanfx/arcade/pacman/graphics/flyer-page-2.jpg"),
+        RM.loadImage("/de/amr/pacmanfx/arcade/pacman/graphics/flyer-page-3.jpg")
+    };
+
     public ArcadePacMan_StartPage() {
         super("Pac-Man (Arcade)");
-        flyer.setImages(
-            RM.loadImage("/de/amr/pacmanfx/arcade/pacman/graphics/flyer-page-1.jpg"),
-            RM.loadImage("/de/amr/pacmanfx/arcade/pacman/graphics/flyer-page-2.jpg"),
-            RM.loadImage("/de/amr/pacmanfx/arcade/pacman/graphics/flyer-page-3.jpg")
-        );
+        flyer.setImages(FLYER_IMAGES);
     }
 
     @Override
     public void onEnterStartPage(Game game) {
         game.selectGameVariant(GameVariantID.ARCADE_PACMAN.name());
-        flyer.selectPage(0);
         game.ui().sounds().playVoice(VOICE);
+        flyer.selectPage(0);
     }
 }
