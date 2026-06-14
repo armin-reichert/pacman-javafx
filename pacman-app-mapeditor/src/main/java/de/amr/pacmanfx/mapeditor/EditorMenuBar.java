ece86c6a20dbb0b7332a4b04b216e0fa901204f7
Armin Reichert <armin.reichert@web.de>
2026-06-14 10:42:33 +0200

    Improve menus

--- pacman-app-mapeditor/src/main/java/de/amr/pacmanfx/mapeditor/EditorMenuBar.java
+++ pacman-app-mapeditor/src/main/java/de/amr/pacmanfx/mapeditor/EditorMenuBar.java
@@ -3,8 +3,13 @@
  */
 package de.amr.pacmanfx.mapeditor;
 
+import de.amr.basics.math.Vector2i;
 import de.amr.pacmanfx.mapeditor.actions.*;
+import de.amr.pacmanfx.model.world.TerrainLayer;
+import de.amr.pacmanfx.model.world.TerrainTile;
+import de.amr.pacmanfx.model.world.WorldMap;
 import javafx.beans.binding.Bindings;
+import javafx.beans.binding.BooleanBinding;
 import javafx.scene.control.*;
 
 import static de.amr.pacmanfx.mapeditor.Globals_MapEditor.NO_GRAPHIC;
@@ -68,57 +73,68 @@ public class EditorMenuBar extends MenuBar {
 
     private Menu buildEditMenu(TileMapEditorUI ui) {
         final TileMapEditor editor = ui.editor();
+        final BooleanBinding isInspectingMap = ui.editModeProperty().isEqualTo(EditMode.INSPECT);
 
-        var miObstacleJoining = new CheckMenuItem(translated("menu.edit.obstacles_joining"));
+        final var miObstacleJoining = new CheckMenuItem(translated("menu.edit.obstacles_joining"));
         miObstacleJoining.selectedProperty().bindBidirectional(ui.obstaclesJoiningProperty());
 
-        var miAddBorder = new MenuItem(translated("menu.edit.add_border"));
+        final var miAddBorder = new MenuItem(translated("menu.edit.add_border"));
         miAddBorder.setOnAction(_ -> new Action_AddBorderWall(editor).execute());
-        miAddBorder.disableProperty().bind(ui.editModeProperty().isEqualTo(EditMode.INSPECT));
 
-        var miAddHouse = new MenuItem(translated("menu.edit.add_house"));
+        final var miAddHouse = new MenuItem(translated("menu.edit.add_house"));
         miAddHouse.setOnAction(_ -> {
             int numRows = editor.currentWorldMap().numRows(), numCols = editor.currentWorldMap().numCols();
             new Action_MoveArcadeHouse(editor, tile(numCols / 2 - 4, numRows / 2 - 3)).execute();
         });
 
-        miAddHouse.disableProperty().bind(ui.editModeProperty().isEqualTo(EditMode.INSPECT));
-
-        var miDeleteHouse = new MenuItem(translated("menu.edit.delete_house"));
+        final var miDeleteHouse = new MenuItem(translated("menu.edit.delete_house"));
         miDeleteHouse.setOnAction(_ -> new Action_DeleteArcadeHouse(editor).execute());
-        miDeleteHouse.disableProperty().bind(ui.editModeProperty().isEqualTo(EditMode.INSPECT));
 
-        var miClearTerrain = new MenuItem(translated("menu.edit.clear_terrain"));
+        final var miClearTerrain = new MenuItem(translated("menu.edit.clear_terrain"));
         miClearTerrain.setOnAction(_ -> new Action_ClearTerrain(editor).execute());
-        miClearTerrain.disableProperty().bind(ui.editModeProperty().isEqualTo(EditMode.INSPECT));
 
-        var miClearFood = new MenuItem(translated("menu.edit.clear_food"));
+        final var miFloodWithPellets = new MenuItem(translated("menu.edit.flood_with_pellets"));
+        miFloodWithPellets.setOnAction(_ -> {
+            final Vector2i tile = findFreeTileInsideMap(editor.currentWorldMap());
+            if (tile != null) {
+                new Action_FloodWithPellets(editor, tile).execute();
+            }
+        });
+
+        final var miClearFood = new MenuItem(translated("menu.edit.clear_food"));
         miClearFood.setOnAction(_ -> new Action_ClearFood(editor).execute());
-        miClearFood.disableProperty().bind(ui.editModeProperty().isEqualTo(EditMode.INSPECT));
 
-        var miIdentifyTiles = new MenuItem(translated("menu.edit.identify_tiles"));
+        final var miClearFoodAroundHouse = new MenuItem(translated("menu.edit.clear_food_around_house"));
+        miClearFoodAroundHouse.setOnAction(_ -> new Action_ClearFoodAroundHouse(editor, editor.currentWorldMap()).execute());
+
+        final var miIdentifyTiles = new MenuItem(translated("menu.edit.identify_tiles"));
         miIdentifyTiles.disableProperty().bind(Bindings.createBooleanBinding(
-            () -> ui.editMode() == EditMode.INSPECT
-                || editor.templateImageProperty().get() == null,
-            ui.editModeProperty(), editor.templateImageProperty()));
-
+            () -> ui.editMode() == EditMode.INSPECT || editor.templateImageProperty().get() == null,
+            ui.editModeProperty(), editor.templateImageProperty())
+        );
         miIdentifyTiles.setOnAction(_ -> new Action_FillMapFromTemplate(ui).execute());
 
-        var miAssignDefaultColors = new MenuItem(translated("menu.edit.assign_default_colors"));
+        final var miAssignDefaultColors = new MenuItem(translated("menu.edit.assign_default_colors"));
         miAssignDefaultColors.setOnAction(_ -> new Action_SetDefaultMapColors(editor).execute());
-        miAssignDefaultColors.disableProperty().bind(ui.editModeProperty().isEqualTo(EditMode.INSPECT));
 
-        return new Menu(translated("menu.edit"), NO_GRAPHIC,
+        final Menu menu = new Menu(translated("menu.edit"), NO_GRAPHIC,
             miObstacleJoining,
             new SeparatorMenuItem(),
             miAddBorder,
             miAddHouse,
             miDeleteHouse,
             miClearTerrain,
+            new SeparatorMenuItem(),
+            miFloodWithPellets,
             miClearFood,
+            miClearFoodAroundHouse,
+            new SeparatorMenuItem(),
             miIdentifyTiles,
             miAssignDefaultColors);
 
+        menu.disableProperty().bind(isInspectingMap);
+
+        return menu;
     }
 
     private Menu buildViewMenu(TileMapEditorUI ui) {
@@ -151,5 +167,20 @@ public class EditorMenuBar extends MenuBar {
             miSegmentNumbersVisible,
             miObstacleInnerAreaVisible,
             miGridVisible);
+    }
+
+    //TODO This is not 100% correct because it could start outside the outer wall and flood the wrong area
+    private Vector2i findFreeTileInsideMap(WorldMap worldMap) {
+        final TerrainLayer terrain = worldMap.terrainLayer();
+        int startRow = terrain.emptyRowsOverMaze();
+        int endRow = terrain.numRows() - terrain.emptyRowsBelowMaze();
+        for (int r = startRow; r <= endRow; ++r) {
+            for (int c = 1; c < worldMap.numCols() - 1; ++c) {
+                if (terrain.content(r, c) == TerrainTile.EMPTY.$) {
+                    return new Vector2i(c, r);
+                }
+            }
+        }
+        return null;
     }
 }
