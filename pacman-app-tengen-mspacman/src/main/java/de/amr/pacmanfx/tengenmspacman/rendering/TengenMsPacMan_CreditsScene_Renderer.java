3ba87f6cbc19e5491abf8e63ff13f66a58951678
Armin Reichert <armin.reichert@web.de>
2026-06-21 09:16:23 +0200

    Refac

--- pacman-app-tengen-mspacman/src/main/java/de/amr/pacmanfx/tengenmspacman/rendering/TengenMsPacMan_CreditsScene_Renderer.java
+++ pacman-app-tengen-mspacman/src/main/java/de/amr/pacmanfx/tengenmspacman/rendering/TengenMsPacMan_CreditsScene_Renderer.java
@@ -1,6 +1,7 @@
 /*
  * Copyright (c) 2021-2026 Armin Reichert (MIT License)
  */
+
 package de.amr.pacmanfx.tengenmspacman.rendering;
 
 import de.amr.pacmanfx.tengenmspacman.scenes.TengenMsPacMan_CreditsScene;
@@ -14,8 +15,54 @@ import static de.amr.pacmanfx.model.world.WorldMap.TS;
 
 public class TengenMsPacMan_CreditsScene_Renderer extends BaseRenderer implements GameScene2D_Renderer, TengenMsPacMan_SceneRendererMixin {
 
+    record Line(String text, int paletteIndex, int column, int skipTiles) {}
+
+    private static final Line[] ORIGINAL_AUTHORS_LINES = {
+        new Line("CREDITS FOR MS PAC-MAN",  0x20,  3, 4),
+        new Line("GAME PROGRAMMER:",        0x23,  4, 2),
+        new Line("FRANZ LANZINGER",         0x23, 10, 3),
+        new Line("SPECIAL THANKS:",         0x23,  4, 2),
+        new Line("JEFF YONAN",              0x23, 10, 1),
+        new Line("DAVE O'RIVA",             0x23, 10, 4),
+        new Line("MS PAC-MAN TM NAMCO LTD", 0x19,  5, 1),
+        new Line("©1990 TENGEN INC",        0x19,  7, 1),
+        new Line("ALL RIGHTS RESERVED",     0x19,  6, 1),
+    };
+
+    private static final Line[] REMAKE_AUTHORS_LINES = {
+        new Line("CREDITS FOR JAVAFX REMAKE",  0x20,  3, 4),
+        new Line("GAME PROGRAMMER:",           0x23,  4, 2),
+        new Line("ARMIN REICHERT",             0x23, 10, 3),
+        new Line("SPECIAL THANKS:",            0x23,  4, 2),
+        new Line("@RUSSIANMANSMWC",            0x23, 10, 1),
+        new Line("@FLICKY1211",                0x23, 10, 1),
+        new Line("ANDYANA JONSEPH",            0x23, 10, 3),
+        new Line("GITHUB.COM/ARMIN-REICHERT",  0x19,  3, 1),
+        new Line("©2021 MIT LICENSE",          0x19,  6, 1),
+        new Line("ALL RIGHTS GRANTED",         0x19,  5, 1),
+    };
+
+    private class Pen {
+        private double y;
+
+        public void setY(double y) {
+            this.y = y;
+        }
+
+        public void drawLines(Line[] lines) {
+            for (var line : lines) {
+                drawLine(line);
+            }
+        }
+
+        public void drawLine(Line line) {
+            fillText(line.text(), NES_Palette.color(line.paletteIndex()), TS(line.column()), y);
+            y += TS(line.skipTiles());
+        }
+    }
+
     private final BaseDebugInfoRenderer debugRenderer;
-    private int y;
+    private final Pen pen = new Pen();
 
     public TengenMsPacMan_CreditsScene_Renderer(GameScene2D scene, Canvas canvas) {
         super(canvas);
@@ -35,49 +82,24 @@ public class TengenMsPacMan_CreditsScene_Renderer extends BaseRenderer implement
         }
         drawHorizontalBar(NES_Palette.color(0x20), NES_Palette.color(0x13), scene.unscaledWidth(), TS, 20);
         drawHorizontalBar(NES_Palette.color(0x20), NES_Palette.color(0x13), scene.unscaledWidth(), TS, 212);
+
         ctx.setFont(arcadeFont8());
-        y = 7 * TS; // important: reset on every draw!
         switch (creditsScene.displayMode) {
-            case ORIGINAL_AUTHORS -> drawOriginalCreditsText();
+            case ORIGINAL_AUTHORS -> {
+                pen.setY(TS(7));
+                pen.drawLines(ORIGINAL_AUTHORS_LINES);
+            }
             case REMAKE_AUTHORS -> {
                 ctx.save();
                 ctx.setGlobalAlpha(creditsScene.fadeProgress);
-                drawJavaFXRemakeCreditsText();
+                pen.setY(TS(7));
+                pen.drawLines(REMAKE_AUTHORS_LINES);
                 ctx.restore();
             }
         }
+
         if (scene.game().ui().settings().debugModeOnProperty().get()) {
             debugRenderer.draw(scene);
         }
-    }
-
-    private void drawOriginalCreditsText() {
-        drawTextAndSkip(4, "CREDITS FOR MS PAC-MAN",    0x20,  3);
-        drawTextAndSkip(2, "GAME PROGRAMMER:",          0x23,  4);
-        drawTextAndSkip(3, "FRANZ LANZINGER",           0x23, 10);
-        drawTextAndSkip(2, "SPECIAL THANKS:",           0x23,  4);
-        drawTextAndSkip(1, "JEFF YONAN",                0x23, 10);
-        drawTextAndSkip(4, "DAVE O'RIVA",               0x23, 10);
-        drawTextAndSkip(1, "MS PAC-MAN TM NAMCO LTD",   0x19,  5);
-        drawTextAndSkip(1, "©1990 TENGEN INC",          0x19,  7);
-        drawTextAndSkip(0, "ALL RIGHTS RESERVED",       0x19,  6);
-    }
-
-    private void drawJavaFXRemakeCreditsText() {
-        drawTextAndSkip(4, "CREDITS FOR JAVAFX REMAKE", 0x20,  3);
-        drawTextAndSkip(2, "GAME PROGRAMMER:",          0x23,  4);
-        drawTextAndSkip(3, "ARMIN REICHERT",            0x23, 10);
-        drawTextAndSkip(2, "SPECIAL THANKS:",           0x23,  4);
-        drawTextAndSkip(1, "@RUSSIANMANSMWC",           0x23, 10);
-        drawTextAndSkip(1, "@FLICKY1211",               0x23, 10);
-        drawTextAndSkip(3, "ANDYANA JONSEPH",           0x23, 10);
-        drawTextAndSkip(1, "GITHUB.COM/ARMIN-REICHERT", 0x19,  3);
-        drawTextAndSkip(1, "©2024 MIT LICENSE",         0x19,  6);
-        drawTextAndSkip(0, "ALL RIGHTS GRANTED",        0x19,  5);
-    }
-
-    private void drawTextAndSkip(int numTiles, String text, int colorIndex, int tilesX) {
-        fillText(text, NES_Palette.color(colorIndex), TS(tilesX), y);
-        y += numTiles * TS;
     }
 }
