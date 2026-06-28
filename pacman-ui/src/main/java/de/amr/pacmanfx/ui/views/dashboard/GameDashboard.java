4c26c3488802f9d677906ee6eb283428265172a5
Armin Reichert <armin.reichert@web.de>
2026-06-28 16:14:47 +0200

    Separate dashboard into game-specific and core classes

--- pacman-ui/src/main/java/de/amr/pacmanfx/ui/views/dashboard/GameDashboard.java
+++ pacman-ui/src/main/java/de/amr/pacmanfx/ui/views/dashboard/GameDashboard.java
@@ -4,6 +4,7 @@
 
 package de.amr.pacmanfx.ui.views.dashboard;
 
+import de.amr.basics.Identifier;
 import de.amr.pacmanfx.ui.game.Game;
 import de.amr.pacmanfx.uilib.widgets.Dashboard;
 import de.amr.pacmanfx.uilib.widgets.DashboardSection;
@@ -19,28 +20,29 @@ public class GameDashboard extends Dashboard<GameDashboardSection> {
     }
 
     public void connect(Game game) {
-        sectionMap().values().forEach(section -> section.connect(game));
+        sections().forEach(section -> section.connect(game));
     }
 
     public void update(Game game) {
-        sectionMap().values().stream()
+        sections()
             .filter(GameDashboardSection::isExpanded)
             .forEach(section -> section.update(game));
     }
 
     public void updateLayout() {
-        final List<DashboardSection> reorderedSections = new ArrayList<>(sectionMap().entrySet().stream()
+        final Map<Identifier, GameDashboardSection> sectionMap = sectionMap();
+        final List<DashboardSection> reorderedSections = new ArrayList<>(sectionMap.entrySet().stream()
             .filter(e -> e.getValue().isVisible())
             .filter(e -> e.getKey() != DashboardID.README)
             .filter(e -> e.getKey() != DashboardID.ABOUT)
             .map(Map.Entry::getValue)
             .toList());
 
-        if (sectionMap().containsKey(DashboardID.README)) {
-            reorderedSections.addFirst(sectionMap().get(DashboardID.README));
+        if (sectionMap.containsKey(DashboardID.README)) {
+            reorderedSections.addFirst(sectionMap.get(DashboardID.README));
         }
-        if (sectionMap().containsKey(DashboardID.ABOUT)) {
-            reorderedSections.addLast(sectionMap().get(DashboardID.ABOUT));
+        if (sectionMap.containsKey(DashboardID.ABOUT)) {
+            reorderedSections.addLast(sectionMap.get(DashboardID.ABOUT));
         }
         getChildren().setAll(reorderedSections);
     }
