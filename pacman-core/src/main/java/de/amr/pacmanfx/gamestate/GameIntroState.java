50b7cd545977b000be1ccdf4c859df90a973f355
Armin Reichert <armin.reichert@web.de>
2026-06-29 13:59:52 +0200

    Remove old level when intro state starts

--- pacman-core/src/main/java/de/amr/pacmanfx/gamestate/GameIntroState.java
+++ pacman-core/src/main/java/de/amr/pacmanfx/gamestate/GameIntroState.java
@@ -15,6 +15,7 @@ public class GameIntroState extends GameState {
     @Override
     public void onEnter(GameContext context) {
         context.model().hud().levelCounterOn().livesCounterOff().creditOn().scoreOn().show();
+        context.model().setLevel(null);
         lock();
     }
 
