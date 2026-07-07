5fdc6f606c314f393e927bf41d6e33168a282e5b
Armin Reichert <armin.reichert@web.de>
2026-07-07 06:43:02 +0200

    Refac

--- pacman-ui/src/main/java/de/amr/pacmanfx/ui/game/GameVariant.java
+++ pacman-ui/src/main/java/de/amr/pacmanfx/ui/game/GameVariant.java
@@ -1,3 +1,7 @@
+/*
+ * Copyright (c) 2021-2026 Armin Reichert (MIT License)
+ */
+
 package de.amr.pacmanfx.ui.game;
 
 import de.amr.pacmanfx.flow.GameFlow;
@@ -7,38 +11,20 @@ import de.amr.pacmanfx.model.GameModel;
 import de.amr.pacmanfx.simulation.GamePlay;
 import de.amr.pacmanfx.ui.GameVariantConfig;
 
-public class GameVariant {
-    private final GamePlay gamePlay;
-    private final GameFlow gameFlow;
-    private final GameModel gameModel;
-    private final GameCheats cheats;
-    private final GameVariantConfig config;
-
+public record GameVariant(
+    GamePlay gamePlay,
+    GameFlow gameFlow,
+    GameModel gameModel,
+    GameCheats cheats,
+    GameVariantConfig config)
+{
     public GameVariant(Cartridge cartridge) {
-        gamePlay = cartridge.gamePlayFactory().get();
-        gameFlow = cartridge.gameFlowFactory().get();
-        gameModel = cartridge.gameModelFactory().get();
-        cheats = new DefaultCheatsImpl();
-        config = cartridge.uiConfigFactory().get();
-    }
-
-    public GamePlay gamePlay() {
-        return gamePlay;
-    }
-
-    public GameFlow gameFlow() {
-        return gameFlow;
-    }
-
-    public GameModel gameModel() {
-        return gameModel;
-    }
-
-    public GameCheats cheats() {
-        return cheats;
-    }
-
-    public GameVariantConfig config() {
-        return config;
+        this(
+            cartridge.gamePlayFactory().get(),
+            cartridge.gameFlowFactory().get(),
+            cartridge.gameModelFactory().get(),
+            new DefaultCheatsImpl(),
+            cartridge.uiConfigFactory().get()
+        );
     }
 }
