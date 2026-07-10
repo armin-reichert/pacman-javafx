6403886a2bb0dceb97dd7415bfcb225ffc178896
Armin Reichert <armin.reichert@web.de>
2026-07-10 15:00:29 +0200

    Refac

--- pacman-ui/src/main/java/de/amr/pacmanfx/ui/game/GameVariantContext.java
+++ pacman-ui/src/main/java/de/amr/pacmanfx/ui/game/GameVariantContext.java
@@ -6,17 +6,11 @@ package de.amr.pacmanfx.ui.game;
 
 import de.amr.pacmanfx.core.CoinMechanism;
 import de.amr.pacmanfx.core.GameContext;
-import de.amr.pacmanfx.event.GameEvent;
-import de.amr.pacmanfx.event.GameEventListener;
 import de.amr.pacmanfx.event.GameEventManager;
 import de.amr.pacmanfx.flow.GameFlow;
 import de.amr.pacmanfx.model.GameCheats;
 import de.amr.pacmanfx.model.GameModel;
 import de.amr.pacmanfx.simulation.GamePlay;
-import org.tinylog.Logger;
-
-import java.util.HashSet;
-import java.util.Set;
 
 import static java.util.Objects.requireNonNull;
 
@@ -32,15 +26,12 @@ public class GameVariantContext implements GameContext {
     private final GameEventManager eventManager;
 
     public GameVariantContext(CoinMechanism coinMechanism, GameVariant gameVariant) {
-        this.gameVariant = requireNonNull(gameVariant);
-        this.eventManager = new EventManager();
-
         this.coinMechanism = requireNonNull(coinMechanism);
+        this.gameVariant = requireNonNull(gameVariant);
+        this.eventManager = new GameEventManagerImpl();
         //TODO rethink this
         model().hudState().creditProperty().bind(coinMechanism().numCoinsProperty());
     }
-
-    // --- GameContext
 
     @Override
     public GameCheats cheats() {
@@ -70,41 +61,5 @@ public class GameVariantContext implements GameContext {
     @Override
     public GamePlay gamePlay() {
         return gameVariant.gamePlay();
-    }
-
-    // --- GameEventManager
-
-    private static class EventManager implements GameEventManager {
-
-        private final Set<GameEventListener> subscribers = new HashSet<>();
-
-        @Override
-        public void addGameEventSubscriber(GameEventListener subscriber) {
-            requireNonNull(subscriber);
-            final boolean added = subscribers.add(subscriber);
-            if (added) {
-                Logger.info("{}: Game event subscriber registered: {}", getClass().getSimpleName(), subscriber);
-            }
-        }
-
-        @Override
-        public void removeGameEventSubscriber(GameEventListener subscriber) {
-            requireNonNull(subscriber);
-            boolean removed = subscribers.remove(subscriber);
-            if (removed) {
-                Logger.info("{}: Game event subscriber removed: {}", getClass().getSimpleName(), subscriber);
-            } else {
-                Logger.warn("{}: Game event subscriber not removed (was not registered): {}", getClass().getSimpleName(), subscriber);
-            }
-        }
-
-        @Override
-        public void publishGameEvent(GameEvent event) {
-            requireNonNull(event);
-            if (Logger.isTraceEnabled()) {
-                Logger.trace("Publish game event: {}", event);
-            }
-            subscribers.forEach(subscriber -> subscriber.onGameEvent(event));
-        }
     }
 }
