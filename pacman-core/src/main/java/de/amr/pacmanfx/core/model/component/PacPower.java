eb7c6871ac859538dc858fe09cb24cbd69513b02
Armin Reichert <armin.reichert@web.de>
2026-07-27 08:13:27 +0200

    Refac: "pac power" system/component

---
+++ pacman-core/src/main/java/de/amr/pacmanfx/core/model/component/PacPower.java
@@ -0,0 +1,22 @@
+/*
+ * Copyright (c) 2021-2026 Armin Reichert (MIT License)
+ */
+
+package de.amr.pacmanfx.core.model.component;
+
+import de.amr.basics.timer.TickTimer;
+
+public class PacPower implements EntityComponent {
+
+    private final TickTimer timer = new TickTimer("PacPower-Timer");
+
+    @Override
+    public void reset() {
+        timer.stop();
+        timer.reset(0);
+    }
+
+    public TickTimer timer() {
+        return timer;
+    }
+}
