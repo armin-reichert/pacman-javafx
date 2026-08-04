f6be88e4a848e08d2320353d2c3412c9403bb4fb
Armin Reichert <armin.reichert@web.de>
2026-08-04 08:29:02 +0200

    Refac

--- pacman-core/src/main/java/de/amr/pacmanfx/core/model/entities/bonus/BonusDataComp.java
+++ pacman-core/src/main/java/de/amr/pacmanfx/core/model/entities/bonus/BonusDataComp.java
@@ -6,24 +6,7 @@ package de.amr.pacmanfx.core.model.entities.bonus;
 
 import de.amr.pacmanfx.core.ecs.GameEntityComponent;
 
-public class BonusDataComp implements GameEntityComponent {
-
-    private final int symbolCode;
-
-    private final int points;
-
-    public BonusDataComp(int symbolCode, int points) {
-        this.symbolCode = symbolCode;
-        this.points = points;
-    }
-
-    public int symbolCode() {
-        return symbolCode;
-    }
-
-    public int points() {
-        return points;
-    }
+public record BonusDataComp(int symbolCode, int points) implements GameEntityComponent {
 
     @Override
     public void reset() {}
