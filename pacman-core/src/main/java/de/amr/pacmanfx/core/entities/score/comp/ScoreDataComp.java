96eeb224e88686c1312c8f900c49747db9ee74d1
Armin Reichert <armin.reichert@web.de>
2026-08-08 22:51:31 +0200

    Refac

--- pacman-core/src/main/java/de/amr/pacmanfx/core/entities/score/comp/ScoreDataComp.java
+++ pacman-core/src/main/java/de/amr/pacmanfx/core/entities/score/comp/ScoreDataComp.java
@@ -1,49 +1,18 @@
 package de.amr.pacmanfx.core.entities.score.comp;
 
 import de.amr.pacmanfx.core.ecs.GameEntityComponent;
-import javafx.beans.property.*;
 
 import java.time.LocalDate;
 
 public class ScoreDataComp implements GameEntityComponent {
 
-    private final BooleanProperty enabled = new SimpleBooleanProperty();
+    private boolean enabled;
 
-    private final IntegerProperty points = new SimpleIntegerProperty();
+    private int points;
 
-    private final IntegerProperty levelNumber = new SimpleIntegerProperty();
+    private int levelNumber;
 
-    private final ObjectProperty<LocalDate> date = new SimpleObjectProperty<>();
-
-    public BooleanProperty enabledProperty() { return enabled; }
-
-    public boolean isEnabled() { return enabledProperty().get(); }
-
-    public void setEnabled(boolean enabled) { enabledProperty().set(enabled); }
-
-    public IntegerProperty pointsProperty() { return points; }
-
-    public void setPoints(int points) { pointsProperty().set(points);  }
-
-    public int points() {
-        return pointsProperty().get();
-    }
-
-    public IntegerProperty levelNumberProperty() { return levelNumber; }
-
-    public void setLevelNumber(int levelNumber) { levelNumberProperty().set(levelNumber); }
-
-    public int levelNumber() {
-        return levelNumberProperty().get();
-    }
-
-    public ObjectProperty<LocalDate> dateProperty() { return date; }
-
-    public void setDate(LocalDate date) { dateProperty().set(date); }
-
-    public LocalDate date() {
-        return dateProperty().get();
-    }
+    private LocalDate date;
 
     public ScoreDataComp() {
         reset();
@@ -51,9 +20,31 @@ public class ScoreDataComp implements GameEntityComponent {
 
     @Override
     public void reset() {
-        setEnabled(true);
-        setPoints(0);
-        setLevelNumber(1);
-        setDate(LocalDate.now());
+        enabled = true;
+        points = 0;
+        levelNumber = 1;
+        date = LocalDate.now();
+    }
+
+    public boolean isEnabled() { return enabled; }
+
+    public void setEnabled(boolean enabled) { this.enabled = enabled; }
+
+    public void setPoints(int points) { this.points = points;  }
+
+    public int points() {
+        return points;
+    }
+
+    public void setLevelNumber(int levelNumber) { this.levelNumber = levelNumber; }
+
+    public int levelNumber() {
+        return levelNumber;
+    }
+
+    public void setDate(LocalDate date) { this.date = date; }
+
+    public LocalDate date() {
+        return date;
     }
 }
