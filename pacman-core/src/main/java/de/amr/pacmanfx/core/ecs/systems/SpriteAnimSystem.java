e31baa4c20266d294d4a9a1efb76984610a791ed
Armin Reichert <armin.reichert@web.de>
2026-08-12 14:50:50 +0200

    Fixes

--- pacman-core/src/main/java/de/amr/pacmanfx/core/ecs/systems/SpriteAnimSystem.java
+++ pacman-core/src/main/java/de/amr/pacmanfx/core/ecs/systems/SpriteAnimSystem.java
@@ -60,7 +60,7 @@ public class SpriteAnimSystem {
 
     public void advanceFrame(GameEntity actor) {
         final int currentFrame = currentFrame(actor);
-        if (currentFrame < spriteAnim(actor).numFrames()) {
+        if (currentFrame < spriteAnim(actor).numFrames() - 1) {
             spriteAnim(actor).setAnimationFrame(selectedAnimationID(actor), currentFrame + 1);
         }
     }
