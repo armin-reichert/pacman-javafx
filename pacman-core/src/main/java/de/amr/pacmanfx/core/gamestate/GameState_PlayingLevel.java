e68c3a90c2dadd157a254dc1cdd2b1a67fe41a87
Armin Reichert <armin.reichert@web.de>
2026-08-13 06:20:35 +0200

    Refac

--- pacman-core/src/main/java/de/amr/pacmanfx/core/gamestate/GameState_PlayingLevel.java
+++ pacman-core/src/main/java/de/amr/pacmanfx/core/gamestate/GameState_PlayingLevel.java
@@ -11,7 +11,6 @@ import de.amr.pacmanfx.core.entities.Pac;
 import de.amr.pacmanfx.core.gameplay.hunt.HuntingStep;
 import de.amr.pacmanfx.core.level.GameLevel;
 import de.amr.pacmanfx.core.level.GameLevelMessageType;
-import de.amr.pacmanfx.core.model.rules.GameRules;
 import de.amr.pacmanfx.core.session.GameSession;
 import org.tinylog.Logger;
 
@@ -25,7 +24,7 @@ public final class GameState_PlayingLevel extends GameState {
 
     @Override
     public void onEnter(GameContext game) {
-        final GameSystems sys = game.systems();
+        final GameSystems systems = game.systems();
         final GameLevel level = game.session().assertLevel();
         final Pac pac = level.entities().pac();
 
@@ -36,8 +35,8 @@ public final class GameState_PlayingLevel extends GameState {
         level.heartbeat().setStartState(Pulse.State.ON);
         level.heartbeat().restart();
 
-        sys.spriteAnim().playSelected(pac);
-        level.entities().ghosts().forEach(sys.spriteAnim()::playSelected);
+        systems.spriteAnim().playSelected(pac);
+        level.entities().ghosts().forEach(systems.spriteAnim()::playSelected);
 
         // This call fires a game event!
         level.huntingTimerStrategy().startFirstPhase(game, level.number());
@@ -49,26 +48,21 @@ public final class GameState_PlayingLevel extends GameState {
         final GameLevel level = session.assertLevel();
 
         game.gamePlay().hunt(game, level);
-        logHuntingStepResult(game.session().thisFrame().huntingStep());
 
-        game.session().cheats().update(game);
-        session.gameFlow().enterState(game, computeNextState(game));
-    }
+        final HuntingStep huntingStep = game.session().thisFrame().huntingStep();
+        logHuntingStepResult(huntingStep);
 
-    private CommonGameStateID computeNextState(GameContext game) {
-        final GameLevel level = game.session().assertLevel();
-        final GameRules rules = game.model().rules();
+        session.cheats().update(game);
 
-        if (rules.isLevelCompleted(level)) {
-            return CommonGameStateID.GAME_LEVEL_COMPLETE;
+        if (game.model().rules().isLevelCompleted(level)) {
+            session.gameFlow().enterState(game, CommonGameStateID.GAME_LEVEL_COMPLETE);
         }
-        else if (game.session().thisFrame().huntingStep().pacKilled()) {
-            return CommonGameStateID.GAME_LEVEL_PACMAN_DYING;
+        else if (huntingStep.pacKilled()) {
+            session.gameFlow().enterState(game, CommonGameStateID.GAME_LEVEL_PACMAN_DYING);
         }
-        else if (game.session().thisFrame().huntingStep().hasGhostBeenKilled()) {
-            return CommonGameStateID.GAME_LEVEL_EATING_GHOST;
+        else if (huntingStep.hasGhostBeenKilled()) {
+            session.gameFlow().enterState(game, CommonGameStateID.GAME_LEVEL_EATING_GHOST);
         }
-        return CommonGameStateID.GAME_LEVEL_PLAYING;
     }
 
     private void logHuntingStepResult(HuntingStep result) {
