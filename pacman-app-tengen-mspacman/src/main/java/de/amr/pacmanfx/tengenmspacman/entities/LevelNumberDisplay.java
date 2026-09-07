b7f686417e15bd0f916f602d5004ea76ca721bcd
Armin Reichert <armin.reichert@web.de>
2026-09-07 21:58:33 +0200

    Implement Renderable interface

--- pacman-app-tengen-mspacman/src/main/java/de/amr/pacmanfx/tengenmspacman/entities/LevelNumberDisplay.java
+++ pacman-app-tengen-mspacman/src/main/java/de/amr/pacmanfx/tengenmspacman/entities/LevelNumberDisplay.java
@@ -1,12 +1,20 @@
 package de.amr.pacmanfx.tengenmspacman.entities;
 
+import de.amr.pacmanfx.core.HUD;
+import de.amr.pacmanfx.core.Renderable;
 import de.amr.pacmanfx.core.ecs.GameEntity;
+import de.amr.pacmanfx.core.ecs.comp.RenderingLayer;
 import de.amr.pacmanfx.tengenmspacman.entities.levelnumberdisplay.LevelNumberComp;
 
-public class LevelNumberDisplay extends GameEntity {
+public class LevelNumberDisplay extends GameEntity implements Renderable {
 
     public LevelNumberDisplay() {
         setComp(LevelNumberComp.class, new LevelNumberComp());
+    }
+
+    @Override
+    public RenderingLayer layer() {
+        return RenderingLayer.HUD;
     }
 
     public LevelNumberComp levelNumber() {
