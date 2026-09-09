a076f3081ff30a60c9b644a9a5a8fbd51539ce22
Armin Reichert <armin.reichert@web.de>
2026-09-09 17:45:53 +0200

    Fix Tengen rendering partially (still buggy)

--- pacman-app-tengen-mspacman/src/main/java/de/amr/pacmanfx/tengenmspacman/entities/GameOptionsDisplay.java
+++ pacman-app-tengen-mspacman/src/main/java/de/amr/pacmanfx/tengenmspacman/entities/GameOptionsDisplay.java
@@ -1,9 +1,11 @@
 package de.amr.pacmanfx.tengenmspacman.entities;
 
+import de.amr.pacmanfx.core.Renderable;
 import de.amr.pacmanfx.core.ecs.GameEntity;
+import de.amr.pacmanfx.core.ecs.comp.RenderingLayer;
 import de.amr.pacmanfx.tengenmspacman.entities.gameoptionsdisplay.GameOptionsDataComp;
 
-public class GameOptionsDisplay extends GameEntity {
+public class GameOptionsDisplay extends GameEntity implements Renderable {
 
     public GameOptionsDisplay() {
         setComp(GameOptionsDataComp.class, new GameOptionsDataComp());
@@ -11,5 +13,10 @@ public class GameOptionsDisplay extends GameEntity {
 
     public GameOptionsDataComp options() {
         return reqComp(GameOptionsDataComp.class);
+    }
+
+    @Override
+    public RenderingLayer layer() {
+        return RenderingLayer.HUD;
     }
 }
