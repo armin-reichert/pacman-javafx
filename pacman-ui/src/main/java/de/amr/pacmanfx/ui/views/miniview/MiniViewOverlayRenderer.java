cddd38a393afca09ab44ccfe5365dbdd0e655ee5
Armin Reichert <armin.reichert@web.de>
2026-09-22 12:44:54 +0200

    Refac

--- pacman-ui/src/main/java/de/amr/pacmanfx/ui/views/miniview/MiniViewOverlayRenderer.java
+++ pacman-ui/src/main/java/de/amr/pacmanfx/ui/views/miniview/MiniViewOverlayRenderer.java
@@ -8,7 +8,6 @@ import de.amr.basics.ui.ecs.systems.ActorSpriteAnimController;
 import de.amr.basics.ui.rendering.BaseRenderer;
 import de.amr.basics.ui.rendering.Renderable;
 import de.amr.basics.ui.rendering.Renderer;
-import de.amr.basics.ui.rendering.RenderableWrapper;
 import de.amr.pacmanfx.game.GameVariantRenderConfig;
 import de.amr.pacmanfx.uilib.rendering.RenderableGameLevel;
 
@@ -40,7 +39,6 @@ public class MiniViewOverlayRenderer extends BaseRenderer {
     public void render(Renderable r, long tick) {
         switch (r) {
             case RenderableGameLevel _ -> levelRenderer.render(r, tick);
-            case RenderableWrapper wrapper -> render(wrapper.content(), tick);
             default -> entityRenderer.render(r, tick);
         }
     }
