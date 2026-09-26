f2c057bde52f745f813502163492a7d56eff5290
Armin Reichert <armin.reichert@web.de>
2026-09-26 12:48:41 +0200

    Cleanup

--- pacman-app-tengen-mspacman/src/main/java/de/amr/pacmanfx/tengenmspacman/entities/LevelNumberDisplay.java
+++ pacman-app-tengen-mspacman/src/main/java/de/amr/pacmanfx/tengenmspacman/entities/LevelNumberDisplay.java
@@ -1,19 +1,12 @@
 package de.amr.pacmanfx.tengenmspacman.entities;
 
-import de.amr.basics.ui.rendering.RenderingLayer;
-import de.amr.basics.ui.rendering.Renderable;
 import de.amr.basics.ecs.GameEntity;
 import de.amr.pacmanfx.tengenmspacman.entities.levelnumberdisplay.LevelNumberComp;
 
-public class LevelNumberDisplay extends GameEntity implements Renderable {
+public class LevelNumberDisplay extends GameEntity {
 
     public LevelNumberDisplay() {
         setComp(LevelNumberComp.class, new LevelNumberComp());
-    }
-
-    @Override
-    public RenderingLayer layer() {
-        return RenderingLayer.HUD;
     }
 
     public LevelNumberComp levelNumber() {
