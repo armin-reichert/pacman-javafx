7fe536d8e284e101c23ab8d726d00c4cfca7a88a
Armin Reichert <armin.reichert@web.de>
2026-09-26 11:37:33 +0200

    Refac

--- pacman-ui-lib/src/main/java/de/amr/pacmanfx/uilib/rendering/GameLevelView.java
+++ pacman-ui-lib/src/main/java/de/amr/pacmanfx/uilib/rendering/GameLevelView.java
@@ -15,7 +15,14 @@ import de.amr.pacmanfx.core.level.GameLevel;
  *
  * @param level the game level
  * @param renderInfo information for the game level renderer
- * @param layer the rendering layer e.g. {@link RenderingLayer#LEVEL}
- * @param z the z-layer value
+ * @param layer the rendering layer, here usually {@link RenderingLayer#LEVEL}
+ * @param z the z-order value
+ * @param offset the rendering offset
  */
-public record GameLevelView(GameLevel level, InfoMap renderInfo, RenderingLayer layer, int z, Vector2f offset) implements Renderable {}
+public record GameLevelView(
+    GameLevel level,
+    InfoMap renderInfo,
+    RenderingLayer layer,
+    int z,
+    Vector2f offset
+) implements Renderable {}
